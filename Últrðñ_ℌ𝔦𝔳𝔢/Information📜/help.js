2bd86a702cfcf01bce304e051b4939314e85d106
krakinz <krakinzkon@gmail.com>
2021-12-01 23:56:11 -0800

    🎮Últrðñ™-v2

--- Últrðñ_ℌ𝔦𝔳𝔢/Information📜/help.js
+++ Últrðñ_ℌ𝔦𝔳𝔢/Information📜/help.js
@@ -15,8 +15,6 @@ module.exports = {
   description: `Get the command list and info on modules`,
   ƈʏɮօʀɢʍօʀɛ: `This module is used to get info on other modules and their triggers.`,
   async handle(ӄʀǟӄɨռʐ, Últrðñ) {
-    var PreÚltrðñReg = new RegExp(UltronSitreper.ULTRONIX, `g`);
-    var PreÚltrðñ = /\/\^\[(.)+\]\/\g/g.exec(PreÚltrðñReg)[1];
     ӄʀǟӄɨռʐ
       .sendMessage(
         Últrðñ.chatId, {
@@ -27,93 +25,93 @@ module.exports = {
           caption: `*🎮Últrðñ 𝐇𝐞𝐥𝐩 𝐌𝐞𝐧𝐮*⧴ ᴜꜱᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇ ᴡᴀʏ!
 
 *••••••••••••••••➛  ❓𝗜𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻*
-*${PreÚltrðñ}help* = Get the command list and info on modules 
-*${PreÚltrðñ}invite* = Module to create group invite link
-*${PreÚltrðñ}alive* = Check if bot is online
+*${UltronSitreper.ULTRONIX}help* = Get the command list and info on modules 
+*${UltronSitreper.ULTRONIX}invite* = Module to create group invite link
+*${UltronSitreper.ULTRONIX}alive* = Check if bot is online
 
 
 *••••••••••••••••➛  🤩𝗔𝗻𝗶𝗺𝗲*
-*${PreÚltrðñ}anime* = Search Anime
+*${UltronSitreper.ULTRONIX}anime* = Search Anime
 *more coming!*
 
 
 *••••••••••••••••➛  🎼𝗠𝘂𝘀𝗶𝗰* 
-*${PreÚltrðñ}song* = Download songs
-*${PreÚltrðñ}lyrics* = Module to find lyrics of song
-*${PreÚltrðñ}yt* = Get recommendations and links from Youtube
+*${UltronSitreper.ULTRONIX}song* = Download songs
+*${UltronSitreper.ULTRONIX}lyrics* = Module to find lyrics of song
+*${UltronSitreper.ULTRONIX}yt* = Get recommendations and links from Youtube
 
 
 *••••••••••••••••➛  🎙️𝗖𝗼𝗻𝘃𝗲𝗿𝘀𝗮𝘁𝗶𝗼𝗻*
-*${PreÚltrðñ}cuddle* = Get an Anime-cuddle Sticker
-*${PreÚltrðñ}cuddlehd* = Sticker with HD-cuddle Download Link
-*${PreÚltrðñ}hug* = Get an Anime-hug Sticker
-*${PreÚltrðñ}hughd* = Sticker with HD-hug Download Link
-*${PreÚltrðñ}kill* = Get an Anime-kill Sticker
-*${PreÚltrðñ}killhd* = Sticker with HD-kill Download Link
-*${PreÚltrðñ}pat* = Get an Anime-pat Sticker
-*${PreÚltrðñ}pathd* = Sticker with HD-pat Download Link
-*${PreÚltrðñ}punch* = Get an Anime-punch Sticker
-*${PreÚltrðñ}punchhd* = Sticker with HD-punch Download Link
-*${PreÚltrðñ}slap* = Get an Anime-slap Sticker  
-*${PreÚltrðñ}slaphd* = Sticker with HD-slap Download Link
-*${PreÚltrðñ}wink* = Get an Anime-wink Sticker  
-*${PreÚltrðñ}winkhd* = Sticker with HD-wink Download Link
+*${UltronSitreper.ULTRONIX}cuddle* = Get an Anime-cuddle Sticker
+*${UltronSitreper.ULTRONIX}cuddlehd* = Sticker with HD-cuddle Download Link
+*${UltronSitreper.ULTRONIX}hug* = Get an Anime-hug Sticker
+*${UltronSitreper.ULTRONIX}hughd* = Sticker with HD-hug Download Link
+*${UltronSitreper.ULTRONIX}kill* = Get an Anime-kill Sticker
+*${UltronSitreper.ULTRONIX}killhd* = Sticker with HD-kill Download Link
+*${UltronSitreper.ULTRONIX}pat* = Get an Anime-pat Sticker
+*${UltronSitreper.ULTRONIX}pathd* = Sticker with HD-pat Download Link
+*${UltronSitreper.ULTRONIX}punch* = Get an Anime-punch Sticker
+*${UltronSitreper.ULTRONIX}punchhd* = Sticker with HD-punch Download Link
+*${UltronSitreper.ULTRONIX}slap* = Get an Anime-slap Sticker  
+*${UltronSitreper.ULTRONIX}slaphd* = Sticker with HD-slap Download Link
+*${UltronSitreper.ULTRONIX}wink* = Get an Anime-wink Sticker  
+*${UltronSitreper.ULTRONIX}winkhd* = Sticker with HD-wink Download Link
 
 
 *••••••••••••••••➛  👑𝗔𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝘁𝗶𝗼𝗻* _(only groups)
-*${PreÚltrðñ}add* = Module to add a person to a group
-*${PreÚltrðñ}admins* = Tag admins
-*${PreÚltrðñ}block* = Block contact
-*${PreÚltrðñ}demote* = Demote a person from admin
-*${PreÚltrðñ}disappear* = Toggle disappearing messages
-*${PreÚltrðñ}goodbye* = A goodbye message for group chat whenever someone leaves
-*${PreÚltrðñ}mute* = Mute group chat for a specified time
-*${PreÚltrðñ}promote* = Promote a member to admin
-*${PreÚltrðñ}remove* = Module to remove a person from a group
-*${PreÚltrðñ}setdp* = Change the group icon
-*${PreÚltrðñ}unblock* = Unblock contact
-*${PreÚltrðñ}unmute* = Unmute group chat
-*${PreÚltrðñ}welcome* = Welcome new members to the group with a custom message
+*${UltronSitreper.ULTRONIX}add* = Module to add a person to a group
+*${UltronSitreper.ULTRONIX}admins* = Tag admins
+*${UltronSitreper.ULTRONIX}block* = Block contact
+*${UltronSitreper.ULTRONIX}demote* = Demote a person from admin
+*${UltronSitreper.ULTRONIX}disappear* = Toggle disappearing messages
+*${UltronSitreper.ULTRONIX}goodbye* = A goodbye message for group chat whenever someone leaves
+*${UltronSitreper.ULTRONIX}mute* = Mute group chat for a specified time
+*${UltronSitreper.ULTRONIX}promote* = Promote a member to admin
+*${UltronSitreper.ULTRONIX}remove* = Module to remove a person from a group
+*${UltronSitreper.ULTRONIX}setdp* = Change the group icon
+*${UltronSitreper.ULTRONIX}unblock* = Unblock contact
+*${UltronSitreper.ULTRONIX}unmute* = Unmute group chat
+*${UltronSitreper.ULTRONIX}welcome* = Welcome new members to the group with a custom message
 
 
 *••••••••••••••••➛  🧀𝗙𝘂𝗻* 
-*${PreÚltrðñ}carbon* = Convert text/code to a carbon image
-*${PreÚltrðñ}pfp* = Get display picture
-*${PreÚltrðñ}github* = Find Github Profile
-*${PreÚltrðñ}neko* = Copy your text to nekobin
-*${PreÚltrðñ}joke* = Get Random Jokes
-*${PreÚltrðñ}meme* = Get Random Memes
-*${PreÚltrðñ}read* = Optical Character Recognition
-*${PreÚltrðñ}sticker* = Module to convert image to sticker
-*${PreÚltrðñ}tagall* = Module to tag evryone in a group
-*${PreÚltrðñ}translate* = Language Translator
-*${PreÚltrðñ}tts* = Text To Speech
-*${PreÚltrðñ}weather* = Get weather data of a city
-*${PreÚltrðñ}ud* = Urban Dictionary
+*${UltronSitreper.ULTRONIX}carbon* = Convert text/code to a carbon image
+*${UltronSitreper.ULTRONIX}pfp* = Get display picture
+*${UltronSitreper.ULTRONIX}github* = Find Github Profile
+*${UltronSitreper.ULTRONIX}neko* = Copy your text to nekobin
+*${UltronSitreper.ULTRONIX}joke* = Get Random Jokes
+*${UltronSitreper.ULTRONIX}meme* = Get Random Memes
+*${UltronSitreper.ULTRONIX}read* = Optical Character Recognition
+*${UltronSitreper.ULTRONIX}sticker* = Module to convert image to sticker
+*${UltronSitreper.ULTRONIX}tagall* = Module to tag evryone in a group
+*${UltronSitreper.ULTRONIX}translate* = Language Translator
+*${UltronSitreper.ULTRONIX}tts* = Text To Speech
+*${UltronSitreper.ULTRONIX}weather* = Get weather data of a city
+*${UltronSitreper.ULTRONIX}ud* = Urban Dictionary
 
 
 *••••••••••••••••➛  🔞𝗡𝗦𝗙𝗪* (private & group-admins!)
-*${PreÚltrðñ}ass* = I know you like anime ass~
-*${PreÚltrðñ}bdsm* = If you don't know what it is, search it up
-*${PreÚltrðñ}blowjob* = Basically an image of a girl sucking on a sharp blade!
-*${PreÚltrðñ}cum* = Basically sticky white stuff that is usually milked from sharpies
-*${PreÚltrðñ}doujin* = Sends a random doujin page imageURL!
-*${PreÚltrðñ}feet* = So you like smelly feet huh?
-*${PreÚltrðñ}femdom* = Female Domination?
-*${PreÚltrðñ}foxgirl* = Girl's that are wannabe foxes, yes
-*${PreÚltrðñ}glasses* = Girls that wear glasses
-*${PreÚltrðñ}hentai* = Sends a random vanilla hentai imageURL~
-*${PreÚltrðñ}maid* = Maids, Maid Uniforms, etc, you know what maids are :3
-*${PreÚltrðñ}masturbation* = Solo Queue in CSGO! Lol
-*${PreÚltrðñ}netorare* = Wow, I won't even question your fetishes
-*${PreÚltrðñ}orgy* = Group Lewd Acts
-*${PreÚltrðñ}panties* = I mean... just why? You like underwear?
-*${PreÚltrðñ}pussy* = The genitals of a female, or a cat, you give the meaning
-*${PreÚltrðñ}school* = School Uniforms!~ Yatta~!
-*${PreÚltrðñ}tentacles* = I'm sorry but, why do they look like intestines?
-*${PreÚltrðñ}thighs* = The top part of your legs, very hot, isn't it?
-*${PreÚltrðñ}uniform* = Military, Konbini, Work, Nurse Uniforms, etc!~ Sexy~
-*${PreÚltrðñ}yuri* = Girls on Girls, and Girl's only!
+*${UltronSitreper.ULTRONIX}ass* = I know you like anime ass~
+*${UltronSitreper.ULTRONIX}bdsm* = If you don't know what it is, search it up
+*${UltronSitreper.ULTRONIX}blowjob* = Basically an image of a girl sucking on a sharp blade!
+*${UltronSitreper.ULTRONIX}cum* = Basically sticky white stuff that is usually milked from sharpies
+*${UltronSitreper.ULTRONIX}doujin* = Sends a random doujin page imageURL!
+*${UltronSitreper.ULTRONIX}feet* = So you like smelly feet huh?
+*${UltronSitreper.ULTRONIX}femdom* = Female Domination?
+*${UltronSitreper.ULTRONIX}foxgirl* = Girl's that are wannabe foxes, yes
+*${UltronSitreper.ULTRONIX}glasses* = Girls that wear glasses
+*${UltronSitreper.ULTRONIX}hentai* = Sends a random vanilla hentai imageURL~
+*${UltronSitreper.ULTRONIX}maid* = Maids, Maid Uniforms, etc, you know what maids are :3
+*${UltronSitreper.ULTRONIX}masturbation* = Solo Queue in CSGO! Lol
+*${UltronSitreper.ULTRONIX}netorare* = Wow, I won't even question your fetishes
+*${UltronSitreper.ULTRONIX}orgy* = Group Lewd Acts
+*${UltronSitreper.ULTRONIX}panties* = I mean... just why? You like underwear?
+*${UltronSitreper.ULTRONIX}pussy* = The genitals of a female, or a cat, you give the meaning
+*${UltronSitreper.ULTRONIX}school* = School Uniforms!~ Yatta~!
+*${UltronSitreper.ULTRONIX}tentacles* = I'm sorry but, why do they look like intestines?
+*${UltronSitreper.ULTRONIX}thighs* = The top part of your legs, very hot, isn't it?
+*${UltronSitreper.ULTRONIX}uniform* = Military, Konbini, Work, Nurse Uniforms, etc!~ Sexy~
+*${UltronSitreper.ULTRONIX}yuri* = Girls on Girls, and Girl's only!
 
 𝐖𝐨𝐫𝐤 𝐈𝐧 𝐏𝐫𝐨𝐠𝐫𝐞𝐬𝐬:
 https://github.com/Krakinz/Ultron.git`,
