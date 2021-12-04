b5704a92f50927c40c01636707a7acf4e8547f36
krakinzkon <krakinzkon@gmail.com>
2021-12-04 10:01:49 -0800

    fix updates

--- ᴜʟᴛʀᴏɴ💀ᴄᴜʟᴀʀ/Information📜/update.js
+++ ᴜʟᴛʀᴏɴ💀ᴄᴜʟᴀʀ/Information📜/update.js
@@ -10,7 +10,7 @@ module.exports = {
   description: `This module can be used to update the bot`,
   async handle(υℓтяσηℓιєηт, chat, ᴜʟᴛʀᴏɴ, Arc) {
     if (!ᴜʟᴛʀᴏɴ.fromMe) {
-      await υℓтяσηℓιєηт
+      return await υℓтяσηℓιєηт
         .sendMessage(
           ᴜʟᴛʀᴏɴ.chatId,
           {
@@ -24,28 +24,31 @@ Only My Owner Can use that command!`,
           }
         )
         .catch((error) => ℓιєηт.catch(error, υℓтяσηℓιєηт, ᴜʟᴛʀᴏɴ));
-    }
-    await υℓтяσηℓιєηт
-      .sendMessage(
-        ᴜʟᴛʀᴏɴ.chatId,
-        {
-          url: `https://i.postimg.cc/LXXS56kT/reboot.png`,
-        },
-        MessageType.image,
-        {
-          mimetype: Mimetype.png,
-          caption: `
+    } else {
+      await υℓтяσηℓιєηт
+        .sendMessage(
+          ᴜʟᴛʀᴏɴ.chatId,
+          {
+            url: `https://i.postimg.cc/LXXS56kT/reboot.png`,
+          },
+          MessageType.image,
+          {
+            mimetype: Mimetype.png,
+            caption: `
 ❗𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍❗
 ʏᴏᴜʀ ᴜʟᴛʀᴏɴ ʙᴏᴛ ɪꜱ ɴᴏᴡ ᴜᴘᴅᴀᴛɪɴɢ...
 ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ꜰᴏʀ 30-60ꜱᴇᴄ
 
 ⚠️𝗜𝗳 𝗶𝘁 𝘄𝗮𝘀 𝗮 𝗕𝗜𝗚 ⚡𝐔𝐋𝐓𝐑𝐎𝐍™ 𝘂𝗽𝗱𝗮𝘁𝗲,
 𝘁𝗵𝗲𝗻 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗱𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗯𝗼𝘁!`,
-        }
-      )
-      .catch((error) => ℓιєηт.catch(error, υℓтяσηℓιєηт, ᴜʟᴛʀᴏɴ));
-    heroku
-      .delete("/apps/" + UltronSitreper.HEROKU_BOT_NAME + "/dynos/" + "worker")
-      .then((x) => console.log(x));
+          }
+        )
+        .catch((error) => ℓιєηт.catch(error, υℓтяσηℓιєηт, ᴜʟᴛʀᴏɴ));
+      heroku
+        .delete(
+          "/apps/" + UltronSitreper.HEROKU_BOT_NAME + "/dynos/" + "worker"
+        )
+        .then((x) => console.log(x));
+    }
   },
 };
