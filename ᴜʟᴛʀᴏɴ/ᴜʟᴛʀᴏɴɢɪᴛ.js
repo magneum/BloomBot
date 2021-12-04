5e7ff121110be697ff8d1f321ce5d687ad216bc2
krakinzkon <krakinzkon@gmail.com>
2021-12-04 09:28:18 -0800

    ⚡𝐔𝐋𝐓𝐑𝐎𝐍™

--- ᴜʟᴛʀᴏɴ/ᴜʟᴛʀᴏɴɢɪᴛ.js
+++ ᴜʟᴛʀᴏɴ/ᴜʟᴛʀᴏɴɢɪᴛ.js
@@ -13,18 +13,21 @@ const gitPull = async () => {
           exec("npm install").stderr.pipe(process.stderr);
         }
         console.log(
-          chalk.greenBright.bold("💡⬰ Updated the bot with latest changes.")
+          chalk.greenBright.bold(
+            "💡⬰ 𝗔𝘂𝘁𝗼-𝗨𝗽𝗱𝗮𝘁𝗲𝗱 ⚡𝐔𝐋𝐓𝐑𝐎𝐍™ 𝘄𝗶𝘁𝗵 𝗹𝗮𝘁𝗲𝘀𝘁 𝗰𝗵𝗮𝗻𝗴𝗲𝘀."
+          )
         );
       } else if (err) {
         console.log(
-          chalk.redBright.bold("❌⬰ Could not pull latest changes!")
+          chalk.redBright.bold(
+            "❌⬰ 𝗜𝗳 𝗶𝘁 𝘄𝗮𝘀 𝗮 𝗕𝗜𝗚 ⚡𝐔𝐋𝐓𝐑𝐎𝐍™ 𝘂𝗽𝗱𝗮𝘁𝗲 𝘁𝗵𝗲𝗻 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗱𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗯𝗼𝘁!"
+          )
         );
-        console.log(err);
       }
     });
   } else {
     console.log(
-      chalk.greenBright.bold("💡⬰ Bot is already working on latest version.")
+      chalk.greenBright.bold("💡⬰ ⚡𝐔𝐋𝐓𝐑𝐎𝐍™ 𝗶𝘀 𝗮𝗹𝗿𝗲𝗮𝗱𝘆 𝗼𝗻 𝗹𝗮𝘁𝗲𝘀𝘁 𝘃𝗲𝗿𝘀𝗶𝗼𝗻.")
     );
   }
 };
