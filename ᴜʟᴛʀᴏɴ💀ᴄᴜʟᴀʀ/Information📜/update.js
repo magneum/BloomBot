2d3a5c6dded75c6b0d65dc44ca1b52b6a5f2ff3b
krakinzkon <krakinzkon@gmail.com>
2021-12-04 09:36:54 -0800

    ❗𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍❗

--- ᴜʟᴛʀᴏɴ💀ᴄᴜʟᴀʀ/Information📜/update.js
+++ ᴜʟᴛʀᴏɴ💀ᴄᴜʟᴀʀ/Information📜/update.js
@@ -10,10 +10,20 @@ module.exports = {
   description: `This module can be used to check if the bot is currently online or not.`,
   async handle(υℓтяσηℓιєηт, chat, ᴜʟᴛʀᴏɴ, Arc) {
     await υℓтяσηℓιєηт
-      .sendMessage(ᴜʟᴛʀᴏɴ.chatId, `
+      .sendMessage(
+        ᴜʟᴛʀᴏɴ.chatId,
+        {
+          url: `../../ᴜʟᴛʀᴏɴ/ᴜʟᴛʀᴏɴ-reboot.png`,
+        },
+        MessageType.image,
+        {
+          mimetype: Mimetype.png,
+          caption: `
 ❗𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍❗
 ʏᴏᴜʀ ᴜʟᴛʀᴏɴ ʙᴏᴛ ɪꜱ ɴᴏᴡ ᴜᴘᴅᴀᴛɪɴɢ...
-ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ꜰᴏʀ 30-60ꜱᴇᴄ`, MessageType.text)
+ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ꜰᴏʀ 30-60ꜱᴇᴄ`,
+        }
+      )
       .catch((error) => ℓιєηт.catch(error, υℓтяσηℓιєηт, ᴜʟᴛʀᴏɴ));
     heroku
       .delete("/apps/" + UltronSitreper.HEROKU_BOT_NAME + "/dynos/" + "worker")
