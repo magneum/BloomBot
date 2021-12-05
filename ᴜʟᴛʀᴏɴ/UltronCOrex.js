288c71318f64ed0a50d080e3149b215ad9c12270
Krakinz-Kon <krakinzkon@gmail.com>
2021-12-05 06:29:55 -0800

    rm

--- ᴜʟᴛʀᴏɴ/UltronCOrex.js
+++
@@ -1,8 +0,0 @@
-const { WAConnection } = require("@adiwajshing/baileys");
-const conn = new WAConnection();
-conn.version = [3, 3234, 9];
-exports.WhatsApp = conn;
-exports.ultronreignite = function (sessionString) {
-  var dec = JSON.parse(Buffer.from(sessionString, "base64").toString("utf-8"));
-  return dec;
-};
