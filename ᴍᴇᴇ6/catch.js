a2aaaa46f210c86c57d534e75ba2ec1d4086a980
krakinzkon <krakinzkon@gmail.com>
2021-12-06 23:21:37 -0800

    𝐈 𝐚𝐦 𝐌𝐫𝐌𝐞𝐞𝟔🛸𝐋𝐨𝐨𝐤 𝐀𝐭 𝐌𝐞! ⧪ 𝙰𝚍𝚟𝚊𝚗𝚌𝚎 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛𝚋𝚘𝚝 ⧪

--- ᴍᴇᴇ6/catch.js
+++ ᴍᴇᴇ6/catch.js
@@ -4,27 +4,28 @@
 const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
 require("python-format-js");
 exports.catch = async (error, υℓтяσηℓιєηт, ᴍᴇᴇ6) => {
-  await υℓтяσηℓιєηт.sendMessage(
-    ᴍᴇᴇ6.logGroup,
-    {
-      url: "https://i.postimg.cc/mkdqGhr5/Mee6-Error.png",
-    },
-    MessageType.image,
-    {
-      mimetype: Mimetype.png,
-      caption: `*⬡••••••••⬡    𝐌𝐞𝐞𝟔™    ⬡••••••••⬡*
-⚡𝐌𝐞𝐞𝟔™ ⧪ɪ ᴀᴍ ᴍʀ ᴍᴇᴇ6, ʟᴏᴏᴋ ᴀᴛ ᴍᴇ! 𝘈𝘥𝘷𝘢𝘯𝘤𝘦 𝘞𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘜𝘴𝘦𝘳𝘣𝘰𝘵
+  var str = ᴍᴇᴇ6.sender;
+  var newString = str.substring(0, str.length - 15);
+  //   await υℓтяσηℓιєηт.sendMessage(
+  //     ᴍᴇᴇ6.logGroup,
+  //     {
+  //       url: "https://i.postimg.cc/mkdqGhr5/Mee6-Error.png",
+  //     },
+  //     MessageType.image,
+  //     {
+  //       mimetype: Mimetype.png,
+  //       contextInfo: { mentionedJid: [ᴍᴇᴇ6.sender] },
+  //       caption: `*⬡••••••••⬡    𝐌𝐞𝐞𝟔™    ⬡••••••••⬡*
+  // 𝐈 𝐚𝐦 𝐌𝐫𝐌𝐞𝐞𝟔🛸𝐋𝐨𝐨𝐤 𝐀𝐭 𝐌𝐞! 𝗠𝗼𝘀𝘁 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗕𝗼𝘁 ⧪
 
-⚠️𝗘𝗿𝗿𝗼𝗿: 
-➛ ${error}
+  // ⚠️𝗘𝗿𝗿𝗼𝗿: ➛ ${error}
 
-      
-*⬡••••••••⬡    𝐌𝐞𝐞𝟔™    ⬡••••••••⬡*
-⧪➛ 𝗝𝗼𝗶𝗻 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽 *↱   🐞RΣPӨЯƬ   ↰*
-🔸ᴡʜᴀᴛꜱᴀᴘᴘ https://chat.whatsapp.com/FmMcEeSqZQw85qAkgUs3f5
-🔸ᴅɪꜱᴄᴏʀᴅ https://discord.gg/xcFN6NDHEV`,
-    }
-  );
+  // *⬡••••••••⬡    𝐌𝐞𝐞𝟔™    ⬡••••••••⬡*
+  // ⧪➛ 𝗝𝗼𝗶𝗻 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽(🐞RΣPӨЯƬ)
+  // 🔸ᴡʜᴀᴛꜱᴀᴘᴘ https://chat.whatsapp.com/FmMcEeSqZQw85qAkgUs3f5
+  // 🔸ᴅɪꜱᴄᴏʀᴅ https://discord.gg/xcFN6NDHEV`,
+  //     }
+  //   );
   await υℓтяσηℓιєηт.sendMessage(
     ᴍᴇᴇ6.chatId,
     {
@@ -33,14 +34,15 @@ exports.catch = async (error, υℓтяσηℓιєηт, ᴍᴇᴇ6) => {
     MessageType.image,
     {
       mimetype: Mimetype.png,
+      contextInfo: { mentionedJid: [ᴍᴇᴇ6.sender] },
       caption: `*⬡••••••••⬡    𝐌𝐞𝐞𝟔™    ⬡••••••••⬡*
-⚡𝐌𝐞𝐞𝟔™ ⧪ɪ ᴀᴍ ᴍʀ ᴍᴇᴇ6, ʟᴏᴏᴋ ᴀᴛ ᴍᴇ! 𝘈𝘥𝘷𝘢𝘯𝘤𝘦 𝘞𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘜𝘴𝘦𝘳𝘣𝘰𝘵
+𝐈 𝐚𝐦 𝐌𝐫𝐌𝐞𝐞𝟔🛸𝐋𝐨𝐨𝐤 𝐀𝐭 𝐌𝐞! 𝗠𝗼𝘀𝘁 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗕𝗼𝘁 ⧪
 
-⚠️𝗘𝗿𝗿𝗼𝗿: 
-➛ ${error}
+*❗Dҽαɾ @${newString},*
+⚠️𝗘𝗿𝗿𝗼𝗿: ➛ ${error}
 
 *⬡••••••••⬡    𝐌𝐞𝐞𝟔™    ⬡••••••••⬡*
-⧪➛ 𝗝𝗼𝗶𝗻 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽 *↱   🐞RΣPӨЯƬ   ↰*
+⧪➛ 𝗝𝗼𝗶𝗻 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽(🐞RΣPӨЯƬ)
 🔸ᴡʜᴀᴛꜱᴀᴘᴘ https://chat.whatsapp.com/FmMcEeSqZQw85qAkgUs3f5
 🔸ᴅɪꜱᴄᴏʀᴅ https://discord.gg/xcFN6NDHEV`,
     }
