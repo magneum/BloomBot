cdc6b3602ce0de162ac526cc7a098ad7ff925b86
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2021-12-12 06:02:35 -0800

    Update _𝔏𝔞𝔟_.js

--- 𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/_𝔏𝔞𝔟_.js
+++ 𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/_𝔏𝔞𝔟_.js
@@ -47,7 +47,7 @@ const env = {
           },
         }),
   ERROR: "https://i.postimg.cc/q7GB8m6s/Error0.png",
-  MEE: "https://i.postimg.cc/fW5DtrMQ/Mizuki-min.png",
+  MEE: "https://i.postimg.cc/02Z02hjK/Mizuki-Low.jpg",
   INVL: "https://i.postimg.cc/v8p25RV9/Mee6-Invalid.png",
   ARC: "https://i.postimg.cc/Nf3D2RZx/Mee6-Args.png",
   UPT: "https://i.postimg.cc/FH22ny07/Meeseeks-Box.png",
