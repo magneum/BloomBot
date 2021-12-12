6bf3b85267b70afa813d8db8d9ddab51d43cd612
krakinzkon <krakinzkon@gmail.com>
2021-12-12 10:06:58 -0800

    ⬡==================⬡-----------------⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡

--- 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/⚡Fun/beautycheck.js
+++ 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/⚡Fun/beautycheck.js
@@ -1,9 +1,123 @@
 const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
 const ℓιєηт = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/catch`);
 const _𝔏𝔞𝔟_ = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/_𝔏𝔞𝔟_`);
+var randomMC = require(`random-material-color`);
 var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, "g");
 var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
 const 𝓜𝓮𝓮git = require("simple-git")();
+const { registerFont, createCanvas, loadImage } = require(`canvas`);
+registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/Honeycomb-Round.otf`, {
+  family: `Honeycomb`,
+});
+registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/LobsterTwo-Bold.ttf`, {
+  family: `Lobster`,
+});
+registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/HousttelySignature-GOonZ.ttf`, {
+  family: `Housttely`,
+});
+const fs = require("fs");
+const can = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+  "32",
+  "33",
+  "34",
+  "35",
+  "36",
+  "37",
+  "38",
+  "39",
+  "40",
+  "41",
+  "42",
+  "43",
+  "44",
+  "45",
+  "46",
+  "47",
+  "48",
+  "49",
+  "50",
+  "51",
+  "52",
+  "53",
+  "54",
+  "55",
+  "56",
+  "57",
+  "58",
+  "59",
+  "60",
+  "61",
+  "62",
+  "63",
+  "64",
+  "65",
+  "66",
+  "67",
+  "68",
+  "69",
+  "70",
+  "71",
+  "72",
+  "73",
+  "74",
+  "75",
+  "76",
+  "77",
+  "78",
+  "79",
+  "80",
+  "81",
+  "82",
+  "83",
+  "84",
+  "85",
+  "86",
+  "87",
+  "88",
+  "89",
+  "90",
+  "91",
+  "92",
+  "93",
+  "94",
+  "95",
+  "96",
+  "97",
+  "98",
+  "99",
+  "100",
+];
 // ⬡==================⬡-----------------⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡
 // 🛸𝓜𝖎𝖟𝖚ӄ𝖎™ ᴀᴅᴠᴀɴᴄᴇᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀʙᴏᴛ ᴡɪᴛʜ 80+ ᴄᴏᴍᴍᴀɴᴅꜱ ꜰᴏʀ ʙᴏᴛʜ ᴘʀɪᴠᴀᴛᴇ ᴀɴᴅ ɢʀᴏᴜᴘ-ᴜꜱᴀɢᴇ🛸
 // ⬡==================⬡-----------------⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡
@@ -54,34 +168,61 @@ module.exports = {
         personreceived = Receiver.substring(0, Receiver.length - 15);
         var Sender = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
         personsending = Sender.substring(0, Sender.length - 15);
-        const can = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
         const Much = can[Math.floor(Math.random() * can.length)];
-        await 𝓚𝓻𝓪𝓴𝓲𝓷𝔃
-          .sendMessage(
-            𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-            `⬡••• *𝐌𝐢𝐳𝐮𝐤𝐢 by ₭𝔯𝔞ӄ𝔦𝔫𝔷𝔏𝔞𝔟* •••⦿
-
-Question : How Beautyfull is *@${personreceived}*
-Asked By? @${personsending} 
-Answer : ${Much}%`,
-            MessageType.text,
-            {
-              contextInfo: {
-                mentionedJid: [𝓜𝖎𝖟𝖚ӄ𝖎.sender],
+        const width = 1280;
+        const height = 720;
+        const canvas = createCanvas(width, height);
+        const context = canvas.getContext("2d");
+        context.fillStyle = "#252525";
+        context.fillRect(0, 0, width, height);
+        context.font = "bold 90pt Housttely";
+        context.textAlign = `center`;
+        context.textBaseline = `top`;
+        // context.fillStyle = randomMC.getColor();
+        const text = "Beauty💄Checker";
+        const textWidth = context.measureText(text).width;
+        context.fillRect(
+          600 - textWidth / 2 - 10,
+          170 - 5,
+          textWidth + 20,
+          120
+        );
+        context.fillStyle = randomMC.getColor();
+        context.fillText(text, 600, 170);
+        context.fillStyle = "#fff";
+        context.font = "bold 50pt Honeycomb";
+        context.fillText("Percentage: " + Much + "%", 600, 530);
+        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png").then(async () => {
+          const buffer = canvas.toBuffer("image/png");
+          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png", buffer);
+          𝓚𝓻𝓪𝓴𝓲𝓷𝔃
+            .sendMessage(
+              𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
+              {
+                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png",
               },
-            }
-          )
-          .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
+              MessageType.image,
+              {
+                mimetype: Mimetype.png,
+                contextInfo: { mentionedJid: [Sender, Receiver] },
+                caption: `⬡••• *𝐌𝐢𝐳𝐮𝐤𝐢 by ₭𝔯𝔞ӄ𝔦𝔫𝔷𝔏𝔞𝔟* •••⦿
+
+𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ʜᴏᴡ ʙᴇᴀᴜᴛʏꜰᴜʟʟ ɪꜱ *@${personreceived}*
+Asked By? @${personsending} 
+𝗣𝗲𝗿𝗰𝗲𝗻𝘁𝗮𝗴𝗲: ${Much}%`,
+              }
+            )
+            .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
+        });
       } else {
         var Sender = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
         personsending = Sender.substring(0, Sender.length - 15);
-        const can = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
         const Much = can[Math.floor(Math.random() * can.length)];
         await 𝓚𝓻𝓪𝓴𝓲𝓷𝔃
           .sendMessage(
             𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-            `Question : How Beautyfull is *@${personsending}*` +
-              "*\n\nAnswer : " +
+            `𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ʜᴏᴡ ʙᴇᴀᴜᴛʏꜰᴜʟʟ ɪꜱ *@${personsending}*` +
+              "*\n\n𝗣𝗲𝗿𝗰𝗲𝗻𝘁𝗮𝗴𝗲: " +
               Much +
               "%",
             MessageType.text,
@@ -92,25 +233,17 @@ Answer : ${Much}%`,
             }
           )
           .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
-        const { registerFont, createCanvas, loadImage } = require(`canvas`);
-        registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/Honeycomb-Round.otf`, {
-          family: `Honeycomb`,
-        });
-        registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/LobsterTwo-Bold.ttf`, {
-          family: `Lobster`,
-        });
-        const fs = require("fs");
-        const width = 1200;
-        const height = 630;
+        const width = 1280;
+        const height = 720;
         const canvas = createCanvas(width, height);
         const context = canvas.getContext("2d");
-        context.fillStyle = "#000";
+        context.fillStyle = "#252525";
         context.fillRect(0, 0, width, height);
-        context.font = "bold 70pt Lobster";
-        context.textAlign = "center";
-        context.textBaseline = "top";
-        context.fillStyle = "#3574d4";
-        const text = "Answer : " + Much + "%";
+        context.font = "bold 90pt Housttely";
+        context.textAlign = `center`;
+        context.textBaseline = `top`;
+        // context.fillStyle = randomMC.getColor();
+        const text = "Beauty💄Checker";
         const textWidth = context.measureText(text).width;
         context.fillRect(
           600 - textWidth / 2 - 10,
@@ -118,24 +251,28 @@ Answer : ${Much}%`,
           textWidth + 20,
           120
         );
-        context.fillStyle = "#fff";
+        context.fillStyle = randomMC.getColor();
         context.fillText(text, 600, 170);
         context.fillStyle = "#fff";
-        context.font = "bold 30pt Honeycomb";
-        context.fillText(`How Much Beautyful `, 600, 530);
-        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.jpg").then(async () => {
+        context.font = "bold 50pt Honeycomb";
+        context.fillText("Percentage: " + Much + "%", 600, 530);
+        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png").then(async () => {
           const buffer = canvas.toBuffer("image/png");
-          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/test.png", buffer);
+          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png", buffer);
           𝓚𝓻𝓪𝓴𝓲𝓷𝔃
             .sendMessage(
               𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
               {
-                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/test.png",
+                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png",
               },
               MessageType.image,
               {
                 mimetype: Mimetype.png,
-                caption: `⬡••• *𝐌𝐢𝐳𝐮𝐤𝐢 by ₭𝔯𝔞ӄ𝔦𝔫𝔷𝔏𝔞𝔟* •••⦿`,
+                contextInfo: { mentionedJid: [Sender] },
+                caption: `⬡••• *𝐌𝐢𝐳𝐮𝐤𝐢 by ₭𝔯𝔞ӄ𝔦𝔫𝔷𝔏𝔞𝔟* •••⦿
+
+𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ʜᴏᴡ ʙᴇᴀᴜᴛʏꜰᴜʟʟ ɪꜱ *@${personsending}*
+𝗣𝗲𝗿𝗰𝗲𝗻𝘁𝗮𝗴𝗲: ${Much}%`,
               }
             )
             .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
