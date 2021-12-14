34f1a2925770565bb74f952febdc58a911cd6309
krakinzkon <krakinzkon@gmail.com>
2021-12-14 01:00:08 -0800

    𝓜𝖎𝖟𝖚ӄ𝖎™

---
+++ ytmp3.js
@@ -0,0 +1,99 @@
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+//•⬡• ɢɴᴜ ɢᴇɴᴇʀᴀʟ ᴘᴜʙʟɪᴄ ʟɪᴄᴇɴꜱᴇ ᴠᴇʀꜱɪᴏɴ 3, 29 ᴊᴜɴᴇ 2007 ᴄᴏᴘʏʀɪɢʜᴛ (ᴄ) 2007 ꜰʀᴇᴇ ꜱᴏꜰᴛᴡᴀʀᴇ ꜰᴏᴜɴᴅᴀᴛɪᴏɴ
+//•⬡• ᴇᴠᴇʀʏᴏɴᴇ ɪꜱ ᴘᴇʀᴍɪᴛᴛᴇᴅ ᴛᴏ 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 ᴠᴇʀʙᴀᴛɪᴍ ᴄᴏᴘɪᴇꜱ ᴏꜰ ᴛʜɪꜱ ʟɪᴄᴇɴꜱᴇ ᴅᴏᴄᴜᴍᴇɴᴛ, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+//•⬡• —••÷[ 𝓜𝖎𝖟𝖚ӄ𝖎™ ]÷••—  ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀʙᴏᴛ has been licensed under Apache License
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const Carbon = require(`unofficial-carbon-now`);
+const ℓιєηт = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/catch`);
+const _𝔏𝔞𝔟_ = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/_𝔏𝔞𝔟_`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const fs = require(`fs`);
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+//•⬡• ɢɴᴜ ɢᴇɴᴇʀᴀʟ ᴘᴜʙʟɪᴄ ʟɪᴄᴇɴꜱᴇ ᴠᴇʀꜱɪᴏɴ 3, 29 ᴊᴜɴᴇ 2007 ᴄᴏᴘʏʀɪɢʜᴛ (ᴄ) 2007 ꜰʀᴇᴇ ꜱᴏꜰᴛᴡᴀʀᴇ ꜰᴏᴜɴᴅᴀᴛɪᴏɴ
+//•⬡• ᴇᴠᴇʀʏᴏɴᴇ ɪꜱ ᴘᴇʀᴍɪᴛᴛᴇᴅ ᴛᴏ 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 ᴠᴇʀʙᴀᴛɪᴍ ᴄᴏᴘɪᴇꜱ ᴏꜰ ᴛʜɪꜱ ʟɪᴄᴇɴꜱᴇ ᴅᴏᴄᴜᴍᴇɴᴛ, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+//•⬡• —••÷[ 𝓜𝖎𝖟𝖚ӄ𝖎™ ]÷••—  ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀʙᴏᴛ has been licensed under Apache License
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+const 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱 = require(`simple-git`)();
+await 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱.fetch();
+var 𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓 = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
+var 𝓢𝓮𝓷𝓭𝓮𝓻𝓒𝓸𝓷𝓯𝓲𝓰 = 𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓.substring(0, 𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓.length - 15);
+var 𝙵𝚎𝚝𝚌𝚑𝙼𝚒𝚣𝚞𝚔𝚒𝙽𝚎𝚜𝚜 = await 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱.log([`KrakinzLab..origin/KrakinzLab`]);
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+const sendMediaURL = async (to, url, text = ``, mids = []) => {
+  if (mids.length > 0) {
+    text = normalizeMention(to, text, mids);
+  } else {
+    const fn = Date.now() / 10000;
+    const filename = fn.toString();
+    let mime = ``;
+    var download = function (uri, filename, callback) {
+      request.head(uri, function (err, res, body) {
+        mime = res.headers[`content-type`];
+        request(uri).pipe(fs.createWriteStream(filename)).on(`close`, callback);
+      });
+    };
+  }
+  `⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+  download(url, filename, async function () {
+    console.log(`done`);
+    let media = fs.readFileSync(filename);
+    let type = mime.split(`/`)[0] + `Message`;
+    if (mime === `image/gif`) {
+      type = MessageType.video;
+      mime = Mimetype.gif;
+    } else if (mime.split(`/`)[0] === `audio`) {
+      mime = Mimetype.mp4Audio;
+    }
+    𝓚𝓻𝓪𝓴𝓲𝓷𝔃.sendMessage(to, media, type, {
+      quoted: chat,
+      mimetype: mime,
+      caption: text,
+      contextInfo: { mentionedJid: mids },
+    });
+    fs.unlinkSync(filename);
+  });
+};
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+if (args.length === 0) {
+  return reply(
+    `Send orders *${prefix}play* _The title of the song to be search for_`
+  );
+} else {
+  var search = args.join(` `);
+  found = await yts(search);
+  everything = found.all;
+  var tojsoned = everything[0].url;
+  yta(tojsoned).then((res) => {
+    const { dl_link, thumb, title, filesizeF, filesize } = res;
+    axios
+      .get(`https://tinyurl.com/api-create.php?url=${dl_link}`)
+      .then(async (auto) => {
+        if (Number(filesize) >= 100000) {
+          return sendMediaURL(
+            𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓,
+            thumb,
+            `*PLAY MUSIC*
+
+*Title* : ${title}
+*Ext* : MP3
+*Filesize* : ${filesizeF}
+*Link* : ${auto.data}
+_For the duration of more than the limit is presented in the form of auto link_`
+          );
+        } else {
+          const captions = `🎧 *PLAY MUSIC*
+
+*Title* : ${title}
+*Ext* : MP3
+*Size* : ${filesizeF}
+*Link* : ${auto.data}
+_Please wait for the media file to be sent it may take auto few minutes_`;
+          await sendMediaURL(𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓, thumb, captions);
+          sendMediaURL(𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓, dl_link).catch(() => reply(`error`));
+        }
+      });
+  });
+}
--- 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/⚡Fun/beautycheck.js
+++ 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/⚡Fun/beautycheck.js
@@ -1,13 +1,13 @@
-"⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡";
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
 //•⬡• ɢɴᴜ ɢᴇɴᴇʀᴀʟ ᴘᴜʙʟɪᴄ ʟɪᴄᴇɴꜱᴇ ᴠᴇʀꜱɪᴏɴ 3, 29 ᴊᴜɴᴇ 2007 ᴄᴏᴘʏʀɪɢʜᴛ (ᴄ) 2007 ꜰʀᴇᴇ ꜱᴏꜰᴛᴡᴀʀᴇ ꜰᴏᴜɴᴅᴀᴛɪᴏɴ
 //•⬡• ᴇᴠᴇʀʏᴏɴᴇ ɪꜱ ᴘᴇʀᴍɪᴛᴛᴇᴅ ᴛᴏ 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 ᴠᴇʀʙᴀᴛɪᴍ ᴄᴏᴘɪᴇꜱ ᴏꜰ ᴛʜɪꜱ ʟɪᴄᴇɴꜱᴇ ᴅᴏᴄᴜᴍᴇɴᴛ, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
 //•⬡• —••÷[ 𝓜𝖎𝖟𝖚ӄ𝖎™ ]÷••—  ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀʙᴏᴛ has been licensed under Apache License
-"⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡";
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
 const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
 const ℓιєηт = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/catch`);
 const _𝔏𝔞𝔟_ = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/_𝔏𝔞𝔟_`);
 var randomMC = require(`random-material-color`);
-var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, "g");
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
 var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
 const { registerFont, createCanvas, loadImage } = require(`canvas`);
 registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/Honeycomb-Round.otf`, {
@@ -19,128 +19,128 @@ registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/LobsterTwo-Bold.t
 registerFont(`./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/HousttelySignature-GOonZ.ttf`, {
   family: `Housttely`,
 });
-const fs = require("fs");
+const fs = require(`fs`);
 const can = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-  "32",
-  "33",
-  "34",
-  "35",
-  "36",
-  "37",
-  "38",
-  "39",
-  "40",
-  "41",
-  "42",
-  "43",
-  "44",
-  "45",
-  "46",
-  "47",
-  "48",
-  "49",
-  "50",
-  "51",
-  "52",
-  "53",
-  "54",
-  "55",
-  "56",
-  "57",
-  "58",
-  "59",
-  "60",
-  "61",
-  "62",
-  "63",
-  "64",
-  "65",
-  "66",
-  "67",
-  "68",
-  "69",
-  "70",
-  "71",
-  "72",
-  "73",
-  "74",
-  "75",
-  "76",
-  "77",
-  "78",
-  "79",
-  "80",
-  "81",
-  "82",
-  "83",
-  "84",
-  "85",
-  "86",
-  "87",
-  "88",
-  "89",
-  "90",
-  "91",
-  "92",
-  "93",
-  "94",
-  "95",
-  "96",
-  "97",
-  "98",
-  "99",
-  "100",
+  `1`,
+  `2`,
+  `3`,
+  `4`,
+  `5`,
+  `6`,
+  `7`,
+  `8`,
+  `9`,
+  `10`,
+  `11`,
+  `12`,
+  `13`,
+  `14`,
+  `15`,
+  `16`,
+  `17`,
+  `18`,
+  `19`,
+  `20`,
+  `21`,
+  `22`,
+  `23`,
+  `24`,
+  `25`,
+  `26`,
+  `27`,
+  `28`,
+  `29`,
+  `30`,
+  `31`,
+  `32`,
+  `33`,
+  `34`,
+  `35`,
+  `36`,
+  `37`,
+  `38`,
+  `39`,
+  `40`,
+  `41`,
+  `42`,
+  `43`,
+  `44`,
+  `45`,
+  `46`,
+  `47`,
+  `48`,
+  `49`,
+  `50`,
+  `51`,
+  `52`,
+  `53`,
+  `54`,
+  `55`,
+  `56`,
+  `57`,
+  `58`,
+  `59`,
+  `60`,
+  `61`,
+  `62`,
+  `63`,
+  `64`,
+  `65`,
+  `66`,
+  `67`,
+  `68`,
+  `69`,
+  `70`,
+  `71`,
+  `72`,
+  `73`,
+  `74`,
+  `75`,
+  `76`,
+  `77`,
+  `78`,
+  `79`,
+  `80`,
+  `81`,
+  `82`,
+  `83`,
+  `84`,
+  `85`,
+  `86`,
+  `87`,
+  `88`,
+  `89`,
+  `90`,
+  `91`,
+  `92`,
+  `93`,
+  `94`,
+  `95`,
+  `96`,
+  `97`,
+  `98`,
+  `99`,
+  `100`,
 ];
-("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
 //•⬡• ɢɴᴜ ɢᴇɴᴇʀᴀʟ ᴘᴜʙʟɪᴄ ʟɪᴄᴇɴꜱᴇ ᴠᴇʀꜱɪᴏɴ 3, 29 ᴊᴜɴᴇ 2007 ᴄᴏᴘʏʀɪɢʜᴛ (ᴄ) 2007 ꜰʀᴇᴇ ꜱᴏꜰᴛᴡᴀʀᴇ ꜰᴏᴜɴᴅᴀᴛɪᴏɴ
 //•⬡• ᴇᴠᴇʀʏᴏɴᴇ ɪꜱ ᴘᴇʀᴍɪᴛᴛᴇᴅ ᴛᴏ 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 ᴠᴇʀʙᴀᴛɪᴍ ᴄᴏᴘɪᴇꜱ ᴏꜰ ᴛʜɪꜱ ʟɪᴄᴇɴꜱᴇ ᴅᴏᴄᴜᴍᴇɴᴛ, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
 //•⬡• —••÷[ 𝓜𝖎𝖟𝖚ӄ𝖎™ ]÷••—  ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀʙᴏᴛ has been licensed under Apache License
-("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
 module.exports = {
   name: `beautycheck`,
   description: `ᴄʜᴇᴄᴋ ʜᴏᴡ ʙᴇᴀᴜᴛɪꜰᴜʟ ʏᴏᴜ/ᴛᴀɢɢᴇᴅ ᴘᴇʀꜱᴏɴ ɪꜱ`,
   𝓜𝓮𝓮6ʍօʀɛ: `${ᴋᴇɪ}`,
   async handle(𝓚𝓻𝓪𝓴𝓲𝓷𝔃, chat, 𝓜𝖎𝖟𝖚ӄ𝖎, arg, 𝓜𝓲𝔃𝓾𝓴𝓲𝓢𝓲𝓽𝓻𝓮𝓹) {
-    ("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
-    const 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱 = require("simple-git")();
+    `⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+    const 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱 = require(`simple-git`)();
     await 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱.fetch();
     var 𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓 = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
     var 𝓢𝓮𝓷𝓭𝓮𝓻𝓒𝓸𝓷𝓯𝓲𝓰 = 𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓.substring(0, 𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓.length - 15);
     var 𝙵𝚎𝚝𝚌𝚑𝙼𝚒𝚣𝚞𝚔𝚒𝙽𝚎𝚜𝚜 = await 𝓜𝖎𝖟𝖚ӄ𝖎𝔊𝔦𝔱.log([
-      "KrakinzLab..origin/KrakinzLab",
+      `KrakinzLab..origin/KrakinzLab`,
     ]);
-    ("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+    `⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
     if (𝙵𝚎𝚝𝚌𝚑𝙼𝚒𝚣𝚞𝚔𝚒𝙽𝚎𝚜𝚜.total != 0) {
       await 𝓚𝓻𝓪𝓴𝓲𝓷𝔃
         .sendMessage(
@@ -154,8 +154,8 @@ module.exports = {
         )
         .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
     }
-    ("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
-    if (𝓜𝖎𝖟𝖚ӄ𝖎.chatId === "120363025343298860@g.us" && !𝓜𝖎𝖟𝖚ӄ𝖎.isSenderSUDO) {
+    `⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
+    if (𝓜𝖎𝖟𝖚ӄ𝖎.chatId === `120363025343298860@g.us` && !𝓜𝖎𝖟𝖚ӄ𝖎.isSenderSUDO) {
       await 𝓚𝓻𝓪𝓴𝓲𝓷𝔃
         .sendMessage(
           𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
@@ -173,22 +173,22 @@ module.exports = {
         )
         .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
       return;
-      ("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+      `⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
     } else {
-      ("⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Private And No Reply");
+      `⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Private And No Reply`;
       if (!𝓜𝖎𝖟𝖚ӄ𝖎.Group && !𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
         const Much = can[Math.floor(Math.random() * can.length)];
         const width = 1280;
         const height = 720;
         const canvas = createCanvas(width, height);
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#252525";
+        const context = canvas.getContext(`2d`);
+        context.fillStyle = `#252525`;
         context.fillRect(0, 0, width, height);
-        context.font = "bold 120pt Housttely";
+        context.font = `bold 120pt Housttely`;
         context.textAlign = `center`;
         context.textBaseline = `top`;
         // context.fillStyle = randomMC.getColor();
-        const text = "Beauty Checker";
+        const text = `Beauty Checker`;
         const textWidth = context.measureText(text).width;
         context.fillRect(
           600 - textWidth / 2 - 10,
@@ -198,17 +198,17 @@ module.exports = {
         );
         context.fillStyle = randomMC.getColor();
         context.fillText(text, 600, 170);
-        context.fillStyle = "#fff";
-        context.font = "bold 80pt Honeycomb";
-        context.fillText("Percentage: " + Much + "%", 600, 530);
-        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png").then(async () => {
-          const buffer = canvas.toBuffer("image/png");
-          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png", buffer);
+        context.fillStyle = `#fff`;
+        context.font = `bold 80pt Honeycomb`;
+        context.fillText(`Percentage: ` + Much + `%`, 600, 530);
+        loadImage(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`).then(async () => {
+          const buffer = canvas.toBuffer(`image/png`);
+          fs.writeFileSync(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`, buffer);
           𝓚𝓻𝓪𝓴𝓲𝓷𝔃
             .sendMessage(
               𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
               {
-                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png",
+                url: `./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`,
               },
               MessageType.image,
               {
@@ -224,7 +224,7 @@ module.exports = {
             .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
         });
       }
-      ("⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Private And Reply");
+      `⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Private And Reply`;
       if (!𝓜𝖎𝖟𝖚ӄ𝖎.Group && 𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
         var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
         personreceived = Receiver.substring(0, Receiver.length - 15);
@@ -234,14 +234,14 @@ module.exports = {
         const width = 1280;
         const height = 720;
         const canvas = createCanvas(width, height);
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#252525";
+        const context = canvas.getContext(`2d`);
+        context.fillStyle = `#252525`;
         context.fillRect(0, 0, width, height);
-        context.font = "bold 120pt Housttely";
+        context.font = `bold 120pt Housttely`;
         context.textAlign = `center`;
         context.textBaseline = `top`;
         // context.fillStyle = randomMC.getColor();
-        const text = "Beauty Checker";
+        const text = `Beauty Checker`;
         const textWidth = context.measureText(text).width;
         context.fillRect(
           600 - textWidth / 2 - 10,
@@ -251,17 +251,17 @@ module.exports = {
         );
         context.fillStyle = randomMC.getColor();
         context.fillText(text, 600, 170);
-        context.fillStyle = "#fff";
-        context.font = "bold 80pt Honeycomb";
-        context.fillText("Percentage: " + Much + "%", 600, 530);
-        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png").then(async () => {
-          const buffer = canvas.toBuffer("image/png");
-          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png", buffer);
+        context.fillStyle = `#fff`;
+        context.font = `bold 80pt Honeycomb`;
+        context.fillText(`Percentage: ` + Much + `%`, 600, 530);
+        loadImage(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`).then(async () => {
+          const buffer = canvas.toBuffer(`image/png`);
+          fs.writeFileSync(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`, buffer);
           𝓚𝓻𝓪𝓴𝓲𝓷𝔃
             .sendMessage(
               𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
               {
-                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png",
+                url: `./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`,
               },
               MessageType.image,
               {
@@ -277,7 +277,7 @@ module.exports = {
             .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
         });
       }
-      ("⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Group And Reply");
+      `⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Group And Reply`;
       if (𝓜𝖎𝖟𝖚ӄ𝖎.Group && 𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
         var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
         personreceived = Receiver.substring(0, Receiver.length - 15);
@@ -287,14 +287,14 @@ module.exports = {
         const width = 1280;
         const height = 720;
         const canvas = createCanvas(width, height);
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#252525";
+        const context = canvas.getContext(`2d`);
+        context.fillStyle = `#252525`;
         context.fillRect(0, 0, width, height);
-        context.font = "bold 120pt Housttely";
+        context.font = `bold 120pt Housttely`;
         context.textAlign = `center`;
         context.textBaseline = `top`;
         // context.fillStyle = randomMC.getColor();
-        const text = "Beauty Checker";
+        const text = `Beauty Checker`;
         const textWidth = context.measureText(text).width;
         context.fillRect(
           600 - textWidth / 2 - 10,
@@ -304,17 +304,17 @@ module.exports = {
         );
         context.fillStyle = randomMC.getColor();
         context.fillText(text, 600, 170);
-        context.fillStyle = "#fff";
-        context.font = "bold 80pt Honeycomb";
-        context.fillText("Percentage: " + Much + "%", 600, 530);
-        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png").then(async () => {
-          const buffer = canvas.toBuffer("image/png");
-          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png", buffer);
+        context.fillStyle = `#fff`;
+        context.font = `bold 80pt Honeycomb`;
+        context.fillText(`Percentage: ` + Much + `%`, 600, 530);
+        loadImage(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`).then(async () => {
+          const buffer = canvas.toBuffer(`image/png`);
+          fs.writeFileSync(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`, buffer);
           𝓚𝓻𝓪𝓴𝓲𝓷𝔃
             .sendMessage(
               𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
               {
-                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png",
+                url: `./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`,
               },
               MessageType.image,
               {
@@ -331,20 +331,20 @@ Asked By? @${𝓢𝓮𝓷𝓭𝓮𝓻𝓒𝓸𝓷𝓯𝓲𝓰}
             .catch((error) => ℓιєηт.catch(error, 𝓚𝓻𝓪𝓴𝓲𝓷𝔃, 𝓜𝖎𝖟𝖚ӄ𝖎));
         });
       }
-      ("⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Private And No Reply");
+      `⦿𝐌𝐢𝐳𝐮𝐤𝐢™⦿-----------------⬡==================⬡  Private And No Reply`;
       if (𝓜𝖎𝖟𝖚ӄ𝖎.Group && !𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
         const Much = can[Math.floor(Math.random() * can.length)];
         const width = 1280;
         const height = 720;
         const canvas = createCanvas(width, height);
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#252525";
+        const context = canvas.getContext(`2d`);
+        context.fillStyle = `#252525`;
         context.fillRect(0, 0, width, height);
-        context.font = "bold 120pt Housttely";
+        context.font = `bold 120pt Housttely`;
         context.textAlign = `center`;
         context.textBaseline = `top`;
         // context.fillStyle = randomMC.getColor();
-        const text = "Beauty Checker";
+        const text = `Beauty Checker`;
         const textWidth = context.measureText(text).width;
         context.fillRect(
           600 - textWidth / 2 - 10,
@@ -354,17 +354,17 @@ Asked By? @${𝓢𝓮𝓷𝓭𝓮𝓻𝓒𝓸𝓷𝓯𝓲𝓰}
         );
         context.fillStyle = randomMC.getColor();
         context.fillText(text, 600, 170);
-        context.fillStyle = "#fff";
-        context.font = "bold 80pt Honeycomb";
-        context.fillText("Percentage: " + Much + "%", 600, 530);
-        loadImage("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png").then(async () => {
-          const buffer = canvas.toBuffer("image/png");
-          fs.writeFileSync("./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png", buffer);
+        context.fillStyle = `#fff`;
+        context.font = `bold 80pt Honeycomb`;
+        context.fillText(`Percentage: ` + Much + `%`, 600, 530);
+        loadImage(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`).then(async () => {
+          const buffer = canvas.toBuffer(`image/png`);
+          fs.writeFileSync(`./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`, buffer);
           𝓚𝓻𝓪𝓴𝓲𝓷𝔃
             .sendMessage(
               𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
               {
-                url: "./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png",
+                url: `./𝓜𝖎𝖟𝖚ӄ𝖎🗑️𝓑𝓲𝓷/爪𝖎𝖟𝖚ӄ𝖎Tempa.png`,
               },
               MessageType.image,
               {
@@ -381,9 +381,9 @@ Asked By? @${𝓢𝓮𝓷𝓭𝓮𝓻𝓒𝓸𝓷𝓯𝓲𝓰}
         });
       }
     }
-    ("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+    `⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
     //   try {
-    //     var Receiver = "ME";
+    //     var Receiver = `ME`;
     //     xic(𝓢𝓮𝓷𝓭𝓮𝓻𝓘𝓓, Receiver).then((toxic) => {
     //       𝓚𝓻𝓪𝓴𝓲𝓷𝔃
     //         .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, toxic, MessageType.text, {
@@ -398,8 +398,8 @@ Asked By? @${𝓢𝓮𝓷𝓭𝓮𝓻𝓒𝓸𝓷𝓯𝓲𝓰}
     //   }
   },
 };
-("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
 //•⬡• ɢɴᴜ ɢᴇɴᴇʀᴀʟ ᴘᴜʙʟɪᴄ ʟɪᴄᴇɴꜱᴇ ᴠᴇʀꜱɪᴏɴ 3, 29 ᴊᴜɴᴇ 2007 ᴄᴏᴘʏʀɪɢʜᴛ (ᴄ) 2007 ꜰʀᴇᴇ ꜱᴏꜰᴛᴡᴀʀᴇ ꜰᴏᴜɴᴅᴀᴛɪᴏɴ
 //•⬡• ᴇᴠᴇʀʏᴏɴᴇ ɪꜱ ᴘᴇʀᴍɪᴛᴛᴇᴅ ᴛᴏ 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 ᴠᴇʀʙᴀᴛɪᴍ ᴄᴏᴘɪᴇꜱ ᴏꜰ ᴛʜɪꜱ ʟɪᴄᴇɴꜱᴇ ᴅᴏᴄᴜᴍᴇɴᴛ, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
 //•⬡• —••÷[ 𝓜𝖎𝖟𝖚ӄ𝖎™ ]÷••—  ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀʙᴏᴛ has been licensed under Apache License
-("⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡");
+`⬡==================⬡-----------------⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿-----------------⬡==================⬡`;
