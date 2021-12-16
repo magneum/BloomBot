251fe46809219ec8a36cf0e0e5189de1b86805da
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2021-12-16 15:41:15 +0530

    Add files via upload

---
+++ 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/🛸Helpers/help.js
@@ -0,0 +1,117 @@
+// ⦿──────────────────────────────────────────────────────────────────⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿──────────────────────────────────────────────────────────────────⦿`;
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const ℓιєηт = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/catch`);
+const speed = require(`performance-now`);
+const _𝔏𝔞𝔟_ = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/_𝔏𝔞𝔟_`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const fs = require(`fs`);
+const os = require("os");
+const { formatp } = require(`../../𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/mizu`);
+const vers = require(`../../package.json`);
+// ⦿──────────────────────────────────────────────────────────────────⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿──────────────────────────────────────────────────────────────────⦿`;
+module.exports = {
+  name: `help`,
+  description: `ɢᴇᴛ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ ᴀɴᴅ ɪɴꜰᴏ ᴏɴ ᴍᴏᴅᴜʟᴇꜱ`,
+  𝓜𝓮𝓮6ʍօʀɛ: `${ᴋᴇɪ}`,
+  async handle(ӄʀǟӄɨռʐ, chat, 𝓜𝖎𝖟𝖚ӄ𝖎, arg, 𝓜𝓲𝔃𝓾𝓴𝓲𝓢𝓲𝓽𝓻𝓮𝓹) {
+    try {
+      // ⦿──────────────────────────────────────────────────────────────────⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿──────────────────────────────────────────────────────────────────⦿`;
+      const ᴍɪᴢᴜᴋɪɢɪᴛ = require(`simple-git`)();
+      await ᴍɪᴢᴜᴋɪɢɪᴛ.fetch();
+      var ꜱᴇɴᴅᴇʀɪᴅ = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
+      var ꜱᴇɴᴅᴇʀᴄᴏɴꜰ = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
+      var ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ = await ᴍɪᴢᴜᴋɪɢɪᴛ.log([
+        `KrakinzLab..origin/KrakinzLab`,
+      ]);
+      // ⦿──────────────────────────────────────────────────────────────────⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿──────────────────────────────────────────────────────────────────⦿`;
+      const runtime = function (seconds) {
+        seconds = Number(seconds);
+        var d = Math.floor(seconds / (3600 * 24));
+        var h = Math.floor((seconds % (3600 * 24)) / 3600);
+        var m = Math.floor((seconds % 3600) / 60);
+        var s = Math.floor(seconds % 60);
+        var dDisplay = d > 0 ? d + (d == 1 ? ` day, ` : ` Day, `) : ``;
+        var hDisplay = h > 0 ? h + (h == 1 ? ` hour, ` : ` Hour, `) : ``;
+        var mDisplay = m > 0 ? m + (m == 1 ? ` minute, ` : ` Minute, `) : ``;
+        var sDisplay = s > 0 ? s + (s == 1 ? ` second` : ` Second`) : ``;
+        return dDisplay + hDisplay + mDisplay + sDisplay;
+      };
+      uptime = process.uptime();
+      let timestamp = speed();
+      let latensi = speed() - timestamp;
+      neww = performance.now();
+      oldd = performance.now();
+      // ⦿──────────────────────────────────────────────────────────────────⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿──────────────────────────────────────────────────────────────────⦿`;
+      var ᴛ = [
+        `./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/𝓜𝖎𝖟𝖚ӄ𝖎.jpg`,
+        `./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/𝓜𝖎𝖟𝖚ӄ𝖎2.jpg`,
+        `./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/𝓜𝖎𝖟𝖚ӄ𝖎3.jpg`,
+        `./𝓜𝖎𝖟𝖚ӄ𝖎🧀𝓒𝓻𝓪𝓽𝓮/𝓜𝖎𝖟𝖚ӄ𝖎4.jpg`,
+      ];
+      const MizukiMain = ᴛ[Math.floor(Math.random() * ᴛ.length)];
+      let content = fs.readFileSync(MizukiMain);
+      const media = await ӄʀǟӄɨռʐ.prepareMessage(
+        𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
+        content,
+        MessageType.image
+      );
+      const buttons = [
+        {
+          buttonId: `${ᴋᴇɪ}menu`,
+          buttonText: { displayText: `${ᴋᴇɪ}menu` },
+          type: 1,
+        },
+        {
+          buttonId: `${ᴋᴇɪ}mizuki`,
+          buttonText: { displayText: `${ᴋᴇɪ}mizuki` },
+          type: 1,
+        },
+        // {
+        //   buttonId: `${ᴋᴇɪ}group`,
+        //   buttonText: { displayText: `${ᴋᴇɪ}group` },
+        //   type: 1,
+        // },
+      ];
+      const buttonMessage = {
+        contentText: `*© Mïzµkï* _${vers.vers}_
+
+𝗔𝗵𝗺 •@${ꜱᴇɴᴅᴇʀᴄᴏɴꜰ},*𝘽𝙊𝙏 𝙄𝙉 𝘽𝙀𝙏𝘼*
+シ︎🛸𝐆𝐫𝐨𝐮𝐩: ${_𝔏𝔞𝔟_.HASH}
+
+┌────⭓ 𝐀𝐥𝐥 𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐞𝐬
+│🐙 *${ᴋᴇɪ}ownerside:* _ᴏᴡɴᴇʀ_
+│⚙️ *${ᴋᴇɪ}systemside:* _ꜱʏꜱᴛᴇᴍ_
+│🎋 *${ᴋᴇɪ}animeside:* _ᴀɴɪᴍᴇ_
+│🍫 *${ᴋᴇɪ}musicside:* _ᴍᴜꜱɪᴄ_
+│🍭 *${ᴋᴇɪ}groupside:* _ɢʀᴏᴜᴘꜱ_
+│🦄 *${ᴋᴇɪ}funside:* _ꜰᴜɴꜱ_
+│🍄 *${ᴋᴇɪ}nsfwside:* _ᴀᴅᴜʟᴛ_
+└───────────⭓
+
+┌────⭓ 𝐒𝐲𝐦𝐥𝐢𝐧𝐤 𝐈𝐧𝐟𝐨𝐬  
+│💡  *ᴘʀᴇꜰɪx:* ${ᴋᴇɪ}
+│⚙️ *ᴇɴɢɪɴᴇ:* ${vers.vers}
+│💻 *ᴘʟᴀᴛꜰᴏʀᴍ:* ${os.platform()}
+│🛰️ *ʟᴀᴛᴇɴᴄʏ:* ${latensi.toFixed(4)}(s)
+│☕ *ᴜᴘᴛɪᴍᴇ:* ${runtime(process.uptime())}
+│🎮 *ʀᴀᴍ:* ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
+└───────────⭓`,
+        footerText: `*© Mïzµkï* _${vers.vers}_`,
+        buttons: buttons,
+        headerType: 4,
+        imageMessage: media.message.imageMessage,
+      };
+      await ӄʀǟӄɨռʐ
+        .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, buttonMessage, MessageType.buttonsMessage, {
+          quoted: chat,
+          contextInfo: { mentionedJid: [ꜱᴇɴᴅᴇʀɪᴅ] },
+        })
+        .catch((ℓαвєяяσя) => ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎));
+    } catch (ℓαвєяяσя) {
+      ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎);
+      console.log(ℓαвєяяσя);
+    }
+  },
+};
+// ⦿──────────────────────────────────────────────────────────────────⦿   𝐌𝐢𝐳𝐮𝐤𝐢™   ⦿──────────────────────────────────────────────────────────────────⦿`;
