9f8465273a39095038f3d55a760f186563cb5b3e
krakinzkon <krakinzkon@gmail.com>
2021-12-19 11:35:00 -0800

    𝐌𝐢𝐳𝐮𝐤𝐢™-nightlyv17.1

---
+++ 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/👅Human/blowjob.js
@@ -0,0 +1,10 @@
+const got = require("got");
+
+
+got("https://www.reddit.com/r/Blowjobs/random.json").then((response) => {
+  let content = JSON.parse(response.body);
+  var title = content[0].data.children[0].data.title;
+  var amazeme = content[0].data.children[0].data.url;
+  console.log(title + "\n" + amazeme);
+});
+
---
+++ 𝓜𝖎𝖟𝖚ӄ𝖎😈𝓢𝔂𝓶𝓛𝓲𝓷𝓴𝓼/👅Human/slut.js
@@ -0,0 +1,8 @@
+const got = require("got");
+
+got("https://www.reddit.com/r/cumsluts/random.json").then((response) => {
+  let content = JSON.parse(response.body);
+  var title = content[0].data.children[0].data.title;
+  var amazeme = content[0].data.children[0].data.url;
+  console.log(title + "\n" + amazeme);
+});
