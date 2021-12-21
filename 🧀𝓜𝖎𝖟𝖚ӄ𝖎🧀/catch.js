bd1d950bd94879fe34f66e8239a9b83996f30b04
krakinzkon <krakinzkon@gmail.com>
2021-12-21 04:37:24 -0800

    🍁𝓜𝖎𝖟𝖚ӄ𝖎™-ɴɪɢʜᴛʟʏ-v18

--- 🧀𝓜𝖎𝖟𝖚ӄ𝖎🧀/catch.js
+++ 🧀𝓜𝖎𝖟𝖚ӄ𝖎🧀/catch.js
@@ -4,38 +4,14 @@ require(`python-format-js`);
 const fs = require(`fs`);
 `🐙============================================================================================================================<⚡>`;
 exports.catch = async (ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat) => {
-  let content = fs.readFileSync(`./𝓜𝖎𝖟𝖚ӄ𝖎/MizukiError.png`);
-  const media = await ӄʀǟӄɨռʐ.prepareMessage(
-    𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-    content,
-    MessageType.image,
-    {
-      mimetype: Mimetype.png,
-    }
-  );
   await ӄʀǟӄɨռʐ
     .sendMessage(
       𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-      {
-        contentText: `*🐙Please try Again Later!*
+      `_𝐌𝐢𝐳𝐮𝐤𝐢™_
+
+*🐙Please try Again Later!*
 There Was temporary erorr in that command.`,
-        footerText: `_𝐌𝐢𝐳𝐮𝐤𝐢™_`,
-        buttons: [
-          {
-            buttonId: `${ᴋᴇɪ}help`,
-            buttonText: { displayText: `${ᴋᴇɪ}help` },
-            type: 1,
-          },
-          {
-            buttonId: `${ᴋᴇɪ}report`,
-            buttonText: { displayText: `${ᴋᴇɪ}report` },
-            type: 1,
-          },
-        ],
-        headerType: 4,
-        imageMessage: media.message.imageMessage,
-      },
-      MessageType.buttonsMessage,
+      MessageType.text,
       {
         quoted: chat,
         contextInfo: {
