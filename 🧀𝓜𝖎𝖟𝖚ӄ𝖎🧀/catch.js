ae1695127b6f1a958cbab2a309ce795de1199fc7
krakinzkon <krakinzkon@gmail.com>
2021-12-21 03:21:36 -0800

    𝐌𝐢𝐳𝐮𝐤𝐢™💡ɴɪɢʜᴛʟʏ-v18

--- 🧀𝓜𝖎𝖟𝖚ӄ𝖎🧀/catch.js
+++ 🧀𝓜𝖎𝖟𝖚ӄ𝖎🧀/catch.js
@@ -1,17 +1,41 @@
 // ⚡•=•=•=•=•=•=•=•=•=•=•=•=•=•──────[🐙  𝐌𝐢𝐳𝐮𝐤𝐢™   🐙]──────•=•=•=•=•=•=•=•=•=•=•=•=•=•⚡
 const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
 require(`python-format-js`);
+const fs = require(`fs`);
 // ⚡•=•=•=•=•=•=•=•=•=•=•=•=•=•──────[🐙  𝐌𝐢𝐳𝐮𝐤𝐢™   🐙]──────•=•=•=•=•=•=•=•=•=•=•=•=•=•⚡
 exports.catch = async (ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat) => {
+  let content = fs.readFileSync(`./𝓜𝖎𝖟𝖚ӄ𝖎/MizukiError.png`);
+  const media = await ӄʀǟӄɨռʐ.prepareMessage(
+    𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
+    content,
+    MessageType.image,
+    {
+      mimetype: Mimetype.png,
+    }
+  );
   await ӄʀǟӄɨռʐ
     .sendMessage(
       𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-      `_𝐌𝐢𝐳𝐮𝐤𝐢™_
-
-
-*🐙Please try Again Later!*
+      {
+        contentText: `*🐙Please try Again Later!*
 There Was temporary erorr in that command.`,
-      MessageType.text,
+        footerText: `_𝐌𝐢𝐳𝐮𝐤𝐢™_ ${vers.vers}`,
+        buttons: [
+          {
+            buttonId: `${ᴋᴇɪ}help`,
+            buttonText: { displayText: `${ᴋᴇɪ}help` },
+            type: 1,
+          },
+          {
+            buttonId: `${ᴋᴇɪ}report`,
+            buttonText: { displayText: `${ᴋᴇɪ}report` },
+            type: 1,
+          },
+        ],
+        headerType: 4,
+        imageMessage: media.message.imageMessage,
+      },
+      MessageType.buttonsMessage,
       {
         quoted: chat,
         contextInfo: {
