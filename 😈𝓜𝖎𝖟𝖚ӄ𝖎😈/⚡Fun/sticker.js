fbd60ac0309da5d0da2612cb87bc5a3e12181c13
krakinzkon <krakinzkon@gmail.com>
2021-12-22 02:31:14 -0800

    🍁爪𝖎𝖟𝖚ӄ𝖎™-ɴɪɢʜᴛʟʏ-v18.56

--- 😈𝓜𝖎𝖟𝖚ӄ𝖎😈/⚡Fun/sticker.js
+++ 😈𝓜𝖎𝖟𝖚ӄ𝖎😈/⚡Fun/sticker.js
@@ -21,127 +21,104 @@ module.exports = {
       var ꜱᴇɴᴅᴇʀɪᴅ = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
       var ꜱᴇɴᴅᴇʀᴄᴏɴꜰ = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
       `🐙============================================================================================================================<⚡>`;
-      if (
-        (𝓜𝖎𝖟𝖚ӄ𝖎.isReplyImage && 𝓜𝖎𝖟𝖚ӄ𝖎.type === "image") ||
-        𝓜𝖎𝖟𝖚ӄ𝖎.isReplyVideo ||
-        𝓜𝖎𝖟𝖚ӄ𝖎.isReplyGIF
-      ) {
-        if (𝓜𝖎𝖟𝖚ӄ𝖎.isReplyImage) {
-          var STICKER_OBJECT = {
-            message: chat.message.extendedTextMessage.contextInfo.quotedMessage,
-          };
-          var STICKER_ID =
-            chat.message.extendedTextMessage.contextInfo.stanzaId;
-          console.log(STICKER_OBJECT);
-          try {
-            await ӄʀǟӄɨռʐ
-              .downloadAndSaveMediaMessage(
-                STICKER_OBJECT,
-                `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}`
-              )
-              .then(async () => {
-                ffmpeg(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.jpeg`)
-                  .outputOptions([`-y`, `-vcodec libwebp`])
-                  .videoFilters(
-                    `scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease`,
-                    `format=rgba`,
-                    `pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1`
-                  )
-                  .save(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.webp`)
-                  .on(`end`, async () => {
-                    await ӄʀǟӄɨռʐ
-                      .sendMessage(
-                        𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-                        fs.readFileSync(
-                          `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.webp`
-                        ),
-                        MessageType.sticker,
-                        { quoted: chat }
+      if (𝓜𝖎𝖟𝖚ӄ𝖎.isReplyImage && 𝓜𝖎𝖟𝖚ӄ𝖎.type === "image") {
+        var STICKER_OBJECT = {
+          message: chat.message.extendedTextMessage.contextInfo.quotedMessage,
+        };
+        var STICKER_ID = chat.message.extendedTextMessage.contextInfo.stanzaId;
+        console.log(STICKER_OBJECT);
+        try {
+          await ӄʀǟӄɨռʐ
+            .downloadAndSaveMediaMessage(
+              STICKER_OBJECT,
+              `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}`
+            )
+            .then(async () => {
+              ffmpeg(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.jpeg`)
+                .outputOptions([`-y`, `-vcodec libwebp`])
+                .videoFilters(
+                  `scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease`,
+                  `format=rgba`,
+                  `pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1`
+                )
+                .save(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.webp`)
+                .on(`end`, async () => {
+                  await ӄʀǟӄɨռʐ
+                    .sendMessage(
+                      𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
+                      fs.readFileSync(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.webp`),
+                      MessageType.sticker,
+                      { quoted: chat }
+                    )
+                    .then(
+                      fs.unlinkSync(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.jpeg`),
+                      fs.unlinkSync(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.webp`)
+                    );
+                })
+                .on(`ℓαвєяяσя`, async (ℓαвєяяσя) => {
+                  ℓιєηт.catch((ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎));
+                });
+            });
+        } catch (ℓαвєяяσя) {
+          ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎);
+        }
+        `🐙============================================================================================================================<⚡>`;
+      } else if (𝓜𝖎𝖟𝖚ӄ𝖎.isReplyVideo || 𝓜𝖎𝖟𝖚ӄ𝖎.isReplyGIF) {
+        var VIDEO_STICKER_OBJECT = {
+          message: chat.message.extendedTextMessage.contextInfo.quotedMessage,
+        };
+        var VIDEO_STICKER_ID =
+          chat.message.extendedTextMessage.contextInfo.stanzaId;
+        console.log(VIDEO_STICKER_OBJECT);
+        try {
+          await ӄʀǟӄɨռʐ
+            .downloadAndSaveMediaMessage(
+              VIDEO_STICKER_OBJECT,
+              `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}`
+            )
+            .then(async () => {
+              ffmpeg(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.mp4`)
+                .duration(8)
+                .outputOptions([
+                  "-y",
+                  "-vcodec libwebp",
+                  "-lossless 1",
+                  "-qscale 1",
+                  "-preset default",
+                  "-loop 0",
+                  "-an",
+                  "-vsync 0",
+                  "-s 600x600",
+                ])
+                .videoFilters(
+                  "scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1"
+                )
+                .save(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.webp`)
+                .on(`end`, async () => {
+                  await ӄʀǟӄɨռʐ
+                    .sendMessage(
+                      𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
+                      fs.readFileSync(
+                        `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.webp`
+                      ),
+                      MessageType.sticker,
+                      { quoted: chat }
+                    )
+                    .then(
+                      fs.unlinkSync(
+                        `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.mp4`
+                      ),
+                      fs.unlinkSync(
+                        `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.webp`
                       )
-                      .then(
-                        fs.unlinkSync(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.jpeg`),
-                        fs.unlinkSync(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${STICKER_ID}.webp`)
-                      );
-                  })
-                  .on(`ℓαвєяяσя`, async (ℓαвєяяσя) => {
-                    ℓιєηт.catch((ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎));
-                  });
-              });
-          } catch (ℓαвєяяσя) {
-            ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎);
-          }
-          `🐙============================================================================================================================<⚡>`;
-        } else if (𝓜𝖎𝖟𝖚ӄ𝖎.isReplyVideo || 𝓜𝖎𝖟𝖚ӄ𝖎.isReplyGIF) {
-          var VIDEO_STICKER_OBJECT = {
-            message: chat.message.extendedTextMessage.contextInfo.quotedMessage,
-          };
-          var VIDEO_STICKER_ID =
-            chat.message.extendedTextMessage.contextInfo.stanzaId;
-          console.log(VIDEO_STICKER_OBJECT);
-          try {
-            await ӄʀǟӄɨռʐ
-              .downloadAndSaveMediaMessage(
-                VIDEO_STICKER_OBJECT,
-                `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}`
-              )
-              .then(async () => {
-                ffmpeg(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.mp4`)
-                  .duration(8)
-                  .outputOptions([
-                    "-y",
-                    "-vcodec libwebp",
-                    "-lossless 1",
-                    "-qscale 1",
-                    "-preset default",
-                    "-loop 0",
-                    "-an",
-                    "-vsync 0",
-                    "-s 600x600",
-                  ])
-                  .videoFilters(
-                    "scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1"
-                  )
-                  .save(`./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.webp`)
-                  .on(`end`, async () => {
-                    await ӄʀǟӄɨռʐ
-                      .sendMessage(
-                        𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-                        fs.readFileSync(
-                          `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.webp`
-                        ),
-                        MessageType.sticker,
-                        { quoted: chat }
-                      )
-                      .then(
-                        fs.unlinkSync(
-                          `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.mp4`
-                        ),
-                        fs.unlinkSync(
-                          `./🗑️𝓜𝖎𝖟𝖚ӄ𝖎🗑️/𝓜𝖎𝖟𝖚ӄ𝖎_${VIDEO_STICKER_ID}.webp`
-                        )
-                      );
-                  })
-                  .on(`ℓαвєяяσя`, async (ℓαвєяяσя) => {
-                    ℓιєηт.catch((ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎));
-                  });
-              });
-          } catch (ℓαвєяяσя) {
-            ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎);
-          }
-          `🐙============================================================================================================================<⚡>`;
-        } else {
-          var 𝓜Usage = 𝓜𝓲𝔃𝓾𝓴𝓲𝓢𝓲𝓽𝓻𝓮𝓹.get(𝓜𝖎𝖟𝖚ӄ𝖎.commandName);
-          𝓜𝓮𝓮ʍօʀɛ = 𝓜Usage.𝓜𝓮𝓮6ʍօʀɛ === undefined ? `Null` : 𝓜Usage.𝓜𝓮𝓮6ʍօʀɛ;
-          const nahargs = require(`../../🧀𝓜𝖎𝖟𝖚ӄ𝖎🧀/nahargs`);
-          return nahargs.nahargs(
-            ӄʀǟӄɨռʐ,
-            chat,
-            𝓜𝖎𝖟𝖚ӄ𝖎,
-            ꜱᴇɴᴅᴇʀɪᴅ,
-            ꜱᴇɴᴅᴇʀᴄᴏɴꜰ,
-            𝓜𝖎𝖟𝖚ӄ𝖎.commandName,
-            𝓜𝓮𝓮ʍօʀɛ
-          );
+                    );
+                })
+                .on(`ℓαвєяяσя`, async (ℓαвєяяσя) => {
+                  ℓιєηт.catch((ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎));
+                });
+            });
+        } catch (ℓαвєяяσя) {
+          ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎);
         }
         `🐙============================================================================================================================<⚡>`;
       } else {
@@ -158,6 +135,7 @@ module.exports = {
           𝓜𝓮𝓮ʍօʀɛ
         );
       }
+      `🐙============================================================================================================================<⚡>`;
     } catch (ℓαвєяяσя) {
       ℓιєηт.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎);
       console.log(ℓαвєяяσя);
