8a26beb092975e6745dd50dc0fb66053cfe13736
krakinzkon <krakinzkon@gmail.com>
2021-12-26 21:24:13 -0800

    🍁爪𝖎𝖟𝖚ӄ𝖎™

---
+++ ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/ʟᴀʙᴛᴇꜱᴛ/new.js
@@ -0,0 +1,80 @@
+// ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+const Mizuki_Buttons = require(`../../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Mizuki_Buttons`);
+const Mizuki_Static = require(`../../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Mizuki_Static`);
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const ffmpeg = require(`fluent-ffmpeg`);
+const _𝔏𝔞𝔟_ = require(`../../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_`);
+const ꜰᴜᴄᴋ = require(`../../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/oShit`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const Downloader = require(`nodejs-file-downloader`);
+const anime = require(`anime-actions`);
+const fs = require(`fs`);
+const vers = require(`../../package.json`);
+var path = require("path");
+var scriptName = path.basename(__filename);
+var str = scriptName;
+var newScpt = str.slice(0, -3).toLowerCase();
+// ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+module.exports = {
+  name: newScpt,
+  async handle(ӄʀǟӄɨռʐ, chat, 𝓜𝖎𝖟𝖚ӄ𝖎, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+    // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+    var ꜱᴇɴᴅᴇʀɪᴅ = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
+    const Raw = await anime.bite();
+    const defaultnm = 𝓜𝖎𝖟𝖚ӄ𝖎.commandName;
+    var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
+    const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+    console.log("🍁爪𝖎𝖟𝖚ӄ𝖎™ ᴠᴇʀɪꜰɪᴇᴅ ꜰɪʟᴇ > " + FinalName.toUpperCase() + "✔️");
+    // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+    if (!Raw.endsWith(`.gif`) && !Raw.endsWith(`.mp4`)) {
+      const Raw = await anime.bite();
+      Raw = Raw;
+      ʟᴀʙᴛᴇꜱᴛ = Raw;
+    } else {
+      ʟᴀʙᴛᴇꜱᴛ = Raw;
+    }
+    console.log(ʟᴀʙᴛᴇꜱᴛ);
+    // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+    if (𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
+      var ꜱᴇɴᴅᴇʀɪᴅ = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
+      var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
+      var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
+      var personreceived = Receiver.substring(0, Receiver.length - 15);
+      // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+      await ӄʀǟӄɨռʐ
+        .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, { url: ʟᴀʙᴛᴇꜱᴛ }, MessageType.video, {
+          contextInfo: { mentionedJid: [ꜱᴇɴᴅᴇʀɪᴅ, Receiver] },
+          quoted: chat,
+          mimetype: "video/gif",
+          caption: `*(c)爪𝖎𝖟𝖚ӄ𝖎🍁乃օȶ* _${vers.vers}_
+
+┌────◆▶
+│
+│⚡𝐓𝐨𝐩𝐢𝐜: ${FinalName}
+│🥳𝐅𝐫𝐨𝐦: @${ꜱᴇɴᴅᴇʀeceived}
+│🤩𝐓𝐨: @${personreceived}
+└───────◆▶`,
+        })
+        .catch((ℓαвєяяσя) => ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat));
+    }
+    // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
+    if (!𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
+      await ӄʀǟӄɨռʐ
+        .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, { url: ʟᴀʙᴛᴇꜱᴛ }, MessageType.video, {
+          contextInfo: { mentionedJid: [ꜱᴇɴᴅᴇʀɪᴅ] },
+          quoted: chat,
+          mimetype: "video/gif",
+          caption: `*(c)爪𝖎𝖟𝖚ӄ𝖎🍁乃օȶ* _${vers.vers}_
+
+┌────◆▶
+│
+│⚡𝐓𝐨𝐩𝐢𝐜: ${FinalName}
+│🍯𝐓𝐡𝐞𝐦𝐬𝐞𝐥𝐯𝐞: @${ꜱᴇɴᴅᴇʀeceived}
+└───────◆▶`,
+        })
+        .catch((ℓαвєяяσя) => ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat));
+    }
+  },
+};
+// ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
