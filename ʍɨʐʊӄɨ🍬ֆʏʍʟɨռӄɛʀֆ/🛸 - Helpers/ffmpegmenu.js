156fc6fcf6692ce0ac1d5720b9fac69280151630
krakinzkon <krakinzkon@gmail.com>
2021-12-27 06:00:48 -0800

    🍁爪𝖎𝖟𝖚ӄ𝖎™-v25-Stable

--- ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/🛸 - Helpers/ffmpegmenu.js
+++ ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/🛸 - Helpers/ffmpegmenu.js
@@ -34,11 +34,10 @@ module.exports = {
 ⧪𝘾𝙤𝙢𝙞𝙣𝙜 𝙎𝙤𝙤𝙣! _many many more_
 
 ┌──────────────◆◇ 𝙁𝙁𝙈𝙋𝙀𝙂
-│⏯️• ${ᴋᴇɪ}fastvid _convert normal vid to fast vid_
-│⏯️• ${ᴋᴇɪ}slowvid _convert normal vid to slow vid_
-│⏯️• ${ᴋᴇɪ}reversevid _convert normal vid to reverse vid_
-│⏯️• ${ᴋᴇɪ}tomp3 _convert video tp music_
-│⏯️• ${ᴋᴇɪ}tourl  _convert vid/image to url_
+│🗂️• ${ᴋᴇɪ}fastvid _convert normal vid to fast vid_
+│🗂️• ${ᴋᴇɪ}slowvid _convert normal vid to slow vid_
+│🗂️• ${ᴋᴇɪ}reversevid _convert normal vid to reverse vid_
+│🗂️• ${ᴋᴇɪ}tomp3 _convert video tp music_
 └───────◆◇`;
       return Mizuki_Static.MGS(
         ӄʀǟӄɨռʐ,
