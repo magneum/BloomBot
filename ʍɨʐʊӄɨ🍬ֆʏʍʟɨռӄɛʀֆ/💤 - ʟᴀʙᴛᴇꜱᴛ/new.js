90927c7a094227b44ad84ed0295a64373ffc44f2
krakinzkon <krakinzkon@gmail.com>
2021-12-27 23:51:14 -0800

    🍁爪𝖎𝖟𝖚ӄ𝖎™-v25-Stable

--- ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/💤 - ʟᴀʙᴛᴇꜱᴛ/new.js
+++ ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/💤 - ʟᴀʙᴛᴇꜱᴛ/new.js
@@ -29,21 +29,21 @@ module.exports = {
     // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
     // var last = 𝓜𝖎𝖟𝖚ӄ𝖎.body.replace(      𝓜𝖎𝖟𝖚ӄ𝖎.body[0] + 𝓜𝖎𝖟𝖚ӄ𝖎.commandName + " ",      ""    );
 
-    check = 𝓜𝖎𝖟𝖚ӄ𝖎.budy;
+    check = 𝓜𝖎𝖟𝖚ӄ𝖎.replyMessage;
     console.log(check);
-    neko.nekobin(check).then((data) => {
-      dt = data.url;
-      console.log(dt);
-      ӄʀǟӄɨռʐ.sendMessage(
-        𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
-        { url: "https://i.postimg.cc/3wGtxscV/Mem-Err.png" },
-        MessageType.image,
-        {
-          quoted: chat,
-          caption: dt,
-        }
-      );
-    });
+    // neko.nekobin(check).then((data) => {
+    //   dt = data.url;
+    //   console.log(dt);
+    //   ӄʀǟӄɨռʐ.sendMessage(
+    //     𝓜𝖎𝖟𝖚ӄ𝖎.chatId,
+    //     { url: "https://i.postimg.cc/3wGtxscV/Mem-Err.png" },
+    //     MessageType.image,
+    //     {
+    //       quoted: chat,
+    //       caption: dt,
+    //     }
+    //   );
+    // });
   },
 };
 // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
