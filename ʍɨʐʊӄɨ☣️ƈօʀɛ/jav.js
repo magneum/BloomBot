a747faeca174b16ab753967c18a70180385c351e
krakinzkon <krakinzkon@gmail.com>
2021-12-28 05:47:07 -0800

    🍁爪𝖎𝖟𝖚ӄ𝖎™-v25.1-Stable

---
+++ ʍɨʐʊӄɨ☣️ƈօʀɛ/jav.js
@@ -0,0 +1,9 @@
+const spawn = require("child_process").spawn;
+
+const pythonProcess = spawn("python3", ["py.py"]);
+pythonProcess.stdout.on("data", (data) => {
+  mystr = data.toString();
+  myjson = JSON.parse(mystr);
+  console.log(`Json is : ${mystr}`);
+  console.log(myjson.Data);
+});
