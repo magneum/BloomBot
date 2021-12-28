17820032ef57820c2f3d3c090a29caf712c008e4
krakinzkon <krakinzkon@gmail.com>
2021-12-28 00:59:44 -0800

    🍁爪𝖎𝖟𝖚ӄ𝖎™-v25.1-Stable

--- ӄʀǟӄɨռʐ🐙ʟǟɮ.py
+++ ӄʀǟӄɨռʐ🐙ʟǟɮ.py
@@ -1,2 +1,12 @@
 import os 
+import platform
+ 
+my_system = platform.uname()
+ 
+print(f"System: {my_system.system}")
+print(f"Node Name: {my_system.node}")
+print(f"Release: {my_system.release}")
+print(f"Version: {my_system.version}")
+print(f"Machine: {my_system.machine}")
+print(f"Processor: {my_system.processor}")
 os.system("node 🍁爪𝖎𝖟𝖚ӄ𝖎™.js")
