2ea1305b82647625372e2de469134771ffec4bf0
krakinzkon <krakinzkon@gmail.com>
2022-01-03 04:48:29 -0800

    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁

--- ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/🛸 - Helpers/humanmenu.js
+++ ʍɨʐʊӄɨ🍬ֆʏʍʟɨռӄɛʀֆ/🛸 - Helpers/humanmenu.js
@@ -46,6 +46,7 @@ module.exports = {
 │🥃• ${ᴋᴇɪ}milk: _get random milk img/gif_
 │🥃• ${ᴋᴇɪ}pussy: _get random pussy img/gif_
 │🥃• ${ᴋᴇɪ}slut: _get random slut img/gif_
+│🥃• ${ᴋᴇɪ}tikporn: _get random tikporn video_
 └───────〇`;
       return Mizuki_Buttons.MIB(
         ӄʀǟӄɨռʐ,
