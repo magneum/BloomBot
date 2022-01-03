a36cd7559112b0b8ca80c307037c492c6467a60e
krakinzkon <krakinzkon@gmail.com>
2022-01-03 10:14:32 -0800

    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁-v26.7

--- ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_.js
+++ ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_.js
@@ -47,7 +47,7 @@ try {
               ssl: { require: true, rejectUnauthorized: false },
             },
           }),
-    MEE: `https://i.postimg.cc/qRmLF2Ln/image.png`,
+    MEE: `https://i.postimg.cc/wjPrhJh2/image.png`,
     PRIVACY: process.env.PRIVACY === undefined ? `public` : process.env.PRIVACY,
   };
   module.exports = env;
