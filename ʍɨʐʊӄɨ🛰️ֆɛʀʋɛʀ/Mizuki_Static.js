c8e905460dc78ea587729255bf4892571a178cd2
krakinzkon <krakinzkon@gmail.com>
2022-01-03 09:58:38 -0800

    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁-v26.5

--- ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Mizuki_Static.js
+++ ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Mizuki_Static.js
@@ -23,25 +23,33 @@ try {
     MainText,
     MediaUrl
   ) => {
+    const defaultnm = 𝓜𝖎𝖟𝖚ӄ𝖎.commandName;
+    const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
     if (𝓜𝖎𝖟𝖚ӄ𝖎.isReply) {
       var ꜱᴇɴᴅᴇʀɪᴅ = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
       var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
       var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
       var personreceived = Receiver.substring(0, Receiver.length - 15);
+      return await ӄʀǟӄɨռʐ
+        .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, { url: MediaUrl }, MessageType.video, {
+          mimetype: `video/gif`,
+          quoted: chat,
+          caption: MainText,
+          contextInfo: { mentionedJid: [ꜱᴇɴᴅᴇʀɪᴅ, Receiver] },
+        })
+        .catch((ℓαвєяяσя) => ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat));
     } else {
       var ꜱᴇɴᴅᴇʀɪᴅ = 𝓜𝖎𝖟𝖚ӄ𝖎.sender;
       var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
-      const defaultnm = 𝓜𝖎𝖟𝖚ӄ𝖎.commandName;
-      const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+      return await ӄʀǟӄɨռʐ
+        .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, { url: MediaUrl }, MessageType.video, {
+          mimetype: `video/gif`,
+          quoted: chat,
+          caption: MainText,
+          contextInfo: { mentionedJid: [ꜱᴇɴᴅᴇʀɪᴅ] },
+        })
+        .catch((ℓαвєяяσя) => ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat));
     }
-    return await ӄʀǟӄɨռʐ
-      .sendMessage(𝓜𝖎𝖟𝖚ӄ𝖎.chatId, { url: MediaUrl }, MessageType.video, {
-        mimetype: `video/gif`,
-        quoted: chat,
-        caption: MainText,
-        contextInfo: { mentionedJid: [ꜱᴇɴᴅᴇʀɪᴅ] },
-      })
-      .catch((ℓαвєяяσя) => ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, 𝓜𝖎𝖟𝖚ӄ𝖎, chat));
   };
   // ⬡ ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳 ⬡==========================⬡    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁    ⬡==========================⬡ 𝐋𝐚𝐛™ ⬡
   module.exports = {
