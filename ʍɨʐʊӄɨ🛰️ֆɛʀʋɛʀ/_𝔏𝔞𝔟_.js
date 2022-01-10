58b1948a4266d0b4b08942106e2e5bb3fcd17ed8
krakinzkon <krakinzkon@gmail.com>
2022-01-10 03:33:26 -0800

    https://i.postimg.cc/YqZ5pJW4/image.png

--- ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_.js
+++ ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_.js
@@ -30,7 +30,7 @@ const env = {
   ɴᴏᴛᴍᴇᴀᴅᴍɪɴ: `./ʍɨʐʊӄɨ☣️ƈօʀɛ/𝓜𝖎𝖟𝖚ӄ𝖎NotMeAdmin.png`,
   ɴᴏᴛᴀᴅᴍɪɴ: `./ʍɨʐʊӄɨ☣️ƈօʀɛ/𝓜𝖎𝖟𝖚ӄ𝖎MemErr.png`,
   WAPI: `6729ac2b2e2bb5c686ff427a2f06df92`,
-  UPT: `https://i.postimg.cc/sDXbg5xF/image.png`,
+  UPT: `https://i.postimg.cc/YqZ5pJW4/image.png`,
   ERROR: `https://i.postimg.cc/sDPF83Mx/Mizuki-Error.png`,
   INVL: `https://i.postimg.cc/q7k5WK9Z/Mizuki-Invalid.png`,
   HASH: `https://chat.whatsapp.com/EKyTcSlzOmF20MELthQwgy`,
@@ -50,7 +50,7 @@ const env = {
             ssl: { require: true, rejectUnauthorized: false },
           },
         }),
-  MEE: `https://i.postimg.cc/VNN6stGY/image.png`,
+  MEE: `https://i.postimg.cc/YqZ5pJW4/image.png`,
   PRIVACY: process.env.PRIVACY === undefined ? `public` : process.env.PRIVACY,
 };
 module.exports = env;
