0078f2d7859318a080351f7b59ee119d74e726bb
krakinzkon <krakinzkon@gmail.com>
2022-01-16 08:04:43 -0800

    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁v28.13

--- ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/MizukiCalm.js
+++ ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/MizukiCalm.js
@@ -1,14 +1,14 @@
 `🍁--------------------------------------------------------------------| 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ |`;
 exports.MizukiCalm = async (𝓜𝖎𝖟𝖚ӄ𝖎) => {
   const fs = require("fs");
-  const jsoncool = JSON.parse(fs.readFileSync(`./Coolist.json`));
+  const jsoncool = JSON.parse(fs.readFileSync(`./ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Coolist.json`));
   jsoncool.push(𝓜𝖎𝖟𝖚ӄ𝖎.sender);
-  fs.writeFileSync(`./Coolist.json`, JSON.stringify(jsoncool));
+  fs.writeFileSync(`./ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Coolist.json`, JSON.stringify(jsoncool));
   console.log("Written!");
   setTimeout(async (error) => {
     if (error) console.log(error);
     jsoncool.splice(𝓜𝖎𝖟𝖚ӄ𝖎.sender);
-    fs.writeFileSync(`./Coolist.json`, JSON.stringify(jsoncool));
+    fs.writeFileSync(`./ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/Coolist.json`, JSON.stringify(jsoncool));
     console.log("Removed!");
   }, 15000);
 };
