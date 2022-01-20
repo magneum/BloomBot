6593d31441a51320573b6ba77ed92bbc97ea4a83
krakinzkon <krakinzkon@gmail.com>
2022-01-20 09:05:18 -0800

    🍁 (c)爪𝖎𝖟𝖚ӄ𝖎 🍁v29

--- Mizuki☣️Reactor/🍁爪𝖎𝖟𝖚ӄ𝖎™.js
+++ Mizuki☣️Reactor/🍁爪𝖎𝖟𝖚ӄ𝖎™.js
@@ -1,29 +1,33 @@
 try {
-  (async () => {
-    try {
-      const { 爪𝖎𝖟𝖚ӄ𝖎 } = require(`../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/🍁爪𝖎𝖟𝖚ӄ𝖎™`);
-      const AutoKrakz = require(`../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/𝓜𝖎𝖟𝖚ӄ𝖎ɢɪᴛ`);
-      const _𝔏𝔞𝔟_ = require(`../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_`);
-      const ʍɨʐʊӄɨgoose = require("mongoose");
-      const Kolor = require(`chalk`);
-      await ʍɨʐʊӄɨgoose
-        .connect(_𝔏𝔞𝔟_.MizukiMdb, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useCreateIndex: true,
-          useFindAndModify: false,
-        })
-        .then(console.log(Kolor.bgGreenBright.black(`ʍɨʐʊӄɨgoose Connected`)))
-        .catch((err) => {
-          console.log(Kolor.bgRedBright.black(`ʍɨʐʊӄɨgoose Error: \n${err}`));
-          process.exit(1);
-        });
-      await AutoKrakz();
-      await 爪𝖎𝖟𝖚ӄ𝖎();
-    } catch (ℓαвєяяσя) {
-      console.log(ℓαвєяяσя);
-    }
-  })();
+(async () => {
+try {
+const { 爪𝖎𝖟𝖚ӄ𝖎 } = require(`../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/🍁爪𝖎𝖟𝖚ӄ𝖎™`);
+const AutoKrakz = require(`../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/𝓜𝖎𝖟𝖚ӄ𝖎ɢɪᴛ`);
+const _𝔏𝔞𝔟_ = require(`../ʍɨʐʊӄɨ🛰️ֆɛʀʋɛʀ/_𝔏𝔞𝔟_`);
+const ʍɨʐʊӄɨgoose = require("mongoose");
+const Kolor = require(`chalk`);
+await ʍɨʐʊӄɨgoose
+.connect(_𝔏𝔞𝔟_.MizukiMdb, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false,
+})
+.then(async (err) => {
+if (err) return console.log(err);
+console.log(Kolor.bgGreenBright.black(`ʍɨʐʊӄɨgoose Connecting!`)
+await AutoKrakz();
+await 爪𝖎𝖟𝖚ӄ𝖎();
+}))
+.catch ((err) => {
+console.log(Kolor.bgRedBright.black(`ʍɨʐʊӄɨgoose Error: \n${err}`));
+process.exit(1);
+});
+
 } catch (ℓαвєяяσя) {
-  console.log(ℓαвєяяσя);
+console.log(ℓαвєяяσя);
+}
+})();
+} catch (ℓαвєяяσя) {
+console.log(ℓαвєяяσя);
 }
