80a1621ade8b8af174536e495a05bdbff194f049
krakinzkon <krakinzkon@gmail.com>
2022-01-22 03:09:08 -0800

    🍹(𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭-v1.3🍹

--- Sakura🛰️Server/ֆǟӄʊʀǟɢɪᴛ.js
+++ Sakura🛰️Server/ֆǟӄʊʀǟɢɪᴛ.js
@@ -3,32 +3,38 @@
 `|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
 `|🍹|`;
 `|🍹|`;
-const chalk = require(`chalk`);
 const git = require(`simple-git`)();
+const chalk = require(`chalk`);
 const exec = require(`child_process`).exec;
 const gitPull = async () => {
   await git.fetch();
   var newCommits = await git.log([`KrakinzLab..origin/KrakinzLab`]);
-  console.log(chalk.yellow(`🍹INFO ⬰ 𝗖𝗵𝗲𝗰𝗸𝗶𝗻𝗴 𝗙𝗼𝗿 🍹ֆǟӄʊʀǟ™ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ...`));
   if (newCommits.total) {
-    console.log(chalk.yellow(`💡INFO ⬰ 𝗡𝗲𝘄 𝗨𝗽𝗱𝗮𝘁𝗲 𝗽𝗲𝗻𝗱𝗶𝗻𝗴, 𝘂𝗽𝗱𝗮𝘁𝗶𝗻𝗴...`));
+    console.log(chalk.blueBright(`💡INFO ⬰ 𝐍𝐞𝐰 𝐔𝐩𝐝𝐚𝐭𝐞 𝐩𝐞𝐧𝐝𝐢𝐧𝐠, 𝐮𝐩𝐝𝐚𝐭𝐢𝐧𝐠...`));
     await git.pull(`origin`, `KrakinzLab`, (err, update) => {
-      if (err) {
+      if (update && update.summary.changes) {
+        if (update.files.includes(`package.json`)) {
+          exec(`npm i --force`).stderr.pipe(process.stderr);
+        }
         console.log(
-          chalk.red(
+          chalk.bgGreenBright.black(
+            `💡INFO ⬰ 𝗔𝘂𝘁𝗼-𝗨𝗽𝗱𝗮𝘁𝗲𝗱 ⚡𝐒𝐚𝐤𝐮𝐫𝐚™ 𝘄𝗶𝘁𝗵 𝗹𝗮𝘁𝗲𝘀𝘁 𝗰𝗵𝗮𝗻𝗴𝗲𝘀.`
+          )
+        );
+      } else if (err) {
+        console.log(
+          chalk.bgRedBright.black(
             `❌ERROR ⬰ 𝗜𝗳 𝗶𝘁 𝘄𝗮𝘀 𝗮 𝗕𝗜𝗚 ⚡𝐒𝐚𝐤𝐮𝐫𝐚™ 𝘂𝗽𝗱𝗮𝘁𝗲 𝘁𝗵𝗲𝗻 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗱𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗯𝗼𝘁!`
           )
         );
       }
-      if (update && update.summary.changes) {
-        if (update.files.includes(`package.json`)) {
-          exec(`npm install`).stderr.pipe(process.stderr);
-        }
-        console.log(
-          chalk.green(`💡INFO ⬰ 𝗔𝘂𝘁𝗼-𝗨𝗽𝗱𝗮𝘁𝗲𝗱 ⚡𝐒𝐚𝐤𝐮𝐫𝐚™ 𝘄𝗶𝘁𝗵 𝗹𝗮𝘁𝗲𝘀𝘁 𝗰𝗵𝗮𝗻𝗴𝗲𝘀.`)
-        );
-      }
     });
+  } else {
+    console.log(
+      chalk.bgGreenBright.black(
+        `💡INFO ⬰ ⚡𝐒𝐚𝐤𝐮𝐫𝐚™ 𝗶𝘀 𝗮𝗹𝗿𝗲𝗮𝗱𝘆 𝗼𝗻 𝗹𝗮𝘁𝗲𝘀𝘁 𝘃𝗲𝗿𝘀𝗶𝗼𝗻.`
+      )
+    );
   }
 };
 
