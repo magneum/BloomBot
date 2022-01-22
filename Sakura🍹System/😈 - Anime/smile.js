14f46e919bea07300de0e3867ea68f5d3818049d
krakinzkon <krakinzkon@gmail.com>
2022-01-22 07:29:50 -0800

    🍹(c)ֆǟӄʊʀǟ🍹v1.5

--- Sakura🍹System/😈 - Anime/smile.js
+++ Sakura🍹System/😈 - Anime/smile.js
@@ -22,126 +22,128 @@ var str = scriptName;
 var newScpt = str.slice(0, -3).toLowerCase();
 //|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
 module.exports = {
-  name: newScpt,
-  async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
-    try {
-      var Raw = await anime.smile();
-      const defaultnm = ֆǟӄʊʀǟ.commandName;
-      const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
-      `⬡  ⬡=========================================================================================⬡ 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ ⬡`;
-      if (ֆǟӄʊʀǟ.isReply) {
-        var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
-        var personreceived = Receiver.substring(0, Receiver.length - 15);
-        var ꜱᴇɴᴅᴇʀɪᴅ = ֆǟӄʊʀǟ.sender;
-        var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
-      } else {
-        var ꜱᴇɴᴅᴇʀɪᴅ = ֆǟӄʊʀǟ.sender;
-        var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
-      }
-      const GotFile = `./Sakura🗑️Temp/${Ping}_${FinalName}.gif`;
-      const GiveFile = `./Sakura🗑️Temp/${Ping}_${FinalName}.mp4`;
-      //|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
-      const jsoncool = await JSON.parse(
-        fs.readFileSync(`./Sakura🛰️Server/Coolist.json`)
-      );
-      await jsoncool.push(ֆǟӄʊʀǟ.sender);
-      await fs.writeFileSync(
-        `./Sakura🛰️Server/Coolist.json`,
-        JSON.stringify(jsoncool)
-      );
-      setTimeout(async (error) => {
-        if (error) console.log(error);
-        await jsoncool.splice(ֆǟӄʊʀǟ.sender);
-        await fs.writeFileSync(
-          `./Sakura🛰️Server/Coolist.json`,
-          JSON.stringify(jsoncool)
-        );
-      }, 15000);
-      //|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
-      if (!Raw.endsWith(`.gif`) && !Raw.endsWith(`.mp4`)) {
-        var Raw = await anime.smile();
-        var ʟᴀʙᴛᴇꜱᴛ = Raw;
-      } else {
-        var ʟᴀʙᴛᴇꜱᴛ = Raw;
-      }
-      console.log("🍹====⬡ " + ʟᴀʙᴛᴇꜱᴛ + " ⬡====🍹");
+name: newScpt,
+async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+try {
+var Raw = await anime.smile();
+const defaultnm = ֆǟӄʊʀǟ.commandName;
+const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+`⬡  ⬡=========================================================================================⬡ 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ ⬡`;
+if (ֆǟӄʊʀǟ.isReply) {
+var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
+var personreceived = Receiver.substring(0, Receiver.length - 15);
+var ꜱᴇɴᴅᴇʀɪᴅ = ֆǟӄʊʀǟ.sender;
+var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
+} else {
+var ꜱᴇɴᴅᴇʀɪᴅ = ֆǟӄʊʀǟ.sender;
+var ꜱᴇɴᴅᴇʀeceived = ꜱᴇɴᴅᴇʀɪᴅ.substring(0, ꜱᴇɴᴅᴇʀɪᴅ.length - 15);
+}
+const GotFile = `./Sakura🗑️Temp/${Ping}_${FinalName}.gif`;
+const GiveFile = `./Sakura🗑️Temp/${Ping}_${FinalName}.mp4`;
+//|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
+const jsoncool = await JSON.parse(
+fs.readFileSync(`./Sakura🛰️Server/Coolist.json`)
+);
+await jsoncool.push(ֆǟӄʊʀǟ.sender);
+await fs.writeFileSync(
+`./Sakura🛰️Server/Coolist.json`,
+JSON.stringify(jsoncool)
+);
+setTimeout(async (error) => {
+if (error) console.log(error);
+await jsoncool.splice(ֆǟӄʊʀǟ.sender);
+await fs.writeFileSync(
+`./Sakura🛰️Server/Coolist.json`,
+JSON.stringify(jsoncool)
+);
+}, 15000);
+//|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
+if (!Raw.endsWith(`.gif`) && !Raw.endsWith(`.mp4`)) {
+var Raw = await anime.smile();
+var ʟᴀʙᴛᴇꜱᴛ = Raw;
+} else {
+var ʟᴀʙᴛᴇꜱᴛ = Raw;
+}
+console.log("🍹====⬡ " + ʟᴀʙᴛᴇꜱᴛ + " ⬡====🍹");
 
-      const downloader = await new Downloader({
-        url: ʟᴀʙᴛᴇꜱᴛ,
-        directory: `./Sakura🗑️Temp`,
-        fileName: `${Ping}_${FinalName}.gif`,
-        cloneFiles: false,
-      });
-      try {
-        await downloader.download();
-      } catch (ℓαвєяяσя) {
-        return Sakura_Buttons.MTB(
-          ӄʀǟӄɨռʐ,
-          chat,
-          ֆǟӄʊʀǟ,
-          `✋🏽‍𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚, @${ꜱᴇɴᴅᴇʀeceived},
+const downloader = await new Downloader({
+url: ʟᴀʙᴛᴇꜱᴛ,
+directory: `./Sakura🗑️Temp`,
+fileName: `${Ping}_${FinalName}.gif`,
+cloneFiles: false,
+});
+try {
+await downloader.download();
+} catch (ℓαвєяяσя) {
+return Sakura_Buttons.MTB(
+ӄʀǟӄɨռʐ,
+chat,
+ֆǟӄʊʀǟ,
+`✋🏽‍𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚, @${ꜱᴇɴᴅᴇʀeceived},
 
 ╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
 ║🥺 𝐎𝐩𝐩𝐬𝐢𝐞 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧!
 ║🍹 𝐒𝐞𝐫𝐯𝐞𝐫 𝐃𝐢𝐝𝐧'𝐭 𝐑𝐞𝐬𝐩𝐨𝐧𝐝!
 ╚════════════╝
 `
-        );
-      }
-      if (ֆǟӄʊʀǟ.isReply) {
-        ffmpeg(GotFile)
-          .outputOptions([
-            `-pix_fmt yuv420p`,
-            `-c:v libx264`,
-            `-movflags +faststart`,
-            `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-          ])
-          .save(GiveFile)
-          .on(`end`, async () => {
-            await Sakura_Static.MGS(
-              ӄʀǟӄɨռʐ,
-              chat,
-              ֆǟӄʊʀǟ,
-              `
+);
+}
+if (ֆǟӄʊʀǟ.isReply) {
+ffmpeg(GotFile)
+.outputOptions([
+`-pix_fmt yuv420p`,
+`-c:v libx264`,
+`-movflags +faststart`,
+`-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+])
+.save(GiveFile)
+.on(`end`, async () => {
+await Sakura_Static.MGS(
+ӄʀǟӄɨռʐ,
+chat,
+ֆǟӄʊʀǟ,
+`
 ╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
 ║🍕𝐅𝐫𝐨𝐦: @${ꜱᴇɴᴅᴇʀeceived}
 ║🌭𝐓𝐨: @${personreceived}
 ╚════════════╝
 `,
-              GiveFile
-            );
-            return await cleanRF.cleanRF(GiveFile, GotFile);
-          });
-      } else {
-        ffmpeg(GotFile)
-          .outputOptions([
-            `-pix_fmt yuv420p`,
-            `-c:v libx264`,
-            `-movflags +faststart`,
-            `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-          ])
-          .save(GiveFile)
-          .on(`end`, async () => {
-            await Sakura_Static.MGS(
-              ӄʀǟӄɨռʐ,
-              chat,
-              ֆǟӄʊʀǟ,
-              `
+GiveFile
+);
+return await cleanRF.cleanRF(GiveFile, GotFile);
+});
+} else {
+ffmpeg(GotFile)
+.outputOptions([
+`-pix_fmt yuv420p`,
+`-c:v libx264`,
+`-movflags +faststart`,
+`-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+])
+.save(GiveFile)
+.on(`end`, async () => {
+await Sakura_Static.MGS(
+ӄʀǟӄɨռʐ,
+chat,
+ֆǟӄʊʀǟ,
+`
 ╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
 ║🍯𝐓𝐡𝐞𝐦𝐬𝐞𝐥𝐯𝐞: @${ꜱᴇɴᴅᴇʀeceived}  
 ╚════════════╝
 `,
-              GiveFile
-            );
-            return await cleanRF.cleanRF(GiveFile, GotFile);
-          });
-      }
-    } catch (ℓαвєяяσя) {
-      ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
-      console.log(ℓαвєяяσя);
-    }
-  },
+GiveFile
+);
+return await cleanRF.cleanRF(GiveFile, GotFile);
+});
+}
+//|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
+} catch (ℓαвєяяσя) {
+ꜰᴜᴄᴋ.catch(ℓαвєяяσя, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
+return;
+}
+},
 };
+//|🍹  ⬡===========================================|  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™  |===========================================⬡  🍹|
 `|🍹|`;
 `|🍹|`;
 `|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
