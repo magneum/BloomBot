71efdf4aa2bc225cb82a96e45c49485c1eb10f66
krakinzkon <krakinzkon@gmail.com>
2022-01-21 21:17:28 -0800

    🍹(c)ֆǟӄʊʀǟ🍹v1.3

--- Sakura🛰️Server/_𝔏𝔞𝔟_.js
+++ Sakura🛰️Server/_𝔏𝔞𝔟_.js
@@ -38,10 +38,7 @@ const env = {
   INVL: `https://i.postimg.cc/XqPBcyq0/image.png`,
   MEE: `https://i.postimg.cc/XqPBcyq0/image.png`,
   HASH: `https://chat.whatsapp.com/LKN8uVBd8ucHRHofz0jBSd`,
-  DEV:
-    process.env.SUDO === undefined
-      ? `918436686758,917430922909`
-      : `918436686758,917430922909,` + process.env.SUDO,
+  DEV: `918436686758,917430922909`,
   HEROKU: process.env.HEROKU,
   SAKURAQL: (process.env.SAKURAQL =
     process.env.SAKURAQL === undefined
