6e4d9a96020e46c9676ea3621d717df330bbae82
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-01-25 13:20:45 +0530

    Delete Player.js

--- Player.js
+++
@@ -1,7 +0,0 @@
-exec(
-  `youtube-dl --extract-audio --audio-format mp3 -o '${ꜱᴇɴᴅᴇʀeceived}_${youfound.title}.%(ext)s'  ${youfound.url}`,
-  (Error) => {
-    if (Error) return console.log(Error);
-    console.log("Done");
-  }
-);
