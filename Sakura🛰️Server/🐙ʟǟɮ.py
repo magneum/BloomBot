aa032d8052f70c0a9f918a5d224b702225434ce8
krakinzkon <krakinzkon@gmail.com>
2022-01-26 09:58:47 +0530

    (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |🍹| version 3

--- Sakura🛰️Server/🐙ʟǟɮ.py
+++ Sakura🛰️Server/🐙ʟǟɮ.py
@@ -7,8 +7,6 @@ import os
 my_system = platform.uname()
 if os.path.exists('Sakura🛰️Server/Coolist.json'):
     os.remove('Sakura🛰️Server/Coolist.json')
-    print('💡INFO ⬰ 𝗖𝗼𝗼𝗹𝗷𝘀𝗼𝗻.𝗷𝘀𝗼𝗻 -𝗿𝗺 𝗱𝗼𝗻𝗲!')
-    print('💡INFO ⬰ 𝗖𝗼𝗻𝘁𝗶𝗻𝘂𝗶𝗻𝗴 𝗧𝗼𝘄𝗮𝗿𝗱𝘀 𝗧𝗼𝘂𝗰𝗵.𝗷𝘀')
 else:
     pass
 os.system('touch Sakura🛰️Server/Coolist.json')
