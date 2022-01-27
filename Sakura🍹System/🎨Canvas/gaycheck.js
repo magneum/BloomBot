045be6c48df001e788a9c4a8b47ae2314c76eb0e
krakinzkon <krakinzkon@gmail.com>
2022-01-27 11:00:14 +0530

    (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |🍹| version 4

---
+++ Sakura🍹System/🎨Canvas/gaycheck.js
@@ -0,0 +1,86 @@
+`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
+`|🍹|      (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 | 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!       |`;
+`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
+`|🍹|`;
+`|🍹|`;
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+const Sakura_Buttons = require(`../../Sakura🛰️Server/Sakura_Buttons`);
+const Sakura_Static = require(`../../Sakura🛰️Server/Sakura_Static`);
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const _𝔏𝔞𝔟_ = require(`../../Sakura🛰️Server/_𝔏𝔞𝔟_`);
+const ꜰᴜᴄᴋ = require(`../../Sakura🛰️Server/oShit`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const fs = require(`fs`);
+var path = require(`path`);
+var scriptName = path.basename(__filename);
+var newScpt = scriptName.slice(0, -3).toLowerCase();
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+module.exports = {
+  name: newScpt,
+  async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+    var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+    const defaultnm = ֆǟӄʊʀǟ.commandName;
+    var ꜱᴇɴᴅᴇʀeceived = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
+    const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+    `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    const Type = Object.keys(chat.message)[0];
+    const GayBody =
+      Type === "listResponseMessage" && chat.message.listResponseMessage.title
+        ? chat.message.listResponseMessage.title
+        : Type === "buttonsResponseMessage" &&
+          chat.message.buttonsResponseMessage.selectedButtonId
+        ? chat.message.buttonsResponseMessage.selectedButtonId
+        : Type === "conversation" && chat.message.conversation.startsWith(ᴋᴇɪ)
+        ? chat.message.conversation
+        : Type == "imageMessage" &&
+          chat.message.imageMessage.caption.startsWith(ᴋᴇɪ)
+        ? chat.message.imageMessage.caption
+        : Type == "videoMessage" &&
+          chat.message.videoMessage.caption.startsWith(ᴋᴇɪ)
+        ? chat.message.videoMessage.caption
+        : Type == "extendedTextMessage" &&
+          chat.message.extendedTextMessage.text.startsWith(ᴋᴇɪ)
+        ? chat.message.extendedTextMessage.text
+        : "";
+    `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    var GayCheck = GayBody.slice(1);
+    var Tag = GayCheck.split(" ").join("");
+    var ToRm = ᴋᴇɪ + newScpt;
+    var Num = ToRm.length;
+    var Mention = Tag.substring(Num);
+    var MentionPerson = Mention + "@s.whatsapp.net";
+    const FinalGay = Math.ceil(Math.random() * 100);
+    `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    const buttonMessage = {
+      contentText: `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚
+
+╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
+║ 𝗚𝗮𝘆 𝗠𝗲𝗺𝗯𝗲𝗿: _@${Mention}_
+║ 𝗔𝘀𝗸𝗲𝗱 𝗕𝘆: _@${ꜱᴇɴᴅᴇʀeceived}_
+║ 𝗣𝗲𝗿𝗰𝗲𝗻𝘁𝗮𝗴𝗲: _${FinalGay}%_
+╚════════════╝`,
+      footerText: `(𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 | 𝐄𝐧𝐠𝐢𝐧𝐞: _KryoZen 3_\n${Clock} | ${Ping}ms`,
+      buttons: [
+        {
+          buttonId: `${ᴋᴇɪ}help`,
+          buttonText: { displayText: `${ᴋᴇɪ}help` },
+          type: 1,
+        },
+      ],
+      headerType: 1,
+    };
+    return await ӄʀǟӄɨռʐ
+      .sendMessage(ֆǟӄʊʀǟ.chatId, buttonMessage, MessageType.buttonsMessage, {
+        quoted: chat,
+        contextInfo: { mentionedJid: [աɦօֆɛռȶɦǟȶ, MentionPerson] },
+      })
+      .catch((Error) => ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat));
+  },
+};
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+`|🍹|`;
+`|🍹|`;
+`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
+`|🍹|      (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 | 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!       |`;
+`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
