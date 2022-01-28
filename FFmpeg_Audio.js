12012726c717ca05e46c7ed43427263ba1079fa7
krakinzkon <krakinzkon@gmail.com>
2022-01-29 01:13:59 +0530

    (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |🍹| version 6

--- FFmpeg_Audio.js
+++ FFmpeg_Audio.js
@@ -1,8 +1,35 @@
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
 const { YouTube_Music } = require(`./Sakura🛰️Server/youmaker`);
-const ffmpeg = require("fluent-ffmpeg");
+const FFmpeg = require("fluent-FFmpeg");
+const readline = require(`readline`);
 const TinyURL = require("tinyurl");
-const proc = new ffmpeg();
-
+const FFmpegProc = new FFmpeg();
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+YouTube_Music("https://youtu.be/UKunvvN2iCk").then((res) => {
+  const { dl_link, thumb, title, filesizeF, filesize } = res;
+  TinyURL.shorten(dl_link).then(
+    async function (gotTiny) {
+      FFmpegProc.addInput(gotTiny)
+        .output("./" + title.trim().toString() + ".mp3")
+        .format("mp3")
+        .on(`progress`, (p) => {
+          readline.cursorTo(process.stdout, 0);
+          process.stdout.write(`⬡═════| 💯𝐏𝐫𝐨𝐠𝐫𝐞𝐬𝐬: ${p.targetSize}kb`);
+        })
+        .on("error", (Error) =>
+          console.log(`⬡═════| 🐞𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐄𝐫𝐫𝐨𝐫: ` + Error)
+        )
+        .on("end", () =>
+          console.log(`\n⬡═════| 🥂𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐅𝐢𝐧𝐢𝐬𝐞𝐝: ` + gotTiny)
+        );
+      FFmpegProc.run();
+    },
+    async function (Error) {
+      console.log(`⬡═════| 🐞𝐄𝐫𝐫𝐨𝐫: ` + Error);
+    }
+  );
+});
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
 // const { spawn } = require("child_process");
 // const child = spawn("node", ["Sakura☣️Reactor/🍹ֆǟӄʊʀǟ™.js"]);
 // child.stdout.on("data", (data) => {
@@ -17,25 +44,4 @@ const proc = new ffmpeg();
 //   if (signal) console.log("Process Exited With Signal: " + signal);
 //   console.log("All Done!");
 // });
-
-YouTube_Music(Found.url).then((res) => {
-  const { dl_link, thumb, title, filesizeF, filesize } = res;
-  require("tinyurl")
-    .TinyURL.shorten(dl_link)
-    .then(
-      async function (gotTiny) {
-        console.log(gotTiny);
-        const File = title.trim();
-        proc
-          .addInput(gotTiny)
-          .output("./" + File.toStrimg() + ".mp3")
-          .format("mp3")
-          .on("error", (err) => console.log(err))
-          .on("end", () => console.log("Done FFmpeg"));
-        proc.run();
-      },
-      async function (Error) {
-        console.log(Error);
-      }
-    );
-});
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
