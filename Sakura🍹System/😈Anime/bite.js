d3f6452d7c07d0e1c6c60d652552fe2516538688
krakinzkon <krakinzkon@gmail.com>
2022-01-29 11:00:44 +0530

    (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |🍹| version 6.1

--- Sakura🍹System/😈Anime/bite.js
+++ Sakura🍹System/😈Anime/bite.js
@@ -1,9 +1,3 @@
-`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
-`|🍹|      (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 | 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!       |`;
-`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
-`|🍹|`;
-`|🍹|`;
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
 const Sakura_Buttons = require(`../../Sakura🛰️Server/Sakura_Buttons`);
 const Sakura_Static = require(`../../Sakura🛰️Server/Sakura_Static`);
 const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
@@ -25,131 +19,96 @@ var newScpt = str.slice(0, -3).toLowerCase();
 module.exports = {
   name: newScpt,
   async handle(ӄʀǟӄɨռʐ, mozart, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+    var AnimeLink = await anime.bite();
+    const defaultnm = ֆǟӄʊʀǟ.commandName;
+    const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+    `⬡  ⬡=========================================================================================⬡ 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ ⬡`;
+    if (ֆǟӄʊʀǟ.isReply) {
+      var Receiver = mozart.message.extendedTextMessage.contextInfo.participant;
+      var քɛʀֆօռɢօȶռʊʍ = Receiver.substring(0, Receiver.length - 15);
+      var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+      var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
+    } else {
+      var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+      var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
+    }
+    const GotFile = `${__dirname}/${Ping}_${FinalName}.gif`;
+    const GiveFile = `${__dirname}/${Ping}_${FinalName}.mp4`;
+    `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    if (!AnimeLink.endsWith(`.gif`) && !AnimeLink.endsWith(`.mp4`)) {
+      ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, mozart);
+      return;
+    }
+    console.log("🍹====⬡ " + AnimeLink + " ⬡====🍹");
+    const downloader = await new Downloader({
+      url: AnimeLink,
+      directory: `${__dirname}`,
+      fileName: `${Ping}_${FinalName}.gif`,
+      cloneFiles: false,
+    });
+    `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
     try {
-      var Raw = await anime.bite();
-      const defaultnm = ֆǟӄʊʀǟ.commandName;
-      const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
-      `⬡  ⬡=========================================================================================⬡ 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ ⬡`;
-      if (ֆǟӄʊʀǟ.isReply) {
-        var Receiver = mozart.message.extendedTextMessage.contextInfo.participant;
-        var քɛʀֆօռɢօȶռʊʍ = Receiver.substring(0, Receiver.length - 15);
-        var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
-        var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
-      } else {
-        var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
-        var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
-      }
-      const GotFile = `${__dirname}/${Ping}_${FinalName}.gif`;
-      const GiveFile = `${__dirname}/${Ping}_${FinalName}.mp4`;
-      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-      const jsoncool = await JSON.parse(
-        fs.readFileSync(`./Sakura🛰️Server/Coolist.json`)
-      );
-      await jsoncool.push(ֆǟӄʊʀǟ.sender);
-      await fs.writeFileSync(
-        `./Sakura🛰️Server/Coolist.json`,
-        JSON.stringify(jsoncool)
-      );
-      setTimeout(async (error) => {
-        if (error) console.log(error);
-        await jsoncool.splice(ֆǟӄʊʀǟ.sender);
-        await fs.writeFileSync(
-          `./Sakura🛰️Server/Coolist.json`,
-          JSON.stringify(jsoncool)
-        );
-      }, 15000);
-      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-      if (!Raw.endsWith(`.gif`) && !Raw.endsWith(`.mp4`)) {
-        var Raw = await anime.bite();
-        var ʟᴀʙᴛᴇꜱᴛ = Raw;
-      } else {
-        var ʟᴀʙᴛᴇꜱᴛ = Raw;
-      }
-      console.log("🍹====⬡ " + ʟᴀʙᴛᴇꜱᴛ + " ⬡====🍹");
-      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-      const downloader = await new Downloader({
-        url: ʟᴀʙᴛᴇꜱᴛ,
-        directory: `${__dirname}`,
-        fileName: `${Ping}_${FinalName}.gif`,
-        cloneFiles: false,
-      });
-      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-      try {
-        await downloader.download();
-      } catch (Error) {
-        return Sakura_Buttons.MTB(
-          ӄʀǟӄɨռʐ,
-          mozart,
-          ֆǟӄʊʀǟ,
-          `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚, @${աɦօֆɛռȶɦǟȶռʊʍ},
+      await downloader.download();
+    } catch (Error) {
+      return Sakura_Buttons.MTB(
+        ӄʀǟӄɨռʐ,
+        mozart,
+        ֆǟӄʊʀǟ,
+        `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚, @${աɦօֆɛռȶɦǟȶռʊʍ},
 
 ╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
 ║🥺 𝐎𝐩𝐩𝐬𝐢𝐞 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧!
 ║🍹 𝐒𝐞𝐫𝐯𝐞𝐫 𝐃𝐢𝐝𝐧'𝐭 𝐑𝐞𝐬𝐩𝐨𝐧𝐝!
-╚════════════╝
-`
-        );
-      }
-      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-      if (ֆǟӄʊʀǟ.isReply) {
-        ffmpeg(GotFile)
-          .outputOptions([
-            `-pix_fmt yuv420p`,
-            `-c:v libx264`,
-            `-movflags +faststart`,
-            `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-          ])
-          .save(GiveFile)
-          .on(`end`, async () => {
-            await Sakura_Static.MGS(
-              ӄʀǟӄɨռʐ,
-              mozart,
-              ֆǟӄʊʀǟ,
-              `
+╚════════════╝`
+      );
+    }
+    `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    if (ֆǟӄʊʀǟ.isReply) {
+      ffmpeg(GotFile)
+        .outputOptions([
+          `-pix_fmt yuv420p`,
+          `-c:v libx264`,
+          `-movflags +faststart`,
+          `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+        ])
+        .save(GiveFile)
+        .on(`end`, async () => {
+          await Sakura_Static.MGS(
+            ӄʀǟӄɨռʐ,
+            mozart,
+            ֆǟӄʊʀǟ,
+            `
 ╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
 ║🍕𝐅𝐫𝐨𝐦: @${աɦօֆɛռȶɦǟȶռʊʍ}
 ║🌭𝐓𝐨: @${քɛʀֆօռɢօȶռʊʍ}
-╚════════════╝
-`,
-              GiveFile
-            );
-            return await cleanRF.cleanRF(GiveFile, GotFile);
-          });
-        `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-      } else {
-        ffmpeg(GotFile)
-          .outputOptions([
-            `-pix_fmt yuv420p`,
-            `-c:v libx264`,
-            `-movflags +faststart`,
-            `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-          ])
-          .save(GiveFile)
-          .on(`end`, async () => {
-            await Sakura_Static.MGS(
-              ӄʀǟӄɨռʐ,
-              mozart,
-              ֆǟӄʊʀǟ,
-              `
+╚════════════╝`,
+            GiveFile
+          );
+          return await cleanRF.cleanRF(GiveFile, GotFile);
+        });
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    } else {
+      ffmpeg(GotFile)
+        .outputOptions([
+          `-pix_fmt yuv420p`,
+          `-c:v libx264`,
+          `-movflags +faststart`,
+          `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+        ])
+        .save(GiveFile)
+        .on(`end`, async () => {
+          await Sakura_Static.MGS(
+            ӄʀǟӄɨռʐ,
+            mozart,
+            ֆǟӄʊʀǟ,
+            `
 ╔════◇🌿𝐓𝐨𝐩𝐢𝐜: ${FinalName}
 ║🍯𝐓𝐡𝐞𝐦𝐬𝐞𝐥𝐯𝐞: @${աɦօֆɛռȶɦǟȶռʊʍ}  
-╚════════════╝
-`,
-              GiveFile
-            );
-            return await cleanRF.cleanRF(GiveFile, GotFile);
-          });
-      }
-      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-    } catch (Error) {
-      ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, mozart);
-      return;
+╚════════════╝`,
+            GiveFile
+          );
+          return await cleanRF.cleanRF(GiveFile, GotFile);
+        });
     }
   },
 };
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-`|🍹|`;
-`|🍹|`;
-`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
-`|🍹|      (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 | 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!       |`;
-`|🍹|======================================================================================================⬡  𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™`;
