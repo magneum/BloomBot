4ba08b675a2155691a36fdb1b023d2c0ac75e257
krakinzkon <krakinzkon@gmail.com>
2022-01-29 15:34:50 +0530

    (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |🍹| version 6.1

--- Sakura🛰️Server/FFmpegAudio.js
+++ Sakura🛰️Server/FFmpegAudio.js
@@ -152,18 +152,3 @@ exports.FFmpegAudio = async (ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, mozart, Found, userBa
   }
 };
 `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-// const { spawn } = require("child_process");
-// const child = spawn("node", ["Sakura☣️Reactor/🍹ֆǟӄʊʀǟ™.js"]);
-// child.stdout.on("data", (data) => {
-//   console.log("Stdout:" + data);
-// });
-// child.stderr.on("data", (data) => {
-//   console.log("Stderr:" + data);
-// });
-// child.on("error", (Error) => console.log("Error:" + Error));
-// child.on("exit", (code, signal) => {
-//   if (code) console.log("Process Exited With Code: " + code);
-//   if (signal) console.log("Process Exited With Signal: " + signal);
-//   console.log("All Done!");
-// });
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
--- child.js
+++
@@ -1,5 +0,0 @@
-var child = require("child_process").exec("node run.js");
-child.stdout.pipe(process.stdout);
-child.on("exit", function () {
-  process.exit();
-});
