5a876162f4ad33f6b2c8564ff58102534e0ccfb2
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-02-04 19:46:23 +0530

    (𝐜)Sakura Bot 📡 version-debug-10
    (𝐜)Sakura Bot 📡 version-debug-10

---
+++ Sakura📡System/⚡Randoms/🛸Helpers/downloadmenu.js
@@ -0,0 +1,62 @@
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+`|                                                                                                                        |`;
+`|        (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!            |`;
+`|                                                                                                                        |`;
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+const Sakura_Buttons = require(`../../Sakura🛰️Server/Sakura_Buttons`);
+const Sakura_Static = require(`../../Sakura🛰️Server/Sakura_Static`);
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const cleanRF = require(`../../Sakura🛰️Server/cleanRF`);
+const ꜰᴜᴄᴋ = require(`../../Sakura🛰️Server/oShit`);
+const _𝔏𝔞𝔟_ = require(`../../Sakura🛰️Server/_𝔏𝔞𝔟_`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const fs = require(`fs`);
+var path = require(`path`);
+var scriptName = path.basename(__filename);
+var newScpt = scriptName.slice(0, -3).toLowerCase();
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+module.exports = {
+name: newScpt,
+async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping, Timers) {
+try {
+var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
+const defaultnm = ֆǟӄʊʀǟ.commandName;
+const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+const hold = require("../../Sakura🛰️Server/Hold");
+await hold.Hold(ֆǟӄʊʀǟ);
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+var MediaUrl = `./Sakura☣️Reactor/ֆǟӄʊʀǟ.png`;
+var MainText = `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 •@${աɦօֆɛռȶɦǟȶռʊʍ}, ${Timers}
+
+╔════◇📥 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫𝐬
+║
+║• *${ᴋᴇɪ}play*  _youtube-audio max 10min_
+║• *${ᴋᴇɪ}song*  _youtube-audio max 20min_
+║• *${ᴋᴇɪ}stream*  _youtube-video max 10min_
+║• *${ᴋᴇɪ}watch*  _youtube-video max 20min_
+║• *${ᴋᴇɪ}ytmlink _yt-audio-max 15min_
+║• *${ᴋᴇɪ}ytvlink _yt-video-max 15min_
+║• *${ᴋᴇɪ}instagram*  _image/video from ig_
+╚════════════╝`;
+return Sakura_Buttons.MIB(
+ӄʀǟӄɨռʐ,
+chat,
+ֆǟӄʊʀǟ,
+MainText,
+MediaUrl
+).catch((Error) => ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat));
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+} catch (Error) {
+ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
+return;
+}
+},
+};
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+`|                                                                                                                        |`;
+`|        (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!            |`;
+`|                                                                                                                        |`;
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
---
+++ Sakura📡System/⚡Randoms/🛸Helpers/help.js
@@ -0,0 +1,95 @@
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+`|                                                                                                                        |`;
+`|        (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!            |`;
+`|                                                                                                                        |`;
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const ꜰᴜᴄᴋ = require(`../../Sakura🛰️Server/oShit`);
+const vers = require(`../../package.json`);
+const _𝔏𝔞𝔟_ = require(`../../Sakura🛰️Server/_𝔏𝔞𝔟_`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+var path = require(`path`);
+var scriptName = path.basename(__filename);
+var newScpt = scriptName.slice(0, -3).toLowerCase();
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+module.exports = {
+name: newScpt,
+async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping, Timers) {
+try {
+var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15);
+const defaultnm = ֆǟӄʊʀǟ.commandName;
+const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+const hold = require("../../Sakura🛰️Server/Hold");
+await hold.Hold(ֆǟӄʊʀǟ);
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+const media = await ӄʀǟӄɨռʐ.prepareMessage(
+ֆǟӄʊʀǟ.chatId,
+{ url: _𝔏𝔞𝔟_.MEE },
+MessageType.image,
+{
+mimetype: Mimetype.png,
+}
+);
+await ӄʀǟӄɨռʐ
+.sendMessage(
+ֆǟӄʊʀǟ.chatId,
+{
+contentText: `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 @${աɦօֆɛռȶɦǟȶռʊʍ}, 🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭: _𝐈𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!_
+
+╔════◇👇🏽𝐄𝐯𝐞𝐫𝐲𝐨𝐧𝐞 
+║🍣 _${ᴋᴇɪ}animemenu_
+║💰 _${ᴋᴇɪ}gamemenu_
+║📥 _${ᴋᴇɪ}downloadmenu_
+║🔰 _${ᴋᴇɪ}groupmenu_
+║🦄 _${ᴋᴇɪ}randommenu_
+║🔎 _${ᴋᴇɪ}searchmenu_
+║🗂️ _${ᴋᴇɪ}ffmpegmenu_
+║🏝️ _${ᴋᴇɪ}filtersmenu_
+║🎨 _${ᴋᴇɪ}checkmenu_
+╚◇║
+╔◇║👇🏽𝐀𝐝𝐦𝐢𝐧-𝐎𝐧𝐥𝐲
+║🔰 _${ᴋᴇɪ}groupmenu_
+║🍄 _${ᴋᴇɪ}hentaimenu_ 
+║🥃 _${ᴋᴇɪ}humanmenu_ 
+║🐙 _${ᴋᴇɪ}ownermenu_
+╚════════════╝
+
+☘️𝐏𝐫𝐞𝐬𝐬 𝐅𝐨𝐫 𝐒𝐚𝐤𝐮𝐫𝐚 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬👇🏽‍`,
+footerText: `⎿ (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐁𝐨𝐭 ⏋\n⎿ 𝐄𝐧𝐠𝐢𝐧𝐞: _kryozenV${vers.vers}_ ⏋`,
+buttons: [
+{
+buttonId: `${ᴋᴇɪ}menu`,
+buttonText: { displayText: `${ᴋᴇɪ}menu` },
+type: 1,
+},
+{
+buttonId: `${ᴋᴇɪ}sakura`,
+buttonText: { displayText: `${ᴋᴇɪ}sakura` },
+type: 1,
+},
+],
+headerType: 4,
+imageMessage: media.message.imageMessage,
+},
+MessageType.buttonsMessage,
+{
+quoted: chat,
+contextInfo: { mentionedJid: [աɦօֆɛռȶɦǟȶ] },
+}
+)
+.catch((Error) => ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat));
+`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+} catch (Error) {
+ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
+return;
+}
+},
+};
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+`|                                                                                                                        |`;
+`|        (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!            |`;
+`|                                                                                                                        |`;
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
