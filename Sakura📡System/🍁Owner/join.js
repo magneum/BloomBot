dbcb381fa69999ba124afc7c86f6af29d4818b68
krakinzkon <krakinzkon@gmail.com>
2022-02-04 21:17:02 +0530

    (𝐜)Sakura Bot 📡 version-debug-10

--- Sakura📡System/🍁Owner/join.js
+++ Sakura📡System/🍁Owner/join.js
@@ -17,76 +17,78 @@ var path = require(`path`);
 var scriptName = path.basename(__filename);
 var newScpt = scriptName.slice(0, -3).toLowerCase();
 module.exports = {
-name: newScpt,
-async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
-try {
-var filePath;
-var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
-const defaultnm = ֆǟӄʊʀǟ.commandName;
-var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15).replace(/[+ ]/g, "");
-const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-if (!ֆǟӄʊʀǟ.fromMe && !ֆǟӄʊʀǟ.isSenderDev) {
-return Sakura_Buttons.MIB(
-ӄʀǟӄɨռʐ,
-chat,
-ֆǟӄʊʀǟ,
-`*❌ Owner Only | You Are Not Allowed! ❌*`,
-`./Sakura☣️Reactor/ֆǟӄʊʀǟMemErr.png`
-);
-}
-if (ֆǟӄʊʀǟ.isReply) {
-return Sakura_Buttons.MTB(
-ӄʀǟӄɨռʐ,
-chat,
-ֆǟӄʊʀǟ,
-`*Sir Please Don't Reply To any Message.*\n\n *Use:* ${ᴋᴇɪ}${newScpt} _Invite Link_`
-);
-}
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-const hold = require(`../../Sakura🛰️Server/Hold`);
-await hold.Hold(ֆǟӄʊʀǟ);
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-var InvLink = Needs.join(" ");
-let [_, code] = InvLink.match(linkRegex) || [];
-if (!code) {
-return Sakura_Buttons.MTB(
-ӄʀǟӄɨռʐ,
-chat,
-ֆǟӄʊʀǟ,
-`🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 @${աɦօֆɛռȶɦǟȶռʊʍ}, 
+  name: newScpt,
+  async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+    try {
+      var filePath;
+      var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+      const defaultnm = ֆǟӄʊʀǟ.commandName;
+      var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ
+        .substring(0, աɦօֆɛռȶɦǟȶ.length - 15)
+        .replace(/[+ ]/g, "");
+      const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+      if (!ֆǟӄʊʀǟ.fromMe && !ֆǟӄʊʀǟ.isSenderDev) {
+        return Sakura_Buttons.MIB(
+          ӄʀǟӄɨռʐ,
+          chat,
+          ֆǟӄʊʀǟ,
+          `*❌ Owner Only | You Are Not Allowed! ❌*`,
+          `./Sakura☣️Reactor/ֆǟӄʊʀǟMemErr.png`
+        );
+      }
+      if (ֆǟӄʊʀǟ.isReply) {
+        return Sakura_Buttons.MTB(
+          ӄʀǟӄɨռʐ,
+          chat,
+          ֆǟӄʊʀǟ,
+          `*Sir Please Don't Reply To any Message.*\n\n *Use:* ${ᴋᴇɪ}${newScpt} _Invite Link_`
+        );
+      }
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+      const hold = require(`../../Sakura🛰️Server/Hold`);
+      await hold.Hold(ֆǟӄʊʀǟ);
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+      let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+      var InvLink = Needs.join(" ");
+      let [_, code] = InvLink.match(linkRegex) || [];
+      if (!code) {
+        return Sakura_Buttons.MTB(
+          ӄʀǟӄɨռʐ,
+          chat,
+          ֆǟӄʊʀǟ,
+          `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 @${աɦօֆɛռȶɦǟȶռʊʍ}, 
 
 ╔════◇
 ║❌ 𝗘𝗿𝗿𝗼𝗿: _Invalid Link_
 ╚════════════╝`
-);
-}
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-try {
-await ӄʀǟӄɨռʐ.acceptInvite(code).then(async (res) => {
-return Sakura_Buttons.MTB(
-ӄʀǟӄɨռʐ,
-chat,
-ֆǟӄʊʀǟ,
-`🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 @${աɦօֆɛռȶɦǟȶռʊʍ}, 
+        );
+      }
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+      try {
+        await ӄʀǟӄɨռʐ.acceptInvite(code).then(async (res) => {
+          return Sakura_Buttons.MTB(
+            ӄʀǟӄɨռʐ,
+            chat,
+            ֆǟӄʊʀǟ,
+            `🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 @${աɦօֆɛռȶɦǟȶռʊʍ}, 
 
 ╔════◇
 ║🔓 𝐒𝐭𝐚𝐭𝐮𝐬: _Joined Group!_
 ║📜𝐆𝐫𝐨𝐮𝐩: ${res.gid}
 ╚════════════╝`
-);
-});
-} catch (Error) {
-console.log(Error);
-return ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
-}
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-} catch (Error) {
-console.log(Error);
-return ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
-}
-},
+          );
+        });
+      } catch (Error) {
+        console.log(Error);
+        return ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
+      }
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    } catch (Error) {
+      console.log(Error);
+      return ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
+    }
+  },
 };
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
 `|                                                                                                                        |`;
--- Sakura📡System/🍁Owner/sakura.js
+++ Sakura📡System/🍁Owner/sakura.js
@@ -22,54 +22,56 @@ var scriptName = path.basename(__filename);
 var newScpt = scriptName.slice(0, -3).toLowerCase();
 `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
 module.exports = {
-name: newScpt,
-async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
-try {
-var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
-const defaultnm = ֆǟӄʊʀǟ.commandName;
-var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ.substring(0, աɦօֆɛռȶɦǟȶ.length - 15).replace(/[+ ]/g, "");
-const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-const hold = require(`../../Sakura🛰️Server/Hold`);
-await hold.Hold(ֆǟӄʊʀǟ);
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-const used = process.memoryUsage();
-const cpus = os.cpus().map((cpu) => {
-cpu.total = Object.keys(cpu.times).reduce(
-(last, type) => last + cpu.times[type],
-0
-);
-return cpu;
-});
-const cpu = cpus.reduce(
-(last, cpu, _, { length }) => {
-last.total += cpu.total;
-last.speed += cpu.speed / length;
-last.times.user += cpu.times.user;
-last.times.nice += cpu.times.nice;
-last.times.sys += cpu.times.sys;
-last.times.idle += cpu.times.idle;
-last.times.irq += cpu.times.irq;
-return last;
-},
-{
-speed: 0,
-total: 0,
-times: {
-user: 0,
-nice: 0,
-sys: 0,
-idle: 0,
-irq: 0,
-},
-}
-);
+  name: newScpt,
+  async handle(ӄʀǟӄɨռʐ, chat, ֆǟӄʊʀǟ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+    try {
+      var աɦօֆɛռȶɦǟȶ = ֆǟӄʊʀǟ.sender;
+      const defaultnm = ֆǟӄʊʀǟ.commandName;
+      var աɦօֆɛռȶɦǟȶռʊʍ = աɦօֆɛռȶɦǟȶ
+        .substring(0, աɦօֆɛռȶɦǟȶ.length - 15)
+        .replace(/[+ ]/g, "");
+      const FinalName = defaultnm.charAt(0).toUpperCase() + defaultnm.slice(1);
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+      const hold = require(`../../Sakura🛰️Server/Hold`);
+      await hold.Hold(ֆǟӄʊʀǟ);
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+      const used = process.memoryUsage();
+      const cpus = os.cpus().map((cpu) => {
+        cpu.total = Object.keys(cpu.times).reduce(
+          (last, type) => last + cpu.times[type],
+          0
+        );
+        return cpu;
+      });
+      const cpu = cpus.reduce(
+        (last, cpu, _, { length }) => {
+          last.total += cpu.total;
+          last.speed += cpu.speed / length;
+          last.times.user += cpu.times.user;
+          last.times.nice += cpu.times.nice;
+          last.times.sys += cpu.times.sys;
+          last.times.idle += cpu.times.idle;
+          last.times.irq += cpu.times.irq;
+          return last;
+        },
+        {
+          speed: 0,
+          total: 0,
+          times: {
+            user: 0,
+            nice: 0,
+            sys: 0,
+            idle: 0,
+            irq: 0,
+          },
+        }
+      );
 
-let timestamp = speed();
-let latensi = speed() - timestamp;
-neww = performance.now();
-oldd = performance.now();
-respon = `
+      let timestamp = speed();
+      let latensi = speed() - timestamp;
+      neww = performance.now();
+      oldd = performance.now();
+      respon = `
 ╔══════════════★
 ║▷ 🔑 𝐏𝐫𝐞𝐟𝐢𝐱: ${ᴋᴇɪ} (slash)
 ║▷ 🦞 𝐆𝐢𝐭𝐇𝐮𝐛: ${ᴋᴇɪ}repo
@@ -88,72 +90,72 @@ respon = `
 
 ────★ 𝐍𝐨𝐝𝐞𝐉𝐒 𝐌𝐞𝐦-𝐔𝐬𝐚𝐠𝐞:
 ${Object.keys(used)
-.map(
-(key, _, arr) =>
-`${key.padEnd(Math.max(...arr.map((v) => v.length)), ` `)}: ${formatp(
-used[key]
-)}`
-)
-.join(`\n`)}
+  .map(
+    (key, _, arr) =>
+      `${key.padEnd(Math.max(...arr.map((v) => v.length)), ` `)}: ${formatp(
+        used[key]
+      )}`
+  )
+  .join(`\n`)}
 
 ────★ ${
-cpus[0]
-? `𝐓𝐨𝐭𝐚𝐥 𝐂𝐏𝐔 𝐔𝐬𝐚𝐠𝐞
+        cpus[0]
+          ? `𝐓𝐨𝐭𝐚𝐥 𝐂𝐏𝐔 𝐔𝐬𝐚𝐠𝐞
 ${cpus[0].model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
-.map(
-(type) =>
-`- *${(type + `*`).padEnd(6)}: ${(
-(100 * cpu.times[type]) /
-cpu.total
-).toFixed(2)}%`
-)
-.join(`\n`)}
+              .map(
+                (type) =>
+                  `- *${(type + `*`).padEnd(6)}: ${(
+                    (100 * cpu.times[type]) /
+                    cpu.total
+                  ).toFixed(2)}%`
+              )
+              .join(`\n`)}
 
 
 ────★ 𝐂𝐏𝐔 𝐂𝐨𝐫𝐞(𝐬) 𝐔𝐬𝐚𝐠𝐞 (_${cpus.length}_ 𝐂𝐨𝐫𝐞 𝐂𝐏𝐔)
 ${cpus
-.map(
-(cpu, i) =>
-`${i + 1}. ${cpu.model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
-.map(
-(type) =>
-`- *${(type + `*`).padEnd(6)}: ${(
-(100 * cpu.times[type]) /
-cpu.total
-).toFixed(2)}%`
-)
-.join(`\n`)}`
-)
-.join(`\n\n`)}`
-: ``
-}`.trim();
+  .map(
+    (cpu, i) =>
+      `${i + 1}. ${cpu.model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
+        .map(
+          (type) =>
+            `- *${(type + `*`).padEnd(6)}: ${(
+              (100 * cpu.times[type]) /
+              cpu.total
+            ).toFixed(2)}%`
+        )
+        .join(`\n`)}`
+  )
+  .join(`\n\n`)}`
+          : ``
+      }`.trim();
 
-await ӄʀǟӄɨռʐ
-.sendMessage(
-ֆǟӄʊʀǟ.chatId,
-{
-url: _𝔏𝔞𝔟_.MEE,
-},
-MessageType.image,
-{
-quoted: chat,
-mimetype: Mimetype.png,
-contextInfo: {
-mentionedJid: [աɦօֆɛռȶɦǟȶ],
-},
-caption: `| (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |
+      await ӄʀǟӄɨռʐ
+        .sendMessage(
+          ֆǟӄʊʀǟ.chatId,
+          {
+            url: _𝔏𝔞𝔟_.MEE,
+          },
+          MessageType.image,
+          {
+            quoted: chat,
+            mimetype: Mimetype.png,
+            contextInfo: {
+              mentionedJid: [աɦօֆɛռȶɦǟȶ],
+            },
+            caption: `| (𝐜)𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 |
 
 🦋𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚 @${աɦօֆɛռȶɦǟȶռʊʍ}, 
 ${respon}`,
-}
-)
-.catch((Error) => ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat));
-`|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
-} catch (Error) {
-ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
-return;
-}
-},
+          }
+        )
+        .catch((Error) => ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat));
+      `|⬡════════════════════════════════════════════|  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛🍹𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭(𝐜) |════════════════════════════════════════════⬡|`;
+    } catch (Error) {
+      ꜰᴜᴄᴋ.catch(Error, ӄʀǟӄɨռʐ, ֆǟӄʊʀǟ, chat);
+      return;
+    }
+  },
 };
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
 `|                                                                                                                        |`;
