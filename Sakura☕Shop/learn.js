0e1580b53f77a761a35f625843a133aa0b05a6bc
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-02-04 19:37:21 +0530

    (𝐜)Sakura Bot 📡 version-debug-10
    (𝐜)Sakura Bot 📡 version-debug-10

---
+++ Sakura☕Shop/learn.js
@@ -0,0 +1,44 @@
+`
+╔════◇⚡ 𝐅𝐫𝐞𝐪𝐮𝐞𝐧𝐭𝐥𝐲 𝐀𝐬𝐤𝐞𝐝 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬
+║
+║ *1. What is the reason/topic for this group?*
+║ _Sakura is an Advance Whatsapp User-Bot. You Can Use The Bot in this group or in your respective groups._
+║
+║ *2. How Do I add Sakura In My Group?*
+║   -Send Your Group Link In: _${_𝔏𝔞𝔟_.HASHREQ}_
+║   -Tag any in that group and ask them to use /join command.
+║   -Remember No commands will work in that group!
+║   -Your Group Should Have 10 members atleast.
+║   -Turn Disappearing Message Off in your Gc._
+║   
+║ *3. I want to know how to deploy Sakura.*
+║ _Not Allowed anymore!_
+║
+║ *4. Please Help Me Out With The Commands.*
+║ _You can find all commands in the /menu and their respective category in /help._
+║
+║ *5. I don't want to understand. I am stupid and dumb.*
+║ _Click on top right corner and press leave._
+║
+║ *6. Can i send any link?*
+║ _Allowed Links: YouTube. Anything else = bot will kick you._
+║
+║ *7. Who made Sakura?*
+║ _KrakinzLab_
+║
+║ *8. License and Copyright?*
+║ Visit: _http://github.com/krakinzlab/sakura_
+╚════════════╝
+
+╔════◇💡 𝐀𝐥𝐥 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐞𝐬 𝐁𝐫𝐢𝐞𝐟𝐥𝐲 𝐄𝐱𝐩𝐥𝐚𝐢𝐧𝐞𝐝
+║
+║ *1. Animemenu* = _search anime, get anime gifs,get random anime wallpapers._
+║ *2. Checkmenu* = _tag or reply to someone to check what category they are._
+║ *3. Downloadmenu* = _download videos and audio files from YouTube,Instagram._
+║ *4. Filtermenu* = _add custom audio filter to mp4 files._
+║ *5. Gamemenu* = _play random games on whatsapp._
+║ *6. Searchmenu* = _surf the internet within whatsapp._
+║ *7. Randommenu* = _some random stuffs look yourself._
+║ *8. Hentaimenu* = _not safe for work adult stuffs!_
+║ *9. Humanmenu* = _not safe for work adult stuffs!_
+╚════════════╝`;
