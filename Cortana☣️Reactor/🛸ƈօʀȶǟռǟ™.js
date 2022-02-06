175d5cbf5f67a92ddf6150e0a5e77befcdeb8021
krakinzkon <krakinzkon@gmail.com>
2022-02-07 01:57:32 +0530

    ⎿ ©️ 𝐂𝐨𝐫𝐭𝐚𝐧𝐚 𝐀𝐈 ⏋ 🛸 Stable-yet-debug-14

--- Cortana☣️Reactor/🛸ƈօʀȶǟռǟ™.js
+++ Cortana☣️Reactor/🛸ƈօʀȶǟռǟ™.js
@@ -4,71 +4,84 @@
 `|                                                                                                                        |`;
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
 (async () => {
-const { ƈօʀȶǟռǟ, ӄʀǟӄɨռʐ } = require(`../Cortana🛰️Server/🛸ƈօʀȶǟռǟ™`);
-const _𝔏𝔞𝔟_ = require(`../Cortana🛰️Server/_𝔏𝔞𝔟_`);
-const ƈօʀȶǟռǟgoose = require("mongoose");
-const Kolor = require(`chalk`);
-`|⬡════════════════════════════════════════════|   (𝐜)𝐂𝐨𝐫𝐭𝐚𝐧𝐚𝐀𝐈  🛸  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛   |═══════════════════════════════════════════⬡|`;
-try {
-const ᴍɪᴢᴜᴋɪɢɪᴛ = require(`simple-git`)();
-await ᴍɪᴢᴜᴋɪɢɪᴛ.fetch();
-var ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ = await ᴍɪᴢᴜᴋɪɢɪᴛ.log([`KrakinzLab..origin/KrakinzLab`]);
-if (ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ.total != 0) {
-require(`simple-git`)()
-.exec(async () => {
-console.log(
-Kolor.yellow(
-`💡 𝐈𝐧𝐟𝐨⬰ Updating Cortana System With Latest Patch...`
-)
-);
-})
-.pull(async (Error, update) => {
-if (Error) {
-try {
-const mergeSummary = await ᴍɪᴢᴜᴋɪɢɪᴛ.merge();
-console.log(
-Kolor.green(
-`💡 𝐈𝐧𝐟𝐨⬰ Auto-Updating Finished. Total Changes: [${mergeSummary.merges.length}]`
-)
-);
-} catch (Error) {
-console.log(
-Kolor.red(
-`❌ 𝐄𝐫𝐫𝐨𝐫⬰ Merge Resulted with Total-Conflicts: ${Kolor.red(
-Error
-)} `
-)
-);
-}
-} else if (update && update.summary.changes) {
-var childs = require(`child_process`).exec(`python3 ᴄᴏʀᴛᴀɴᴀ.py`);
-childs.stdout.pipe(process.stdout);
-childs.on(`exit`, async function () {
-process.exitCode = 1;
-});
-}
-});
-}
-} catch (Error) {
-console.log(Error);
-console.log(Kolor.red(`❌ 𝐄𝐫𝐫𝐨𝐫⬰ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗱𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗯𝗼𝘁!`));
-}
-`|⬡════════════════════════════════════════════|   (𝐜)𝐂𝐨𝐫𝐭𝐚𝐧𝐚𝐀𝐈  🛸  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛   |═══════════════════════════════════════════⬡|`;
-console.log(Kolor.yellow(`💡 𝐈𝐧𝐟𝐨⬰ CortanaGoose Connecting!`));
-await ƈօʀȶǟռǟgoose
-.connect(_𝔏𝔞𝔟_.CORTANAMDB, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-useCreateIndex: true,
-useFindAndModify: false,
-})
-.catch((err) => {
-console.log(Kolor.red(`💡 𝐈𝐧𝐟𝐨⬰ CortanaGoose Error: \n${err}`));
-process.exit(0);
-});
-console.log(Kolor.green(`💡 𝐈𝐧𝐟𝐨⬰ CortanaGoose Connected!`));
-ӄʀǟӄɨռʐ.logger.level = "error";
-ƈօʀȶǟռǟ();
+  const { ƈօʀȶǟռǟ, ӄʀǟӄɨռʐ } = require(`../Cortana🛰️Server/🛸ƈօʀȶǟռǟ™`);
+  const _𝔏𝔞𝔟_ = require(`../Cortana🛰️Server/_𝔏𝔞𝔟_`);
+  const ƈօʀȶǟռǟgoose = require("mongoose");
+  const Kolor = require(`chalk`);
+  `|⬡════════════════════════════════════════════|   (𝐜)𝐂𝐨𝐫𝐭𝐚𝐧𝐚𝐀𝐈  🛸  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛   |═══════════════════════════════════════════⬡|`;
+  try {
+    const ᴍɪᴢᴜᴋɪɢɪᴛ = require(`simple-git`)();
+    await ᴍɪᴢᴜᴋɪɢɪᴛ.fetch();
+    var ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ = await ᴍɪᴢᴜᴋɪɢɪᴛ.log([`KrakinzLab..origin/KrakinzLab`]);
+    if (ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ.total != 0) {
+      require(`simple-git`)()
+        .exec(async () => {
+          console.log(
+            Kolor.yellow(
+              `💡 𝐈𝐧𝐟𝐨⬰ Updating Cortana System With Latest Patch...`
+            )
+          );
+        })
+        .pull(async (Error, update) => {
+          if (Error) {
+            try {
+              const mergeSummary = await ᴍɪᴢᴜᴋɪɢɪᴛ.merge();
+              console.log(
+                Kolor.green(
+                  `💡 𝐈𝐧𝐟𝐨⬰ Auto-Updating Finished. Total Changes: [${mergeSummary.merges.length}]`
+                )
+              );
+            } catch (Error) {
+              console.log(
+                Kolor.red(
+                  `❌ 𝐄𝐫𝐫𝐨𝐫⬰ Merge Resulted with Total-Conflicts: ${Kolor.red(
+                    Error
+                  )} `
+                )
+              );
+            }
+          } else if (update && update.summary.changes) {
+            var childs = require(`child_process`).exec(`python3 ᴄᴏʀᴛᴀɴᴀ.py`);
+            childs.stdout.pipe(process.stdout);
+            childs.on(`exit`, async function () {
+              process.exitCode = 1;
+            });
+          }
+        });
+    }
+  } catch (Error) {
+    console.log(Error);
+    console.log(Kolor.red(`❌ 𝐄𝐫𝐫𝐨𝐫⬰ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗱𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗯𝗼𝘁!`));
+  }
+  `|⬡════════════════════════════════════════════|   (𝐜)𝐂𝐨𝐫𝐭𝐚𝐧𝐚𝐀𝐈  🛸  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛   |═══════════════════════════════════════════⬡|`;
+  console.log(Kolor.yellow(`💡 𝐈𝐧𝐟𝐨⬰ CortanaGoose Connecting!`));
+  await ƈօʀȶǟռǟgoose
+    .connect(_𝔏𝔞𝔟_.CORTANAMDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .catch((err) => {
+      console.log(Kolor.red(`💡 𝐈𝐧𝐟𝐨⬰ CortanaGoose Error: \n${err}`));
+      process.exit(0);
+    });
+  console.log(Kolor.green(`💡 𝐈𝐧𝐟𝐨⬰ CortanaGoose Connected!`));
+  ӄʀǟӄɨռʐ.logger.level = "error";
+  ƈօʀȶǟռǟ().catch((Error) => {
+    console.log(`Error: ${Error}`);
+    const { spawn } = require("child_process");
+    const child = spawn("python3", ["ᴄᴏʀᴛᴀɴᴀ.py"], { shell: true });
+    child.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    child.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  });
 })();
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
 `|                                                                                                                        |`;
