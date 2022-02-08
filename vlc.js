0607ffdc983bb6f39f1f8fe6cf748ad3c4b811e7
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-02-08 05:26:15 -0800

    🛰️𝖈𝖔𝖗𝖑𝖊𝖝𝖆 • 𝕾𝖙𝖆𝖇𝖑𝖊 15🛰️

--- vlc.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-
-var FFmpegFile = `./ᴄᴏʀʟᴇxᴀ☕ꜱʜᴏᴘ/${Date.now()}.mp4`;
-
-(async () => {
-  exec(
-    `youtube-dl -o '${FFmpegFile}' -f 'best[filesize<50M]' 'https://youtu.be/8oVjbVmN_ZY'`
-  );
-})();
