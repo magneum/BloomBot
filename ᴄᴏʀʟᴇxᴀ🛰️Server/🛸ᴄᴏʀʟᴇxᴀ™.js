3d3b55637ac1e042c3509db2513b9becd4e65eb5
krakinzkon <krakinzkon@gmail.com>
2022-02-08 22:49:52 -0800

    🛰️𝖈𝖔𝖗𝖑𝖊𝖝𝖆 • 𝕾𝖙𝖆𝖇𝖑𝖊 15🛰️

--- ᴄᴏʀʟᴇxᴀ🛰️Server/🛸ᴄᴏʀʟᴇxᴀ™.js
+++ ᴄᴏʀʟᴇxᴀ🛰️Server/🛸ᴄᴏʀʟᴇxᴀ™.js
@@ -415,8 +415,8 @@ chat,
 ║🔖 _Do not call bot, if you do then you'll get blocked by bot._
 ║🔖 _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end._
 ║🔖 _Do not abuse/disrespect bot and its owner._
-║🔖 _If you see any bug/error in bot then report it to owner with the command !bug/report <problem>._
-║🔖 _If you want this bot in your group then contact owner by typing !owner/creator._
+║🔖 _If you see any bug/error in bot then report it to devs in the above group chats._
+║🔖 _If you want this bot in your group then contact devs._
 ║🔖 _Enjoy the bot and have fun._
 ╚════════════╝`
 );
