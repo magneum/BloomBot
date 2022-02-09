76c04b4b8d22737f118a019129a052e51fe5a7fb
krakinzkon <krakinzkon@gmail.com>
2022-02-08 22:48:51 -0800

    🛰️𝖈𝖔𝖗𝖑𝖊𝖝𝖆 • 𝕾𝖙𝖆𝖇𝖑𝖊 15🛰️

---
+++ ᴄᴏʀʟᴇxᴀ📡System/🍁Owner/delete.js
@@ -0,0 +1,42 @@
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+`|                                                                                                                        |`;
+`|        (𝐜)𝐂𝐨𝐫𝐥𝐞𝐱𝐚𝐀𝐈 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!            |`;
+`|                                                                                                                        |`;
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+const ᴄᴏʀʟᴇxᴀ_Buttons = require(`../../ᴄᴏʀʟᴇxᴀ🛰️Server/ᴄᴏʀʟᴇxᴀ_Buttons`);
+const ᴄᴏʀʟᴇxᴀ_Static = require(`../../ᴄᴏʀʟᴇxᴀ🛰️Server/ᴄᴏʀʟᴇxᴀ_Static`);
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const _𝔏𝔞𝔟_ = require(`../../ᴄᴏʀʟᴇxᴀ🛰️Server/_𝔏𝔞𝔟_`);
+const ꜰᴜᴄᴋ = require(`../../ᴄᴏʀʟᴇxᴀ🛰️Server/oShit`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const ms = require(`parse-ms`);
+const fs = require(`fs`);
+var path = require(`path`);
+var scriptName = path.basename(__filename);
+var newScpt = scriptName.slice(0, -3).toLowerCase();
+`|⬡════════════════════════════════════════════|   (𝐜)𝐂𝐨𝐫𝐥𝐞𝐱𝐚𝐀𝐈  🛸  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛   |═══════════════════════════════════════════⬡|`;
+module.exports = {
+name: newScpt,
+async handle(ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, chat, 𝖈𝖔𝖗𝖑𝖊𝖝𝖆, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+console.log(𝖈𝖔𝖗𝖑𝖊𝖝𝖆.chatId);
+console.log(chat.message.extendedTextMessage.contextInfo.stanzaId);
+console.log(chat.message.extendedTextMessage.contextInfo.quotedMessage);
+return;
+await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
+.deleteMessage(𝖈𝖔𝖗𝖑𝖊𝖝𝖆.chatId, {
+fromMe: true,
+id: chat.message.extendedTextMessage.contextInfo.stanzaId,
+remoteJid: 𝖈𝖔𝖗𝖑𝖊𝖝𝖆.chatId,
+})
+.catch((err) => {
+console.log(err.message);
+console.log("The message was not sent by a bot!");
+});
+},
+};
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
+`|                                                                                                                        |`;
+`|        (𝐜)𝐂𝐨𝐫𝐥𝐞𝐱𝐚𝐀𝐈 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!            |`;
+`|                                                                                                                        |`;
+`|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
--- ᴄᴏʀʟᴇxᴀ🛰️Server/🛸ᴄᴏʀʟᴇxᴀ™.js
+++ ᴄᴏʀʟᴇxᴀ🛰️Server/🛸ᴄᴏʀʟᴇxᴀ™.js
@@ -408,6 +408,16 @@ chat,
 ║💼 𝐆𝐫𝐨𝐮𝐩: _${_𝔏𝔞𝔟_.HASH}_
 ║💼 𝐆𝐫𝐨𝐮𝐩: _${_𝔏𝔞𝔟_.HASHS}_
 ║💼 𝐆𝐫𝐨𝐮𝐩: _${_𝔏𝔞𝔟_.HASHSH}_
+╚════════════╝
+
+╔◇║🏮 *Corlexa Rules*
+║
+║🔖 _Do not call bot, if you do then you'll get automatically blocked by bot._
+║🔖 _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end._
+║🔖 _Do not abuse/disrespect bot and its owner._
+║🔖 _If you see any bug/error in bot then report it to owner with the command !bug/report <problem>._
+║🔖 _If you want this bot in your group then contact owner by typing !owner/creator._
+║🔖 _Enjoy the bot and have fun._
 ╚════════════╝`
 );
 }
