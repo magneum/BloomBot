4da2dde8a2736bf24b158819e4579b51f1af3174
krakinzkon <krakinzkon@gmail.com>
2022-02-09 08:58:27 -0800

    🛰️𝖈𝖔𝖗𝖑𝖊𝖝𝖆 • 𝕾𝖙𝖆𝖇𝖑𝖊 16🛰️

---
+++ ᴄᴏʀʟᴇxᴀ🛰️Server/var.js
@@ -0,0 +1,27 @@
+const { YouTube_Video } = require(`./youmaker`);
+const Downloader = require(`nodejs-file-downloader`);
+const TinyURL = require("tinyurl");
+const fs = require(`fs`);
+
+YouTube_Video("https://youtu.be/ZceKV-l9d_k").then((res) => {
+const { dl_link, thumb, title, filesizeF, filesize } = res;
+TinyURL.shorten(dl_link).then(
+async function (DirectFile) {
+const downloader = await new Downloader({
+url: DirectFile,
+directory: `${__dirname}`,
+fileName: Date.now() + ".mp4",
+cloneFiles: false,
+});
+try {
+await downloader.download();
+} catch (Error) {
+console.log(`⬡═══════════════════| 🐞𝐄𝐫𝐫𝐨𝐫: ` + Error);
+}
+
+},
+async function (Error) {
+console.log(`⬡═══════════════════| 🐞𝐄𝐫𝐫𝐨𝐫: ` + Error);
+}
+);
+});
