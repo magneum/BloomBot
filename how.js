bdf015207b49cf09a4ba8fe0607b50dfb5a1ea27
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-02-10 00:47:53 -0800

    🛰️𝖈𝖔𝖗𝖑𝖊𝖝𝖆 • 𝕾𝖙𝖆𝖇𝖑𝖊 16🛰️

--- how.js
+++
@@ -1,9 +0,0 @@
-let { execSync } = require("child_process");
-
-try {
-let res = execSync("node ./ᴄᴏʀʟᴇxᴀ🛰️Server/🛸ᴄᴏʀʟᴇxᴀ™.js");
-console.log(res.toString());
-} catch (err) {
-console.log("output", err);
-console.log("sdterr", err.stderr.toString());
-}
--- ᴄᴏʀʟᴇxᴀ🛰️Server/ӄʀʏօȶɛӄ.js
+++ ᴄᴏʀʟᴇxᴀ🛰️Server/ӄʀʏօȶɛӄ.js
@@ -141,12 +141,18 @@ participants[i].isAdmin ? admins.push(participants[i].jid) : ``;
 return admins;
 }
 } catch (Error) {
-var childs = require(`child_process`).exec(`python3 🐙ᴋʀʏᴏᴛᴇᴋ.py`);
-childs.stdout.pipe(process.stdout);
-childs.on(`exit`, async function () {
-process.exitCode = 1;
-});
+console.log(Error.message);
+let { execSync } = require("child_process");
+let res = execSync("python3 🐙ᴋʀʏᴏᴛᴇᴋ.py");
+console.log(res.toString());
 }
+// } catch (Error) {
+// var childs = require(`child_process`).exec(`python3 🐙ᴋʀʏᴏᴛᴇᴋ.py`);
+// childs.stdout.pipe(process.stdout);
+// childs.on(`exit`, async function () {
+// process.exitCode = 1;
+// });
+// }
 };
 
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
