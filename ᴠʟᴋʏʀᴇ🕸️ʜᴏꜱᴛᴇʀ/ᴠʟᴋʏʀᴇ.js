d86d14335f7c985d8dc6b8dc46dcf8a81b4d3ae9
krakinzkon <krakinzkon@gmail.com>
2022-02-12 12:09:37 -0800

    〲𝐕𝐥𝐤𝐲𝐫𝐞🍂𝐒𝐭𝐚𝐛𝐥𝐞〴

--- ᴠʟᴋʏʀᴇ🕸️ʜᴏꜱᴛᴇʀ/ᴠʟᴋʏʀᴇ.js
+++ ᴠʟᴋʏʀᴇ🕸️ʜᴏꜱᴛᴇʀ/ᴠʟᴋʏʀᴇ.js
@@ -5,29 +5,25 @@
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
 (async () => {
 try {
-const 𝖗𝖊𝖖ᴠʟᴋʏʀᴇ𝖌𝖎𝖙 = require(`simple-git`)();
-await 𝖗𝖊𝖖ᴠʟᴋʏʀᴇ𝖌𝖎𝖙.fetch();
-var ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ = await 𝖗𝖊𝖖ᴠʟᴋʏʀᴇ𝖌𝖎𝖙.log([
-`KryTek..origin/KryTek`,
-]);
-if (ꜰᴇᴛᴄʜᴇᴅᴍɪᴢᴜᴋɪ.total != 0) {
+const VlkyreGit = require(`simple-git`)();
+await VlkyreGit.fetch();
+var VlkyreFetched = await VlkyreGit.log([`KryTek..origin/KryTek`]);
+if (VlkyreFetched.total != 0) {
 var rebase = require(`child_process`).exec(
 `git config pull.rebase false`
 );
 rebase.stdout.pipe(process.stdout);
 require(`simple-git`)()
-.exec(async () => {
-console.log(`💡 𝐈𝐧𝐟𝐨⬰ Updating Vlkyre System With Latest Patch...`);
-})
-.pull(async (Error, update) => {
-if (Error) {
+.exec(() =>
+console.log(`💡 𝐈𝐧𝐟𝐨⬰ Updating Vlkyre System With Latest Patch...`)
+)
+.pull((Error, update) => {
+if (Error)
 console.log(
 `❌ 𝐄𝐫𝐫𝐨𝐫⬰ Merge Resulted with Total-Conflicts: ` + Error
 );
-}
-if (update && update.summary.changes) {
+if (update && update.summary.changes)
 require("child_process").execSync("python3 🐙ᴋʀʏᴏᴛᴇᴋ.py");
-}
 });
 }
 } catch (Error) {
@@ -35,6 +31,11 @@ console.log(`⬡═══════════════════| 🐞�
 console.log(`❌ 𝐄𝐫𝐫𝐨𝐫⬰ ReDeployment Is Needed!`);
 require("child_process").execSync("python3 🐙ᴋʀʏᴏᴛᴇᴋ.py");
 }
+const ᴠʟᴋʏʀᴇ = require("./ᴠʟᴋʏʀᴇ™");
+await ᴠʟᴋʏʀᴇ().catch((Error) => {
+console.log(`⬡═══════════════════| 🐞𝐄𝐫𝐫𝐨𝐫: \n` + Error);
+require("child_process").execSync("python3 🐙ᴋʀʏᴏᴛᴇᴋ.py");
+});
 })();
 `|⬡════════════════════════════════════════════|                            |═══════════════════════════════════════════⬡|`;
 `|                                                                                                                        |`;
