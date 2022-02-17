8cc1931b84ad2059f56f78dc552cbc3873b55f80
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-02-17 01:16:45 -0800

    〲𝐕𝐥𝐤𝐲𝐫𝐞🍂𝐒𝐭𝐚𝐛𝐥𝐞〴

--- ☢.js
+++ ☢.js
@@ -8,18 +8,18 @@ const fs = require("fs");
 const path = require("path");
 const assert = require("assert");
 const { spawn } = require("child_process");
-let folders = [".", ...Object.keys(require("./package.json").directories)];
-let files = [];
-for (let folder of folders)
-for (let file of fs.readdirSync(folder).filter((v) => v.endsWith(".js")))
-files.push(path.resolve(path.join(folder, file)));
-for (let file of files) {
-if (file == path.join(__dirname, __filename)) continue;
-console.error("Verifying: ", file);
-spawn("node", ["-c", file])
-.on("exit", () => assert.ok(file) & console.log("Verified: ", file))
-.stderr.on("data", (chunk) => assert.fail(chunk.toString()));
-}
+// let folders = [".", ...Object.keys(require("./package.json").directories)];
+// let files = [];
+// for (let folder of folders)
+// for (let file of fs.readdirSync(folder).filter((v) => v.endsWith(".js")))
+// files.push(path.resolve(path.join(folder, file)));
+// for (let file of files) {
+// if (file == path.join(__dirname, __filename)) continue;
+// console.error("Verifying: ", file);
+// spawn("node", ["-c", file])
+// .on("exit", () => assert.ok(file) & console.log("Verified: ", file))
+// .stderr.on("data", (chunk) => assert.fail(chunk.toString()));
+// }
 `|⬡════════════════════════════════════════════|   (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞  🛸  ™𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛   |═══════════════════════════════════════════⬡|`;
 try {
 const ɢɪᴛ = require(`simple-git`)();
