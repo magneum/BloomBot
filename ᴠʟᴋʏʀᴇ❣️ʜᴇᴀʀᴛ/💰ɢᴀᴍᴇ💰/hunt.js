7e3bd7bdd5a599c2b97c958a75f82d3395a095f0
KryKnz <KryKnz@yandex.com>
2022-02-21 12:16:15 -0800

    ⚡❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞: 37.4 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞⚡

--- ᴠʟᴋʏʀᴇ❣️ʜᴇᴀʀᴛ/💰ɢᴀᴍᴇ💰/hunt.js
+++ ᴠʟᴋʏʀᴇ❣️ʜᴇᴀʀᴛ/💰ɢᴀᴍᴇ💰/hunt.js
@@ -206,16 +206,27 @@ await userHunt
 ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat)
 );
 `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
+exec(
+`ffmpeg -i '${
+PokemonData.sprites.animated
+}' -pix_fmt yuv420p -c:v libx264 -movflags +faststart -filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2' './ᴠʟᴋʏʀᴇ🀄ᴇxʜᴀᴜꜱᴛ/${ᴘɴᴀᴍᴇ.toString()}.mp4'`,
+async (error) => {
+if (error) {
+ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat);
+return;
+}
+`|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
 return await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
 .sendMessage(
 ᴠʟᴋʏʀᴇ.chatId,
-{ url: PokemonData.sprites.animated },
+fs.readFileSync(
+`./ᴠʟᴋʏʀᴇ🀄ᴇxʜᴀᴜꜱᴛ/${ᴘɴᴀᴍᴇ.toString()}.mp4`
+),
 MessageType.video,
 {
 mimetype: `video/gif`,
 quoted: chat,
-caption:
-`*×፝֟͜×𝗩𝗹𝗸𝘆𝗿𝗲 ☊${vers.vers}*\n_❝ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞_\n\n
+caption: `*×፝֟͜×𝗩𝗹𝗸𝘆𝗿𝗲 ☊${vers.vers}*\n_❝ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞_\n\n
 
 ❣️𝗠𝗲𝗻𝘁𝗶𝗼𝗻:  ꧁ *@${ᴘɴᴀᴍᴇ}* ꧂
 ╔◇══════════════◇╗
@@ -237,8 +248,15 @@ caption:
 contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ] },
 }
 )
+.then(
+cleanRF.cleanRF(
+`./ᴠʟᴋʏʀᴇ🀄ᴇxʜᴀᴜꜱᴛ/${ᴘɴᴀᴍᴇ.toString()}.mp4`
+)
+)
 .catch((error) =>
 ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat)
+);
+}
 );
 });
 }
