9044e4bdd595a31b2841a438251b4a98f3fcc740
KryKnz <KryKnz@yandex.com>
2022-02-21 07:31:04 -0800

    ⚡❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞: 37.4 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞⚡

--- afk.js
+++ afk.js
@@ -1,39 +1,39 @@
-let user = global.DATABASE.data.users[chat.sender];
-if (user.afk > -1) {
-  chat.reply(
-    `
-you stop AFK${user.afkReason ? " after " + user.afkReason : ""}
-During ${clockString(new Date() - user.afk)}
-`.trim()
-  );
-  user.afk = -1;
-  user.afkReason = "";
-}
-let jids = [
-  ...new Set([
-    ...(chat.mentionedJid || []),
-    ...(chat.quoted ? [chat.quoted.sender] : []),
-  ]),
-];
-for (let jid of jids) {
-  let user = global.DATABASE.data.users[jid];
-  if (!user) continue;
-  let afkTime = user.afk;
-  if (!afkTime || afkTime < 0) continue;
-  let reason = user.afkReason || "";
-  chat.reply(
-    `
-Don't tag him!
-he's AFK ${reason ? "with reason " + reason : "no reason"}
-During ${clockString(new Date() - afkTime)}
-`.trim()
-  );
-}
-return true;
+// let user = global.DATABASE.data.users[chat.sender];
+// if (user.afk > -1) {
+//   chat.reply(
+//     `
+// you stop AFK${user.afkReason ? " after " + user.afkReason : ""}
+// During ${clockString(new Date() - user.afk)}
+// `.trim()
+//   );
+//   user.afk = -1;
+//   user.afkReason = "";
+// }
+// let jids = [
+//   ...new Set([
+//     ...(chat.mentionedJid || []),
+//     ...(chat.quoted ? [chat.quoted.sender] : []),
+//   ]),
+// ];
+// for (let jid of jids) {
+//   let user = global.DATABASE.data.users[jid];
+//   if (!user) continue;
+//   let afkTime = user.afk;
+//   if (!afkTime || afkTime < 0) continue;
+//   let reason = user.afkReason || "";
+//   chat.reply(
+//     `
+// Don't tag him!
+// he's AFK ${reason ? "with reason " + reason : "no reason"}
+// During ${clockString(new Date() - afkTime)}
+// `.trim()
+//   );
+// }
+// return true;
 
-function clockString(ms) {
-  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000);
-  let chat = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60;
-  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60;
-  return [h, chat, s].map((v) => v.toString().padStart(2, 0)).join(":");
-}
+// function clockString(ms) {
+//   let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000);
+//   let chat = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60;
+//   let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60;
+//   return [h, chat, s].map((v) => v.toString().padStart(2, 0)).join(":");
+// }
