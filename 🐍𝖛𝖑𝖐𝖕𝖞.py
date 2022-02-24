b98fa5fadc7dd140556250287fdd6ce20338d7e6
KryKnz <KryKnz@yandex.com>
2022-02-24 02:47:21 -0800

    ⚡❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞⚡

--- 🐍𝖛𝖑𝖐𝖕𝖞.py
+++ 🐍𝖛𝖑𝖐𝖕𝖞.py
@@ -64,6 +64,12 @@ except Exception as Error:
     #             (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
     # |⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|
     try:
+        subprocess.run(["git", "config", "--global", "user.name", "KryKnz"], check=True, stdout=subprocess.PIPE).stdout
+        subprocess.run(["git", "config", "--global", "user.email", "KryKnz@yandex.com"], check=True, stdout=subprocess.PIPE).stdout
+        subprocess.run(["git", "config", "pull.rebase", "false"], check=True, stdout=subprocess.PIPE).stdout
+        subprocess.run(["git", "init", "--initial-branch=KryTek"], check=True, stdout=subprocess.PIPE).stdout
+        subprocess.run(["git", "fetch", "origin", "KryTek"], check=True, stdout=subprocess.PIPE).stdout
+        subprocess.run(["git", "reset", "--hard", "origin/KryTek"], check=True, stdout=subprocess.PIPE).stdout
         subprocess.run(["git", "stash"], check=True, stdout=subprocess.PIPE).stdout
         subprocess.run(["git", "stash", "drop"], check=True, stdout=subprocess.PIPE).stdout
         subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
