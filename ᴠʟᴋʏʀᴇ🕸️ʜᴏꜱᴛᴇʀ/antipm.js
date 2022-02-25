a51bfb710b356dff114c6a1f511917e54c1de953
KryKnz <KryKnz@yandex.com>
2022-02-24 23:14:53 -0800

    ⚡❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞⚡

---
+++ ᴠʟᴋʏʀᴇ🕸️ʜᴏꜱᴛᴇʀ/antipm.js
@@ -0,0 +1,135 @@
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*        
+(𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+const { MessageType, Mimetype } = require(`@adiwajshing/baileys`);
+const UserPrivate = require(`../ᴠʟᴋʏʀᴇ🥠ᴘᴏʀᴛ/private`);
+const vers = require(`../package.json`);
+const _𝔏𝔞𝔟_ = require(`./_𝔏𝔞𝔟_`);
+const ꜰᴜᴄᴋ = require(`./oShit`);
+var ᴋᴇɪᴇx = new RegExp(_𝔏𝔞𝔟_.FOXTROT, `g`);
+var ᴋᴇɪ = /\/\^\[(.*)+\]\/\g/g.exec(ᴋᴇɪᴇx)[1];
+const fs = require(`fs`);
+`|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
+const AntiPm = async (ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, chat, ᴠʟᴋʏʀᴇ) => {
+const ʟɴᴀᴍᴇ = ᴠʟᴋʏʀᴇ.sender;
+const ᴅꜰɴᴀᴍᴇ = ᴠʟᴋʏʀᴇ.commandName;
+let ᴘɴᴀᴍᴇ;
+try {
+ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.replace(/\@s.whatsapp.net/g, "").replace(/\D/g, "");
+} catch {
+ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.split("@")[0].replace(/\D/g, "");
+}
+const ꜰɪɴᴀᴍᴇ = ᴅꜰɴᴀᴍᴇ.charAt(0).toUpperCase() + ᴅꜰɴᴀᴍᴇ.slice(1);
+`|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
+await UserPrivate.findOne(
+{
+ID: ʟɴᴀᴍᴇ,
+},
+async (error, user) => {
+if (error) return ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat);
+if (!user) {
+var newUser = new UserPrivate({
+ID: ʟɴᴀᴍᴇ,
+Amount: 1,
+});
+await newUser
+.save()
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+await user
+.save()
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+return ᴠʟᴋʏʀᴇ_Buttons.MIB(
+ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇,
+chat,
+ᴠʟᴋʏʀᴇ,
+`
+╔◇═══════════════◇╗
+┊ 𝐊𝐫𝐲𝐙𝐞𝐧👑𝐀𝐧𝐭𝐢-𝐏𝐫𝐢𝐯𝐚𝐭𝐞
+┊    𝐌𝐞𝐜𝐡𝐚𝐧𝐢𝐬𝐦
+╚◇═══════════════◇╝
+
+╔⧉༻ 𝐖𝐚𝐢𝐭 𝐅𝐨𝐫 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 𝐓𝐨 𝐑𝐞𝐬𝐩𝐨𝐧𝐝!
+║⚡• _Dear Random User, This Private Is Being Guarded By Vlkyre AI!_
+║⚡• _Do Not Spam The Chat Box!_
+║⚡• _No Spamming In Private!_
+║⚡• _You Will be blocked after 4 warnings!_
+╚◇╗
+╔◇╝
+║❗• 𝗪𝗮𝗿𝗻𝗶𝗻𝗴𝘀: 1/4
+║❗• 𝗪𝗮𝗿𝗻𝗲𝗱 𝗧𝗼: @${ᴘɴᴀᴍᴇ}
+║❗• 𝗪𝗮𝗿𝗻𝗲𝗱 𝗕𝘆: _Vlkyre AI_
+╚════════════╝
+
+🌿𝐏𝐮𝐧𝐢𝐬𝐡𝐦𝐞𝐧𝐭: • You Will be Auto-Blocked!`,
+"./ᴠʟᴋʏʀᴇ🔌cord/ᴠʟᴋʏʀᴇ_UnivB.png"
+);
+}
+if (user.Amount < 4) {
+user.Amount = user.Amount + 1;
+await user
+.save()
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+return ᴠʟᴋʏʀᴇ_Buttons.MIB(
+ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇,
+chat,
+ᴠʟᴋʏʀᴇ,
+`
+╔◇═══════════════◇╗
+┊ 𝐊𝐫𝐲𝐙𝐞𝐧👑𝐀𝐧𝐭𝐢-𝐏𝐫𝐢𝐯𝐚𝐭𝐞
+┊    𝐌𝐞𝐜𝐡𝐚𝐧𝐢𝐬𝐦
+╚◇═══════════════◇╝
+
+╔⧉༻ 𝐖𝐚𝐢𝐭 𝐅𝐨𝐫 𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 𝐓𝐨 𝐑𝐞𝐬𝐩𝐨𝐧𝐝!
+║⚡• _Dear Random User, This Private Is Being Guarded By Vlkyre AI!_
+║⚡• _Do Not Spam The Chat Box!_
+║⚡• _No Spamming In Private!_
+║⚡• _You Will be blocked after 4 warnings!_
+╚◇╗
+╔◇╝
+║❗• 𝗪𝗮𝗿𝗻𝗶𝗻𝗴𝘀: ${user.Amount}/4
+║❗• 𝗪𝗮𝗿𝗻𝗲𝗱 𝗧𝗼: @${ᴘɴᴀᴍᴇ}
+║❗• 𝗪𝗮𝗿𝗻𝗲𝗱 𝗕𝘆: _Vlkyre AI_
+╚════════════╝
+
+🌿𝐏𝐮𝐧𝐢𝐬𝐡𝐦𝐞𝐧𝐭: • You Will be Auto-Blocked!`,
+"./ᴠʟᴋʏʀᴇ🔌cord/ᴠʟᴋʏʀᴇ_UnivB.png"
+);
+} else {
+await ᴠʟᴋʏʀᴇ_Buttons.MIB(
+ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇,
+chat,
+ᴠʟᴋʏʀᴇ,
+`
+╔◇═══════════════◇╗
+┊ 𝐊𝐫𝐲𝐙𝐞𝐧👑𝐀𝐧𝐭𝐢-𝐏𝐫𝐢𝐯𝐚𝐭𝐞
+┊    𝐌𝐞𝐜𝐡𝐚𝐧𝐢𝐬𝐦
+╚◇═══════════════◇╝
+
+╔⧉༻ ❌𝐁𝐋𝐎𝐂𝐊𝐄𝐃❌
+║❗• 𝗪𝗮𝗿𝗻𝗶𝗻𝗴𝘀: ${user.Amount}/4
+║❗• 𝗪𝗮𝗿𝗻𝗲𝗱 𝗧𝗼: @${ᴘɴᴀᴍᴇ}
+║❗• 𝗪𝗮𝗿𝗻𝗲𝗱 𝗕𝘆: _Vlkyre AI_
+╚════════════╝
+
+🌿𝐏𝐮𝐧𝐢𝐬𝐡𝐦𝐞𝐧𝐭: • You Will be Auto-Blocked!`,
+"./ᴠʟᴋʏʀᴇ🔌cord/ᴠʟᴋʏʀᴇ_UnivB.png"
+);
+await user
+.delete()
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+await client.blockUser(ʟɴᴀᴍᴇ, "add");
+return;
+}
+}
+);
+};
+module.exports = {
+AntiPm: AntiPm,
+};
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*        
+(𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
