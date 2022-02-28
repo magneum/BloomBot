ba215a8ee803a3500678b5365adf789295ce55cd
KryKnz <KryKnz@yandex.com>
2022-02-27 20:56:02 -0800

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : v56 ⚡ 𝐃𝐚𝐭𝐞: 2022-02-27 20:56:0 ❞

--- ᴠʟᴋʏʀᴇ🕸️ʜᴏꜱᴛᴇʀ/afkong.js
+++ ᴠʟᴋʏʀᴇ🕸️ʜᴏꜱᴛᴇʀ/afkong.js
@@ -4,41 +4,41 @@
 */
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 async function Filters() {
-  return {
-    bassboost: "bass=g=10,dynaudnorm=f=150",
-    vaporwave: "aresample=48000,asetrate=48000*0.8",
-    nightcore: "aresample=48000,asetrate=48000*1.25",
-    speed: "atempo=2",
-    superspeed: "atempo=3",
-    slow: "atempo=0.8",
-    superslow: "atempo=0.5",
-    phaser: "aphaser=in_gain=0.4",
-    subboost: "asubboost",
-    flanger: "flanger",
-    vibrato: "vibrato=f=6.5",
-    echo: "aecho=0.8:0.9:1000:0.3",
-    surround: "surround",
-    "8D": "apulsator=hz=0.08",
-  };
+return {
+bassboost: "bass=g=10,dynaudnorm=f=150",
+vaporwave: "aresample=48000,asetrate=48000*0.8",
+nightcore: "aresample=48000,asetrate=48000*1.25",
+speed: "atempo=2",
+superspeed: "atempo=3",
+slow: "atempo=0.8",
+superslow: "atempo=0.5",
+phaser: "aphaser=in_gain=0.4",
+subboost: "asubboost",
+flanger: "flanger",
+vibrato: "vibrato=f=6.5",
+echo: "aecho=0.8:0.9:1000:0.3",
+surround: "surround",
+"8D": "apulsator=hz=0.08",
+};
 }
 async function AllFilters(Queue) {
-  let EncodeFilters = [],
-    Encoder = [];
-  for (let Filter of Object.keys(Queue.Filters)) {
-    if (Queue.Filters[Filter]) {
-      EncodeFilters.push((await Filters())[Filter]);
-    }
-  }
-  if (EncodeFilters.length < 1) {
-    Encoder = [];
-  } else {
-    Encoder = ["-af", EncodeFilters.join(",")];
-  }
-  return Encoder;
+let EncodeFilters = [],
+Encoder = [];
+for (let Filter of Object.keys(Queue.Filters)) {
+if (Queue.Filters[Filter]) {
+EncodeFilters.push((await Filters())[Filter]);
+}
+}
+if (EncodeFilters.length < 1) {
+Encoder = [];
+} else {
+Encoder = ["-af", EncodeFilters.join(",")];
+}
+return Encoder;
 }
 module.exports = {
-  Filters,
-  AllFilters,
+Filters,
+AllFilters,
 };
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /*        
