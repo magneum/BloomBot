e5ed4210367587cabf316aaef463b8b5123812c7
KryKnz <KryKnz@yandex.com>
2022-02-28 08:40:03 -0800

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : vv60 🦋 𝐃𝐚𝐭𝐞: 2022-02-28 8:40:0 ❞

---
+++ ᴠʟᴋʏʀᴇ🕸️ʜᴏꜱᴛᴇʀ/ᴠʟᴋʏʀᴇƈʟǟ.js
@@ -0,0 +1,91 @@
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*        
+(𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+class ӄʀʏօȶɛӄ {
+constructor(
+mimeType,
+type,
+isReply,
+body,
+isCmd,
+commandName,
+chatId,
+fromMe,
+owner,
+Myself,
+isGroup,
+isPm,
+sender,
+groupName,
+groupMembers,
+groupAdmins,
+groupId,
+isBotGroupAdmin,
+isSenderGroupAdmin,
+isSenderDev,
+replyMessageId,
+replyMessage,
+replyParticipant,
+isImage,
+isReplyImage,
+imageCaption,
+isGIF,
+isReplyGIF,
+isSticker,
+isReplySticker,
+isReplyVideo,
+isReplyAudio,
+isVideo,
+isAudio,
+isReplyAnimatedSticker,
+isAntilink
+) {
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+this.mimeType = mimeType;
+this.type = type;
+this.body = body;
+this.chatId = chatId;
+this.fromMe = fromMe;
+this.owner = owner;
+this.sender = sender;
+this.isReply = isReply;
+this.replyMessageId = replyMessageId;
+this.replyParticipant = replyParticipant;
+this.replyMessage = replyMessage;
+this.isGroup = isGroup;
+this.isPm = isPm;
+this.isImage = isImage;
+this.isReplyImage = isReplyImage;
+this.imageCaption = imageCaption;
+this.isBotGroupAdmin = isBotGroupAdmin;
+this.isGIF = isGIF;
+this.isReplyGIF = isReplyGIF;
+this.isSticker = isSticker;
+this.isReplySticker = isReplySticker;
+this.isReplyAnimatedSticker = isReplyAnimatedSticker;
+this.isVideo = isVideo;
+this.isReplyVideo = isReplyVideo;
+this.isAudio = isAudio;
+this.isReplyAudio = isReplyAudio;
+this.isSenderGroupAdmin = isSenderGroupAdmin;
+this.isSenderDev = isSenderDev;
+this.isCmd = isCmd;
+this.commandName = commandName;
+this.Myself = Myself;
+this.groupName = groupName;
+this.groupMembers = groupMembers;
+this.groupAdmins = groupAdmins;
+this.groupId = groupId;
+this.isAntilink = isAntilink;
+}
+}
+module.exports = ӄʀʏօȶɛӄ;
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*        
+(𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
