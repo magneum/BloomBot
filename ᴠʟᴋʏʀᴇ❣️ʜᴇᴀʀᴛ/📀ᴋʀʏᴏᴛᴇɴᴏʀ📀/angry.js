c921278721c3c9bbb636c36cee09ab6c4e210bd8
KryKnz <KryKnz@yandex.com>
2022-02-27 20:52:03 -0800

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : v56 ⚡ 𝐃𝐚𝐭𝐞: 2022-02-27 20:52:0 ❞

--- ᴠʟᴋʏʀᴇ❣️ʜᴇᴀʀᴛ/📀ᴋʀʏᴏᴛᴇɴᴏʀ📀/angry.js
+++ ᴠʟᴋʏʀᴇ❣️ʜᴇᴀʀᴛ/📀ᴋʀʏᴏᴛᴇɴᴏʀ📀/angry.js
@@ -119,56 +119,60 @@ var newScpt = scriptName.slice(0, -3).toLowerCase();
 /*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 module.exports = {
-  name: newScpt,
-  async handle(ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, chat, ᴠʟᴋʏʀᴇ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
-    try {
-      const ʟɴᴀᴍᴇ = ᴠʟᴋʏʀᴇ.sender;
-      const ᴅꜰɴᴀᴍᴇ = ᴠʟᴋʏʀᴇ.commandName;
-      let ᴘɴᴀᴍᴇ;
-      try {
-        ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.replace(/\@s.whatsapp.net/g, "").replace(/\D/g, "");
-      } catch {
-        ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.split("@")[0].replace(/\D/g, "");
-      }
-      const ꜰɪɴᴀᴍᴇ = ᴅꜰɴᴀᴍᴇ.charAt(0).toUpperCase() + ᴅꜰɴᴀᴍᴇ.slice(1);
-      const AniLink = ᴠʟᴋʏʀᴇTenorMaker(ꜰɪɴᴀᴍᴇ.toLowerCase());
-      `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
-      const GotFile = `./${chat.key.id}${Date.now()}.gif`;
-      const GiveFile = `./${chat.key.id}${Date.now()}.mp4`;
-      const downloader = await new Downloader({
-        url: AniLink,
-        directory: `./`,
-        fileName: GotFile,
-        cloneFiles: false,
-      });
-      try {
-        await downloader.download();
-      } catch (error) {
-        console.log(`═════⬡|🐞𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐄𝐫𝐫𝐨𝐫: ` + error);
-        return ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat);
-      }
-      `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
-      if (Needs[0] && Needs[0].startsWith("@")) {
-        var TagPerson = Needs[0].replace(/[^0-9]/g, "");
-        var TagMention = TagPerson + "@s.whatsapp.net";
-        ffmpeg(GotFile)
-          .outputOptions([
-            `-pix_fmt yuv420p`,
-            `-c:v libx264`,
-            `-movflags +faststart`,
-            `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-          ])
-          .save(GiveFile)
-          .on(`end`, async () => {
-            await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
-              .sendMessage(
-                ᴠʟᴋʏʀᴇ.chatId,
-                fs.readFileSync(GiveFile),
-                MessageType.video,
-                {
-                  mimetype: `video/gif`,
-                  quoted: chat,
-                  caption: `
+name: newScpt,
+async handle(ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, chat, ᴠʟᴋʏʀᴇ, Needs, ꜱɪᴛʀᴀᴘ, Clock, Ping) {
+try {
+const ʟɴᴀᴍᴇ = ᴠʟᴋʏʀᴇ.sender;
+const ᴅꜰɴᴀᴍᴇ = ᴠʟᴋʏʀᴇ.commandName;
+let ᴘɴᴀᴍᴇ;
+try {
+ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.replace(/\@s.whatsapp.net/g, "").replace(/\D/g, "");
+} catch {
+ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.split("@")[0].replace(/\D/g, "");
+}
+const ꜰɪɴᴀᴍᴇ = ᴅꜰɴᴀᴍᴇ.charAt(0).toUpperCase() + ᴅꜰɴᴀᴍᴇ.slice(1);
+const AniLink = ᴠʟᴋʏʀᴇTenorMaker(ꜰɪɴᴀᴍᴇ.toLowerCase());
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+const GotFile = `./${chat.key.id}${Date.now()}.gif`;
+const GiveFile = `./${chat.key.id}${Date.now()}.mp4`;
+const downloader = await new Downloader({
+url: AniLink,
+directory: `./`,
+fileName: GotFile,
+cloneFiles: false,
+});
+try {
+await downloader.download();
+} catch (error) {
+console.log(`═════⬡|🐞𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐄𝐫𝐫𝐨𝐫: ` + error);
+return ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat);
+}
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+if (Needs[0] && Needs[0].startsWith("@")) {
+var TagPerson = Needs[0].replace(/[^0-9]/g, "");
+var TagMention = TagPerson + "@s.whatsapp.net";
+ffmpeg(GotFile)
+.outputOptions([
+`-pix_fmt yuv420p`,
+`-c:v libx264`,
+`-movflags +faststart`,
+`-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+])
+.save(GiveFile)
+.on(`end`, async () => {
+await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
+.sendMessage(
+ᴠʟᴋʏʀᴇ.chatId,
+fs.readFileSync(GiveFile),
+MessageType.video,
+{
+mimetype: `video/gif`,
+quoted: chat,
+caption: `
 ╔⧕🌿𝐓𝐨𝐩𝐢𝐜: _${ᴋᴇɪ}${ꜰɪɴᴀᴍᴇ}_
 ║
 ║🍕 𝐅𝐫𝐨𝐦: @${ᴘɴᴀᴍᴇ}
@@ -176,67 +180,71 @@ module.exports = {
 ╚════════════╝
 
 ⌜𝐕𝐥𝐤𝐲𝐫𝐞 ×፝֟͜×${vers.vers}⌟\nᴘʏᴛʜᴏɴ ☊ ᴊᴀᴠᴀꜱᴄʀɪᴘᴛ\nᴄᴏᴘʏʀɪɢʜᴛ Ⓒ ᴋʀᴀᴋɪɴᴢʟᴀʙ™`,
-                  contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ, TagMention] },
-                }
-              )
-              .catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
-            return await cleanRF.cleanRF(GiveFile, GotFile);
-          });
-        `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
-      } else if (ᴠʟᴋʏʀᴇ.isReply) {
-        var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
-        var քɛʀֆօռɢօȶռʊʍ = Receiver.substring(0, Receiver.length - 15);
-        if (Receiver === ʟɴᴀᴍᴇ) {
-          ffmpeg(GotFile)
-            .outputOptions([
-              `-pix_fmt yuv420p`,
-              `-c:v libx264`,
-              `-movflags +faststart`,
-              `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-            ])
-            .save(GiveFile)
-            .on(`end`, async () => {
-              await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
-                .sendMessage(
-                  ᴠʟᴋʏʀᴇ.chatId,
-                  fs.readFileSync(GiveFile),
-                  MessageType.video,
-                  {
-                    mimetype: `video/gif`,
-                    quoted: chat,
-                    caption: `
+contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ, TagMention] },
+}
+)
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+return await cleanRF.cleanRF(GiveFile, GotFile);
+});
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+} else if (ᴠʟᴋʏʀᴇ.isReply) {
+var Receiver = chat.message.extendedTextMessage.contextInfo.participant;
+var քɛʀֆօռɢօȶռʊʍ = Receiver.substring(0, Receiver.length - 15);
+if (Receiver === ʟɴᴀᴍᴇ) {
+ffmpeg(GotFile)
+.outputOptions([
+`-pix_fmt yuv420p`,
+`-c:v libx264`,
+`-movflags +faststart`,
+`-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+])
+.save(GiveFile)
+.on(`end`, async () => {
+await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
+.sendMessage(
+ᴠʟᴋʏʀᴇ.chatId,
+fs.readFileSync(GiveFile),
+MessageType.video,
+{
+mimetype: `video/gif`,
+quoted: chat,
+caption: `
 ╔⧕🌿𝐓𝐨𝐩𝐢𝐜: _${ᴋᴇɪ}${ꜰɪɴᴀᴍᴇ}_
 ║
 ║🍕 𝐅𝐨𝐫: @${ᴘɴᴀᴍᴇ}
 ╚════════════╝
 
 ⌜𝐕𝐥𝐤𝐲𝐫𝐞 ×፝֟͜×${vers.vers}⌟\nᴘʏᴛʜᴏɴ ☊ ᴊᴀᴠᴀꜱᴄʀɪᴘᴛ\nᴄᴏᴘʏʀɪɢʜᴛ Ⓒ ᴋʀᴀᴋɪɴᴢʟᴀʙ™`,
-                    contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ] },
-                  }
-                )
-                .catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
-              return await cleanRF.cleanRF(GiveFile, GotFile);
-            });
-          `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
-        } else {
-          ffmpeg(GotFile)
-            .outputOptions([
-              `-pix_fmt yuv420p`,
-              `-c:v libx264`,
-              `-movflags +faststart`,
-              `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-            ])
-            .save(GiveFile)
-            .on(`end`, async () => {
-              await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
-                .sendMessage(
-                  ᴠʟᴋʏʀᴇ.chatId,
-                  fs.readFileSync(GiveFile),
-                  MessageType.video,
-                  {
-                    mimetype: `video/gif`,
-                    quoted: chat,
-                    caption: `
+contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ] },
+}
+)
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+return await cleanRF.cleanRF(GiveFile, GotFile);
+});
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+} else {
+ffmpeg(GotFile)
+.outputOptions([
+`-pix_fmt yuv420p`,
+`-c:v libx264`,
+`-movflags +faststart`,
+`-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+])
+.save(GiveFile)
+.on(`end`, async () => {
+await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
+.sendMessage(
+ᴠʟᴋʏʀᴇ.chatId,
+fs.readFileSync(GiveFile),
+MessageType.video,
+{
+mimetype: `video/gif`,
+quoted: chat,
+caption: `
 ╔⧕🌿𝐓𝐨𝐩𝐢𝐜: _${ᴋᴇɪ}${ꜰɪɴᴀᴍᴇ}_
 ║
 ║🍕 𝐅𝐫𝐨𝐦: @${ᴘɴᴀᴍᴇ}
@@ -244,52 +252,56 @@ module.exports = {
 ╚════════════╝
 
 ⌜𝐕𝐥𝐤𝐲𝐫𝐞 ×፝֟͜×${vers.vers}⌟\nᴘʏᴛʜᴏɴ ☊ ᴊᴀᴠᴀꜱᴄʀɪᴘᴛ\nᴄᴏᴘʏʀɪɢʜᴛ Ⓒ ᴋʀᴀᴋɪɴᴢʟᴀʙ™`,
-                    contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ, Receiver] },
-                  }
-                )
-                .catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
-              return await cleanRF.cleanRF(GiveFile, GotFile);
-            });
-        }
-        `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
-      } else {
-        ffmpeg(GotFile)
-          .outputOptions([
-            `-pix_fmt yuv420p`,
-            `-c:v libx264`,
-            `-movflags +faststart`,
-            `-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
-          ])
-          .save(GiveFile)
-          .on(`end`, async () => {
-            await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
-              .sendMessage(
-                ᴠʟᴋʏʀᴇ.chatId,
-                fs.readFileSync(GiveFile),
-                MessageType.video,
-                {
-                  mimetype: `video/gif`,
-                  quoted: chat,
-                  caption: `
+contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ, Receiver] },
+}
+)
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+return await cleanRF.cleanRF(GiveFile, GotFile);
+});
+}
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+} else {
+ffmpeg(GotFile)
+.outputOptions([
+`-pix_fmt yuv420p`,
+`-c:v libx264`,
+`-movflags +faststart`,
+`-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'`,
+])
+.save(GiveFile)
+.on(`end`, async () => {
+await ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇
+.sendMessage(
+ᴠʟᴋʏʀᴇ.chatId,
+fs.readFileSync(GiveFile),
+MessageType.video,
+{
+mimetype: `video/gif`,
+quoted: chat,
+caption: `
 ╔⧕🌿𝐓𝐨𝐩𝐢𝐜: _${ᴋᴇɪ}${ꜰɪɴᴀᴍᴇ}_
 ║
 ║🍕 𝐅𝐨𝐫: @${ᴘɴᴀᴍᴇ}
 ╚════════════╝
 
 ⌜𝐕𝐥𝐤𝐲𝐫𝐞 ×፝֟͜×${vers.vers}⌟\nᴘʏᴛʜᴏɴ ☊ ᴊᴀᴠᴀꜱᴄʀɪᴘᴛ\nᴄᴏᴘʏʀɪɢʜᴛ Ⓒ ᴋʀᴀᴋɪɴᴢʟᴀʙ™`,
-                  contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ] },
-                }
-              )
-              .catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
-            return await cleanRF.cleanRF(GiveFile, GotFile);
-          });
-      }
-      `|⬡════════════════════════════════════════════|⌜ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ⌬ ❝ ᴘᴏᴡᴇʀᴇᴅ ☊ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞ ⌟|═══════════════════════════════════════════⬡|`;
-    } catch (error) {
-      ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat);
-      return;
-    }
-  },
+contextInfo: { mentionedJid: [ʟɴᴀᴍᴇ] },
+}
+)
+.catch((error) => ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat));
+return await cleanRF.cleanRF(GiveFile, GotFile);
+});
+}
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/*              (𝐜)𝐕𝐥𝐤𝐲𝐫𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+} catch (error) {
+ꜰᴜᴄᴋ.catch(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, chat);
+return;
+}
+},
 };
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /*        
