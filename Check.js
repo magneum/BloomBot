5d77ba5ac595e1d3088a451f2c2d527f6cdaea73
KryKnz <KryKnz@yandex.com>
2022-05-07 20:45:12 +0530

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : 𝐍𝐢𝐠𝐡𝐭𝐥𝐲 🦋 𝐃𝐚𝐭𝐞: 2022-05-07 20:45:11 ❞

--- Check.js
+++
@@ -1,4 +0,0 @@
-const paragraph = "https://chat.whatsapp.com/LKN8uVBd8ucHRHofz0jBSd";
-const regex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-const found = paragraph.match(regex);
-console.log(found);
