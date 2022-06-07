b95eb93fe2aa24fa5250b7daddc24ce6e246d1fb
kryknzkon <KryKnz@yandex.com>
2022-06-07 19:43:01 +0530

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : 𝐩𝐫𝐞-𝐚𝐥𝐩𝐡𝐚 🦋 𝐃𝐚𝐭𝐞: 2022-06-07 19:43:0 ❞

--- 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞🀄𝐕𝐞𝐧𝐭/config.ts
+++ 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞🀄𝐕𝐞𝐧𝐭/config.ts
@@ -1,71 +1,81 @@
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/* 
+(𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 import fs from "fs";
 import { Sequelize } from "sequelize";
 
 if (fs.existsSync("νℓкуяιє.env")) {
-  require("dotenv").config({ path: "./νℓкуяιє.env" });
+require("dotenv").config({ path: "./νℓкуяιє.env" });
 } else {
-  require("dotenv");
+require("dotenv");
 }
 
 const convertToLogLevel = (value: string) => {
-  var log: any = false;
-  if (typeof value === "string") {
-    if (value.toLowerCase() === "true") {
-      log = console.log;
-    }
-  }
-  return log;
+var log: any = false;
+if (typeof value === "string") {
+if (value.toLowerCase() === "true") {
+log = console.log;
+}
+}
+return log;
 };
 
 process.env.DATABASE_URL =
-  process.env.DATABASE_URL === undefined
-    ? "./νℓкуяιє.db"
-    : process.env.DATABASE_URL;
+process.env.DATABASE_URL === undefined
+? "./νℓкуяιє.db"
+: process.env.DATABASE_URL;
 process.env.DEBUG =
-  process.env.DEBUG === undefined ? "false" : process.env.DEBUG;
+process.env.DEBUG === undefined ? "false" : process.env.DEBUG;
 
 const config = {
-  VMango: process.env.VMango,
-  SPOTIFY_ID: process.env.SPOTIFY_ID,
-  SPOTIFY_CLIENT: process.env.SPOTIFY_CLIENT,
-  MuveOn: process.env.PREFIX === undefined ? "/" : process.env.PREFIX,
-  STRING_SESSION:
-    process.env.STRING_SESSION === undefined ? "" : process.env.STRING_SESSION,
-  HEROKU: process.env.HEROKU === undefined ? false : true,
-  PREFIX: process.env.PREFIX === undefined ? "^[/]" : process.env.PREFIX,
-  COUNTRY_CODE:
-    process.env.COUNTRY_CODE === undefined ? "91" : process.env.COUNTRY_CODE,
-  OCR_API_KEY:
-    process.env.OCR_API_KEY === undefined
-      ? "9ffb44def388957"
-      : process.env.OCR_API_KEY,
-  WEATHER_API_KEY:
-    process.env.CURRENT_WEATHER_API_KEY === undefined
-      ? "6729ac2b2e2bb5c686ff427a2f06df92"
-      : process.env.CURRENT_WEATHER_API_KEY,
-  DATABASE_URL:
-    process.env.DATABASE_URL === undefined
-      ? "./νℓкуяιє.db"
-      : process.env.DATABASE_URL,
-  DEBUG: process.env.DEBUG === undefined ? false : process.env.DEBUG,
-  DATABASE:
-    process.env.DATABASE_URL === "./νℓкуяιє.db"
-      ? new Sequelize({
-          dialect: "sqlite",
-          storage: process.env.DATABASE_URL,
-          logging: convertToLogLevel(process.env.DEBUG),
-        })
-      : new Sequelize(process.env.DATABASE_URL, {
-          dialect: "postgres",
-          protocol: "postgres",
-          logging: convertToLogLevel(process.env.DEBUG),
-          dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-        }),
-  SUDO: process.env.SUDO === undefined ? "" : process.env.SUDO,
-  OFFLINE_RESPONSE:
-    process.env.OFFLINE_RESPONSE === undefined
-      ? true
-      : process.env.OFFLINE_RESPONSE,
+VMango: process.env.VMango,
+SPOTIFY_ID: process.env.SPOTIFY_ID,
+SPOTIFY_CLIENT: process.env.SPOTIFY_CLIENT,
+MuveOn: process.env.PREFIX === undefined ? "/" : process.env.PREFIX,
+STRING_SESSION:
+process.env.STRING_SESSION === undefined ? "" : process.env.STRING_SESSION,
+HEROKU: process.env.HEROKU === undefined ? false : true,
+PREFIX: process.env.PREFIX === undefined ? "^[/]" : process.env.PREFIX,
+COUNTRY_CODE:
+process.env.COUNTRY_CODE === undefined ? "91" : process.env.COUNTRY_CODE,
+OCR_API_KEY:
+process.env.OCR_API_KEY === undefined
+? "9ffb44def388957"
+: process.env.OCR_API_KEY,
+WEATHER_API_KEY:
+process.env.CURRENT_WEATHER_API_KEY === undefined
+? "6729ac2b2e2bb5c686ff427a2f06df92"
+: process.env.CURRENT_WEATHER_API_KEY,
+DATABASE_URL:
+process.env.DATABASE_URL === undefined
+? "./νℓкуяιє.db"
+: process.env.DATABASE_URL,
+DEBUG: process.env.DEBUG === undefined ? false : process.env.DEBUG,
+DATABASE:
+process.env.DATABASE_URL === "./νℓкуяιє.db"
+? new Sequelize({
+dialect: "sqlite",
+storage: process.env.DATABASE_URL,
+logging: convertToLogLevel(process.env.DEBUG),
+})
+: new Sequelize(process.env.DATABASE_URL, {
+dialect: "postgres",
+protocol: "postgres",
+logging: convertToLogLevel(process.env.DEBUG),
+dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+}),
+SUDO: process.env.SUDO === undefined ? "" : process.env.SUDO,
+OFFLINE_RESPONSE:
+process.env.OFFLINE_RESPONSE === undefined
+? true
+: process.env.OFFLINE_RESPONSE,
 };
 
 export default config;
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/* 
+(𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
