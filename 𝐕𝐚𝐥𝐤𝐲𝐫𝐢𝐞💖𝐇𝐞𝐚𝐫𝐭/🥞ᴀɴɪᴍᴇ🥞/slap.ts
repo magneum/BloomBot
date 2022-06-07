a5d3f3869bc43ed6c756014b6bc5d3c3443fbf43
kryknzkon <KryKnz@yandex.com>
2022-06-07 19:54:41 +0530

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : 𝐩𝐫𝐞-𝐚𝐥𝐩𝐡𝐚 🦋 𝐃𝐚𝐭𝐞: 2022-06-07 19:54:40 ❞

--- 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞💖𝐇𝐞𝐚𝐫𝐭/🥞ᴀɴɪᴍᴇ🥞/slap.ts
+++ 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞💖𝐇𝐞𝐚𝐫𝐭/🥞ᴀɴɪᴍᴇ🥞/slap.ts
@@ -10,7 +10,7 @@ import { MessageType } from "../../𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞⚜️𝐊�
 const Downloader = require(`nodejs-file-downloader`);
 const Oops = require(`../../𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞☘️𝐊𝐞𝐲𝐬/Oops`);
 import { proto } from "@adiwajshing/baileys";
-const VOID = require(`../../𝐊𝐫𝐲𝐨⚜️𝐓𝐞𝐤/void`);
+const VOID = require(`../../𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞⚜️𝐊𝐫𝐲𝐨𝐓𝐞𝐤/void`);
 import Client from "../../𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞⚜️𝐊𝐫𝐲𝐨𝐓𝐞𝐤/client";
 import νℓкуяιє from "../../𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞⚜️𝐊𝐫𝐲𝐨𝐓𝐞𝐤/msb";
 const ffmpeg = require(`fluent-ffmpeg`);
