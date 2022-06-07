b66cc24f4edcce32cfb7ab6a40d41685f2705904
kryknzkon <KryKnz@yandex.com>
2022-06-07 19:42:54 +0530

    ❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ : 𝐩𝐫𝐞-𝐚𝐥𝐩𝐡𝐚 🦋 𝐃𝐚𝐭𝐞: 2022-06-07 19:42:50 ❞

--- 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞⚜️𝐊𝐫𝐲𝐨𝐓𝐞𝐤/msb.ts
+++ 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞⚜️𝐊𝐫𝐲𝐨𝐓𝐞𝐤/msb.ts
@@ -1,41 +1,49 @@
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/* 
+(𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 import { GroupParticipant } from "@adiwajshing/baileys";
-
 class νℓкуяιє {
-  mimeType: string;
-  type: string;
-  body: string;
-  commandName: string;
-  chatId: string;
-  owner: string;
-  logGroup: string;
-  sender: string;
-  groupName: string;
-  groupMembers: GroupParticipant[];
-  groupAdmins: string[];
-  groupId: string;
-  replyMessageId: string;
-  replyMessage: string;
-  imageCaption: string;
-  replyParticipant: string;
-  isTextReply: boolean;
-  isCmd: boolean;
-  fromMe: boolean;
-  isGroup: boolean;
-  isPm: boolean;
-  isBotGroupAdmin: boolean;
-  isSenderGroupAdmin: boolean;
-  isSenderSUDO: boolean;
-  isImage: boolean;
-  isReplyImage: boolean;
-  isGIF: boolean;
-  isReplyGIF: boolean;
-  isSticker: boolean;
-  isReplySticker: boolean;
-  isReplyVideo: boolean;
-  isReplyAudio: boolean;
-  isVideo: boolean;
-  isAudio: boolean;
-  isReplyAnimatedSticker: boolean;
+mimeType: string;
+type: string;
+body: string;
+commandName: string;
+chatId: string;
+owner: string;
+logGroup: string;
+sender: string;
+groupName: string;
+groupMembers: GroupParticipant[];
+groupAdmins: string[];
+groupId: string;
+replyMessageId: string;
+replyMessage: string;
+imageCaption: string;
+replyParticipant: string;
+isTextReply: boolean;
+isCmd: boolean;
+fromMe: boolean;
+isGroup: boolean;
+isPm: boolean;
+isBotGroupAdmin: boolean;
+isSenderGroupAdmin: boolean;
+isSenderSUDO: boolean;
+isImage: boolean;
+isReplyImage: boolean;
+isGIF: boolean;
+isReplyGIF: boolean;
+isSticker: boolean;
+isReplySticker: boolean;
+isReplyVideo: boolean;
+isReplyAudio: boolean;
+isVideo: boolean;
+isAudio: boolean;
+isReplyAnimatedSticker: boolean;
 }
-
 export = νℓкуяιє;
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
+/* 
+(𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+*/
+`|⬡════════════════════════════════════════════|❝ Ⓒ𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
