caa28a6062c3f9ae478adf68056ea031daeecf5a
kryknzkon <KryKnz@yandex.com>
2022-06-13 22:41:12 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🦋 𝐃𝐚𝐭𝐞: 2022-06-13 22:41:10 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱💖𝐇𝐞𝐚𝐫𝐭/🔎ꜱᴇᴀʀᴄʜ🔎/ud.ts
+++ 𝐀𝐫𝐜𝐭𝐢𝐱💖𝐇𝐞𝐚𝐫𝐭/🔎ꜱᴇᴀʀᴄʜ🔎/ud.ts
@@ -83,7 +83,7 @@ AʀƈȶɨӼ,
 📚Defination: ${result.definition}
 🫒Example: ${result.example}
 👍: ${result.thumbs_up} |👎: ${result.thumbs_down}`,
-"./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ.png"
+"./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_Cream.png"
 );
 } catch (error) {
 return Oops.VOp(TUF, Fox, AʀƈȶɨӼ, error);
