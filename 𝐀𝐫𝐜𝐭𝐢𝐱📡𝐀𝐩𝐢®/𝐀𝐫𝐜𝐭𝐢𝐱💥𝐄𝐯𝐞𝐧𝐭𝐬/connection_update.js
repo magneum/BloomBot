0f140e92731d81a7f3caa9c9a6b245893d96c184
kryknzkon <KryKnz@yandex.com>
2022-06-13 11:59:02 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🦋 𝐃𝐚𝐭𝐞: 2022-06-13 11:59:0 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱📡𝐀𝐩𝐢®/𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/connection_update.js
+++ 𝐀𝐫𝐜𝐭𝐢𝐱📡𝐀𝐩𝐢®/𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/connection_update.js
@@ -1,15 +1,12 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+return new (P || (P = Promise))(function (resolve, reject) {
+function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+step((generator = generator.apply(thisArg, _arguments || [])).next());
+});
 };
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /*
@@ -27,30 +24,29 @@ const BanGroup = require(`../𝐀𝐫𝐜𝐭𝐢𝐱✈️𝐂𝐨𝐧𝐧𝐞�
 const Bagde = require(`../𝐀𝐫𝐜𝐭𝐢𝐱✈️𝐂𝐨𝐧𝐧𝐞𝐜𝐭/🍃mongo/badge`);
 const Halt = require(`../𝐀𝐫𝐜𝐭𝐢𝐱✈️𝐂𝐨𝐧𝐧𝐞𝐜𝐭/🍃mongo/halt`);
 const chalkAnimation = require("chalkercli");
-const A_____1 = __importDefault(require("../A\u0280\u0188\u0236\u0268\u04FC"));
 //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
 const connection_update = (update, DisconnectReason, startSock) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b;
-    const { connection, lastDisconnect } = update;
-    if (connection === "close") {
-        if (((_b = (_a = lastDisconnect.error) === null || _a === void 0 ? void 0 : _a.output) === null || _b === void 0 ? void 0 : _b.statusCode) !==
-            DisconnectReason.loggedOut) {
-            startSock();
-        }
-        else {
-            console.log("❌𝐄𝐫𝐫𝐨𝐫꧂ Connection closed. You are logged out.");
-            process.exit(0);
-        }
-    }
-    else if (connection === "connecting") {
-        chalkAnimation.rainbow("💡𝐈𝐧𝐟𝐨꧂  Connecting to WhatsApp...");
-    }
-    else if (connection === "open") {
-        A_____1.default.VAʀƈ();
-    }
-    else {
-        console.log("connection update", update);
-    }
+var _a, _b;
+const { connection, lastDisconnect } = update;
+if (connection === "close") {
+if (((_b = (_a = lastDisconnect.error) === null || _a === void 0 ? void 0 : _a.output) === null || _b === void 0 ? void 0 : _b.statusCode) !==
+DisconnectReason.loggedOut) {
+startSock();
+}
+else {
+console.log("❌𝐄𝐫𝐫𝐨𝐫꧂ Connection closed. You are logged out.");
+process.exit(0);
+}
+}
+else if (connection === "connecting") {
+chalkAnimation.rainbow("💡𝐈𝐧𝐟𝐨꧂  Connecting to WhatsApp...");
+}
+else if (connection === "open") {
+require("child_process").exec("node AʀƈȶɨӼ.js");
+}
+else {
+console.log("connection update", update);
+}
 });
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /*
@@ -58,5 +54,5 @@ const connection_update = (update, DisconnectReason, startSock) => __awaiter(voi
 */
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 module.exports = {
-    connection_update: connection_update,
+connection_update: connection_update,
 };
