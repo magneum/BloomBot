9fd4ea2cb3626e1ceec23d03d41cefa234667587
kryknzkon <KryKnz@yandex.com>
2022-06-13 14:14:51 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🦋 𝐃𝐚𝐭𝐞: 2022-06-13 14:14:50 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱☘️𝐊𝐞𝐲𝐬/FoxNeeded.ts
+++ 𝐀𝐫𝐜𝐭𝐢𝐱☘️𝐊𝐞𝐲𝐬/FoxNeeded.ts
@@ -6,52 +6,52 @@
 import { MessageType } from "../𝐀𝐫𝐜𝐭𝐢𝐱⚜️𝐊𝐫𝐲𝐨𝐓𝐞𝐤/message-type";
 import ʟᴀʏᴏᴜᴛ from "../𝐀𝐫𝐜𝐭𝐢𝐱🀄𝐕𝐞𝐧𝐭/ʟᴀʏᴏᴜᴛ";
 const FoxNeeded = async (
-TUF: any,
-Fox: any,
-AʀƈȶɨӼ: any,
-caption: any,
-dotScrpt: any
+  TUF: any,
+  Fox: any,
+  AʀƈȶɨӼ: any,
+  caption: any,
+  dotScrpt: any
 ): Promise<void> => {
-try {
-await TUF.sendMessage(
-AʀƈȶɨӼ.chatId,
-{
-quoted: Fox,
-timestamp: Date(),
-contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
-text: `
+  try {
+    await TUF.sendMessage(
+      AʀƈȶɨӼ.chatId,
+      {
+        quoted: Fox,
+        timestamp: Date(),
+        contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
+        text: `
 ┌✼  𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢®
-│ *ᴛᴀɢ:* @${AʀƈȶɨӼ.sender.replace(/[^\d+]/g,"")}
+│ *ᴛᴀɢ:* @${AʀƈȶɨӼ.sender.replace(/[^\d+]/g, "")}
 │ *ɴᴀᴍᴇ:* ${Fox.pushName}
-│ *ᴄᴏᴍᴍᴀɴᴅ:* {AʀƈȶɨӼ.
+│ *ᴄᴏᴍᴍᴀɴᴅ:* ${AʀƈȶɨӼ.commandName}
 └✼
 
 ❌𝐄𝐫𝐫𝐨𝐫: Argument Needed To Run This Command.`,
-footer: `⦓ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ⦔ `,
-title: `🔸𝐂𝐨𝐦𝐦𝐚𝐧𝐝: _${ʟᴀʏᴏᴜᴛ.MuveOn}${dotScrpt}_`,
-buttonText: "❝ How To Use? ❞",
-sections: [
-{
-title: "𝐀𝐑𝐆𝐔𝐌𝐄𝐍𝐓 𝐍𝐄𝐄𝐃𝐄𝐃!",
-rows: [
-{
-title: "⚡𝐔𝐬𝐚𝐠𝐞",
-rowId: "argument required...",
-description: `💡𝐈𝐧𝐟𝐨: You must follow below instructions:
+        footer: `⦓ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ⦔ `,
+        title: `🔸𝐂𝐨𝐦𝐦𝐚𝐧𝐝: _${ʟᴀʏᴏᴜᴛ.MuveOn}${dotScrpt}_`,
+        buttonText: "❝ How To Use? ❞",
+        sections: [
+          {
+            title: "𝐀𝐑𝐆𝐔𝐌𝐄𝐍𝐓 𝐍𝐄𝐄𝐃𝐄𝐃!",
+            rows: [
+              {
+                title: "⚡𝐔𝐬𝐚𝐠𝐞",
+                rowId: "argument required...",
+                description: `💡𝐈𝐧𝐟𝐨: You must follow below instructions:
 ${caption}`,
-},
-],
-},
-],
-},
-MessageType.buttonsMessage
-);
-} catch (error) {
-console.log(error);
-}
+              },
+            ],
+          },
+        ],
+      },
+      MessageType.buttonsMessage
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 export = {
-VFox: FoxNeeded,
+  VFox: FoxNeeded,
 };
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /*        
