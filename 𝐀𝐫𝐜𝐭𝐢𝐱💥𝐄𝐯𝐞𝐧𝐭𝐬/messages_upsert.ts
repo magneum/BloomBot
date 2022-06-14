604ce184461fce87430c16cd31cd0d5ecf2a97da
KryKnz <KryKnz@yandex.com>
2022-06-14 23:16:21 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 📡 𝐃𝐚𝐭𝐞: 2022-06-14 23:16:20 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/messages_upsert.ts
+++ 𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/messages_upsert.ts
@@ -56,6 +56,92 @@ await Silent.VSln(TUF, Fox, AʀƈȶɨӼ, ӄʀʏȶɛӄ);
 await Rankr.VRnIT(TUF, Fox, AʀƈȶɨӼ, ӄʀʏȶɛӄ);
 await Link_Proctectron.VFLink(TUF, Fox, AʀƈȶɨӼ, ӄʀʏȶɛӄ);
 //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+try {
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    const MetaFor_A = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata("120363024871653603@g.us");
+    let Member_A = [];
+    for (var i = 0; i < MetaFor_A.participants.length; i++) {
+    Member_A[i] = MetaFor_A.participants[i].id;
+    }
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    const MetaFor_B = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata("120363041425736157@g.us");
+    let Member_B = [];
+    for (var i = 0; i < MetaFor_B.participants.length; i++) {
+    Member_B[i] = MetaFor_B.participants[i].id;
+    }
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    const MetaFor_C = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata("120363022161584857@g.us");
+    let Member_C = [];
+    for (var i = 0; i < MetaFor_C.participants.length; i++) {
+    Member_C[i] = MetaFor_C.participants[i].id;
+    }
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    const MetaFor_D = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata("120363020792949649@g.us");
+    let Member_D = [];
+    for (var i = 0; i < MetaFor_D.participants.length; i++) {
+    Member_D[i] = MetaFor_D.participants[i].id;
+    }
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    if (
+    AʀƈȶɨӼ.isCmd &&
+    !AʀƈȶɨӼ.isSenderTUFs &&
+    !AʀƈȶɨӼ.body.includes(ʟᴀʏᴏᴜᴛ.MuveOn + "help")
+    ) {
+    if (
+    !Member_A.includes(AʀƈȶɨӼ.sender) &&
+    !Member_B.includes(AʀƈȶɨӼ.sender) &&
+    !Member_C.includes(AʀƈȶɨӼ.sender) &&
+    !Member_D.includes(AʀƈȶɨӼ.sender)
+    ) {
+    return Image_Button.VImg(
+    TUF,
+    Fox,
+    AʀƈȶɨӼ,
+    `
+    ╔◇══════════════◇╗
+    ┊  𝐕𝐞𝐫𝐢𝐟𝐢𝐜𝐚𝐭𝐢𝐨𝐧
+    ╚◇══════════════◇╝
+    
+    𝐉𝐨𝐢𝐧 𝐓𝐨 𝐁𝐞 𝐕𝐞𝐫𝐢𝐟𝐢𝐞𝐝:
+    *𝗚roup⛩️1:* chat.whatsapp.com/LKN8uVBd8ucHRHofz0jBSd
+    *𝗚roup⛩️2:* chat.whatsapp.com/HKO5WLEZxMe3xWCyiv6vBu
+    *𝗚roup⛩️3:* chat.whatsapp.com/EIn3YFShFmkJ3fvAqf2rpn
+    *𝗚roup⛩️4:* chat.whatsapp.com/Bc1jBF7OVMRKnyTWYyxz1z
+    *𝗚roup⛩️5:* chat.whatsapp.com/G1NzKSYjAST95ehHk4FOZK`,
+    "./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_Verity.png"
+    );
+    }
+    }
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    // const MetaFor_E = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata("120363042762307739@g.us");
+    // let Member_E = [];
+    // for (var i = 0; i < MetaFor_E.participants.length; i++) {
+    // Member_E[i] = MetaFor_E.participants[i].id;
+    // }
+    // await TUF.sendMessage(
+    // AʀƈȶɨӼ.chatId,
+    // Member_E.toString(),
+    // MessageType.text
+    // );
+    // //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    // const MetaFor_F = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata("120363041425736157@g.us");
+    // let Member_F = [];
+    // for (var i = 0; i < MetaFor_F.participants.length; i++) {
+    // Member_F[i] = MetaFor_F.participants[i].id;
+    // }
+    // await TUF.sendMessage(
+    // AʀƈȶɨӼ.chatId,
+    // Member_F.toString(),
+    // MessageType.text
+    // );
+    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+    } catch (error) {
+    console.log(error);
+    }
+
+
+
+
 if (!command) {
 await TUF.sendMessage(
 AʀƈȶɨӼ.chatId,
