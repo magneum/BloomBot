dacabbbe8cef6f57d1e97a49f24bd9d011368423
KryKnz <KryKnz@yandex.com>
2022-06-15 22:13:22 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 📡 𝐃𝐚𝐭𝐞: 2022-06-15 22:13:20 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱📡𝐏𝐫𝐨𝐭𝐨®/𝐀𝐫𝐜𝐭𝐢𝐱💖𝐇𝐞𝐚𝐫𝐭/🍁𝐎𝐰𝐧𝐞𝐫/support.js
+++ 𝐀𝐫𝐜𝐭𝐢𝐱📡𝐏𝐫𝐨𝐭𝐨®/𝐀𝐫𝐜𝐭𝐢𝐱💖𝐇𝐞𝐚𝐫𝐭/🍁𝐎𝐰𝐧𝐞𝐫/support.js
@@ -16,8 +16,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 (𝐜)𝐀𝐫𝐜𝐭𝐢𝐱 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
 */
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
-const message_type_1 = require("../../\uD835\uDC00\uD835\uDC2B\uD835\uDC1C\uD835\uDC2D\uD835\uDC22\uD835\uDC31\u269C\uFE0F\uD835\uDC0A\uD835\uDC2B\uD835\uDC32\uD835\uDC28\uD835\uDC13\uD835\uDC1E\uD835\uDC24/message-type");
+const Image_Button_1 = __importDefault(require("../../\uD835\uDC00\uD835\uDC2B\uD835\uDC1C\uD835\uDC2D\uD835\uDC22\uD835\uDC31\u2618\uFE0F\uD835\uDC0A\uD835\uDC1E\uD835\uDC32\uD835\uDC2C/Image_Button"));
 const Oops = require(`../../𝐀𝐫𝐜𝐭𝐢𝐱☘️𝐊𝐞𝐲𝐬/Oops`);
+const ______1 = __importDefault(require("../../\uD835\uDC00\uD835\uDC2B\uD835\uDC1C\uD835\uDC2D\uD835\uDC22\uD835\uDC31\uD83C\uDC04\uD835\uDC15\uD835\uDC1E\uD835\uDC27\uD835\uDC2D/\u029F\u1D00\u028F\u1D0F\u1D1C\u1D1B"));
 const path_1 = __importDefault(require("path"));
 var scriptName = path_1.default.basename(__filename);
 var dotScrpt = scriptName.slice(0, -3).toLowerCase();
@@ -34,14 +35,8 @@ module.exports = {
             const ʟɴᴀᴍᴇ = AʀƈȶɨӼ.sender;
             const ᴅꜰɴᴀᴍᴇ = AʀƈȶɨӼ.commandName;
             const ᴘɴᴀᴍᴇ = ʟɴᴀᴍᴇ.replace(/[^\d+]/g, "");
-            const ꜰɪɴᴀᴍᴇ = ᴅꜰɴᴀᴍᴇ.charAt(0).toUpperCase() + ᴅꜰɴᴀᴍᴇ.slice(1);
-            //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
             yield TUF.getGroupMetaData(AʀƈȶɨӼ.chatId, AʀƈȶɨӼ);
-            console.log("💡Is Group: " + AʀƈȶɨӼ.isGroup);
-            console.log("💡Is Sender Group Admin: " + AʀƈȶɨӼ.isSenderGroupAdmin);
-            console.log("💡Is Bot Group Admin: " + AʀƈȶɨӼ.isBotGroupAdmin);
-            console.log("💡Is Reply Message: " + AʀƈȶɨӼ.replyMessage);
-            console.log("💡Is Text Reply: " + AʀƈȶɨӼ.isTextReply);
+            const ꜰɪɴᴀᴍᴇ = ᴅꜰɴᴀᴍᴇ.charAt(0).toUpperCase() + ᴅꜰɴᴀᴍᴇ.slice(1);
             //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
             if (AʀƈȶɨӼ.isTextReply) {
                 var Receiver = Fox.message.extendedTextMessage.contextInfo.participant;
@@ -54,28 +49,27 @@ module.exports = {
                 }
             }
             //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+            let 𝐏𝐩𝐢𝐜 = yield ӄ𝖗𝖞ӄ𝖓𝖟.profilePictureUrl(AʀƈȶɨӼ.sender, "image");
             yield ӄ𝖗𝖞ӄ𝖓𝖟.sendPresenceUpdate("unavailable", AʀƈȶɨӼ.chatId);
             yield ӄ𝖗𝖞ӄ𝖓𝖟.sendPresenceUpdate("available", AʀƈȶɨӼ.chatId);
             yield ӄ𝖗𝖞ӄ𝖓𝖟.sendPresenceUpdate("composing", AʀƈȶɨӼ.chatId);
             yield ӄ𝖗𝖞ӄ𝖓𝖟.sendPresenceUpdate("recording", AʀƈȶɨӼ.chatId);
             yield ӄ𝖗𝖞ӄ𝖓𝖟.sendPresenceUpdate("paused", AʀƈȶɨӼ.chatId);
             //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-            return yield TUF.sendMessage(AʀƈȶɨӼ.chatId, `┌─「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」
-├•♩ *Tαɠ:* @${AʀƈȶɨӼ.sender.replace(/[^\d+]/g, "")}
-├•♭ *Nαɱҽ:* ${Fox.pushName}
-├•♪ *Cσɱɱαɳԃ:* ${AʀƈȶɨӼ.commandName}
-└──•
+            return Image_Button_1.default.VImg(TUF, Fox, AʀƈȶɨӼ, `
+┌─「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」
+├• *ɴᴀᴍᴇ:* ${Fox.pushName}
+├• *ᴛᴀɢ:* @${AʀƈȶɨӼ.sender.replace(/[^\d+]/g, "")}
+├• *ᴄᴏᴍᴍᴀɴᴅ:* ${______1.default.MuveOn}${AʀƈȶɨӼ.commandName}
+└────────────◇
 
-*𝗚roup⛩️A:* chat.whatsapp.com/EIn3YFShFmkJ3fvAqf2rpn
-*𝗚roup⛩️B:* chat.whatsapp.com/EsxcMlXOy1DCJP2eGtZBH5
-*𝗚roup⛩️C:* chat.whatsapp.com/Bc1jBF7OVMRKnyTWYyxz1z
-*𝗚roup⛩️D:* chat.whatsapp.com/HKO5WLEZxMe3xWCyiv6vBu
-*𝗚roup⛩️E:* chat.whatsapp.com/G1NzKSYjAST95ehHk4FOZK
-*𝗚roup⛩️F:* chat.whatsapp.com/LKN8uVBd8ucHRHofz0jBSd`, message_type_1.MessageType.text, {
-                quoted: Fox,
-                timestamp: Date(),
-                contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
-            });
+*👑 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐀𝐫𝐜𝐭𝐢𝐱 𝐂𝐨𝐦𝐦𝐮𝐧𝐢𝐭𝐢𝐞𝐬 👑*
+*𝗚roup⛩️A:* _chat.whatsapp.com/EIn3YFShFmkJ3fvAqf2rpn_
+*𝗚roup⛩️B:* _chat.whatsapp.com/EsxcMlXOy1DCJP2eGtZBH5_
+*𝗚roup⛩️C:* _chat.whatsapp.com/Bc1jBF7OVMRKnyTWYyxz1z_
+*𝗚roup⛩️D:* _chat.whatsapp.com/HKO5WLEZxMe3xWCyiv6vBu_
+*𝗚roup⛩️E:* _chat.whatsapp.com/G1NzKSYjAST95ehHk4FOZK_
+*𝗚roup⛩️F:* _chat.whatsapp.com/LKN8uVBd8ucHRHofz0jBSd_`, 𝐏𝐩𝐢𝐜);
         });
     },
 };
