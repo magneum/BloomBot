90572a0e7e04218de6ad1f61419703251ebf760d
KryKnz <KryKnz@yandex.com>
2022-06-15 21:20:31 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 📡 𝐃𝐚𝐭𝐞: 2022-06-15 21:20:30 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/messages_upsert.ts
+++ 𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/messages_upsert.ts
@@ -34,246 +34,220 @@ import Kolor from "chalk";
 import * as fs from "fs";
 //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
 const messages_upsert = async (
-  update: any,
-  ӄ𝖗𝖞ӄ𝖓𝖟: any,
-  commandHandler: any,
-  Konn: any
+update: any,
+ӄ𝖗𝖞ӄ𝖓𝖟: any,
+commandHandler: any,
+Konn: any
 ): Promise<void> => {
-  if (update.type === "append" && !ʟᴀʏᴏᴜᴛ.OFFLINE_RESPONSE) {
-    return;
-  }
-  if (update.type !== "notify") {
-    return;
-  }
-  let Fox: proto.IWebMessageInfo = update.messages[0];
-  let AʀƈȶɨӼ: AʀƈȶɨӼ = await resolve(Fox, ӄ𝖗𝖞ӄ𝖓𝖟);
-  let TUF: Client = new Client(ӄ𝖗𝖞ӄ𝖓𝖟);
-  //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-  if (AʀƈȶɨӼ.isCmd) {
-    try {
-      await Kooldown.findOne(
-        {
-          ID: AʀƈȶɨӼ.sender,
-        },
-        async (error: any, userHalt: any) => {
-          if (error) {
-            console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error);
-            return;
-          }
-          if (!userHalt) {
-            var newHalt = new Kooldown({
-              ID: AʀƈȶɨӼ.sender,
-              TimeOut: 0,
-              PermaTimeOut: 8000,
-            });
-            await newHalt
-              .save()
-              .catch((error: any) => console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error));
-          }
-        }
-      );
-    } catch (err) {
-      console.log(Kolor.red("❌𝐄𝐫𝐫𝐨𝐫: ", err));
-    }
-    //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-    await Kooldown.findOne(
-      {
-        ID: AʀƈȶɨӼ.sender,
-      },
-      async (error: any, userHalt: any) => {
-        if (error) {
-          console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error);
-          return;
-        }
-        if (!userHalt) {
-          var newHalt = new Kooldown({
-            ID: AʀƈȶɨӼ.sender,
-            TimeOut: Date.now(),
-            PermaTimeOut: 5000,
-          });
-          return await newHalt
-            .save()
-            .catch((error: any) => console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error));
-        }
-        if (
-          !AʀƈȶɨӼ.fromMe &&
-          userHalt.PermaTimeOut - (Date.now() - userHalt.TimeOut) > 0
-        ) {
-          return await TUF.sendMessage(
-            AʀƈȶɨӼ.chatId,
-            `┌─「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」
+if (update.type === "append" && !ʟᴀʏᴏᴜᴛ.OFFLINE_RESPONSE) {
+return;
+}
+if (update.type !== "notify") {
+return;
+}
+let Fox: proto.IWebMessageInfo = update.messages[0];
+let AʀƈȶɨӼ: AʀƈȶɨӼ = await resolve(Fox, ӄ𝖗𝖞ӄ𝖓𝖟);
+let TUF: Client = new Client(ӄ𝖗𝖞ӄ𝖓𝖟);
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+if (AʀƈȶɨӼ.isCmd) {
+await Kooldown.findOne(
+{
+ID: AʀƈȶɨӼ.sender,
+},
+async (error: any, userHalt: any) => {
+if (error) {
+console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error);
+return;
+}
+if (!userHalt) {
+var newHalt = new Kooldown({
+ID: AʀƈȶɨӼ.sender,
+TimeOut: Date.now(),
+PermaTimeOut: 5000,
+});
+return await newHalt
+.save()
+.catch((error: any) => console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error));
+}
+if (
+!AʀƈȶɨӼ.fromMe &&
+userHalt.PermaTimeOut - (Date.now() - userHalt.TimeOut) > 0
+) {
+return await TUF.sendMessage(
+AʀƈȶɨӼ.chatId,
+`┌─「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」
 ├• *Tαɠ:* @${AʀƈȶɨӼ.sender.replace(/[^\d+]/g, "")}
 ├• *Nαɱҽ:* ${Fox.pushName}
 ├• *Cσɱɱαɳԃ:* ${AʀƈȶɨӼ.commandName}
 └──•
 
 ❌𝗘𝗿𝗿𝗼𝗿: Wait For 5seconds!`,
-            MessageType.text,
-            {
-              quoted: Fox,
-              timestamp: Date(),
-              contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
-            }
-          );
-          //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-        } else {
-          console.clear();
-          console.log(
-            Kolor.cyanBright(
-              "|════════════════════════「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」════════════════════════|"
-            )
-          );
-          console.log(Fox);
-          await git.fetch();
-          console.log(AʀƈȶɨӼ);
-          console.log(
-            Kolor.cyanBright(
-              "|════════════════════════「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」════════════════════════|"
-            )
-          );
-          var newCommits = await git.log(["📡𝐏𝐫𝐨𝐭𝐨®..origin/📡𝐏𝐫𝐨𝐭𝐨®"]);
-          if (newCommits.total != 0) {
-            try {
-              let list_chats_user = Konn.chats.all().map((v) => v.id);
-              for (let Jid of list_chats_user) {
-                await TUF.sendMessage(
-                  Jid,
-                  {
-                    quoted: Fox,
-                    timestamp: Date(),
-                    contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
-                    image: { url: "./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_LGrey.png" },
-                    caption: `「 Broadcast 」
+MessageType.text,
+{
+quoted: Fox,
+timestamp: Date(),
+contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
+}
+);
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+} else {
+console.clear();
+console.log(
+Kolor.cyanBright(
+"|════════════════════════「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」════════════════════════|"
+)
+);
+console.log(Fox);
+await git.fetch();
+console.log(AʀƈȶɨӼ);
+console.log(
+Kolor.cyanBright(
+"|════════════════════════「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢® 」════════════════════════|"
+)
+);
+var newCommits = await git.log(["📡𝐏𝐫𝐨𝐭𝐨®..origin/📡𝐏𝐫𝐨𝐭𝐨®"]);
+if (newCommits.total != 0) {
+try {
+let list_chats_user = Konn.chats.all().map((v) => v.id);
+for (let Jid of list_chats_user) {
+await TUF.sendMessage(
+Jid,
+{
+quoted: Fox,
+timestamp: Date(),
+contextInfo: { mentionedJid: [AʀƈȶɨӼ.sender] },
+image: { url: "./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_LGrey.png" },
+caption: `「 Broadcast 」
 
 ╔◇══════════════◇╗
 ┊  🤖𝐑𝐞𝐛𝐨𝐨𝐭𝐢𝐧𝐠🤖
 ╚◇══════════════◇╝
 Please Wait For ARCTIX to reboot itself and all of it's dedicated components & servers.
 Will take 40secs maximum.`,
-                    footer: `⦓ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ⦔`,
-                    buttons: [
-                      {
-                        buttonId: `${ʟᴀʏᴏᴜᴛ.MuveOn}help`,
-                        buttonText: { displayText: `${ʟᴀʏᴏᴜᴛ.MuveOn}help` },
-                        type: 1,
-                      },
-                      {
-                        buttonId: `${ʟᴀʏᴏᴜᴛ.MuveOn}support`,
-                        buttonText: { displayText: `${ʟᴀʏᴏᴜᴛ.MuveOn}support` },
-                        type: 1,
-                      },
-                    ],
-                    headerType: 4,
-                  },
-                  MessageType.buttonsMessage
-                );
-              }
-            } catch (error) {
-              console.log(Kolor.red(error));
-            }
-            //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-            try {
-              var 𝖛𝖑𝖐𝖕 = require("child_process").exec(
-                "git config --global user.name 'KryKnz' && git config --global user.email 'KryKnz@yandex.com' && git config pull.rebase false && git init --initial-branch=📡𝐏𝐫𝐨𝐭𝐨® && git fetch origin 📡𝐏𝐫𝐨𝐭𝐨® && git reset --hard origin/📡𝐏𝐫𝐨𝐭𝐨® && git stash && git stash drop && git pull"
-              );
-              console.log("🛰️ 𝐓𝐡𝐫𝐞𝐚𝐝 𝐈𝐃:", Kolor.green(𝖛𝖑𝖐𝖕.pid));
-              𝖛𝖑𝖐𝖕.stderr.pipe(process.stderr);
-              𝖛𝖑𝖐𝖕.stdout.pipe(process.stdout);
-              𝖛𝖑𝖐𝖕.on("exit", function (code: any, signal: any) {
-                if (code)
-                  console.log("📟 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐂𝐨𝐝𝐞:", Kolor.red(code));
-                if (signal)
-                  console.log(
-                    "📶 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐒𝐢𝐠𝐧𝐚𝐥:",
-                    Kolor.blue(signal)
-                  );
-                process.exitCode = 1;
-              });
-              var h𝖛𝖑𝖐𝖕 = require("child_process").exec(
-                `heroku ps:restart worker -a ${ʟᴀʏᴏᴜᴛ.Heroku_App}`
-              );
-              console.log("🛰️ 𝐓𝐡𝐫𝐞𝐚𝐝 𝐈𝐃:", Kolor.green(h𝖛𝖑𝖐𝖕.pid));
-              h𝖛𝖑𝖐𝖕.stderr.pipe(process.stderr);
-              h𝖛𝖑𝖐𝖕.on("exit", function (code: any, signal: any) {
-                if (code)
-                  console.log("📟 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐂𝐨𝐝𝐞:", Kolor.red(code));
-                if (signal)
-                  console.log(
-                    "📶 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐒𝐢𝐠𝐧𝐚𝐥:",
-                    Kolor.blue(signal)
-                  );
-                process.exitCode = 1;
-              });
-            } catch (error) {
-              console.log(Kolor.red(error));
-            }
-          }
-          //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-          const ARC = commandHandler.get(AʀƈȶɨӼ.commandName);
-          console.log(
-            Kolor.blueBright(`💡𝐈𝐧𝐟𝐨: ${AʀƈȶɨӼ.commandName} ARC executed.`)
-          );
-          var ǟʀɢʊʍɛռȶ = AʀƈȶɨӼ.body.trim().split(/\s+/).slice(1);
-          await Silent.VSln(TUF, Fox, AʀƈȶɨӼ, ӄ𝖗𝖞ӄ𝖓𝖟);
-          await Rankr.VRnIT(TUF, Fox, AʀƈȶɨӼ, ӄ𝖗𝖞ӄ𝖓𝖟);
-          await Link_Proctectron.VFLink(TUF, Fox, AʀƈȶɨӼ, ӄ𝖗𝖞ӄ𝖓𝖟);
-          //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-          try {
-            let MEMBER_A = [];
-            let MEMBER_B = [];
-            let MEMBER_C = [];
-            let MEMBER_D = [];
-            let MEMBER_E = [];
-            let MEMBER_F = [];
-            const MetaFor_A = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
-              "120363024871653603@g.us"
-            );
-            const MetaFor_B = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
-              "120363041425736157@g.us"
-            );
-            const MetaFor_C = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
-              "120363022161584857@g.us"
-            );
-            const MetaFor_D = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
-              "120363020792949649@g.us"
-            );
-            const MetaFor_E = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
-              "120363042762307739@g.us"
-            );
-            const MetaFor_F = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
-              "120363039223842047@g.us"
-            );
-            for (var i = 0; i < MetaFor_A.participants.length; i++)
-              MEMBER_A[i] = MetaFor_A.participants[i].id;
-            for (var i = 0; i < MetaFor_B.participants.length; i++)
-              MEMBER_B[i] = MetaFor_B.participants[i].id;
-            for (var i = 0; i < MetaFor_C.participants.length; i++)
-              MEMBER_C[i] = MetaFor_C.participants[i].id;
-            for (var i = 0; i < MetaFor_D.participants.length; i++)
-              MEMBER_D[i] = MetaFor_D.participants[i].id;
-            for (var i = 0; i < MetaFor_E.participants.length; i++)
-              MEMBER_E[i] = MetaFor_E.participants[i].id;
-            for (var i = 0; i < MetaFor_F.participants.length; i++)
-              MEMBER_F[i] = MetaFor_F.participants[i].id;
-            // //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-            if (
-              AʀƈȶɨӼ.isCmd &&
-              !AʀƈȶɨӼ.isSenderTUFs &&
-              !AʀƈȶɨӼ.body.includes(ʟᴀʏᴏᴜᴛ.MuveOn + "help")
-            ) {
-              if (
-                !MEMBER_A.includes(AʀƈȶɨӼ.sender) &&
-                !MEMBER_B.includes(AʀƈȶɨӼ.sender) &&
-                !MEMBER_C.includes(AʀƈȶɨӼ.sender) &&
-                !MEMBER_D.includes(AʀƈȶɨӼ.sender) &&
-                !MEMBER_E.includes(AʀƈȶɨӼ.sender) &&
-                !MEMBER_F.includes(AʀƈȶɨӼ.sender)
-              ) {
-                return Image_Button.VImg(
-                  TUF,
-                  Fox,
-                  AʀƈȶɨӼ,
-                  `
+footer: `⦓ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ⦔`,
+buttons: [
+{
+buttonId: `${ʟᴀʏᴏᴜᴛ.MuveOn}help`,
+buttonText: { displayText: `${ʟᴀʏᴏᴜᴛ.MuveOn}help` },
+type: 1,
+},
+{
+buttonId: `${ʟᴀʏᴏᴜᴛ.MuveOn}support`,
+buttonText: { displayText: `${ʟᴀʏᴏᴜᴛ.MuveOn}support` },
+type: 1,
+},
+],
+headerType: 4,
+},
+MessageType.buttonsMessage
+);
+}
+} catch (error) {
+console.log(Kolor.red(error));
+}
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+try {
+var 𝖛𝖑𝖐𝖕 = require("child_process").exec(
+"git config --global user.name 'KryKnz' && git config --global user.email 'KryKnz@yandex.com' && git config pull.rebase false && git init --initial-branch=📡𝐏𝐫𝐨𝐭𝐨® && git fetch origin 📡𝐏𝐫𝐨𝐭𝐨® && git reset --hard origin/📡𝐏𝐫𝐨𝐭𝐨® && git stash && git stash drop && git pull"
+);
+console.log("🛰️ 𝐓𝐡𝐫𝐞𝐚𝐝 𝐈𝐃:", Kolor.green(𝖛𝖑𝖐𝖕.pid));
+𝖛𝖑𝖐𝖕.stderr.pipe(process.stderr);
+𝖛𝖑𝖐𝖕.stdout.pipe(process.stdout);
+𝖛𝖑𝖐𝖕.on("exit", function (code: any, signal: any) {
+if (code)
+console.log("📟 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐂𝐨𝐝𝐞:", Kolor.red(code));
+if (signal)
+console.log(
+"📶 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐒𝐢𝐠𝐧𝐚𝐥:",
+Kolor.blue(signal)
+);
+process.exitCode = 1;
+});
+var h𝖛𝖑𝖐𝖕 = require("child_process").exec(
+`heroku ps:restart worker -a ${ʟᴀʏᴏᴜᴛ.Heroku_App}`
+);
+console.log("🛰️ 𝐓𝐡𝐫𝐞𝐚𝐝 𝐈𝐃:", Kolor.green(h𝖛𝖑𝖐𝖕.pid));
+h𝖛𝖑𝖐𝖕.stderr.pipe(process.stderr);
+h𝖛𝖑𝖐𝖕.on("exit", function (code: any, signal: any) {
+if (code)
+console.log("📟 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐂𝐨𝐝𝐞:", Kolor.red(code));
+if (signal)
+console.log(
+"📶 𝐕𝐥𝐤𝐲𝐫𝐞 𝐄𝐱𝐢𝐭𝐞𝐝 𝐖𝐢𝐭𝐡 𝐒𝐢𝐠𝐧𝐚𝐥:",
+Kolor.blue(signal)
+);
+process.exitCode = 1;
+});
+} catch (error) {
+console.log(Kolor.red(error));
+}
+}
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+const ARC = commandHandler.get(AʀƈȶɨӼ.commandName);
+console.log(
+Kolor.blueBright(`💡𝐈𝐧𝐟𝐨: ${AʀƈȶɨӼ.commandName} ARC executed.`)
+);
+var ǟʀɢʊʍɛռȶ = AʀƈȶɨӼ.body.trim().split(/\s+/).slice(1);
+await Silent.VSln(TUF, Fox, AʀƈȶɨӼ, ӄ𝖗𝖞ӄ𝖓𝖟);
+await Rankr.VRnIT(TUF, Fox, AʀƈȶɨӼ, ӄ𝖗𝖞ӄ𝖓𝖟);
+await Link_Proctectron.VFLink(TUF, Fox, AʀƈȶɨӼ, ӄ𝖗𝖞ӄ𝖓𝖟);
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+try {
+let MEMBER_A = [];
+let MEMBER_B = [];
+let MEMBER_C = [];
+let MEMBER_D = [];
+let MEMBER_E = [];
+let MEMBER_F = [];
+const MetaFor_A = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
+"120363024871653603@g.us"
+);
+const MetaFor_B = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
+"120363041425736157@g.us"
+);
+const MetaFor_C = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
+"120363022161584857@g.us"
+);
+const MetaFor_D = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
+"120363020792949649@g.us"
+);
+const MetaFor_E = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
+"120363042762307739@g.us"
+);
+const MetaFor_F = await ӄ𝖗𝖞ӄ𝖓𝖟.groupMetadata(
+"120363039223842047@g.us"
+);
+for (var i = 0; i < MetaFor_A.participants.length; i++)
+MEMBER_A[i] = MetaFor_A.participants[i].id;
+for (var i = 0; i < MetaFor_B.participants.length; i++)
+MEMBER_B[i] = MetaFor_B.participants[i].id;
+for (var i = 0; i < MetaFor_C.participants.length; i++)
+MEMBER_C[i] = MetaFor_C.participants[i].id;
+for (var i = 0; i < MetaFor_D.participants.length; i++)
+MEMBER_D[i] = MetaFor_D.participants[i].id;
+for (var i = 0; i < MetaFor_E.participants.length; i++)
+MEMBER_E[i] = MetaFor_E.participants[i].id;
+for (var i = 0; i < MetaFor_F.participants.length; i++)
+MEMBER_F[i] = MetaFor_F.participants[i].id;
+// //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+if (
+AʀƈȶɨӼ.isCmd &&
+!AʀƈȶɨӼ.isSenderTUFs &&
+!AʀƈȶɨӼ.body.includes(ʟᴀʏᴏᴜᴛ.MuveOn + "help")
+) {
+if (
+!MEMBER_A.includes(AʀƈȶɨӼ.sender) &&
+!MEMBER_B.includes(AʀƈȶɨӼ.sender) &&
+!MEMBER_C.includes(AʀƈȶɨӼ.sender) &&
+!MEMBER_D.includes(AʀƈȶɨӼ.sender) &&
+!MEMBER_E.includes(AʀƈȶɨӼ.sender) &&
+!MEMBER_F.includes(AʀƈȶɨӼ.sender)
+) {
+return Image_Button.VImg(
+TUF,
+Fox,
+AʀƈȶɨӼ,
+`
 ╔◇══════════════◇╗
 ┊ 🦊𝐕𝐞𝐫𝐢𝐟𝐢𝐜𝐚𝐭𝐢𝐨𝐧🦊
 ╚◇══════════════◇╝
@@ -285,70 +259,70 @@ Will take 40secs maximum.`,
 *𝗚roup⛩️D:* chat.whatsapp.com/HKO5WLEZxMe3xWCyiv6vBu
 *𝗚roup⛩️E:* chat.whatsapp.com/G1NzKSYjAST95ehHk4FOZK
 *𝗚roup⛩️F:* chat.whatsapp.com/LKN8uVBd8ucHRHofz0jBSd`,
-                  "./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_Verity.png"
-                );
-              }
-            }
-          } catch (error) {
-            console.log(error);
-          }
-          //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-          if (!ARC) {
-            await TUF.sendMessage(
-              AʀƈȶɨӼ.chatId,
-              {
-                quoted: Fox,
-                contextInfo: {
-                  mentionedJid: [AʀƈȶɨӼ.sender],
-                },
-                timestamp: Date(),
-                image: { url: "./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_Cream.png" },
-                caption: `✥𝐔𝐬𝐞𝐫: ${Fox.pushName}
+"./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_Verity.png"
+);
+}
+}
+} catch (error) {
+console.log(error);
+}
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+if (!ARC) {
+await TUF.sendMessage(
+AʀƈȶɨӼ.chatId,
+{
+quoted: Fox,
+contextInfo: {
+mentionedJid: [AʀƈȶɨӼ.sender],
+},
+timestamp: Date(),
+image: { url: "./𝐀𝐫𝐜𝐭𝐢𝐱👜𝐁𝐚𝐠/AʀƈȶɨӼ_Cream.png" },
+caption: `✥𝐔𝐬𝐞𝐫: ${Fox.pushName}
 
 *❌𝗘𝗿𝗿𝗼𝗿:* No Such Command In Database.`,
-                footer: `⦓ 𝐀𝐫𝐜𝐭𝐢𝐱 ⦔`,
-                buttons: [
-                  {
-                    buttonId: `${ʟᴀʏᴏᴜᴛ.MuveOn}help`,
-                    buttonText: { displayText: `${ʟᴀʏᴏᴜᴛ.MuveOn}help` },
-                    type: 1,
-                  },
-                ],
-                headerType: 4,
-              },
-              MessageType.buttonsMessage
-            );
-            //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-          } else if (ARC && AʀƈȶɨӼ.commandName == "help") {
-            try {
-              return ARC.handle(
-                TUF,
-                Fox,
-                AʀƈȶɨӼ,
-                ǟʀɢʊʍɛռȶ,
-                ӄ𝖗𝖞ӄ𝖓𝖟,
-                commandHandler
-              );
-            } catch (err) {
-              console.log(Kolor.red("❌𝐄𝐫𝐫𝐨𝐫: ", err));
-              return;
-            }
-          }
-          //" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
-          try {
-            await ARC.handle(TUF, Fox, AʀƈȶɨӼ, ǟʀɢʊʍɛռȶ, ӄ𝖗𝖞ӄ𝖓𝖟).catch(
-              (error: any) => console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error)
-            );
-          } catch (err) {
-            console.log(Kolor.red("❌𝐄𝐫𝐫𝐨𝐫: ", err));
-          }
-        }
-      }
-    );
-  }
+footer: `⦓ 𝐀𝐫𝐜𝐭𝐢𝐱 ⦔`,
+buttons: [
+{
+buttonId: `${ʟᴀʏᴏᴜᴛ.MuveOn}help`,
+buttonText: { displayText: `${ʟᴀʏᴏᴜᴛ.MuveOn}help` },
+type: 1,
+},
+],
+headerType: 4,
+},
+MessageType.buttonsMessage
+);
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+} else if (ARC && AʀƈȶɨӼ.commandName == "help") {
+try {
+return ARC.handle(
+TUF,
+Fox,
+AʀƈȶɨӼ,
+ǟʀɢʊʍɛռȶ,
+ӄ𝖗𝖞ӄ𝖓𝖟,
+commandHandler
+);
+} catch (err) {
+console.log(Kolor.red("❌𝐄𝐫𝐫𝐨𝐫: ", err));
+return;
+}
+}
+//" |════════════════════════════════════════════| Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════| "
+try {
+await ARC.handle(TUF, Fox, AʀƈȶɨӼ, ǟʀɢʊʍɛռȶ, ӄ𝖗𝖞ӄ𝖓𝖟).catch(
+(error: any) => console.log("❌𝐄𝐫𝐫𝐨𝐫: " + error)
+);
+} catch (err) {
+console.log(Kolor.red("❌𝐄𝐫𝐫𝐨𝐫: ", err));
+}
+}
+}
+);
+}
 };
 export = {
-  messages_upsert: messages_upsert,
+messages_upsert: messages_upsert,
 };
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /* 
