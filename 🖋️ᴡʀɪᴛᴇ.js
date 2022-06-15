6a60fa492880e71652c318cb3f874c72b3e1c8d3
KryKnz <KryKnz@yandex.com>
2022-06-15 20:31:04 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 📡 𝐃𝐚𝐭𝐞: 2022-06-15 20:31:0 ❞

--- 🖋️ᴡʀɪᴛᴇ.js
+++ 🖋️ᴡʀɪᴛᴇ.js
@@ -21,7 +21,7 @@ shell.exit(1);
 }
 if (
 shell.exec(
-`git pull && git add --all && git commit -am "❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : ${
+`tsc git pull && git add --all && git commit -am "❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : ${
 vers.vers
 } 📡 𝐃𝐚𝐭𝐞: ${
 year +
