416bd77369290ddaa8e678fbb1a0fdc89226dc28
KryKnz <krakinzkon@gmail.com>
2022-06-17 00:55:03 +0530

    ❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 📡 𝐃𝐚𝐭𝐞: 2022-06-17 0:55:0 ❞

--- 𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/connection_update.ts
+++ 𝐀𝐫𝐜𝐭𝐢𝐱💥𝐄𝐯𝐞𝐧𝐭𝐬/connection_update.ts
@@ -23,53 +23,53 @@ import Kolor from "chalk";
 ⦓════════════════════════════════════════════| ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════⦔
 */
 const connection_update = async (
-update: any,
-DisconnectReason: any,
-startӄ𝖗𝖞ӄ𝖓𝖟: any
+  update: any,
+  DisconnectReason: any,
+  startӄ𝖗𝖞ӄ𝖓𝖟: any
 ): Promise<void> => {
-const { connection, lastDisconnect } = update;
-if (connection === "close") {
-if (
-(lastDisconnect.error as Boom)?.output?.statusCode !==
-DisconnectReason.loggedOut
-) {
-await startӄ𝖗𝖞ӄ𝖓𝖟();
-} else {
-console.log("❌𝐄𝐫𝐫𝐨𝐫꧂ Connection closed. You are logged out.");
-process.exit(0);
-}
-}
-/*
+  const { connection, lastDisconnect } = update;
+  if (connection === "close") {
+    if (
+      (lastDisconnect.error as Boom)?.output?.statusCode !==
+      DisconnectReason.loggedOut
+    ) {
+      await startӄ𝖗𝖞ӄ𝖓𝖟();
+    } else {
+      console.log("❌𝐄𝐫𝐫𝐨𝐫꧂ Connection closed. You are logged out.");
+      process.exit(0);
+    }
+  }
+  /*
 ⦓════════════════════════════════════════════| ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════⦔
 「   𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢®   」                                                                            「   𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢®   」
 ⦓════════════════════════════════════════════| ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════⦔
 */
-if (connection === "connecting") {
-if (ʟᴀʏᴏᴜᴛ.Heroku_App !== undefined) {
-console.log("💡𝐈𝐧𝐟𝐨꧂  Connecting to WhatsApp...");
-} else {
-chalkAnimation.rainbow("💡𝐈𝐧𝐟𝐨꧂  Connecting to WhatsApp...");
-}
-}
-/*
+  if (connection === "connecting") {
+    if (ʟᴀʏᴏᴜᴛ.Heroku_App !== undefined) {
+      console.log("💡𝐈𝐧𝐟𝐨꧂  Connecting to WhatsApp...");
+    } else {
+      chalkAnimation.rainbow("💡𝐈𝐧𝐟𝐨꧂  Connecting to WhatsApp...");
+    }
+  }
+  /*
 ⦓════════════════════════════════════════════| ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════⦔
 「   𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢®   」                                                                            「   𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢®   」
 ⦓════════════════════════════════════════════| ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════⦔
 */
-if (connection === "open") {
-if (ʟᴀʏᴏᴜᴛ.Heroku_App !== undefined) {
-console.log("Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 Online!");
-} else {
-const rainbow = chalkAnimation.rainbow(`
+  if (connection === "open") {
+    if (ʟᴀʏᴏᴜᴛ.Heroku_App !== undefined) {
+      console.log("Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 Online!");
+    } else {
+      const rainbow = chalkAnimation.rainbow(`
 |═════════════════════════════════「   𝐀𝐫𝐜𝐭𝐢𝐱 𝐀𝐩𝐢®: Online   」═════════════════════════════════|`);
-setTimeout(() => {
-rainbow.stop();
-}, 10_000);
-}
-}
+      setTimeout(() => {
+        rainbow.stop();
+      }, 10_000);
+    }
+  }
 };
 export = {
-connection_update: connection_update,
+  connection_update: connection_update,
 };
 `|⬡════════════════════════════════════════════|❝ Ⓒ𝐀𝐫𝐜𝐭𝐢𝐱 ☊ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ❞|═══════════════════════════════════════════⬡|`;
 /* 
