464aa9030d145a6182e176e7ad964bc61fdc2a78
KryKnz <krakinzkon@gmail.com>
2022-06-22 00:26:42 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-22 0:26:40

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Image_Button.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Image_Button.js
@@ -6,37 +6,39 @@
 require("../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙");
 const { Caught } = require("./Caught");
 exports.Image_Button = async (ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, A𝖗𝖌𝖘, M𝖊𝖉𝖎𝖆, 𝕮𝖔𝖓𝖙𝖊𝖓𝖙) => {
-await ӄ𝖚𝖓𝖆𝖎.sendPresenceUpdate("composing", Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
-await ӄ𝖚𝖓𝖆𝖎.sendMessage(
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
-image: { url: M𝖊𝖉𝖎𝖆 || "https://i.postimg.cc/qB1f08hd/Uni.png" },
-caption: `┌─「 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐏𝐫𝐨𝐭𝐨® 」
+  await ӄ𝖚𝖓𝖆𝖎.sendPresenceUpdate("composing", Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
+  await ӄ𝖚𝖓𝖆𝖎
+    .sendMessage(
+      Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+      {
+        contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
+        image: { url: M𝖊𝖉𝖎𝖆 || "https://i.postimg.cc/qB1f08hd/Uni.png" },
+        caption: `┌─「 𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐏𝐫𝐨𝐭𝐨® 」
 ├• ɴᴀᴍᴇ: ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"}
 └────────────────◇
 
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*",
-buttons: [
-{
-buttonId: prefix + "help",
-buttonText: { displayText: prefix + "help" },
-type: 1,
-},
-{
-buttonId: prefix + "support",
-buttonText: { displayText: prefix + "support" },
-type: 1,
-},
-],
-headerType: 4,
-},
-{
-quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-}
-);
+        footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*",
+        buttons: [
+          {
+            buttonId: prefix + "help",
+            buttonText: { displayText: prefix + "help" },
+            type: 1,
+          },
+          {
+            buttonId: prefix + "support",
+            buttonText: { displayText: prefix + "support" },
+            type: 1,
+          },
+        ],
+        headerType: 4,
+      },
+      {
+        quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+      }
+    )
+    .catch((error) => Caught(error, ӄ𝖗𝖆ӄ𝖎𝖓𝖟𝕷𝖆𝖇, ᴠʟᴋʏʀᴇ, KryChat));
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
