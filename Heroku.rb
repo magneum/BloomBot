68982f9965667ce0da3c974dbfa2da4dc7cdddf3
KryKnz <krakinzkon@gmail.com>
2022-06-22 09:03:51 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-22 9:3:50

--- Heroku.rb
+++ Heroku.rb
@@ -1,7 +1,7 @@
-curl https://cli-assets.heroku.com/install.sh | sh`
-heroku plugins:install heroku-builds`
-puts `heroku ps:stop run.6654`
-puts `heroku builds:cancel -a vlkyre`
-puts `heroku ps:restart worker -a vlkyre`
-puts `heroku addons:remove heroku-postgresql:hobby-dev --confirm vlkyre`
-puts `heroku addons:add heroku-postgresql:hobby-dev -a  vlkyre`
+curl https://cli-assets.heroku.com/install.sh | sh
+heroku plugins:install heroku-builds
+heroku ps:stop run.6654
+heroku builds:cancel -a vlkyre
+heroku ps:restart worker -a vlkyre
+heroku addons:remove heroku-postgresql:hobby-dev --confirm vlkyre
+heroku addons:add heroku-postgresql:hobby-dev -a  vlkyre
