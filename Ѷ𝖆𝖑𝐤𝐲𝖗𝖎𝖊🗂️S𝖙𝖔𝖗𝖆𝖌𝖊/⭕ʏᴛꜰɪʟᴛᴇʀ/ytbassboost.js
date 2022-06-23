3d1d34228f742bc4ea579ec60ea4ae5534f48696
KryKnz <krakinzkon@gmail.com>
2022-06-23 18:34:32 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-23 18:34:30

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🗂️S𝖙𝖔𝖗𝖆𝖌𝖊/⭕ʏᴛꜰɪʟᴛᴇʀ/ytbassboost.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🗂️S𝖙𝖔𝖗𝖆𝖌𝖊/⭕ʏᴛꜰɪʟᴛᴇʀ/ytbassboost.js
@@ -1,16 +1,16 @@
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
- * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
- */
+* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+*/
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 require("../../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙");
 const {
-  isUrl,
-  fetchJson,
-  getBuffer,
-  jsonformat,
-  GIFBufferToVideoBuffer,
-  getRandom,
+isUrl,
+fetchJson,
+getBuffer,
+jsonformat,
+GIFBufferToVideoBuffer,
+getRandom,
 } = require(`../../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/myfunc`);
 const fs = require(`fs`);
 const util = require(`util`);
@@ -36,7 +36,7 @@ const { Video_Button } = require("../../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚�
 const { Bot_Not_Admin } = require("../../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🧭S𝖜𝖎𝖙𝖈𝖍/Bot_Not_Admin");
 const { Sender_Not_Admin } = require("../../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🧭S𝖜𝖎𝖙𝖈𝖍/Sender_Not_Admin");
 const YouTube_Regex =
-  /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
+/(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
 `|⬡════════════════════════════════════════════════════════════════════「  𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐀𝐩𝐢®  」═══════════════════════════════════════════════════════════════════⬡|`;
 var Present_Path = require(`path`);
 var Script_Name = Present_Path.basename(__filename);
@@ -44,104 +44,104 @@ var Final_Name = Script_Name.slice(0, -3).toLowerCase();
 const { Caught } = require("../../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Caught");
 `|⬡════════════════════════════════════════════════════════════════════「  𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐀𝐩𝐢®  」═══════════════════════════════════════════════════════════════════⬡|`;
 exports.ytbassboost = async (
-  ӄ𝖚𝖓𝖆𝖎,
-  Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-  A𝖗𝖌𝖘,
-  pushName,
-  fromMe,
-  F𝖚𝖑𝖑_A𝖗𝖌𝖘,
-  quoted,
-  mime,
-  isMedia,
-  isGroup,
-  groupMetadata,
-  groupName,
-  participants,
-  groupAdmins,
-  isBotAdmin,
-  isSenderAdmin,
-  isSenderTUF,
-  commandName,
-  body,
-  isReply,
-  mentionByTag,
-  mentionByReply
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+A𝖗𝖌𝖘,
+pushName,
+fromMe,
+F𝖚𝖑𝖑_A𝖗𝖌𝖘,
+quoted,
+mime,
+isMedia,
+isGroup,
+groupMetadata,
+groupName,
+participants,
+groupAdmins,
+isBotAdmin,
+isSenderAdmin,
+isSenderTUF,
+commandName,
+body,
+isReply,
+mentionByTag,
+mentionByReply
 ) => {
-  try {
-    if (!Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isGroup) return await Group_Only(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊);
-    `|⬡════════════════════════════════════════════════════════════════════「  𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐀𝐩𝐢®  」═══════════════════════════════════════════════════════════════════⬡|`;
-    if (A𝖗𝖌𝖘.length === 0) {
-      return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-        ӄ𝖚𝖓𝖆𝖎,
-        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-        `*❌ERROR:* No query provided!
+try {
+if (!Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isGroup) return await Group_Only(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊);
+`|⬡════════════════════════════════════════════════════════════════════「  𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐀𝐩𝐢®  」═══════════════════════════════════════════════════════════════════⬡|`;
+if (A𝖗𝖌𝖘.length === 0) {
+return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+`*❌ERROR:* No query provided!
 
 *⚡USAGE:* ${prefix}${Final_Name} <song name or youtube link>`
-      );
-    }
+);
+}
 
-    if (A𝖗𝖌𝖘.includes("yout")) {
-      if (!YouTube_Regex.test(A𝖗𝖌𝖘[0])) {
-        return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-          ӄ𝖚𝖓𝖆𝖎,
-          Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-          `*❌ERROR:* No query provided!
-
-*⚡USAGE:* ${prefix}${Final_Name} <song name or youtube link>`
-        );
-      }
-    }
-
-    let FinalGot;
-    if (YouTube_Regex.test(A𝖗𝖌𝖘[0])) {
-      FinalGot = A𝖗𝖌𝖘[0];
-    } else {
-      FinalGot = F𝖚𝖑𝖑_A𝖗𝖌𝖘;
-    }
-    const LinkFound = await yts(FinalGot);
-    if (!LinkFound) {
-      return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-        ӄ𝖚𝖓𝖆𝖎,
-        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-        `*❌ERROR:* No Music Found!
+if (A𝖗𝖌𝖘.includes("yout")) {
+if (!YouTube_Regex.test(A𝖗𝖌𝖘[0])) {
+return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+`*❌ERROR:* No query provided!
 
 *⚡USAGE:* ${prefix}${Final_Name} <song name or youtube link>`
-      );
-    }
+);
+}
+}
 
-    var TubeFile = LinkFound.videos.slice(0, 1);
-    TubeFile.forEach(async function (Found) {
-      if (Found.seconds > 1800) {
-        return await Image_Button(
-          ӄ𝖚𝖓𝖆𝖎,
-          Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-          A𝖗𝖌𝖘,
-          Found.thumbnail,
-          `❌𝗘𝗿𝗿𝗼𝗿: _Choose Smaller Audio less then 30mins!_
+let FinalGot;
+if (YouTube_Regex.test(A𝖗𝖌𝖘[0])) {
+FinalGot = A𝖗𝖌𝖘[0];
+} else {
+FinalGot = F𝖚𝖑𝖑_A𝖗𝖌𝖘;
+}
+const LinkFound = await yts(FinalGot);
+if (!LinkFound) {
+return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+`*❌ERROR:* No Music Found!
+
+*⚡USAGE:* ${prefix}${Final_Name} <song name or youtube link>`
+);
+}
+
+var TubeFile = LinkFound.videos.slice(0, 1);
+TubeFile.forEach(async function (Found) {
+if (Found.seconds > 1800) {
+return await Image_Button(
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+A𝖗𝖌𝖘,
+Found.thumbnail,
+`❌𝗘𝗿𝗿𝗼𝗿: _Choose Smaller Audio less then 30mins!_
 
 ⭕️𝐘𝐨𝐮𝐓𝐮𝐛𝐞 𝐀𝐈: ${Found.title}
 🍻𝐓𝐢𝐭𝐥𝐞: ${Found.title}
 ⏰𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧: ${Found.timestamp}`
-        );
-      }
+);
+}
 
-      let { dl_link, thumb, title, filesize, filesizeF } = await yta_var(
-        Found.url,
-        "id4"
-      );
-      let DirectFile;
-      try {
-        DirectFile = await Tinyurl(dl_link);
-      } catch (error) {
-        DirectFile = "Null";
-      }
+let { dl_link, thumb, title, filesize, filesizeF } = await yta_var(
+Found.url,
+"id4"
+);
+let DirectFile;
+try {
+DirectFile = await Tinyurl(dl_link);
+} catch (error) {
+DirectFile = "Null";
+}
 
-      await Image_Button(
-        ӄ𝖚𝖓𝖆𝖎,
-        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-        A𝖗𝖌𝖘,
-        Found.thumbnail,
-        `𝐘𝐨𝐮𝐓𝐮𝐛𝐞⭕️𝐌𝐮𝐬𝐢𝐜
+await Image_Button(
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+A𝖗𝖌𝖘,
+Found.thumbnail,
+`𝐘𝐨𝐮𝐓𝐮𝐛𝐞⭕️𝐌𝐮𝐬𝐢𝐜
 🍻𝐓𝐢𝐭𝐥𝐞: ${Found.title}
 🙈𝐕𝐢𝐞𝐰𝐬: ${Found.views}
 ⏰𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧: ${Found.timestamp}
@@ -150,61 +150,61 @@ exports.ytbassboost = async (
 🌐𝗪𝗲𝗯 𝗗𝗟: ${DirectFile}
 🔗𝐋𝐢𝐧𝐤: ${Found.url}
 📜𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐭𝐢𝐨𝐧: ${Found.description}`
-      );
+);
 
-      var FFmpegFile = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/${Date.now()}${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key.id}.mp3`;
-      var FilteredAudio = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/${Date.now()}-F-${
-        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key.id
-      }.mp3`;
-      ffmpeg(dl_link)
-        .saveToFile(FFmpegFile)
-        .on("end", () => {
-          require("child_process").exec(
-            `ffmpeg -i ${FFmpegFile} -af "bass=g=10,dynaudnorm=f=150" ${FilteredAudio}`,
-            async (error) => {
-              if (error) {
-                return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-                  ӄ𝖚𝖓𝖆𝖎,
-                  Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-                  `*❌ERROR:* Could not convert!
+var FFmpegFile = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/${Date.now()}${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key.id}.mp3`;
+var FilteredAudio = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/${Date.now()}-F-${
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key.id
+}.mp3`;
+ffmpeg(dl_link)
+.saveToFile(FFmpegFile)
+.on("end", () => {
+require("child_process").exec(
+`ffmpeg -i ${FFmpegFile} -af "bass=g=10,dynaudnorm=f=150" ${FilteredAudio}`,
+async (error) => {
+if (error) {
+return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+ӄ𝖚𝖓𝖆𝖎,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+`*❌ERROR:* Could not convert!
 
 *⚡USAGE:* ${prefix}${Final_Name} <Song name or Youtube link>`
-                );
-              }
+);
+}
 
-              return await ӄ𝖚𝖓𝖆𝖎
-                .sendMessage(
-                  Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-                  {
-                    audio: { url: FilteredAudio },
-                    contextInfo: {
-                      externalAdReply: {
-                        title: Found.title,
-                        body: `ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™`,
-                        mediaType: 2,
-                        thumbnail: await getBuffer(
-                          `https://i.ytimg.com/vi/${Found.videoId}/hqdefault.jpg`
-                        ),
-                        mediaUrl: Found.thumbnail,
-                      },
-                    },
-                    mimetype: `audio/mpeg`,
-                    fileName: `${Found.title}.mp3`,
-                  },
-                  { quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 }
-                )
-                .then(fs.unlinkSync(FFmpegFile, FilteredAudio));
-            }
-          );
-        });
-    });
-  } catch (err) {
-    const util = require(`util`);
-    Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, util.format(err));
-  }
+return await ӄ𝖚𝖓𝖆𝖎
+.sendMessage(
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+{
+audio: { url: FilteredAudio },
+contextInfo: {
+externalAdReply: {
+title: Found.title,
+body: `ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™`,
+mediaType: 2,
+thumbnail: await getBuffer(
+`https://i.ytimg.com/vi/${Found.videoId}/hqdefault.jpg`
+),
+mediaUrl: Found.thumbnail,
+},
+},
+mimetype: `audio/mpeg`,
+fileName: `${Found.title}.mp3`,
+},
+{ quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 }
+)
+.then(fs.unlinkSync(FFmpegFile, FilteredAudio));
+}
+);
+});
+});
+} catch (err) {
+const util = require(`util`);
+Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, err);
+}
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
- * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
- */
+* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+*/
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
