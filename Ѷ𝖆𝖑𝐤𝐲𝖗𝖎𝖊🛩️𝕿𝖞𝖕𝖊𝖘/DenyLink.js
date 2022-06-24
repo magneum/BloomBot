2b61896d22fb2b59390083c842dd2b216ee41309
KryKnz <krakinzkon@gmail.com>
2022-06-24 22:47:21 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-24 22:47:20

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🛩️𝕿𝖞𝖕𝖊𝖘/DenyLink.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🛩️𝕿𝖞𝖕𝖊𝖘/DenyLink.js
@@ -1,40 +1,40 @@
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
-* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
-*/
+ * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+ */
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 require("../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙");
 exports.DenyLink = async (
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-ӄ𝖚𝖓𝖆𝖎,
-Caught,
-Image_Button,
-A𝖗𝖌𝖘,
-LinkList,
-Figure
+  Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+  ӄ𝖚𝖓𝖆𝖎,
+  Caught,
+  Image_Button,
+  A𝖗𝖌𝖘,
+  LinkList,
+  Figure
 ) => {
-await LinkList.findOne(
-{
-serverID: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-},
-async (error, server) => {
-if (error) return Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e);
-if (!server) return;
-let FetchCurrentGroupLink = await ӄ𝖚𝖓𝖆𝖎.groupInviteCode(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
-let GroupLinkRegex = /Fox.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-let isGroupLink = GroupLinkRegex.exec(Figure);
-let PresentGroupLink = new RegExp(
-`https://chat.whatsapp.com/${FetchCurrentGroupLink}`,
-"i"
-);
-let isCurrentGroupLink = PresentGroupLink.test(Figure);
-if (isGroupLink && !isCurrentGroupLink) {
-await Image_Button(
-ӄ𝖚𝖓𝖆𝖎,
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-A𝖗𝖌𝖘,
-"./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊_Orange.png",
-`╔◇═══════════════◇╗
+  await LinkList.findOne(
+    {
+      serverID: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+    },
+    async (error, server) => {
+      if (error) return Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e);
+      if (!server) return;
+      let FetchCurrentGroupLink = await ӄ𝖚𝖓𝖆𝖎.groupInviteCode(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
+      let GroupLinkRegex = /Fox.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+      let isGroupLink = GroupLinkRegex.exec(Figure);
+      let PresentGroupLink = new RegExp(
+        `https://chat.whatsapp.com/${FetchCurrentGroupLink}`,
+        "i"
+      );
+      let isCurrentGroupLink = PresentGroupLink.test(Figure);
+      if (isGroupLink && !isCurrentGroupLink) {
+        await Image_Button(
+          ӄ𝖚𝖓𝖆𝖎,
+          Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+          A𝖗𝖌𝖘,
+          "./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊_Orange.png",
+          `╔◇═══════════════◇╗
 ┊ 𝐊𝐫𝐲𝐙𝐞𝐧❌𝐀𝐧𝐭𝐢-𝐋𝐢𝐧𝐤
 ┊    𝐌𝐞𝐜𝐡𝐚𝐧𝐢𝐬𝐦
 ╚◇═══════════════◇╝
@@ -49,24 +49,24 @@ A𝖗𝖌𝖘,
 🐾 Discord Link
 🐾 Telegram Link
 🐾 Whatspp Invite Link`
-);
-return await ӄ𝖗𝖞ӄ𝖓𝖟
-.groupRemove(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender])
-.catch((e) => Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-} else if (
-Figure.includes("https://t.me/") &&
-Figure.includes("discord.gg") &&
-Figure.includes("discord.com") &&
-Figure.includes("/t.me/") &&
-Figure.includes("wa.me/") &&
-Figure.includes("www.")
-) {
-await Image_Button(
-ӄ𝖚𝖓𝖆𝖎,
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-A𝖗𝖌𝖘,
-"./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊_Orange.png",
-`╔◇═══════════════◇╗
+        );
+        return await ӄ𝖗𝖞ӄ𝖓𝖟
+          .groupRemove(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender])
+          .catch((e) => Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+      } else if (
+        Figure.includes("https://t.me/") &&
+        Figure.includes("discord.gg") &&
+        Figure.includes("discord.com") &&
+        Figure.includes("/t.me/") &&
+        Figure.includes("wa.me/") &&
+        Figure.includes("www.")
+      ) {
+        await Image_Button(
+          ӄ𝖚𝖓𝖆𝖎,
+          Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+          A𝖗𝖌𝖘,
+          "./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊_Orange.png",
+          `╔◇═══════════════◇╗
 ┊ 𝐊𝐫𝐲𝐙𝐞𝐧❌𝐀𝐧𝐭𝐢-𝐋𝐢𝐧𝐤
 ┊    𝐌𝐞𝐜𝐡𝐚𝐧𝐢𝐬𝐦
 ╚◇═══════════════◇╝
@@ -81,17 +81,17 @@ A𝖗𝖌𝖘,
 🐾 Discord Link
 🐾 Telegram Link
 🐾 Whatspp Invite Link`
-);
-return await ӄ𝖗𝖞ӄ𝖓𝖟
-.groupRemove(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender])
-.catch((e) => Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-} else {
-}
-}
-);
+        );
+        return await ӄ𝖗𝖞ӄ𝖓𝖟
+          .groupRemove(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender])
+          .catch((e) => Caught(ӄ𝖚𝖓𝖆𝖎, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+      } else {
+      }
+    }
+  );
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
-* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
-*/
+ * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+ */
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
