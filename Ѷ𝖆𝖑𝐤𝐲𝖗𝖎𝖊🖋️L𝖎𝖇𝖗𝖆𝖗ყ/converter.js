1a087130af9658073d5d7427d0ad1ae40f4e4e10
KryKnz <krakinzkon@gmail.com>
2022-06-24 22:48:01 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-24 22:48:0

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/converter.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/converter.js
@@ -1,99 +1,113 @@
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+/*
+ * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+ */
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
 
 function ffmpeg(buffer, args = [], ext = "", ext2 = "") {
-return new Promise(async (resolve, reject) => {
-try {
-let tmp = path.join(__dirname, "../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌", +new Date() + "." + ext);
-let out = tmp + "." + ext2;
-await fs.promises.writeFile(tmp, buffer);
-spawn("ffmpeg", ["-y", "-i", tmp, ...args, out])
-.on("error", reject)
-.on("close", async (code) => {
-try {
-await fs.promises.unlink(tmp);
-if (code !== 0) return reject(code);
-resolve(await fs.promises.readFile(out));
-await fs.promises.unlink(out);
-} catch (e) {
-reject(e);
-}
-});
-} catch (e) {
-reject(e);
-}
-});
+  return new Promise(async (resolve, reject) => {
+    try {
+      let tmp = path.join(
+        __dirname,
+        "../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌",
+        +new Date() + "." + ext
+      );
+      let out = tmp + "." + ext2;
+      await fs.promises.writeFile(tmp, buffer);
+      spawn("ffmpeg", ["-y", "-i", tmp, ...args, out])
+        .on("error", reject)
+        .on("close", async (code) => {
+          try {
+            await fs.promises.unlink(tmp);
+            if (code !== 0) return reject(code);
+            resolve(await fs.promises.readFile(out));
+            await fs.promises.unlink(out);
+          } catch (e) {
+            reject(e);
+          }
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
 /**
-* Convert Audio to Playable WhatsApp Audio
-* @param {Buffer} buffer Audio Buffer
-* @param {String} ext File Extension
-*/
+ * Convert Audio to Playable WhatsApp Audio
+ * @param {Buffer} buffer Audio Buffer
+ * @param {String} ext File Extension
+ */
 function toAudio(buffer, ext) {
-return ffmpeg(
-buffer,
-["-vn", "-ac", "2", "-b:a", "128k", "-ar", "44100", "-f", "mp3"],
-ext,
-"mp3"
-);
+  return ffmpeg(
+    buffer,
+    ["-vn", "-ac", "2", "-b:a", "128k", "-ar", "44100", "-f", "mp3"],
+    ext,
+    "mp3"
+  );
 }
 
 /**
-* Convert Audio to Playable WhatsApp PTT
-* @param {Buffer} buffer Audio Buffer
-* @param {String} ext File Extension
-*/
+ * Convert Audio to Playable WhatsApp PTT
+ * @param {Buffer} buffer Audio Buffer
+ * @param {String} ext File Extension
+ */
 function toPTT(buffer, ext) {
-return ffmpeg(
-buffer,
-[
-"-vn",
-"-c:a",
-"libopus",
-"-b:a",
-"128k",
-"-vbr",
-"on",
-"-compression_level",
-"10",
-],
-ext,
-"opus"
-);
+  return ffmpeg(
+    buffer,
+    [
+      "-vn",
+      "-c:a",
+      "libopus",
+      "-b:a",
+      "128k",
+      "-vbr",
+      "on",
+      "-compression_level",
+      "10",
+    ],
+    ext,
+    "opus"
+  );
 }
 
 /**
-* Convert Audio to Playable WhatsApp Video
-* @param {Buffer} buffer Video Buffer
-* @param {String} ext File Extension
-*/
+ * Convert Audio to Playable WhatsApp Video
+ * @param {Buffer} buffer Video Buffer
+ * @param {String} ext File Extension
+ */
 function toVideo(buffer, ext) {
-return ffmpeg(
-buffer,
-[
-"-c:v",
-"libx264",
-"-c:a",
-"aac",
-"-ab",
-"128k",
-"-ar",
-"44100",
-"-crf",
-"32",
-"-preset",
-"slow",
-],
-ext,
-"mp4"
-);
+  return ffmpeg(
+    buffer,
+    [
+      "-c:v",
+      "libx264",
+      "-c:a",
+      "aac",
+      "-ab",
+      "128k",
+      "-ar",
+      "44100",
+      "-crf",
+      "32",
+      "-preset",
+      "slow",
+    ],
+    ext,
+    "mp4"
+  );
 }
 
 module.exports = {
-toAudio,
-toPTT,
-toVideo,
-ffmpeg,
+  toAudio,
+  toPTT,
+  toVideo,
+  ffmpeg,
 };
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+/*
+ * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+ */
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
