44d11bcee9984fd0ddc26d375bd408a26cb4bbbe
KryKnz <krakinzkon@gmail.com>
2022-06-25 11:52:57 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-25 11:52:56

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/Ѷ𝖆𝖑.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/Ѷ𝖆𝖑.js
@@ -183,6 +183,10 @@ if (
 !Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.fromMe &&
 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isCommand &&
 !Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.Body.includes("help") && 
+!Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.Body.includes("help") && 
+!Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.Body.includes("help") && 
+!Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.Body.includes("help") && 
+!Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.Body.includes("help") && 
 !Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isSenderTUF &&
 !MeA.includes(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender) &&
 !MeB.includes(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender) &&
