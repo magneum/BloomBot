81433c6a6e45f25a3e105a1585d21780ea8ba9a6
KryKnz <krakinzkon@gmail.com>
2022-06-25 21:19:33 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-25 21:19:32

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌗E𝖛𝖊𝖓𝖙𝖘/BeutyFly.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌗E𝖛𝖊𝖓𝖙𝖘/BeutyFly.js
@@ -49,6 +49,7 @@ exports.BeutyFly = async (ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, Kolor) 
       caption: `
 ▷═════|✠|═════◁
 💡Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊: ${new Date()}
+💡𝐒𝐞𝐧𝐝𝐞𝐫:
 💡𝐂𝐨𝐧𝐭𝐞𝐧𝐭: ${
         (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isGroup ? Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName : `Private Chat`, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID)
       }
