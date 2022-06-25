dfb723c1ebca1f4c36cf135a8ce6cdbcbb4f3d54
KryKnz <krakinzkon@gmail.com>
2022-06-25 19:46:49 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-25 19:46:48

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Video_Button.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Video_Button.js
@@ -1,196 +1,203 @@
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
-* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
-*/
+ * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+ */
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 require("../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙");
 let { Caught } = require("./Caught");
 let moment = require(`moment-timezone`);
 let Time = moment.tz(`Asia/Kolkata`).format(`DD/MM HH:mm:ss`);
 exports.Video_Button = async (ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, A𝖗𝖌𝖘, M𝖊𝖉𝖎𝖆, 𝕮𝖔𝖓𝖙𝖊𝖓𝖙) => {
-// await ᴋᴜɴᴀɪ.sendPresenceUpdate("composing", Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
-if (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isReply) {
-var Receiver = Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo.participant;
-await ᴋᴜɴᴀɪ
-.sendMessage(
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
-video: { url: M𝖊𝖉𝖎𝖆 },
-caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+  // await ᴋᴜɴᴀɪ.sendPresenceUpdate("composing", Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
+  if (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isReply) {
+    var Receiver = Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo.participant;
+    await ᴋᴜɴᴀɪ
+      .sendMessage(
+        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
+          video: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 ║ *❣️ ɴᴀᴍᴇ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"}
 ║ *⚡ ɪᴅ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID.split("@")[0]}
 ╚═══════╝
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
-buttons: [
-{
-buttonId: prefix + "help",
-buttonText: { displayText: prefix + "help" },
-type: 1,
-},
-{
-buttonId: prefix + "support",
-buttonText: { displayText: prefix + "support" },
-type: 1,
-},
-],
-headerType: 5,
-},
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
-quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-}
-)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-let reactionMessage = {
-react: {
-text: `⚡`,
-key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
-},
-};
-return await ᴋᴜɴᴀɪ
-.sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-} else if (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.mentionByTag) {
-var Receiver =
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo.mentionedJid;
-await ᴋᴜɴᴀɪ
-.sendMessage(
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
-video: { url: M𝖊𝖉𝖎𝖆 },
-caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+          footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
+          buttons: [
+            {
+              buttonId: prefix + "help",
+              buttonText: { displayText: prefix + "help" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "support",
+              buttonText: { displayText: prefix + "support" },
+              type: 1,
+            },
+          ],
+          headerType: 5,
+        },
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
+          quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+        }
+      )
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    let reactionMessage = {
+      react: {
+        text: `⚡`,
+        key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
+      },
+    };
+    return await ᴋᴜɴᴀɪ
+      .sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+  } else if (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.mentionByTag) {
+    var Receiver =
+      Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.mtype == "extendedTextMessage" &&
+      Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo != null
+        ? Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo.mentionedJid
+        : [];
+    await ᴋᴜɴᴀɪ
+      .sendMessage(
+        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
+          video: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 ║ *❣️ ɴᴀᴍᴇ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"}
 ║ *⚡ ɪᴅ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID.split("@")[0]}
 ╚═══════╝
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
-buttons: [
-{
-buttonId: prefix + "help",
-buttonText: { displayText: prefix + "help" },
-type: 1,
-},
-{
-buttonId: prefix + "support",
-buttonText: { displayText: prefix + "support" },
-type: 1,
-},
-],
-headerType: 5,
-},
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
-quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-}
-)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-let reactionMessage = {
-react: {
-text: `⚡`,
-key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
-},
-};
-return await ᴋᴜɴᴀɪ
-.sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-} else if (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.mentionByReply) {
-var Receiver = Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo.participant;
-await ᴋᴜɴᴀɪ
-.sendMessage(
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
-video: { url: M𝖊𝖉𝖎𝖆 },
-caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+          footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
+          buttons: [
+            {
+              buttonId: prefix + "help",
+              buttonText: { displayText: prefix + "help" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "support",
+              buttonText: { displayText: prefix + "support" },
+              type: 1,
+            },
+          ],
+          headerType: 5,
+        },
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
+          quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+        }
+      )
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    let reactionMessage = {
+      react: {
+        text: `⚡`,
+        key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
+      },
+    };
+    return await ᴋᴜɴᴀɪ
+      .sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+  } else if (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.mentionByReply) {
+    var Receiver =
+      Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.mtype == "extendedTextMessage" &&
+      Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo != null
+        ? Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.message.extendedTextMessage.contextInfo.participant || ""
+        : "";
+    await ᴋᴜɴᴀɪ
+      .sendMessage(
+        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
+          video: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 ║ *❣️ ɴᴀᴍᴇ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"}
 ║ *⚡ ɪᴅ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID.split("@")[0]}
 ╚═══════╝
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
-buttons: [
-{
-buttonId: prefix + "help",
-buttonText: { displayText: prefix + "help" },
-type: 1,
-},
-{
-buttonId: prefix + "support",
-buttonText: { displayText: prefix + "support" },
-type: 1,
-},
-],
-headerType: 5,
-},
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
-quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-}
-)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-let reactionMessage = {
-react: {
-text: `⚡`,
-key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
-},
-};
-return await ᴋᴜɴᴀɪ
-.sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-} else {
-await ᴋᴜɴᴀɪ
-.sendMessage(
-Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-{
-contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
-video: { url: M𝖊𝖉𝖎𝖆 },
-caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+          footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
+          buttons: [
+            {
+              buttonId: prefix + "help",
+              buttonText: { displayText: prefix + "help" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "support",
+              buttonText: { displayText: prefix + "support" },
+              type: 1,
+            },
+          ],
+          headerType: 5,
+        },
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Receiver] },
+          quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+        }
+      )
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    let reactionMessage = {
+      react: {
+        text: `⚡`,
+        key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
+      },
+    };
+    return await ᴋᴜɴᴀɪ
+      .sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+  } else {
+    await ᴋᴜɴᴀɪ
+      .sendMessage(
+        Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+        {
+          contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
+          video: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 ║ *❣️ ɴᴀᴍᴇ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"}
 ║ *⚡ ɪᴅ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID.split("@")[0]}
 ╚═══════╝
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
-buttons: [
-{
-buttonId: prefix + "help",
-buttonText: { displayText: prefix + "help" },
-type: 1,
-},
-{
-buttonId: prefix + "support",
-buttonText: { displayText: prefix + "support" },
-type: 1,
-},
-],
-headerType: 5,
-},
-{
-quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-}
-)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-let reactionMessage = {
-react: {
-text: `⚡`,
-key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
-},
-};
-return await ᴋᴜɴᴀɪ
-.sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
-.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-}
+          footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ",
+          buttons: [
+            {
+              buttonId: prefix + "help",
+              buttonText: { displayText: prefix + "help" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "support",
+              buttonText: { displayText: prefix + "support" },
+              type: 1,
+            },
+          ],
+          headerType: 5,
+        },
+        {
+          quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+        }
+      )
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+    let reactionMessage = {
+      react: {
+        text: `⚡`,
+        key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
+      },
+    };
+    return await ᴋᴜɴᴀɪ
+      .sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, reactionMessage)
+      .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+  }
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
-* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
-*/
+ * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+ */
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
