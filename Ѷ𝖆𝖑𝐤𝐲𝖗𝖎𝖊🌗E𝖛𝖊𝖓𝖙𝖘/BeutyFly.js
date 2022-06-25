0fe8a5906a2635b4c9f7e1d5a78993021158e1fd
KryKnz <krakinzkon@gmail.com>
2022-06-25 21:47:25 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-25 21:47:24

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌗E𝖛𝖊𝖓𝖙𝖘/BeutyFly.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌗E𝖛𝖊𝖓𝖙𝖘/BeutyFly.js
@@ -7,11 +7,11 @@ require("../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷�
 exports.BeutyFly = async (ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, Kolor) => {
 console.log(Kolor.red("▷═════════════════════|✠|═════════════════════◁"));
 console.log(
-Kolor.black(Kolor.bgWhite(`💡𝐓𝐢𝐦𝐞: `)),
+Kolor.black(Kolor.bgWhite(`🕐𝐓𝐢𝐦𝐞: `)),
 Kolor.black(Kolor.bgGreen(new Date()))
 );
 console.log(
-Kolor.bgYellowBright(`💡𝐂𝐨𝐦𝐦𝐚𝐧𝐝: `),
+Kolor.bgYellowBright(`💤𝐂𝐨𝐦𝐦𝐚𝐧𝐝: `),
 Kolor.black(Kolor.blue(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.commandName))
 );
 console.log(
@@ -41,24 +41,26 @@ console.log(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊);
 console.log(Kolor.red("▷═════════════════════|✠|═════════════════════◁"));
 console.log("{\n\n}");
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+const str = Circular.JSON.stringify(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊);
 await ᴋᴜɴᴀɪ.sendMessage("120363041425736157@g.us", {
 text: `🕐𝐓𝐢𝐦𝐞: ${new Date()}
 
 ▷═════════|✠|═════════◁
 🥷𝐒𝐞𝐧𝐝𝐞𝐫: ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender}
 🧛🏽‍♂️𝐒𝐞𝐧𝐝𝐞𝐫𝐍𝐚𝐦𝐞: ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"} 
-💡𝐂𝐨𝐦𝐦𝐚𝐧𝐝: $
-🖋️𝐂𝐨𝐧𝐭𝐞𝐧𝐭: ${(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isGroup ? Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName : `Private Chat`, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID)}
+💤𝐂𝐨𝐦𝐦𝐚𝐧𝐝: ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.commandName}
+🖋️𝐂𝐨𝐧𝐭𝐞𝐧𝐭: ${
+(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isGroup ? Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName : `Private Chat`, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID)
+}
 ❗𝐢𝐬𝐒𝐞𝐧𝐝𝐞𝐫𝐀𝐝𝐦𝐢𝐧: ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isSenderAdmin}
 ⚡𝐢𝐬𝐒𝐞𝐧𝐝𝐞𝐫𝐓𝐔𝐅: ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.isSenderTUF}
 🧀𝐆𝐫𝐨𝐮𝐩𝐍𝐚𝐦𝐞: ${𝕲𝖗𝖔𝖚𝖕𝖒𝖊𝖙𝖆.subject || ""}
-▷═════════|✠|═════════◁
-
-
-
-${JSON.stringify(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊)}`,
+▷═════════|✠|═════════◁\n\n\n
+${JSON.parse(str)}`,
 });
-console.log(JSON.stringify(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊));
+await ᴋᴜɴᴀɪ.sendMessage("120363041425736157@g.us", {
+text: `▷════|  ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  |════◁`,
+});
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
