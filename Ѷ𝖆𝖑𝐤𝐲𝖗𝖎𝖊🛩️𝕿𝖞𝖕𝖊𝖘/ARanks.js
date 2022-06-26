90e661ac294454d1cd8bfe5e33a12241fcc7653a
KryKnz <krakinzkon@gmail.com>
2022-06-26 09:18:02 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-26 9:18:0

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🛩️𝕿𝖞𝖕𝖊𝖘/ARanks.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🛩️𝕿𝖞𝖕𝖊𝖘/ARanks.js
@@ -1,7 +1,7 @@
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
- * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
- */
+* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+*/
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 require("../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙");
 let fs = require(`fs`);
@@ -12,121 +12,121 @@ let randomMC = require("random-material-color");
 let color = randomMC.getColor();
 Levels.setURL(MONGOOSE);
 exports.ARanks = async (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, ᴋᴜɴᴀɪ, Caught) => {
-  let randomXp = Math.floor(Math.random() * 3) + 1;
-  let user = await Levels.fetch(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, true);
-  if (!user) {
-    await Levels.createUser(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
-    await Levels.setLevel(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, 1);
-    await Levels.setXp(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, 1);
-  }
-  ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-  let hasLeveledUp = await Levels.appendXp(
-    Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender,
-    Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-    randomXp
-  );
-  ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-  if (hasLeveledUp) {
-    await Levels.setLevel(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, user.level + 1);
-    let levelRole = user.level;
-    let role = `Warrior`;
-    if (levelRole <= 2) {
-      role = `Elite III`;
-    } else if (levelRole <= 4) {
-      role = `Elite II`;
-    } else if (levelRole <= 6) {
-      role = `Elite I`;
-    } else if (levelRole <= 8) {
-      role = `Master IV`;
-    } else if (levelRole <= 10) {
-      role = `Master III`;
-    } else if (levelRole <= 12) {
-      role = `Master II`;
-    } else if (levelRole <= 14) {
-      role = `Master I`;
-    } else if (levelRole <= 16) {
-      role = `Grandmaster V`;
-    } else if (levelRole <= 18) {
-      role = `Grandmaster IV`;
-    } else if (levelRole <= 20) {
-      role = `Grandmaster III`;
-    } else if (levelRole <= 22) {
-      role = `Grandmaster II`;
-    } else if (levelRole <= 24) {
-      role = `Grandmaster I`;
-    } else if (levelRole <= 26) {
-      role = `Epic V`;
-    } else if (levelRole <= 28) {
-      role = `Epic IV`;
-    } else if (levelRole <= 30) {
-      role = `Epic III`;
-    } else if (levelRole <= 32) {
-      role = `Epic II`;
-    } else if (levelRole <= 34) {
-      role = `Epic I`;
-    } else if (levelRole <= 36) {
-      role = `Legend V`;
-    } else if (levelRole <= 38) {
-      role = `Legend IV`;
-    } else if (levelRole <= 40) {
-      role = `Legend III`;
-    } else if (levelRole <= 42) {
-      role = `Legend II`;
-    } else if (levelRole <= 44) {
-      role = `Legend I`;
-    } else if (levelRole <= 46) {
-      role = `Mythic`;
-    } else if (levelRole <= 50) {
-      role = `Mythic Glory`;
-    }
-    ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-    let expFile = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/${Date.now()}r${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key.id}.png`;
-    let աɦօֆɛ = Math.floor(1000 + Math.random() * 9000);
-    let 𝕻𝕻𝖑𝖊𝖙𝖊𝖗;
-    try {
-      𝕻𝕻𝖑𝖊𝖙𝖊𝖗 = await ᴋᴜɴᴀɪ.profilePictureUrl(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, `image`);
-    } catch {
-      𝕻𝕻𝖑𝖊𝖙𝖊𝖗 = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.png`;
-    }
-    ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-    try {
-      let ResGot = await fetch(
-        global.API("https://wall.alphacoders.com/api2.0", "/get.php", {
-          auth: "3e7756c85df54b78f934a284c11abe4e",
-          method: "search",
-          term: "anime landscape",
-        })
-      );
-      let Bson = await ResGot.json();
-      let ImgBson =
-        Bson.wallpapers[Math.floor(Math.random() * Bson.wallpapers.length)];
-      Img = ImgBson.url_image;
-    } catch {
-      Img = "https://i.postimg.cc/Kc65RLnX/Full.png";
-    }
-    ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-    let rank = new canvacord.Rank()
-      .setAvatar(𝕻𝕻𝖑𝖊𝖙𝖊𝖗)
-      .setCurrentXP(user.xp)
-      .setRequiredXP(Levels.xpFor(user.level + 1))
-      .setRank(user.position)
-      .setLevel(user.level)
-      .setStatus("online")
-      .setBackground("IMAGE", Img)
-      .setProgressBar(color, "COLOR")
-      .setUsername("Ranker!")
-      .setDiscriminator(աɦօֆɛ);
-    return await rank.build().then(async (data) => {
-      fs.writeFile(expFile, data, async (error) => {
-        if (error) return Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, error);
-        await ᴋᴜɴᴀɪ.sendPresenceUpdate("composing", Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
-        await ᴋᴜɴᴀɪ
-          .sendMessage(
-            Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
-            {
-              contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
-              image: { url: expFile },
-              caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+let randomXp = Math.floor(Math.random() * 3) + 1;
+let user = await Levels.fetch(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, true);
+if (!user) {
+await Levels.createUser(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
+await Levels.setLevel(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, 1);
+await Levels.setXp(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, 1);
+}
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+let hasLeveledUp = await Levels.appendXp(
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender,
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+randomXp
+);
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+if (hasLeveledUp) {
+await Levels.setLevel(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, user.level + 1);
+let levelRole = user.level;
+let role = `Warrior`;
+if (levelRole <= 2) {
+role = `Elite III`;
+} else if (levelRole <= 4) {
+role = `Elite II`;
+} else if (levelRole <= 6) {
+role = `Elite I`;
+} else if (levelRole <= 8) {
+role = `Master IV`;
+} else if (levelRole <= 10) {
+role = `Master III`;
+} else if (levelRole <= 12) {
+role = `Master II`;
+} else if (levelRole <= 14) {
+role = `Master I`;
+} else if (levelRole <= 16) {
+role = `Grandmaster V`;
+} else if (levelRole <= 18) {
+role = `Grandmaster IV`;
+} else if (levelRole <= 20) {
+role = `Grandmaster III`;
+} else if (levelRole <= 22) {
+role = `Grandmaster II`;
+} else if (levelRole <= 24) {
+role = `Grandmaster I`;
+} else if (levelRole <= 26) {
+role = `Epic V`;
+} else if (levelRole <= 28) {
+role = `Epic IV`;
+} else if (levelRole <= 30) {
+role = `Epic III`;
+} else if (levelRole <= 32) {
+role = `Epic II`;
+} else if (levelRole <= 34) {
+role = `Epic I`;
+} else if (levelRole <= 36) {
+role = `Legend V`;
+} else if (levelRole <= 38) {
+role = `Legend IV`;
+} else if (levelRole <= 40) {
+role = `Legend III`;
+} else if (levelRole <= 42) {
+role = `Legend II`;
+} else if (levelRole <= 44) {
+role = `Legend I`;
+} else if (levelRole <= 46) {
+role = `Mythic`;
+} else if (levelRole <= 50) {
+role = `Mythic Glory`;
+}
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+let expFile = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/${Date.now()}r${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key.id}.png`;
+let աɦօֆɛ = Math.floor(1000 + Math.random() * 9000);
+let 𝕻𝕻𝖑𝖊𝖙𝖊𝖗;
+try {
+𝕻𝕻𝖑𝖊𝖙𝖊𝖗 = await ᴋᴜɴᴀɪ.profilePictureUrl(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, `image`);
+} catch {
+𝕻𝕻𝖑𝖊𝖙𝖊𝖗 = `./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.png`;
+}
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+try {
+let ResGot = await fetch(
+global.API("https://wall.alphacoders.com/api2.0", "/get.php", {
+auth: "3e7756c85df54b78f934a284c11abe4e",
+method: "search",
+term: "anime landscape",
+})
+);
+let Bson = await ResGot.json();
+let ImgBson =
+Bson.wallpapers[Math.floor(Math.random() * Bson.wallpapers.length)];
+Img = ImgBson.url_image;
+} catch {
+Img = "https://i.postimg.cc/Kc65RLnX/Full.png";
+}
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+let rank = new canvacord.Rank()
+.setAvatar(𝕻𝕻𝖑𝖊𝖙𝖊𝖗)
+.setCurrentXP(user.xp)
+.setRequiredXP(Levels.xpFor(user.level + 1))
+.setRank(user.position)
+.setLevel(user.level)
+.setStatus("online")
+.setBackground("IMAGE", Img)
+.setProgressBar(color, "COLOR")
+.setUsername("Ranker!")
+.setDiscriminator(աɦօֆɛ);
+return await rank.build().then(async (data) => {
+fs.writeFile(expFile, data, async (error) => {
+if (error) return Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, error);
+await ᴋᴜɴᴀɪ.sendPresenceUpdate("composing", Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID);
+await ᴋᴜɴᴀɪ
+.sendMessage(
+Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
+{
+contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
+image: { url: expFile },
+caption: `╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 ║ *❣️ ɴᴀᴍᴇ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "No_Name"}
 ║ *⚡ ɪᴅ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID.split("@")[0]}
 ╚═══════╝
@@ -139,28 +139,28 @@ Congrats!! 🎉🎉
 *🎐Level*: ${user.level}
 *🔮️Role*: *${role}*`,
 
-              footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\n_ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™_",
-              buttons: [
-                {
-                  buttonId: prefix + "rank",
-                  buttonText: { displayText: prefix + "rank" },
-                  type: 1,
-                },
-              ],
-              headerType: 4,
-            },
-            {
-              quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
-            }
-          )
-          .catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
-        return fs.unlinkSync(expFile);
-      });
-    });
-  }
+footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\n_ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™_",
+buttons: [
+{
+buttonId: prefix + "rank",
+buttonText: { displayText: prefix + "rank" },
+type: 1,
+},
+],
+headerType: 4,
+},
+{
+quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
+}
+)
+.catch((e) => Caught(ᴋᴜɴᴀɪ, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, e));
+return fs.unlinkSync(expFile);
+});
+});
+}
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
- * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
- */
+* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+*/
 ("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
