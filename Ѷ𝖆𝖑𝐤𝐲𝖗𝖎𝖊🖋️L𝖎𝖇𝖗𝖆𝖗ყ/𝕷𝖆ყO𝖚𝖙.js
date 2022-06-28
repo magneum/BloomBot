b709f967dde8b0e4f534e7b2decd118a0ec7cbcc
KryKnz <krakinzkon@gmail.com>
2022-06-28 15:44:27 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-28 15:44:26

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙.js
@@ -1,200 +1,104 @@
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
 * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
 */
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-function _0xc116(_0x3fe2e4, _0x322ebf) {
-const _0x3acca4 = _0x3acc();
-return (
-(_0xc116 = function (_0xc116c9, _0x20258c) {
-_0xc116c9 = _0xc116c9 - 0x1e2;
-let _0x2f60d2 = _0x3acca4[_0xc116c9];
-return _0x2f60d2;
-}),
-_0xc116(_0x3fe2e4, _0x322ebf)
-);
+("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
+let fs = require("fs");
+let sequelize = require("sequelize");
+if (fs.existsSync("Ѷ𝖆𝖑.env")) {
+require("dotenv").config({ path: "./Ѷ𝖆𝖑.env" });
+} else {
+require("dotenv");
 }
-const _0x240a5d = _0xc116;
-function _0x3acc() {
-const _0xb2c59c = [
-"pais",
-"dotenv",
-"port",
-"1250898WWZCZA",
-"1686838XkmIFF",
-"toLowerCase",
-"env",
-"prefix",
-"true",
-"https://nurutomo.herokuapp.com",
-"APIs",
-"DATABASE_URL",
-"./Ѷ𝖆𝖑.db",
-"HEROKU_API_KEY",
-"APIKeys",
-"Ѷ𝖆𝖑.env",
-"1223512bXciLc",
-"sqlite",
-"split",
-"https://pencarikode.xyz",
-"34615AqtbDA",
-"sequelize",
-"log",
-"entries",
-"author",
-"https://leyscoders-api.herokuapp.com",
-"packname",
-"https://api.xteam.xyz",
-"existsSync",
-"MONGOOSE",
-"postgres",
-"https://api.zeks.xyz",
-"https://api.dhamzxploit.my.id",
-"Sequelize",
-"MongoDb",
-"5147420VefGyn",
-"HEROKU_APP_NAME",
-"DATABASE",
-"API",
-"944560CCmsLn",
-"MODS",
-"HEROKU",
-"name",
-"false",
-"owner",
-"917430922909",
-"dappakntlll",
-"apivinz",
-"3386790HvVOQk",
-];
-_0x3acc = function () {
-return _0xb2c59c;
+let Log = (value) => {
+var log = false;
+if (typeof value === "string") {
+if (value.toLowerCase() === "true") {
+log = console.log;
+}
+}
+return log;
 };
-return _0x3acc();
-}
-(function (_0x5367ca, _0x479db4) {
-const _0x29cac6 = _0xc116,
-_0x1cf638 = _0x5367ca();
-while (!![]) {
-try {
-const _0x21d93a =
--parseInt(_0x29cac6(0x1e2)) / 0x1 +
-parseInt(_0x29cac6(0x1f0)) / 0x2 +
--parseInt(_0x29cac6(0x1ef)) / 0x3 +
--parseInt(_0x29cac6(0x1fc)) / 0x4 +
-parseInt(_0x29cac6(0x20f)) / 0x5 +
-parseInt(_0x29cac6(0x1eb)) / 0x6 +
--parseInt(_0x29cac6(0x200)) / 0x7;
-if (_0x21d93a === _0x479db4) break;
-else _0x1cf638["push"](_0x1cf638["shift"]());
-} catch (_0x3b20f1) {
-_0x1cf638["push"](_0x1cf638["shift"]());
-}
-}
-})(_0x3acc, 0xbac5b);
-let fs = require("fs"),
-sequelize = require(_0x240a5d(0x201));
-fs[_0x240a5d(0x208)](_0x240a5d(0x1fb))
-? require(_0x240a5d(0x1ed))["config"]({ path: "./Ѷ𝖆𝖑.env" })
-: require(_0x240a5d(0x1ed));
-let Log = (_0x3f9758) => {
-const _0x1a418e = _0x240a5d;
-var _0x488af6 = ![];
-return (
-typeof _0x3f9758 === "string" &&
-_0x3f9758[_0x1a418e(0x1f1)]() === _0x1a418e(0x1f4) &&
-(_0x488af6 = console[_0x1a418e(0x202)]),
-_0x488af6
-);
-};
-process["env"][_0x240a5d(0x1f7)] =
-process[_0x240a5d(0x1f2)][_0x240a5d(0x1f7)] === undefined
-? _0x240a5d(0x1f8)
-: process["env"][_0x240a5d(0x1f7)];
-let gg = process[_0x240a5d(0x1f2)][_0x240a5d(0x1e3)];
-!gg && (gg = _0x240a5d(0x1e8));
-(global["HEROKU"] = process[_0x240a5d(0x1f2)][_0x240a5d(0x1e4)]),
-(global[_0x240a5d(0x1f9)] =
-process[_0x240a5d(0x1f2)][_0x240a5d(0x1f9)] === undefined
-? "Null"
-: process[_0x240a5d(0x1f2)][_0x240a5d(0x1f9)]),
-(global[_0x240a5d(0x210)] =
-process[_0x240a5d(0x1f2)][_0x240a5d(0x210)] === undefined
-? "Null"
-: process[_0x240a5d(0x1f2)][_0x240a5d(0x210)]),
-(global[_0x240a5d(0x1e7)] = gg[_0x240a5d(0x1fe)](",")),
-(global[_0x240a5d(0x209)] = process[_0x240a5d(0x1f2)][_0x240a5d(0x20e)]),
-(global[_0x240a5d(0x1f7)] =
-process[_0x240a5d(0x1f2)]["DATABASE_URL"] === undefined
+process.env.DATABASE_URL =
+process.env.DATABASE_URL === undefined
 ? "./Ѷ𝖆𝖑.db"
-: process[_0x240a5d(0x1f2)][_0x240a5d(0x1f7)]),
-(global[_0x240a5d(0x211)] =
-process[_0x240a5d(0x1f2)][_0x240a5d(0x1f7)] === _0x240a5d(0x1f8)
-? new sequelize[_0x240a5d(0x20d)]({
-dialect: _0x240a5d(0x1fd),
-storage: process[_0x240a5d(0x1f2)]["DATABASE_URL"],
+: process.env.DATABASE_URL;
+
+let gg = process.env.MODS;
+if (!gg) {
+gg = "917430922909";
+}
+global.VlkyreYT = process.env.VlkyreYT;
+global.HEROKU = process.env.HEROKU;
+global.HEROKU_API_KEY =
+process.env.HEROKU_API_KEY === undefined
+? "Null"
+: process.env.HEROKU_API_KEY;
+global.HEROKU_APP_NAME =
+process.env.HEROKU_APP_NAME === undefined
+? "Null"
+: process.env.HEROKU_APP_NAME;
+global.owner = gg.split(",");
+global.MONGOOSE = process.env.MongoDb;
+global.DATABASE_URL =
+process.env.DATABASE_URL === undefined
+? "./Ѷ𝖆𝖑.db"
+: process.env.DATABASE_URL;
+global.DATABASE =
+process.env.DATABASE_URL === "./Ѷ𝖆𝖑.db"
+? new sequelize.Sequelize({
+dialect: "sqlite",
+storage: process.env.DATABASE_URL,
 logging: Log("false"),
 })
-: new sequelize[_0x240a5d(0x20d)](
-process[_0x240a5d(0x1f2)][_0x240a5d(0x1f7)],
-{
-dialect: _0x240a5d(0x20a),
+: new sequelize.Sequelize(process.env.DATABASE_URL, {
+dialect: "postgres",
 protocol: "postgres",
-logging: Log(_0x240a5d(0x1e6)),
-dialectOptions: { ssl: { require: !![], rejectUnauthorized: ![] } },
-}
-)),
-(global[_0x240a5d(0x1f3)] = process["env"]["PREFIX"] || "="),
-(global[_0x240a5d(0x1ee)] = process[_0x240a5d(0x1f2)]["PORT"] || 0x1f90),
-(global[_0x240a5d(0x206)] = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊"),
-(global[_0x240a5d(0x204)] = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊"),
-(global[_0x240a5d(0x1e5)] = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊"),
-(global["APIs"] = {
-nrtm: _0x240a5d(0x1f5),
-dzx: _0x240a5d(0x20c),
-xteam: _0x240a5d(0x207),
+logging: Log("false"),
+dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
+global.prefix = process.env.PREFIX || "=";
+global.port = process.env.PORT || 8080;
+global.packname = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊";
+global.author = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊";
+global.name = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊";
+global.APIs = {
+nrtm: "https://nurutomo.herokuapp.com",
+dzx: "https://api.dhamzxploit.my.id",
+xteam: "https://api.xteam.xyz",
 zahir: "https://zahirr-web.herokuapp.com",
-zeks: _0x240a5d(0x20b),
-pencarikode: _0x240a5d(0x1ff),
-LeysCoder: _0x240a5d(0x205),
-}),
-(global[_0x240a5d(0x1fa)] = {
+zeks: "https://api.zeks.xyz",
+pencarikode: "https://pencarikode.xyz",
+LeysCoder: "https://leyscoders-api.herokuapp.com",
+};
+global.APIKeys = {
 "https://api.xteam.xyz": "d90a9e986e18778b",
 "https://zahirr-web.herokuapp.com": "zahirgans",
-"https://api.zeks.xyz": _0x240a5d(0x1ea),
-"https://pencarikode.xyz": _0x240a5d(0x1ec),
-"https://leyscoders-api.herokuapp.com": _0x240a5d(0x1e9),
-}),
-(global[_0x240a5d(0x212)] = (
-_0x3e5232,
-_0x580dba = "/",
-_0x26f94b = {},
-_0x55ef52
-) =>
-(_0x3e5232 in global[_0x240a5d(0x1f6)]
-? global["APIs"][_0x3e5232]
-: _0x3e5232) +
-_0x580dba +
-(_0x26f94b || _0x55ef52
+"https://api.zeks.xyz": "apivinz",
+"https://pencarikode.xyz": "pais",
+"https://leyscoders-api.herokuapp.com": "dappakntlll",
+};
+global.API = (name, path = "/", query = {}, apikeyqueryname) =>
+(name in global.APIs ? global.APIs[name] : name) +
+path +
+(query || apikeyqueryname
 ? "?" +
 new URLSearchParams(
-Object[_0x240a5d(0x203)]({
-..._0x26f94b,
-...(_0x55ef52
+Object.entries({
+...query,
+...(apikeyqueryname
 ? {
-[_0x55ef52]:
-global["APIKeys"][
-_0x3e5232 in global[_0x240a5d(0x1f6)]
-? global["APIs"][_0x3e5232]
-: _0x3e5232
+[apikeyqueryname]:
+global.APIKeys[
+name in global.APIs ? global.APIs[name] : name
 ],
 }
 : {}),
 })
 )
-: ""));
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+: "");
+("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
 * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
 */
-("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
+("|⬡════════════════════════════════════════════════════════════|▷   ʍǟɖɛ ɮʏ ӄʀǟӄɨռʐʟǟɮ™  ◁|════════════════════════════════════════════════════════════⬡|");
