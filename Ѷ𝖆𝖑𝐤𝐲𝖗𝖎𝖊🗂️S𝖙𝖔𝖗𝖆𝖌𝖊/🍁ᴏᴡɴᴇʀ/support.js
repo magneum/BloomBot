89c13c53a7abda774456f17ac5dd025b631c7808
KryKnz <krakinzkon@gmail.com>
2022-06-29 17:34:29 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-29 17:34:28

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🗂️S𝖙𝖔𝖗𝖆𝖌𝖊/🍁ᴏᴡɴᴇʀ/support.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🗂️S𝖙𝖔𝖗𝖆𝖌𝖊/🍁ᴏᴡɴᴇʀ/support.js
@@ -52,6 +52,9 @@ F𝖚𝖑𝖑_A𝖗𝖌𝖘,
 quoted,
 isMedia,
 isGroup,
+groupMetadata,
+groupName,
+participants,
 groupAdmins,
 isBotAdmin,
 isSenderAdmin,
@@ -62,139 +65,77 @@ isReply,
 mentionByTag,
 mentionByReply
 ) => {
-(function (_0x434395, _0x22377a) {
-const _0x33ce67 = _0x310e,
-_0x46c5fe = _0x434395();
-while (!![]) {
-try {
-const _0x17d173 =
--parseInt(_0x33ce67(0x10a)) / 0x1 +
--parseInt(_0x33ce67(0xf3)) / 0x2 +
-parseInt(_0x33ce67(0x10d)) / 0x3 +
-parseInt(_0x33ce67(0xf2)) / 0x4 +
-parseInt(_0x33ce67(0xfd)) / 0x5 +
-parseInt(_0x33ce67(0x102)) / 0x6 +
-(parseInt(_0x33ce67(0xf7)) / 0x7) *
-(-parseInt(_0x33ce67(0x107)) / 0x8);
-if (_0x17d173 === _0x22377a) break;
-else _0x46c5fe["push"](_0x46c5fe["shift"]());
-} catch (_0x3a4804) {
-_0x46c5fe["push"](_0x46c5fe["shift"]());
-}
-}
-})(_0x52d4, 0x9bcf4);
-function _0x310e(_0x20f2d9, _0x34552f) {
-const _0x52d4ef = _0x52d4();
-return (
-(_0x310e = function (_0x310ee9, _0x9cf02a) {
-_0x310ee9 = _0x310ee9 - 0xf1;
-let _0x5e5316 = _0x52d4ef[_0x310ee9];
-return _0x5e5316;
-}),
-_0x310e(_0x20f2d9, _0x34552f)
+let used = process.memoryUsage();
+let cpus = os.cpus().map((cpu) => {
+cpu.total = Object.keys(cpu.times).reduce(
+(last, type) => last + cpu.times[type],
+0
 );
-}
-let used = process["memoryUsage"](),
-cpus = os[_0x2adf50(0x105)]()["map"]((_0x2962a9) => {
-const _0x5c083d = _0x2adf50;
-return (
-(_0x2962a9[_0x5c083d(0xfc)] = Object[_0x5c083d(0xf1)](
-_0x2962a9[_0x5c083d(0xfa)]
-)[_0x5c083d(0xf4)](
-(_0x5871c9, _0x3c65e6) =>
-_0x5871c9 + _0x2962a9[_0x5c083d(0xfa)][_0x3c65e6],
-0x0
-)),
-_0x2962a9
-);
-}),
-cpu = cpus["reduce"](
-(_0x3424e0, _0x109d26, _0x3c91de, { length: _0x445896 }) => {
-const _0x12bf25 = _0x2adf50;
-return (
-(_0x3424e0[_0x12bf25(0xfc)] += _0x109d26[_0x12bf25(0xfc)]),
-(_0x3424e0[_0x12bf25(0xf9)] +=
-_0x109d26[_0x12bf25(0xf9)] / _0x445896),
-(_0x3424e0[_0x12bf25(0xfa)][_0x12bf25(0x10e)] +=
-_0x109d26[_0x12bf25(0xfa)]["user"]),
-(_0x3424e0["times"]["nice"] += _0x109d26[_0x12bf25(0xfa)]["nice"]),
-(_0x3424e0[_0x12bf25(0xfa)]["sys"] += _0x109d26["times"]["sys"]),
-(_0x3424e0[_0x12bf25(0xfa)]["idle"] +=
-_0x109d26[_0x12bf25(0xfa)][_0x12bf25(0x108)]),
-(_0x3424e0[_0x12bf25(0xfa)][_0x12bf25(0xf8)] +=
-_0x109d26["times"][_0x12bf25(0xf8)]),
-_0x3424e0
-);
+return cpu;
+});
+let cpu = cpus.reduce(
+(last, cpu, _, { length }) => {
+last.total += cpu.total;
+last.speed += cpu.speed / length;
+last.times.user += cpu.times.user;
+last.times.nice += cpu.times.nice;
+last.times.sys += cpu.times.sys;
+last.times.idle += cpu.times.idle;
+last.times.irq += cpu.times.irq;
+return last;
 },
 {
-speed: 0x0,
-total: 0x0,
-times: { user: 0x0, nice: 0x0, sys: 0x0, idle: 0x0, irq: 0x0 },
+speed: 0,
+total: 0,
+times: {
+user: 0,
+nice: 0,
+sys: 0,
+idle: 0,
+irq: 0,
+},
 }
-),
-timestamp = speed(),
-latensi = speed() - timestamp,
-latest = ("════════★\x20𝐍𝐨𝐝𝐞𝐉𝐒\x20𝐌𝐞𝐦-𝐔𝐬𝐚𝐠𝐞:\x0a" +
-Object[_0x2adf50(0xf1)](used)
-[_0x2adf50(0x10c)](
-(_0x38d5b2, _0x264dee, _0x3754ca) =>
-_0x38d5b2[_0x2adf50(0x109)](
-Math[_0x2adf50(0xff)](
-..._0x3754ca[_0x2adf50(0x10c)](
-(_0x17489f) => _0x17489f[_0x2adf50(0x10b)]
+);
+let timestamp = speed();
+let latensi = speed() - timestamp;
+let latest = `════════★ 𝐍𝐨𝐝𝐞𝐉𝐒 𝐌𝐞𝐦-𝐔𝐬𝐚𝐠𝐞:
+${Object.keys(used)
+.map(
+(key, _, arr) =>
+`${key.padEnd(Math.max(...arr.map((v) => v.length)), ` `)}: ${formatp(
+used[key]
+)}`
 )
-),
-"\x20"
-) +
-":\x20" +
-formatp(used[_0x38d5b2])
+.join(`\n`)}
+════════★ ${
+cpus[0]
+? `𝐓𝐨𝐭𝐚𝐥 𝐂𝐏𝐔 𝐔𝐬𝐚𝐠𝐞
+${cpus[0].model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
+.map(
+(type) =>
+`- *${(type + `*`).padEnd(6)}: ${(
+(100 * cpu.times[type]) /
+cpu.total
+).toFixed(2)}%`
 )
-[_0x2adf50(0xf5)]("\x0a") +
-_0x2adf50(0x104) +
-(cpus[0x0]
-? _0x2adf50(0x101) +
-cpus[0x0][_0x2adf50(0x106)][_0x2adf50(0xfb)]() +
-"\x20(" +
-cpu[_0x2adf50(0xf9)] +
-_0x2adf50(0xf6) +
-Object[_0x2adf50(0xf1)](cpu[_0x2adf50(0xfa)])
-[_0x2adf50(0x10c)](
-(_0xa7afcb) =>
-"-\x20*" +
-(_0xa7afcb + "*")["padEnd"](0x6) +
-":\x20" +
-((0x64 * cpu[_0x2adf50(0xfa)][_0xa7afcb]) / cpu["total"])[
-_0x2adf50(0x100)
-](0x2) +
-"%"
+.join(`\n`)}
+════════★ 𝐂𝐏𝐔 𝐂𝐨𝐫𝐞(𝐬) 𝐔𝐬𝐚𝐠𝐞 (_${cpus.length}_ 𝐂𝐨𝐫𝐞 𝐂𝐏𝐔)
+${cpus
+.map(
+(cpu, i) =>
+`${i + 1}. ${cpu.model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
+.map(
+(type) =>
+`- *${(type + `*`).padEnd(6)}: ${(
+(100 * cpu.times[type]) /
+cpu.total
+).toFixed(2)}%`
 )
-[_0x2adf50(0xf5)]("\x0a") +
-_0x2adf50(0xfe) +
-cpus[_0x2adf50(0x10b)] +
-_0x2adf50(0x103) +
-cpus["map"](
-(_0x301d48, _0x8698b8) =>
-_0x8698b8 +
-0x1 +
-".\x20" +
-_0x301d48[_0x2adf50(0x106)][_0x2adf50(0xfb)]() +
-"\x20(" +
-_0x301d48[_0x2adf50(0xf9)] +
-"\x20ᴍʜᴢ)" +
-Object[_0x2adf50(0xf1)](_0x301d48[_0x2adf50(0xfa)])
-[_0x2adf50(0x10c)](
-(_0x1d1d8c) =>
-"-\x20*" +
-(_0x1d1d8c + "*")[_0x2adf50(0x109)](0x6) +
-":\x20" +
-((0x64 * _0x301d48[_0x2adf50(0xfa)][_0x1d1d8c]) /
-_0x301d48[_0x2adf50(0xfc)])[_0x2adf50(0x100)](0x2) +
-"%"
+.join(`\n`)}`
 )
-[_0x2adf50(0xf5)]("\x0a")
-)["join"]("\x0a\x0a")
-: ""))[_0x2adf50(0xfb)]();
-`|⬡════════════════════════════════════════════════════════════════════「  𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐀𝐩𝐢®  」═══════════════════════════════════════════════════════════════════⬡|`;
+.join(`\n\n`)}`
+: ``
+}`.trim();
 let ʀᴇꜱᴘᴏɴꜱᴇ = await ᴋᴜɴᴀɪ.groupInviteCode("120363020792949649@g.us");
 return await Image_Button(
 ᴋᴜɴᴀɪ,
