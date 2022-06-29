289df2fbf4ff528e707c134342c21e8da6536db9
KryKnz <krakinzkon@gmail.com>
2022-06-29 09:37:11 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-06-29 9:37:10

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙.js
@@ -3,198 +3,100 @@
 // ════════════════════════|▷   (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!   ◁|════════════════════════
 
 `|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|`;
-const _0x1ed47c = _0x4f32;
-(function (_0x714d64, _0x31dfd5) {
-const _0x222991 = _0x4f32,
-_0xa09054 = _0x714d64();
-while (!![]) {
-try {
-const _0x50b8e7 =
-parseInt(_0x222991(0x110)) / 0x1 +
-parseInt(_0x222991(0x11f)) / 0x2 +
--parseInt(_0x222991(0x120)) / 0x3 +
--parseInt(_0x222991(0x106)) / 0x4 +
--parseInt(_0x222991(0x11c)) / 0x5 +
-(-parseInt(_0x222991(0x102)) / 0x6) *
-(parseInt(_0x222991(0x128)) / 0x7) +
-parseInt(_0x222991(0x121)) / 0x8;
-if (_0x50b8e7 === _0x31dfd5) break;
-else _0xa09054["push"](_0xa09054["shift"]());
-} catch (_0x48bb18) {
-_0xa09054["push"](_0xa09054["shift"]());
+let fs = require("fs");
+let sequelize = require("sequelize");
+if (fs.existsSync("Ѷ𝖆𝖑.env")) {
+require("dotenv").config({ path: "./Ѷ𝖆𝖑.env" });
+} else {
+require("dotenv");
+}
+let Log = (value) => {
+var log = false;
+if (typeof value === "string") {
+if (value.toLowerCase() === "true") {
+log = console.log;
 }
 }
-})(_0x228e, 0x52ffb);
-let fs = require("fs"),
-sequelize = require(_0x1ed47c(0x126));
-fs[_0x1ed47c(0x11a)](_0x1ed47c(0x103))
-? require(_0x1ed47c(0x10d))[_0x1ed47c(0x10f)]({ path: _0x1ed47c(0x12b) })
-: require("dotenv");
-let Log = (_0x2b42f7) => {
-const _0x3dc3f9 = _0x1ed47c;
-var _0x4f2226 = ![];
-return (
-typeof _0x2b42f7 === _0x3dc3f9(0x108) &&
-_0x2b42f7[_0x3dc3f9(0x10a)]() === "true" &&
-(_0x4f2226 = console[_0x3dc3f9(0x11e)]),
-_0x4f2226
-);
+return log;
 };
-process[_0x1ed47c(0x127)][_0x1ed47c(0x10c)] =
-process[_0x1ed47c(0x127)][_0x1ed47c(0x10c)] === undefined
-? _0x1ed47c(0x129)
-: process["env"][_0x1ed47c(0x10c)];
-let gg = process[_0x1ed47c(0x127)]["MODS"];
-function _0x4f32(_0x536ad1, _0x5cb38e) {
-const _0x228eb2 = _0x228e();
-return (
-(_0x4f32 = function (_0x4f327e, _0x4c93f0) {
-_0x4f327e = _0x4f327e - 0xff;
-let _0x368559 = _0x228eb2[_0x4f327e];
-return _0x368559;
-}),
-_0x4f32(_0x536ad1, _0x5cb38e)
-);
-}
-!gg && (gg = _0x1ed47c(0x11d));
-function _0x228e() {
-const _0x23f879 = [
-"Null",
-"apivinz",
-"existsSync",
-"PREFIX",
-"1009950NDaPkm",
-"917430922909",
-"log",
-"589518yJySVg",
-"872988waiiTz",
-"5729160oohkmr",
-"https://api.dhamzxploit.my.id",
-"MongoDb",
-"APIs",
-"https://nurutomo.herokuapp.com",
-"sequelize",
-"env",
-"1957004ojENEL",
-"./Ѷ𝖆𝖑.db",
-"https://zahirr-web.herokuapp.com",
-"./Ѷ𝖆𝖑.env",
-"VlkyreYT",
-"API",
-"HEROKU_APP_NAME",
-"postgres",
-"Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊",
-"name",
-"dappakntlll",
-"12oVscTz",
-"Ѷ𝖆𝖑.env",
-"HEROKU",
-"sqlite",
-"130984YmDJQi",
-"split",
-"string",
-"MONGOOSE",
-"toLowerCase",
-"packname",
-"DATABASE_URL",
-"dotenv",
-"HEROKU_API_KEY",
-"config",
-"413935EiNZrd",
-"false",
-"d90a9e986e18778b",
-"entries",
-"owner",
-"PORT",
-"pais",
-"author",
-];
-_0x228e = function () {
-return _0x23f879;
-};
-return _0x228e();
-}
-(global[_0x1ed47c(0x12c)] = process[_0x1ed47c(0x127)][_0x1ed47c(0x12c)]),
-(global[_0x1ed47c(0x104)] = process[_0x1ed47c(0x127)][_0x1ed47c(0x104)]),
-(global[_0x1ed47c(0x10e)] =
-process[_0x1ed47c(0x127)][_0x1ed47c(0x10e)] === undefined
-? _0x1ed47c(0x118)
-: process[_0x1ed47c(0x127)][_0x1ed47c(0x10e)]),
-(global[_0x1ed47c(0x12e)] =
-process[_0x1ed47c(0x127)][_0x1ed47c(0x12e)] === undefined
-? _0x1ed47c(0x118)
-: process[_0x1ed47c(0x127)][_0x1ed47c(0x12e)]),
-(global[_0x1ed47c(0x114)] = gg[_0x1ed47c(0x107)](",")),
-(global[_0x1ed47c(0x109)] = process["env"][_0x1ed47c(0x123)]),
-(global[_0x1ed47c(0x10c)] =
-process[_0x1ed47c(0x127)][_0x1ed47c(0x10c)] === undefined
+process.env.DATABASE_URL =
+process.env.DATABASE_URL === undefined
 ? "./Ѷ𝖆𝖑.db"
-: process["env"][_0x1ed47c(0x10c)]),
-(global["DATABASE"] =
-process[_0x1ed47c(0x127)][_0x1ed47c(0x10c)] === "./Ѷ𝖆𝖑.db"
-? new sequelize["Sequelize"]({
-dialect: _0x1ed47c(0x105),
-storage: process[_0x1ed47c(0x127)]["DATABASE_URL"],
-logging: Log(_0x1ed47c(0x111)),
-})
-: new sequelize["Sequelize"](
-process[_0x1ed47c(0x127)][_0x1ed47c(0x10c)],
-{
-dialect: _0x1ed47c(0x12f),
-protocol: "postgres",
-logging: Log(_0x1ed47c(0x111)),
-dialectOptions: { ssl: { require: !![], rejectUnauthorized: ![] } },
+: process.env.DATABASE_URL;
+
+let gg = process.env.MODS;
+if (!gg) {
+gg = "917430922909";
 }
-)),
-(global["prefix"] = process["env"][_0x1ed47c(0x11b)] || "="),
-(global["port"] = process[_0x1ed47c(0x127)][_0x1ed47c(0x115)] || 0x1f90),
-(global[_0x1ed47c(0x10b)] = _0x1ed47c(0xff)),
-(global[_0x1ed47c(0x117)] = _0x1ed47c(0xff)),
-(global[_0x1ed47c(0x100)] = _0x1ed47c(0xff)),
-(global[_0x1ed47c(0x124)] = {
-nrtm: _0x1ed47c(0x125),
-dzx: _0x1ed47c(0x122),
+global.VlkyreYT = process.env.VlkyreYT;
+global.HEROKU = process.env.HEROKU;
+global.HEROKU_API_KEY =
+process.env.HEROKU_API_KEY === undefined
+? "Null"
+: process.env.HEROKU_API_KEY;
+global.HEROKU_APP_NAME =
+process.env.HEROKU_APP_NAME === undefined
+? "Null"
+: process.env.HEROKU_APP_NAME;
+global.owner = gg.split(",");
+global.MONGOOSE = process.env.MongoDb;
+global.DATABASE_URL =
+process.env.DATABASE_URL === undefined
+? "./Ѷ𝖆𝖑.db"
+: process.env.DATABASE_URL;
+global.DATABASE =
+process.env.DATABASE_URL === "./Ѷ𝖆𝖑.db"
+? new sequelize.Sequelize({
+dialect: "sqlite",
+storage: process.env.DATABASE_URL,
+logging: Log("false"),
+})
+: new sequelize.Sequelize(process.env.DATABASE_URL, {
+dialect: "postgres",
+protocol: "postgres",
+logging: Log("false"),
+dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
+global.prefix = process.env.PREFIX || "=";
+global.port = process.env.PORT || 8080;
+global.packname = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊";
+global.author = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊";
+global.name = "Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊";
+global.APIs = {
+nrtm: "https://nurutomo.herokuapp.com",
+dzx: "https://api.dhamzxploit.my.id",
 xteam: "https://api.xteam.xyz",
-zahir: _0x1ed47c(0x12a),
+zahir: "https://zahirr-web.herokuapp.com",
 zeks: "https://api.zeks.xyz",
 pencarikode: "https://pencarikode.xyz",
 LeysCoder: "https://leyscoders-api.herokuapp.com",
-}),
-(global["APIKeys"] = {
-"https://api.xteam.xyz": _0x1ed47c(0x112),
+};
+global.APIKeys = {
+"https://api.xteam.xyz": "d90a9e986e18778b",
 "https://zahirr-web.herokuapp.com": "zahirgans",
-"https://api.zeks.xyz": _0x1ed47c(0x119),
-"https://pencarikode.xyz": _0x1ed47c(0x116),
-"https://leyscoders-api.herokuapp.com": _0x1ed47c(0x101),
-}),
-(global[_0x1ed47c(0x12d)] = (
-_0x27a8e1,
-_0x54c81f = "/",
-_0x22e654 = {},
-_0x4c86b0
-) =>
-(_0x27a8e1 in global[_0x1ed47c(0x124)]
-? global["APIs"][_0x27a8e1]
-: _0x27a8e1) +
-_0x54c81f +
-(_0x22e654 || _0x4c86b0
+"https://api.zeks.xyz": "apivinz",
+"https://pencarikode.xyz": "pais",
+"https://leyscoders-api.herokuapp.com": "dappakntlll",
+};
+global.API = (name, path = "/", query = {}, apikeyqueryname) =>
+(name in global.APIs ? global.APIs[name] : name) +
+path +
+(query || apikeyqueryname
 ? "?" +
 new URLSearchParams(
-Object[_0x1ed47c(0x113)]({
-..._0x22e654,
-...(_0x4c86b0
+Object.entries({
+...query,
+...(apikeyqueryname
 ? {
-[_0x4c86b0]:
-global["APIKeys"][
-_0x27a8e1 in global[_0x1ed47c(0x124)]
-? global[_0x1ed47c(0x124)][_0x27a8e1]
-: _0x27a8e1
+[apikeyqueryname]:
+global.APIKeys[
+name in global.APIs ? global.APIs[name] : name
 ],
 }
 : {}),
 })
 )
-: ""));
+: "");
 `|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|`;
 
 // ════════════════════════|▷   (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!   ◁|════════════════════════
