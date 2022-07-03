32015795600ee14ad5d029f7aa7cdf28ed7d6642
KryKnz <krakinzkon@gmail.com>
2022-07-04 01:13:45 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-07-04 1:13:44

--- 🕯️ᴋᴇʀɴᴇʟ.js
+++ 🕯️ᴋᴇʀɴᴇʟ.js
@@ -1,167 +1,167 @@
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
- * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
- */
+* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+*/
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 const _0x1c912f = _0x1701;
 (function (_0x7d6cd8, _0x18743e) {
-  const _0x31094a = _0x1701,
-    _0x253e53 = _0x7d6cd8();
-  while (!![]) {
-    try {
-      const _0x39fe4d =
-        parseInt(_0x31094a(0x195)) / 0x1 +
-        -parseInt(_0x31094a(0x193)) / 0x2 +
-        (parseInt(_0x31094a(0x198)) / 0x3) *
-          (parseInt(_0x31094a(0x18d)) / 0x4) +
-        (-parseInt(_0x31094a(0x1a2)) / 0x5) *
-          (-parseInt(_0x31094a(0x192)) / 0x6) +
-        -parseInt(_0x31094a(0x1a8)) / 0x7 +
-        parseInt(_0x31094a(0x19b)) / 0x8 +
-        (parseInt(_0x31094a(0x18f)) / 0x9) *
-          (-parseInt(_0x31094a(0x18a)) / 0xa);
-      if (_0x39fe4d === _0x18743e) break;
-      else _0x253e53["push"](_0x253e53["shift"]());
-    } catch (_0x2beff2) {
-      _0x253e53["push"](_0x253e53["shift"]());
-    }
-  }
+const _0x31094a = _0x1701,
+_0x253e53 = _0x7d6cd8();
+while (!![]) {
+try {
+const _0x39fe4d =
+parseInt(_0x31094a(0x195)) / 0x1 +
+-parseInt(_0x31094a(0x193)) / 0x2 +
+(parseInt(_0x31094a(0x198)) / 0x3) *
+(parseInt(_0x31094a(0x18d)) / 0x4) +
+(-parseInt(_0x31094a(0x1a2)) / 0x5) *
+(-parseInt(_0x31094a(0x192)) / 0x6) +
+-parseInt(_0x31094a(0x1a8)) / 0x7 +
+parseInt(_0x31094a(0x19b)) / 0x8 +
+(parseInt(_0x31094a(0x18f)) / 0x9) *
+(-parseInt(_0x31094a(0x18a)) / 0xa);
+if (_0x39fe4d === _0x18743e) break;
+else _0x253e53["push"](_0x253e53["shift"]());
+} catch (_0x2beff2) {
+_0x253e53["push"](_0x253e53["shift"]());
+}
+}
 })(_0x2cd1, 0xdce72);
 let ᴅᴇꜱɢ = require(_0x1c912f(0x196)),
-  ʀɪᴍʀᴀꜰ = require("rimraf"),
-  ɢɪᴛ = require(_0x1c912f(0x1a9))(),
-  vers = require(_0x1c912f(0x199));
+ʀɪᴍʀᴀꜰ = require("rimraf"),
+ɢɪᴛ = require(_0x1c912f(0x1a9))(),
+vers = require(_0x1c912f(0x199));
 _0x1c912f(0x187);
 function _0x2cd1() {
-  const _0x43f454 = [
-    "565zxmSNS",
-    "red",
-    "error",
-    "git\x20pull\x20&&\x20git\x20add\x20--all\x20&&\x20git\x20commit\x20-am\x20\x22「\x20Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊™\x20」:\x20",
-    "keys",
-    "exitCode",
-    "9485994nWkUKk",
-    "simple-git",
-    "🐍Ş𝖎𝖕𝖍𝖔𝖓®..origin/🐍Ş𝖎𝖕𝖍𝖔𝖓®",
-    "|⬡═════════════════════════════════════════════════════════════════════|▷\x20𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞\x20◁|═════════════════════════════════════════════════════════════════════⬡|",
-    "slice",
-    "getMonth",
-    "130ouwUYp",
-    "./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/ꜱᴘᴀᴄᴇ.json",
-    "ꜱᴘᴀᴄᴇ",
-    "5326280MOQnSB",
-    "stderr",
-    "1076418yMJzTo",
-    "git\x20pull",
-    "exec",
-    "1338oNmgHW",
-    "978256BdmLCQ",
-    "blue",
-    "1656845rQCxTb",
-    "chalk",
-    "log",
-    "3acxgof",
-    "./package.json",
-    "exit",
-    "10322400JMzlUG",
-    "getSeconds",
-    "vers",
-    "\x20📅𝐃𝐚𝐭𝐞:\x20",
-    "getMinutes",
-    "fetch",
-    "child_process",
-  ];
-  _0x2cd1 = function () {
-    return _0x43f454;
-  };
-  return _0x2cd1();
+const _0x43f454 = [
+"565zxmSNS",
+"red",
+"error",
+"git\x20pull\x20&&\x20git\x20add\x20--all\x20&&\x20git\x20commit\x20-am\x20\x22「\x20Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊™\x20」:\x20",
+"keys",
+"exitCode",
+"9485994nWkUKk",
+"simple-git",
+"🐍Ş𝖎𝖕𝖍𝖔𝖓®..origin/🐍Ş𝖎𝖕𝖍𝖔𝖓®",
+"|⬡═════════════════════════════════════════════════════════════════════|▷\x20𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞\x20◁|═════════════════════════════════════════════════════════════════════⬡|",
+"slice",
+"getMonth",
+"130ouwUYp",
+"./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/ꜱᴘᴀᴄᴇ.json",
+"ꜱᴘᴀᴄᴇ",
+"5326280MOQnSB",
+"stderr",
+"1076418yMJzTo",
+"git\x20pull",
+"exec",
+"1338oNmgHW",
+"978256BdmLCQ",
+"blue",
+"1656845rQCxTb",
+"chalk",
+"log",
+"3acxgof",
+"./package.json",
+"exit",
+"10322400JMzlUG",
+"getSeconds",
+"vers",
+"\x20📅𝐃𝐚𝐭𝐞:\x20",
+"getMinutes",
+"fetch",
+"child_process",
+];
+_0x2cd1 = function () {
+return _0x43f454;
+};
+return _0x2cd1();
 }
 async function 𝕲𝖊𝖙𝕮𝖔𝖓𝖙𝖆𝖎𝖓𝖊𝖗() {
-  const _0x3f90bb = _0x1c912f;
-  let _0x21e277 = require(_0x3f90bb(0x1a1))["exec"](_0x3f90bb(0x190));
-  _0x21e277[_0x3f90bb(0x18e)]["pipe"](process["stderr"]),
-    _0x21e277["on"](_0x3f90bb(0x19a), function (_0x41e516, _0x1e815f) {
-      const _0x950137 = _0x3f90bb;
-      if (_0x41e516) console[_0x950137(0x197)](ᴅᴇꜱɢ["red"](_0x41e516));
-      if (_0x1e815f) console[_0x950137(0x197)](ᴅᴇꜱɢ["blue"](_0x1e815f));
-      process[_0x950137(0x1a7)] = 0x1;
-    });
+const _0x3f90bb = _0x1c912f;
+let _0x21e277 = require(_0x3f90bb(0x1a1))["exec"](_0x3f90bb(0x190));
+_0x21e277[_0x3f90bb(0x18e)]["pipe"](process["stderr"]),
+_0x21e277["on"](_0x3f90bb(0x19a), function (_0x41e516, _0x1e815f) {
+const _0x950137 = _0x3f90bb;
+if (_0x41e516) console[_0x950137(0x197)](ᴅᴇꜱɢ["red"](_0x41e516));
+if (_0x1e815f) console[_0x950137(0x197)](ᴅᴇꜱɢ["blue"](_0x1e815f));
+process[_0x950137(0x1a7)] = 0x1;
+});
 }
 ("|⬡═════════════════════════════════════════════════════════════════════|▷\x20𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞\x20◁|═════════════════════════════════════════════════════════════════════⬡|");
 async function 𝕮𝖑𝖊𝖆𝖓𝕮𝖔𝖓𝖙𝖆𝖎𝖓𝖊𝖗() {
-  const _0x3d1a6f = _0x1c912f;
-  let _0x560519 = [
-    ".",
-    ...Object[_0x3d1a6f(0x1a6)](require(_0x3d1a6f(0x18b))[_0x3d1a6f(0x18c)]),
-  ];
-  for (let _0x2dac23 of _0x560519) {
-    ʀɪᴍʀᴀꜰ(_0x2dac23, async function (_0x240983) {
-      const _0x5cf9fc = _0x3d1a6f;
-      if (_0x240983)
-        return console[_0x5cf9fc(0x197)](ᴅᴇꜱɢ["bgRed"]("-rm"), _0x240983);
-      console[_0x5cf9fc(0x197)](ᴅᴇꜱɢ["bgGreen"]("-rm"), _0x2dac23);
-    });
-  }
+const _0x3d1a6f = _0x1c912f;
+let _0x560519 = [
+".",
+...Object[_0x3d1a6f(0x1a6)](require(_0x3d1a6f(0x18b))[_0x3d1a6f(0x18c)]),
+];
+for (let _0x2dac23 of _0x560519) {
+ʀɪᴍʀᴀꜰ(_0x2dac23, async function (_0x240983) {
+const _0x5cf9fc = _0x3d1a6f;
+if (_0x240983)
+return console[_0x5cf9fc(0x197)](ᴅᴇꜱɢ["bgRed"]("-rm"), _0x240983);
+console[_0x5cf9fc(0x197)](ᴅᴇꜱɢ["bgGreen"]("-rm"), _0x2dac23);
+});
+}
 }
 _0x1c912f(0x187);
 function _0x1701(_0x47d265, _0x50daf8) {
-  const _0x2cd196 = _0x2cd1();
-  return (
-    (_0x1701 = function (_0x170112, _0x2faf37) {
-      _0x170112 = _0x170112 - 0x186;
-      let _0x62d8c7 = _0x2cd196[_0x170112];
-      return _0x62d8c7;
-    }),
-    _0x1701(_0x47d265, _0x50daf8)
-  );
+const _0x2cd196 = _0x2cd1();
+return (
+(_0x1701 = function (_0x170112, _0x2faf37) {
+_0x170112 = _0x170112 - 0x186;
+let _0x62d8c7 = _0x2cd196[_0x170112];
+return _0x62d8c7;
+}),
+_0x1701(_0x47d265, _0x50daf8)
+);
 }
 async function 𝕻𝖚𝖘𝖍𝕮𝖔𝖓𝖙𝖆𝖎𝖓𝖊𝖗() {
-  const _0x27cd78 = _0x1c912f;
-  let _0x2d339c = new Date(),
-    _0x395b39 = ("0" + _0x2d339c["getDate"]())[_0x27cd78(0x188)](-0x2),
-    _0xf99cc8 = ("0" + (_0x2d339c[_0x27cd78(0x189)]() + 0x1))[_0x27cd78(0x188)](
-      -0x2
-    ),
-    _0x566dba = _0x2d339c["getFullYear"](),
-    _0x21288a = _0x2d339c["getHours"](),
-    _0x239770 = _0x2d339c[_0x27cd78(0x19f)](),
-    _0x2f509a = _0x2d339c[_0x27cd78(0x19c)](),
-    _0x549798 = require("child_process")[_0x27cd78(0x191)](
-      _0x27cd78(0x1a5) +
-        vers[_0x27cd78(0x19d)] +
-        _0x27cd78(0x19e) +
-        (_0x566dba +
-          "-" +
-          _0xf99cc8 +
-          "-" +
-          _0x395b39 +
-          "\x20" +
-          _0x21288a +
-          ":" +
-          _0x239770 +
-          ":" +
-          _0x2f509a) +
-        "\x20\x22\x20&&\x20git\x20push"
-    );
-  _0x549798[_0x27cd78(0x18e)]["pipe"](process["stderr"]),
-    _0x549798["on"](_0x27cd78(0x19a), function (_0x2261f7, _0x33b12c) {
-      const _0x1457c2 = _0x27cd78;
-      if (_0x2261f7)
-        console[_0x1457c2(0x197)](ᴅᴇꜱɢ[_0x1457c2(0x1a3)](_0x2261f7));
-      if (_0x33b12c) console["log"](ᴅᴇꜱɢ[_0x1457c2(0x194)](_0x33b12c));
-      process["exitCode"] = 0x1;
-    });
+const _0x27cd78 = _0x1c912f;
+let _0x2d339c = new Date(),
+_0x395b39 = ("0" + _0x2d339c["getDate"]())[_0x27cd78(0x188)](-0x2),
+_0xf99cc8 = ("0" + (_0x2d339c[_0x27cd78(0x189)]() + 0x1))[_0x27cd78(0x188)](
+-0x2
+),
+_0x566dba = _0x2d339c["getFullYear"](),
+_0x21288a = _0x2d339c["getHours"](),
+_0x239770 = _0x2d339c[_0x27cd78(0x19f)](),
+_0x2f509a = _0x2d339c[_0x27cd78(0x19c)](),
+_0x549798 = require("child_process")[_0x27cd78(0x191)](
+_0x27cd78(0x1a5) +
+vers[_0x27cd78(0x19d)] +
+_0x27cd78(0x19e) +
+(_0x566dba +
+"-" +
+_0xf99cc8 +
+"-" +
+_0x395b39 +
+"\x20" +
+_0x21288a +
+":" +
+_0x239770 +
+":" +
+_0x2f509a) +
+"\x20\x22\x20&&\x20git\x20push"
+);
+_0x549798[_0x27cd78(0x18e)]["pipe"](process["stderr"]),
+_0x549798["on"](_0x27cd78(0x19a), function (_0x2261f7, _0x33b12c) {
+const _0x1457c2 = _0x27cd78;
+if (_0x2261f7)
+console[_0x1457c2(0x197)](ᴅᴇꜱɢ[_0x1457c2(0x1a3)](_0x2261f7));
+if (_0x33b12c) console["log"](ᴅᴇꜱɢ[_0x1457c2(0x194)](_0x33b12c));
+process["exitCode"] = 0x1;
+});
 }
 _0x1c912f(0x187);
 async function ᴄᴏᴍᴍɪᴛ() {
-  const _0x20dcbb = _0x1c912f;
-  await ɢɪᴛ[_0x20dcbb(0x1a0)]();
-  let _0xf07f3d = await ɢɪᴛ[_0x20dcbb(0x197)]([_0x20dcbb(0x186)]);
-  _0xf07f3d["total"] != 0x0 && await𝕲𝖊𝖙𝕮𝖔𝖓𝖙𝖆𝖎𝖓𝖊𝖗();
+const _0x20dcbb = _0x1c912f;
+await ɢɪᴛ[_0x20dcbb(0x1a0)]();
+let _0xf07f3d = await ɢɪᴛ[_0x20dcbb(0x197)]([_0x20dcbb(0x186)]);
+_0xf07f3d["total"] != 0x0 && await𝕲𝖊𝖙𝕮𝖔𝖓𝖙𝖆𝖎𝖓𝖊𝖗();
 }
 ᴄᴏᴍᴍɪᴛ()["catch"]((_0x483018) => console[_0x1c912f(0x1a4)](_0x483018));
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
- * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
- */
+* * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
+*/
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
