f65902d34f9440625a9ac29588a76dc6ebbcc8b7
KryKnz <krakinzkon@gmail.com>
2022-07-03 15:53:19 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-07-03 15:53:18

---
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Housttely_Signature/misc/Readme!.txt
@@ -0,0 +1,44 @@
+Note of the author
+
+By installing or using this font, you are agree to the Product Usage Agreement:
+
+- This font is already FULL VERSION and ONLY for PERSONAL USE. NO COMMERCIAL USE ALLOWED!
+
+- Here is the link to purchase commercial license:
+https://halymuntstudio.com/housttely-signature/
+
+- For Corporate use, you have to purchase Corporate license
+
+- Choose The License You Need
+https://halymuntstudio.com/license/
+
+- If you need a custom license please contact us at
+halymunt.studio@gmail.com
+
+- Please visit our store for more amazing fonts :
+https://halymuntstudio.com/
+
+Follow our Instagram for update: @halymuntstudio
+
+Thank you.
+
+----------------------------------------------------------------------------------------------------------------------------
+
+INDONESIA:
+
+Dengan meng-install font ini, anda dianggap mengerti dan menyetujui semua syarat dan ketentuan penggunaan font dibawah ini:
+
+- Font demo ini hanya dapat digunakan untuk keperluan "Personal Use"/kebutuhan pribadi, atau untuk keperluan yang sifatnya tidak "komersil", alias tidak menghasilkan profit atau keuntungan dari hasil memanfaatkan/menggunakan font kami. Baik itu untuk individu, Agensi Desain Grafis, Percetakan, Distro atau Perusahaan/Korporasi.   
+
+- Silakan gunakan lisensi komersial dengan membeli melalui link ini :
+https://halymuntstudio.com/housttely-signature/
+
+- Dengan hanya lisensi "Personal Use", DILARANG KERAS menggunakan atau memanfaatkan font ini untuk kepeluan Komersial, baik itu untuk Iklan, Promosi, TV, Film, Video, Motion Graphics, Youtube, Desain kaos distro atau untuk Kemasan Produk (baik Fisik ataupun Digital) atau Media apapun dengan tujuan menghasilkan profit/keuntungan.
+
+- Untuk penggunaan keperluan Perusahaan/Korporasi silakan menggunakan Corporate License.
+
+- Menggunakan font ini dengan lisensi "Personal Use" untuk kepentingan Komersial apapun bentuknya TANPA IZIN dari kami, akan dikenakan biaya CORPORATE LICENSE.
+
+Informasi tentang Lisensi apa yang akan anda perlukan, silahkan menghubungi kami di : halymunt.studio@gmail.com
+
+Terima kasih.
--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/N𝖊𝖊𝖉__A𝖗𝖌𝖘.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/N𝖊𝖊𝖉__A𝖗𝖌𝖘.js
@@ -4,7 +4,45 @@
 
 `|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|`;
 require("../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙");
+let Fs = require("fs");
+let ʀᴀɴᴅᴏᴍᴋ = ʀᴀɴᴅᴏᴍᴄ.getColor();
+let ʀᴀɴᴅᴏᴍᴄ = require("random-material-color");
+let { registerFont, createCanvas, loadImage } = require("canvas");
 exports.N𝖊𝖊𝖉__A𝖗𝖌𝖘 = async (ӄ𝖗𝖞ӄ𝖓𝖟, Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊, 𝖀𝖘𝖆𝖌𝖊) => {
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Heart_Of_Everything/Heart_Of_Everything.ttf", { family: "Heart_Of_Everything" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Housttely_Signature/Housttely_Signature.ttf", { family: "Housttely_Signature" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Fall_is_Coming/Fall_is_Coming.ttf", { family: "Fall_is_Coming" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Slouchy_Brush/Slouchy_Brush.ttf", { family: "Slouchy_Brush" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Kashmir_Rosy/Kashmir_Rosy.otf", { family: "Kashmir_Rosy" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Japanese/Japanese.ttf", { family: "Japanese" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Honeybee/Honeybee.ttf", { family: "Honeybee" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Angeles/Angeles.ttf", { family: "Angeles" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Shalma/Shalma.otf", { family: "Shalma" });
+registerFont("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Aquire/Aquire.otf", { family: "Aquire" });
+let W = 1280;
+let H = 720;
+let ᴄᴀɴᴠᴀꜱ = createCanvas(W, H);
+let 𝖈𝖔𝖓𝖙𝖊𝖝𝖙 = ᴄᴀɴᴠᴀꜱ.getContext("2d");
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillStyle = ʀᴀɴᴅᴏᴍᴋ;
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillRect(0, 0, W, H);
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.font = "bold 70pt Kashmir_Rosy";
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.textAlign = "center";
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.textBaseline = "top";
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillStyle = "#000000";
+let ᴛᴇxᴛ = "❣️ Hello There ❣️\n" + (Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "ɴᴏ_ɴᴀᴍᴇ");
+let ᴛᴇxᴛᴡɪᴅᴛʜ = 𝖈𝖔𝖓𝖙𝖊𝖝𝖙.measureText(ᴛᴇxᴛ).W;
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillRect(600 - ᴛᴇxᴛᴡɪᴅᴛʜ / 2 - 10, 170 - 5, ᴛᴇxᴛᴡɪᴅᴛʜ + 20, 120);
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillStyle = "#000000";
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillText(ᴛᴇxᴛ, 600, 170);
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillStyle = "#000000";
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.font = "bold 50pt Aquire";
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.fillText("Valkyrie.com", 650, 600);
+loadImage("./Src/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘.png").then((Cmg) => {
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.drawImage(Cmg, 140, 515, 200, 200);
+𝖈𝖔𝖓𝖙𝖊𝖝𝖙.drawImage(Cmg, 980, 515, 200, 200);
+let CmgB = ᴄᴀɴᴠᴀꜱ.toBuffer("image/png");
+await Fs.writeFileSync("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Valkyrie_com.png", CmgB);
+("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 let usedUser = require(`../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🧆𝕯𝖆𝖙𝖆𝖇𝖆𝖘𝖊/𝖀𝖘𝖊𝖗🍣𝖒𝖔𝖓𝖌𝖔/usedUser`);
 let ᴍꜱᴇᴄ = require("parse-ms");
 let ʙᴏᴛ_ᴜꜱᴇᴅ;
@@ -30,7 +68,7 @@ await ӄ𝖗𝖞ӄ𝖓𝖟
 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID,
 {
 contextInfo: { mentionedJid: [Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.sender] },
-image: { url: "./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊👗𝕯𝖗𝖊𝖘𝖘/Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.png" },
+image: { url: "./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Valkyrie_com.png" },
 caption: `🕊️ *ɴᴀᴍᴇ:* ${Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.pushName || "ɴᴏ_ɴᴀᴍᴇ"}
 
 ${𝖀𝖘𝖆𝖌𝖊}`,
@@ -47,17 +85,15 @@ headerType: 4,
 {
 quoted: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊,
 }
-)
-.catch((e) => console.log(e));
+).then(Fs.unlinkSync("./Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🀄𝕮𝖆𝖓𝖛𝖆𝖘/Valkyrie_com.png")).catch((e) => console.log(e));
 let ʀᴇᴀᴄᴛionMessage = {
 ʀᴇᴀᴄᴛ: {
 text: `⚡`,
 key: Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.key,
 },
 };
-return await ӄ𝖗𝖞ӄ𝖓𝖟
-.sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, ʀᴇᴀᴄᴛionMessage)
-.catch((e) => console.log(e));
+return await ӄ𝖗𝖞ӄ𝖓𝖟.sendMessage(Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊.chatID, ʀᴇᴀᴄᴛionMessage).catch((e) => console.log(e));
+});
 };
 `|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|`;
 
