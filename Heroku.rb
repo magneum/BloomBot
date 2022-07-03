6050cd4bb22511be0ec458a60a4400b137fb6ebf
KryKnz <krakinzkon@gmail.com>
2022-07-03 11:55:50 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-07-03 11:55:48

--- Heroku.rb
+++ Heroku.rb
@@ -4,5 +4,5 @@ heroku ps:stop run.6654
 heroku builds:cancel -a vlkyre
 heroku ps:restart worker -a vlkyre
 heroku addons:remove heroku-postgresql:hobby-dev --confirm vlkyre
-heroku addons:add heroku-postgresql:hobby-dev -a  vlkyre
+heroku addons:add heroku-postgresql:hobby-dev -a vlkyre
 heroku pg:reset -a vlkyre --confirm vlkyre
