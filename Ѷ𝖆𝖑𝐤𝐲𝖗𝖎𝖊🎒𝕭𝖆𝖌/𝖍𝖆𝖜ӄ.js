cfbc5254c0f6b7269cb58802873639c26deff279
KryKnz <krakinzkon@gmail.com>
2022-07-04 00:53:13 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-07-04 0:53:12

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/𝖍𝖆𝖜ӄ.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌/𝖍𝖆𝖜ӄ.js
@@ -6,7 +6,7 @@
 require(`../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/𝕷𝖆ყO𝖚𝖙`);
 require(`../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/Ѷ𝖆𝖑.js`);
 if (process.env.Devalkyrie == "true") {
-console.log("__Default");
+console.log("V__Default");
 let {
 default: ᴋᴜɴᴋᴏɴɴᴇᴄᴛ,
 generateForwardMessageContent,
