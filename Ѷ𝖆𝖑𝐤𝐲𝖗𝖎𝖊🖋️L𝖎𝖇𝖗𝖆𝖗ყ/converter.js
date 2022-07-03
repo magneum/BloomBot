028410589c30c49fad261d92b340da9356a8b5e3
KryKnz <krakinzkon@gmail.com>
2022-07-04 01:23:28 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-07-04 1:23:26

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/converter.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🖋️L𝖎𝖇𝖗𝖆𝖗ყ/converter.js
@@ -3,108 +3,191 @@
 * * * * * * * * * * |        (𝐜)𝐕𝐚𝐥𝐤𝐲𝐫𝐢𝐞 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!        * * * * * * * * * *
 */
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
-let fs = require("fs");
-let path = require("path");
-let { spawn } = require("child_process");
-
-function ffmpeg(buffer, args = [], ext = "", ext2 = "") {
-return new Promise(async (resolve, reject) => {
+const _0x23a597 = _0x57bb;
+(function (_0x58a1b7, _0x272f34) {
+const _0x28b7f7 = _0x57bb,
+_0x57af0b = _0x58a1b7();
+while (!![]) {
 try {
-let tmp = path.join(
-__dirname,
+const _0x202df1 =
+(-parseInt(_0x28b7f7(0x143)) / 0x1) *
+(parseInt(_0x28b7f7(0x13f)) / 0x2) +
+parseInt(_0x28b7f7(0x130)) / 0x3 +
+(-parseInt(_0x28b7f7(0x12e)) / 0x4) *
+(parseInt(_0x28b7f7(0x13e)) / 0x5) +
+(-parseInt(_0x28b7f7(0x146)) / 0x6) *
+(parseInt(_0x28b7f7(0x13c)) / 0x7) +
+parseInt(_0x28b7f7(0x142)) / 0x8 +
+(parseInt(_0x28b7f7(0x14a)) / 0x9) *
+(-parseInt(_0x28b7f7(0x12a)) / 0xa) +
+parseInt(_0x28b7f7(0x12b)) / 0xb;
+if (_0x202df1 === _0x272f34) break;
+else _0x57af0b["push"](_0x57af0b["shift"]());
+} catch (_0x2abfec) {
+_0x57af0b["push"](_0x57af0b["shift"]());
+}
+}
+})(_0x53c8, 0x4add3);
+let fs = require("fs"),
+path = require("path"),
+{ spawn } = require(_0x23a597(0x144));
+function _0x53c8() {
+const _0x25406c = [
+"promises",
+"221367ToyTfy",
+"ffmpeg",
 "../Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🎒𝕭𝖆𝖌",
-+new Date() + "." + ext
-);
-let out = tmp + "." + ext2;
-await fs.promises.writeFile(tmp, buffer);
-spawn("ffmpeg", ["-y", "-i", tmp, ...args, out])
-.on("error", reject)
-.on("close", async (code) => {
+"-c:a",
+"join",
+"unlink",
+"-b:a",
+"-vn",
+"-vbr",
+"mp4",
+"-ar",
+"opus",
+"8078pWFSwM",
+"-c:v",
+"1495UeCrDa",
+"38846WkjBcH",
+"writeFile",
+"close",
+"3229944MCxzIP",
+"12WmvJRi",
+"child_process",
+"error",
+"1266IKEpxN",
+"44100",
+"-preset",
+"128k",
+"4914rmXpVX",
+"slow",
+"-ac",
+"readFile",
+"8680NMxhAr",
+"9999836vZUGVu",
+"libx264",
+"mp3",
+"1732CUxISH",
+];
+_0x53c8 = function () {
+return _0x25406c;
+};
+return _0x53c8();
+}
+function ffmpeg(_0x2e8e0c, _0x5d8189 = [], _0x232bba = "", _0x2e6ee6 = "") {
+return new Promise(async (_0x2579a7, _0x16f524) => {
+const _0x1377c5 = _0x57bb;
 try {
-await fs.promises.unlink(tmp);
-if (code !== 0) return reject(code);
-resolve(await fs.promises.readFile(out));
-await fs.promises.unlink(out);
-} catch (e) {
-reject(e);
+let _0x55ac58 = path[_0x1377c5(0x134)](
+__dirname,
+_0x1377c5(0x132),
++new Date() + "." + _0x232bba
+),
+_0x4ddf18 = _0x55ac58 + "." + _0x2e6ee6;
+await fs[_0x1377c5(0x12f)][_0x1377c5(0x140)](_0x55ac58, _0x2e8e0c),
+spawn(_0x1377c5(0x131), [
+"-y",
+"-i",
+_0x55ac58,
+..._0x5d8189,
+_0x4ddf18,
+])
+["on"](_0x1377c5(0x145), _0x16f524)
+["on"](_0x1377c5(0x141), async (_0x5a2ecf) => {
+const _0x2e20dd = _0x1377c5;
+try {
+await fs[_0x2e20dd(0x12f)][_0x2e20dd(0x135)](_0x55ac58);
+if (_0x5a2ecf !== 0x0) return _0x16f524(_0x5a2ecf);
+_0x2579a7(
+await fs[_0x2e20dd(0x12f)][_0x2e20dd(0x129)](_0x4ddf18)
+),
+await fs[_0x2e20dd(0x12f)]["unlink"](_0x4ddf18);
+} catch (_0x258c65) {
+_0x16f524(_0x258c65);
 }
 });
-} catch (e) {
-reject(e);
+} catch (_0x25eaa1) {
+_0x16f524(_0x25eaa1);
 }
 });
 }
-
-/**
- * Convert Audio to Playable WhatsApp Audio
- * @param {Buffer} buffer Audio Buffer
- * @param {String} ext File Extension
- */
-function toAudio(buffer, ext) {
+function toAudio(_0x46d1f9, _0x1a8692) {
+const _0x2f2c24 = _0x23a597;
 return ffmpeg(
-buffer,
-["-vn", "-ac", "2", "-b:a", "128k", "-ar", "44100", "-f", "mp3"],
-ext,
-"mp3"
+_0x46d1f9,
+[
+_0x2f2c24(0x137),
+_0x2f2c24(0x128),
+"2",
+_0x2f2c24(0x136),
+_0x2f2c24(0x149),
+"-ar",
+_0x2f2c24(0x147),
+"-f",
+_0x2f2c24(0x12d),
+],
+_0x1a8692,
+_0x2f2c24(0x12d)
 );
 }
-
-/**
- * Convert Audio to Playable WhatsApp PTT
- * @param {Buffer} buffer Audio Buffer
- * @param {String} ext File Extension
- */
-function toPTT(buffer, ext) {
+function _0x57bb(_0x33abba, _0x41267c) {
+const _0x53c8a3 = _0x53c8();
+return (
+(_0x57bb = function (_0x57bbd9, _0x30e335) {
+_0x57bbd9 = _0x57bbd9 - 0x128;
+let _0x587070 = _0x53c8a3[_0x57bbd9];
+return _0x587070;
+}),
+_0x57bb(_0x33abba, _0x41267c)
+);
+}
+function toPTT(_0xcf64, _0x5b2fe9) {
+const _0x3666cc = _0x23a597;
 return ffmpeg(
-buffer,
+_0xcf64,
 [
 "-vn",
 "-c:a",
 "libopus",
-"-b:a",
-"128k",
-"-vbr",
+_0x3666cc(0x136),
+_0x3666cc(0x149),
+_0x3666cc(0x138),
 "on",
 "-compression_level",
 "10",
 ],
-ext,
-"opus"
+_0x5b2fe9,
+_0x3666cc(0x13b)
 );
 }
-
-/**
-* Convert Audio to Playable WhatsApp Video
-* @param {Buffer} buffer Video Buffer
-* @param {String} ext File Extension
-*/
-function toVideo(buffer, ext) {
+function toVideo(_0x585521, _0x402897) {
+const _0x491376 = _0x23a597;
 return ffmpeg(
-buffer,
+_0x585521,
 [
-"-c:v",
-"libx264",
-"-c:a",
+_0x491376(0x13d),
+_0x491376(0x12c),
+_0x491376(0x133),
 "aac",
 "-ab",
 "128k",
-"-ar",
-"44100",
+_0x491376(0x13a),
+_0x491376(0x147),
 "-crf",
 "32",
-"-preset",
-"slow",
+_0x491376(0x148),
+_0x491376(0x14b),
 ],
-ext,
-"mp4"
+_0x402897,
+_0x491376(0x139)
 );
 }
-
-module.exports = {
-toAudio,
-toPTT,
-toVideo,
-ffmpeg,
+module["exports"] = {
+toAudio: toAudio,
+toPTT: toPTT,
+toVideo: toVideo,
+ffmpeg: ffmpeg,
 };
 ("|⬡════════════════════════════════════════════════════════════|▷   ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™  ◁|════════════════════════════════════════════════════════════⬡|");
 /*
