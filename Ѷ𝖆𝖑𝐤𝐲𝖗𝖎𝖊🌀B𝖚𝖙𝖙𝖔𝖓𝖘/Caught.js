1e9a48b9f5d8dbb314820b77af1e5798c2dd26a3
KryKnz <krakinzkon@gmail.com>
2022-07-03 18:51:59 +0530

    「 Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🧆 𝐃𝐚𝐭𝐞: 2022-07-03 18:51:58

--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Caught.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Caught.js
@@ -32,8 +32,8 @@ Please Try Again Later!`,
 footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\n▷ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ ◁",
 buttons: [
 {
-buttonId: prefix + "❗HELP❗",
-buttonText: { displayText: prefix + "❗HELP❗" },
+buttonId: prefix + "🌍HELP🌍",
+buttonText: { displayText: prefix + "🌍HELP🌍" },
 type: 1,
 },
 ],
--- Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Image_Button.js
+++ Ѷ𝖆𝖑𝐤𝐲𝖗𝖎𝖊🌀B𝖚𝖙𝖙𝖔𝖓𝖘/Image_Button.js
@@ -49,8 +49,8 @@ ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
 footer: "*⦓ ⒸѶ𝖆𝖑𝐤𝐲𝖗𝖎𝖊 ⦔*\n▷ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ ◁",
 buttons: [
 {
-buttonId: prefix + "❗HELP❗",
-buttonText: { displayText: prefix + "❗HELP❗" },
+buttonId: prefix + "🌍HELP🌍",
+buttonText: { displayText: prefix + "🌍HELP🌍" },
 type: 1,
 },
 ],
