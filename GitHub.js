46d4fc69229d010ea3150cbbdd71b74590b89678
KryKnz <krakinzkon@gmail.com>
2022-07-07 20:11:13 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈

--- GitHub.js
+++ GitHub.js
@@ -15,14 +15,24 @@ const { simpleGit, CleanOptions } = require("simple-git");
 async function commit() {
 simpleGit().clean(simpleGit.CleanOptions.FORCE);
 simpleGit().clean(CleanOptions.FORCE);
-simpleGit()
-.exec(() => console.log("Starting pull..."))
-.pull((err, update) => {
-if (err) throw err;
+simpleGit().exec(() => console.log("Starting pull...")).pull((error, update) => {
+if (error) return console.log(error);
 if (update && update.summary.changes) {
 require("child_process").exec("npm restart");
 }
-})
-.exec(() => console.log("pull done."));
+}).exec(() => console.log("pull done."));
 }
 commit();
+("|⬡═══════════════════════════════════════════════════════════════|▷ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ◁|═══════════════════════════════════════════════════════════════⬡|");
+// ╔⧉༻ [ Ѷ𝖑𝐤𝐲𝖗𝖊🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ Vlkyre was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("|⬡═══════════════════════════════════════════════════════════════|▷ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ◁|═══════════════════════════════════════════════════════════════⬡|");
