6fedca49f42cbe612af0b669ddd6212f25fdc41f
KryKnz <krakinzkon@gmail.com>
2022-07-08 00:33:40 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈

--- Hub.js
+++ Hub.js
@@ -1,4 +1,4 @@
 const simpleGit = require("simple-git");
 const Git = simpleGit({ maxConcurrentProcesses: 10 });
-Git().clean(simpleGit.CleanOptions.FORCE);
-Git().pull("🐍Ş𝖎𝖕𝖍𝖔𝖓®..origin/🐍Ş𝖎𝖕𝖍𝖔𝖓®", "🐍Ş𝖎𝖕𝖍𝖔𝖓®", { "--rebase": "true" });
+Git.clean(simpleGit.CleanOptions.FORCE);
+Git.pull("🐍Ş𝖎𝖕𝖍𝖔𝖓®..origin/🐍Ş𝖎𝖕𝖍𝖔𝖓®", "🐍Ş𝖎𝖕𝖍𝖔𝖓®", { "--rebase": "true" });
