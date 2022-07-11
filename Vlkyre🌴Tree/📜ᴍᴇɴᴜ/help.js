46afff2420cb661213ffc657f77a77f1dabd87d6
KryKnz <krakinzkon@gmail.com>
2022-07-12 01:20:37 +0530

    「 Ѷ𝖑𝐤𝐲𝖗𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🕊️ 𝐃𝐚𝐭𝐞: 2022-07-12 1:20:34

--- Vlkyre🌴Tree/📜ᴍᴇɴᴜ/help.js
+++ Vlkyre🌴Tree/📜ᴍᴇɴᴜ/help.js
@@ -196,9 +196,9 @@ caption: `Ῠ  *ɴᴀᴍᴇ:* ${Vlkyre.pushName || "ɴᴏ_ɴᴀᴍᴇ"}
 ║Ῠ ${prefix}shop
 ║Ῠ ${prefix}mall
 ║Ῠ ${prefix}shopping
-╚═══════╝
+╚══╗
 ╗
-╔══『 🐾𝐀𝐍𝐈𝐌𝐄_𝐌𝐀𝐍𝐆𝐀🐾 』
+╝『 🐾𝐀𝐍𝐈𝐌𝐄_𝐌𝐀𝐍𝐆𝐀🐾 』
 ║Ῠ ${prefix}anime
 ║Ῠ ${prefix}manga
 ╚═══════╝
