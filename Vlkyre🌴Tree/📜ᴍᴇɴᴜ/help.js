0dada49d505835a48890b7abf2416c1a1e6c9e07
KryKnz <krakinzkon@gmail.com>
2022-07-12 01:19:37 +0530

    「 Ѷ𝖑𝐤𝐲𝖗𝖊 」 : 𝐌𝐮𝐥𝐭𝐢-𝐃𝐞𝐯𝐢𝐜𝐞 🕊️ 𝐃𝐚𝐭𝐞: 2022-07-12 1:19:34

--- Vlkyre🌴Tree/📜ᴍᴇɴᴜ/help.js
+++ Vlkyre🌴Tree/📜ᴍᴇɴᴜ/help.js
@@ -150,13 +150,13 @@ caption: `Ῠ  *ɴᴀᴍᴇ:* ${Vlkyre.pushName || "ɴᴏ_ɴᴀᴍᴇ"}
 
 🕊️𝐕𝐥𝐤𝐲𝐫𝐞: is a whatsapp userbot with automation,moderation,music,games and 140+ commands!
 ╔══『 🕊️𝐕𝐥𝐤𝐲𝐫𝐞🕊️ 』
-║Ῠ *ᴘʀᴇꜰɪx:* ${prefix}
-║Ῠ *ɢᴘᴀʏ:* +918436686758
-║Ῠ *ᴜᴘɪ:* krakinzkon@oksbi
-║Ῠ ɢɪᴛʜᴜʙ:* https://github.com/kryknz
-║Ῠ ᴠʟᴋʏʀᴇ:* https://github.com/kryknz/vlkyre
-║Ῠ ᴡᴇʙꜱɪᴛᴇ:*  https://kryknz.github.io/Vlkyre
-╚═══════╝
+║Ῠ ᴘʀᴇꜰɪx: ${prefix}
+║Ῠ ɢᴘᴀʏ: +918436686758
+║Ῠ ᴜᴘɪ: krakinzkon@oksbi
+║Ῠ ɢɪᴛʜᴜʙ: https://github.com/kryknz
+║Ῠ ᴠʟᴋʏʀᴇ: https://github.com/kryknz/vlkyre
+║Ῠ ᴡᴇʙꜱɪᴛᴇ:  https://kryknz.github.io/Vlkyre
+
 
 ╔══『 🐾𝐀𝐍𝐈𝐌𝐄_𝐌𝐀𝐍𝐆𝐀🐾 』
 ║Ῠ ${prefix}anime
