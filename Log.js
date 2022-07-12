b77416a04f86e6fd43b6881f1515f83353ca3d05
KryKnz <krakinzkon@gmail.com>
2022-07-12 21:08:32 +0530

    (c)Ѷ𝖑𝐤𝐲𝖗𝖊🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™

--- Log.js
+++
@@ -1,4 +0,0 @@
-console.log("Done!");
-// ('BUN_INSTALL="/home/kon/.bun" && PATH="$BUN_INSTALL/bin:$PATH" && npx forever --no-warnings --minUptime 1000 --spinSleepTime 1000 bun run Log.js');
-// ('BUN_INSTALL="/home/kon/.bun" && PATH="$BUN_INSTALL/bin:$PATH" && npx forever --minUptime 1000 --spinSleepTime 1000 bun run --no-warnings Log.js');
-// ('BUN_INSTALL="/home/kon/.bun" && PATH="$BUN_INSTALL/bin:$PATH" && bun run --no-warnings && npx forever --minUptime 1000 --spinSleepTime 1000 Log.js');
