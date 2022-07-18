3d2710779664cfff7cead23cd0218cd72358241d
KryKnz <KrakinzKon@gmail.com>
2022-07-19 03:00:50 +0530

    🛰️❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞ 🐞On: 2022-07-19 2:59:56

--- 🌴Modules/📜ᴍᴇɴᴜ/help.js
+++ 🌴Modules/📜ᴍᴇɴᴜ/help.js
@@ -15,12 +15,12 @@
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
 require("../../🤖Utilities/Settings");
 let {
-  isUrl,
-  fetchJson,
-  getBuffer,
-  jsonformat,
-  GIFBufferToVideoBuffer,
-  getRandom,
+isUrl,
+fetchJson,
+getBuffer,
+jsonformat,
+GIFBufferToVideoBuffer,
+getRandom,
 } = require(`../../🤖Utilities/ɴᴇᴄᴛᴏʀ`);
 let os = require(`os`);
 let fs = require(`fs`);
@@ -43,104 +43,104 @@ let { formatp, runtime } = require(`../../🤖Utilities/Human`);
 let { Image_Button } = require("../../👒Buttons/Image_Button");
 let { Video_Button } = require("../../👒Buttons/Video_Button");
 let YouTube_Regex =
-  /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
+/(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
 let Present_Path = require(`path`);
 let Script_Name = Present_Path.basename(__filename);
 let Final_Name = Script_Name.slice(0, -3).toLowerCase();
 let { Caught } = require("../../👒Buttons/Caught");
 ("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
 exports.help = async (
-  ӄryӄnz,
-  Vlkyre,
-  pushName,
-  F𝖚𝖑𝖑_A𝖗𝖌𝖘,
-  quoted,
-  isMedia,
-  isGroup,
-  isBotAdmin,
-  isSenderAdmin,
-  isSenderTUF,
-  commandName,
-  body
+ӄryӄnz,
+Vlkyre,
+pushName,
+F𝖚𝖑𝖑_A𝖗𝖌𝖘,
+quoted,
+isMedia,
+isGroup,
+isBotAdmin,
+isSenderAdmin,
+isSenderTUF,
+commandName,
+body
 ) => {
-  let used = process.memoryUsage();
-  let cpus = os.cpus().map((cpu) => {
-    cpu.total = Object.keys(cpu.times).reduce(
-      (last, type) => last + cpu.times[type],
-      0
-    );
-    return cpu;
-  });
-  let cpu = cpus.reduce(
-    (last, cpu, _, { length }) => {
-      last.total += cpu.total;
-      last.speed += cpu.speed / length;
-      last.times.user += cpu.times.user;
-      last.times.nice += cpu.times.nice;
-      last.times.sys += cpu.times.sys;
-      last.times.idle += cpu.times.idle;
-      last.times.irq += cpu.times.irq;
-      return last;
-    },
-    {
-      speed: 0,
-      total: 0,
-      times: {
-        user: 0,
-        nice: 0,
-        sys: 0,
-        idle: 0,
-        irq: 0,
-      },
-    }
-  );
-  let timestamp = speed();
-  let latensi = speed() - timestamp;
-  let latest = `════════★ 𝐍𝐨𝐝𝐞𝐉𝐒 𝐌𝐞𝐦-𝐔𝐬𝐚𝐠𝐞:
+let used = process.memoryUsage();
+let cpus = os.cpus().map((cpu) => {
+cpu.total = Object.keys(cpu.times).reduce(
+(last, type) => last + cpu.times[type],
+0
+);
+return cpu;
+});
+let cpu = cpus.reduce(
+(last, cpu, _, { length }) => {
+last.total += cpu.total;
+last.speed += cpu.speed / length;
+last.times.user += cpu.times.user;
+last.times.nice += cpu.times.nice;
+last.times.sys += cpu.times.sys;
+last.times.idle += cpu.times.idle;
+last.times.irq += cpu.times.irq;
+return last;
+},
+{
+speed: 0,
+total: 0,
+times: {
+user: 0,
+nice: 0,
+sys: 0,
+idle: 0,
+irq: 0,
+},
+}
+);
+let timestamp = speed();
+let latensi = speed() - timestamp;
+let latest = `════════★ 𝐍𝐨𝐝𝐞𝐉𝐒 𝐌𝐞𝐦-𝐔𝐬𝐚𝐠𝐞:
 ${Object.keys(used)
-  .map(
-    (key, _, arr) =>
-      `${key.padEnd(Math.max(...arr.map((v) => v.length)), ` `)}: ${formatp(
-        used[key]
-      )}`
-  )
-  .join(`\n`)}
+.map(
+(key, _, arr) =>
+`${key.padEnd(Math.max(...arr.map((v) => v.length)), ` `)}: ${formatp(
+used[key]
+)}`
+)
+.join(`\n`)}
 ════════★ ${
-    cpus[0]
-      ? `𝐓𝐨𝐭𝐚𝐥 𝐂𝐏𝐔 𝐔𝐬𝐚𝐠𝐞
+cpus[0]
+? `𝐓𝐨𝐭𝐚𝐥 𝐂𝐏𝐔 𝐔𝐬𝐚𝐠𝐞
 ${cpus[0].model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
-          .map(
-            (type) =>
-              `- *${(type + `*`).padEnd(6)}: ${(
-                (100 * cpu.times[type]) /
-                cpu.total
-              ).toFixed(2)}%`
-          )
-          .join(`\n`)}
+.map(
+(type) =>
+`- *${(type + `*`).padEnd(6)}: ${(
+(100 * cpu.times[type]) /
+cpu.total
+).toFixed(2)}%`
+)
+.join(`\n`)}
 ════════★ 𝐂𝐏𝐔 𝐂𝐨𝐫𝐞(𝐬) 𝐔𝐬𝐚𝐠𝐞 (_${cpus.length}_ 𝐂𝐨𝐫𝐞 𝐂𝐏𝐔)
 ${cpus
-  .map(
-    (cpu, i) =>
-      `${i + 1}. ${cpu.model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
-        .map(
-          (type) =>
-            `- *${(type + `*`).padEnd(6)}: ${(
-              (100 * cpu.times[type]) /
-              cpu.total
-            ).toFixed(2)}%`
-        )
-        .join(`\n`)}`
-  )
-  .join(`\n\n`)}`
-      : ``
-  }`.trim();
-  await ӄryӄnz
-    .sendMessage(
-      Vlkyre.chatID,
-      {
-        contextInfo: { mentionedJid: [Vlkyre.sender] },
-        image: { url: "./👗Skins/Vlkyre.png" },
-        caption: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞 ❞
+.map(
+(cpu, i) =>
+`${i + 1}. ${cpu.model.trim()} (${cpu.speed} ᴍʜᴢ)${Object.keys(cpu.times)
+.map(
+(type) =>
+`- *${(type + `*`).padEnd(6)}: ${(
+(100 * cpu.times[type]) /
+cpu.total
+).toFixed(2)}%`
+)
+.join(`\n`)}`
+)
+.join(`\n\n`)}`
+: ``
+}`.trim();
+await ӄryӄnz
+.sendMessage(
+Vlkyre.chatID,
+{
+contextInfo: { mentionedJid: [Vlkyre.sender] },
+image: { url: "./👗Skins/Vlkyre.png" },
+caption: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞 ❞
 Is a whatsapp userbot with automation,moderation,music,games and 140+ commands!
 
 Ῠ 𝐩𝐫𝐞𝐟𝐢𝐱: ${prefix}
@@ -166,37 +166,37 @@ Is a whatsapp userbot with automation,moderation,music,games and 140+ commands!
 🛍️${prefix}StoreMenu
 🌐${prefix}WebMenu
 ⭕${prefix}YTFilterMenu`,
-        footer: "❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞 ❞ \nhttps://Vlkyre.krakinzkon.repl.co",
-        buttons: [
-          {
-            buttonId: prefix + "MENU",
-            buttonText: {
-              displayText: prefix + "MENU",
-            },
-            type: 1,
-          },
-          {
-            buttonId: prefix + "VLKYRE",
-            buttonText: {
-              displayText: prefix + "VLKYRE",
-            },
-            type: 1,
-          },
-          {
-            buttonId: prefix + "RULES",
-            buttonText: {
-              displayText: prefix + "RULES",
-            },
-            type: 1,
-          },
-        ],
-        headerType: 4,
-      },
-      {
-        quoted: Vlkyre,
-      }
-    )
-    .catch((e) => console.log(e));
+footer: "❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n⭕youtube.com/channel/UC5_VwWIUIdmv96OvR5rFvaA",
+buttons: [
+{
+buttonId: prefix + "MENU",
+buttonText: {
+displayText: prefix + "MENU",
+},
+type: 1,
+},
+{
+buttonId: prefix + "VLKYRE",
+buttonText: {
+displayText: prefix + "VLKYRE",
+},
+type: 1,
+},
+{
+buttonId: prefix + "RULES",
+buttonText: {
+displayText: prefix + "RULES",
+},
+type: 1,
+},
+],
+headerType: 4,
+},
+{
+quoted: Vlkyre,
+}
+)
+.catch((e) => console.log(e));
 };
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
 /*
