060ac39e2b130bd173b18f0ea8dc409c10ba2ec5
Kryknz <krakinzkon@gmail.com>
2022-07-18 10:48:44 +0530

    🕊️🛰️❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞❞ 🛰️🕊️ 🐞On: 2022-07-18 10:48:44

--- 🌴Modules/🔎ꜱᴇᴀʀᴄʜ/manga.js
+++ 🌴Modules/🔎ꜱᴇᴀʀᴄʜ/manga.js
@@ -13,15 +13,15 @@
 ╚════════════╝
 */
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
-require("../../Vlkyre🖋️Utilities/𝕷𝖆ყO𝖚𝖙");
+require("../../🤖Utilities/𝕷𝖆ყO𝖚𝖙");
 let {
-isUrl,
-fetchJson,
-getBuffer,
-jsonformat,
-GIFBufferToVideoBuffer,
-getRandom,
-} = require(`../../Vlkyre🖋️Utilities/ɴᴇᴄᴛᴏʀ`);
+  isUrl,
+  fetchJson,
+  getBuffer,
+  jsonformat,
+  GIFBufferToVideoBuffer,
+  getRandom,
+} = require(`../../🤖Utilities/ɴᴇᴄᴛᴏʀ`);
 let fs = require(`fs`);
 let util = require(`util`);
 let hxz = require(`hxz-api`);
@@ -41,76 +41,76 @@ let { Video_Button } = require("../../👒Buttons/Video_Button");
 let { Bot_Not_Admin } = require("../../Vlkyre🧭Compass/Bot_Not_Admin");
 let { Sender_Not_Admin } = require("../../Vlkyre🧭Compass/Sender_Not_Admin");
 let YouTube_Regex =
-/(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
+  /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
 let Present_Path = require(`path`);
 let Script_Name = Present_Path.basename(__filename);
 let Final_Name = Script_Name.slice(0, -3).toLowerCase();
 let { Caught } = require("../../👒Buttons/Caught");
 ("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
 exports.manga = async (
-ӄryӄnz,
-Vlkyre,
-pushName,
-F𝖚𝖑𝖑_A𝖗𝖌𝖘,
-quoted,
-isMedia,
-isGroup,
-isBotAdmin,
-isSenderAdmin,
-isSenderTUF,
-commandName,
-body,
+  ӄryӄnz,
+  Vlkyre,
+  pushName,
+  F𝖚𝖑𝖑_A𝖗𝖌𝖘,
+  quoted,
+  isMedia,
+  isGroup,
+  isBotAdmin,
+  isSenderAdmin,
+  isSenderTUF,
+  commandName,
+  body
 ) => {
-let { Manga } = require(`@shineiichijo/marika`);
-let manga = new Manga();
-if (!F𝖚𝖑𝖑_A𝖗𝖌𝖘) {
-return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-ӄryӄnz,
-Vlkyre,
-`*❌ERROR:* No query provided!
+  let { Manga } = require(`@shineiichijo/marika`);
+  let manga = new Manga();
+  if (!F𝖚𝖑𝖑_A𝖗𝖌𝖘) {
+    return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+      ӄryӄnz,
+      Vlkyre,
+      `*❌ERROR:* No query provided!
 *⚡USAGE:* ${prefix}${Final_Name} <Term>`
-);
-}
-try {
-let srh = await manga.searchManga(F𝖚𝖑𝖑_A𝖗𝖌𝖘);
-let mang = `🎀 *Title:* ${srh.data[0].title}`;
-mang += `📈 *Status:* ${srh.data[0].status}`;
-mang += `🌸 *Total Volumes:* ${srh.data[0].volumes}`;
-mang += `🎗 *Total Chapters:* ${srh.data[0].chapters}`;
-mang += `🧧 *Genres:*`;
-for (let i = 0; i < srh.data[0].genres.length; i++) {
-mang += `\t\t\t\t\t\t\t\t*${srh.data[0].genres[i].name}*`;
-}
-mang += `✨ *Published on:* ${srh.data[0].published.from}`;
-mang += `🌟 *Score: ${srh.data[0].scored}*`;
-mang += `🎐 *Popularity:* ${srh.data[0].popularity}`;
-mang += `🎏 *Favorites:* ${srh.data[0].favorites}`;
-mang += `✍ *Authors:*`;
-for (let i = 0; i < srh.data[0].authors.length; i++) {
-mang += `\t\t\t\t\t\t\t\t\t*${srh.data[0].authors[i].name}* *(${srh.data[0].authors[0].type})*`;
-}
-mang += `🌐 *URL:* ${srh.data[0].url}`;
-if (srh.data[0].background !== null)
-mang += `🎆 *Background:* ${srh.data[0].background}`;
-mang += `❄️ *Description:* ${srh.data[0].synopsis.replace(
-/\[Written by MAL Rewrite]/g,
-``
-)}`;
-return await Image_Button(
-ӄryӄnz,
-Vlkyre,
-Vlkyre.A𝖗𝖌𝖘,
-srh.data[0].images.jpg.large_image_url,
-mang
-);
-} catch {
-return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-ӄryӄnz,
-Vlkyre,
-`*❌ERROR:* Couldn't find any results on the term *${F𝖚𝖑𝖑_A𝖗𝖌𝖘}*
+    );
+  }
+  try {
+    let srh = await manga.searchManga(F𝖚𝖑𝖑_A𝖗𝖌𝖘);
+    let mang = `🎀 *Title:* ${srh.data[0].title}`;
+    mang += `📈 *Status:* ${srh.data[0].status}`;
+    mang += `🌸 *Total Volumes:* ${srh.data[0].volumes}`;
+    mang += `🎗 *Total Chapters:* ${srh.data[0].chapters}`;
+    mang += `🧧 *Genres:*`;
+    for (let i = 0; i < srh.data[0].genres.length; i++) {
+      mang += `\t\t\t\t\t\t\t\t*${srh.data[0].genres[i].name}*`;
+    }
+    mang += `✨ *Published on:* ${srh.data[0].published.from}`;
+    mang += `🌟 *Score: ${srh.data[0].scored}*`;
+    mang += `🎐 *Popularity:* ${srh.data[0].popularity}`;
+    mang += `🎏 *Favorites:* ${srh.data[0].favorites}`;
+    mang += `✍ *Authors:*`;
+    for (let i = 0; i < srh.data[0].authors.length; i++) {
+      mang += `\t\t\t\t\t\t\t\t\t*${srh.data[0].authors[i].name}* *(${srh.data[0].authors[0].type})*`;
+    }
+    mang += `🌐 *URL:* ${srh.data[0].url}`;
+    if (srh.data[0].background !== null)
+      mang += `🎆 *Background:* ${srh.data[0].background}`;
+    mang += `❄️ *Description:* ${srh.data[0].synopsis.replace(
+      /\[Written by MAL Rewrite]/g,
+      ``
+    )}`;
+    return await Image_Button(
+      ӄryӄnz,
+      Vlkyre,
+      Vlkyre.A𝖗𝖌𝖘,
+      srh.data[0].images.jpg.large_image_url,
+      mang
+    );
+  } catch {
+    return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+      ӄryӄnz,
+      Vlkyre,
+      `*❌ERROR:* Couldn't find any results on the term *${F𝖚𝖑𝖑_A𝖗𝖌𝖘}*
 *⚡USAGE:* ${prefix}${Final_Name} <Term>`
-);
-}
+    );
+  }
 };
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
 /*
--- 🌴Modules/🔎ꜱᴇᴀʀᴄʜ/pokemon.js
+++ 🌴Modules/🔎ꜱᴇᴀʀᴄʜ/pokemon.js
@@ -13,15 +13,15 @@
 ╚════════════╝
 */
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
-require("../../Vlkyre🖋️Utilities/𝕷𝖆ყO𝖚𝖙");
+require("../../🤖Utilities/𝕷𝖆ყO𝖚𝖙");
 let {
-isUrl,
-fetchJson,
-getBuffer,
-jsonformat,
-GIFBufferToVideoBuffer,
-getRandom,
-} = require(`../../Vlkyre🖋️Utilities/ɴᴇᴄᴛᴏʀ`);
+  isUrl,
+  fetchJson,
+  getBuffer,
+  jsonformat,
+  GIFBufferToVideoBuffer,
+  getRandom,
+} = require(`../../🤖Utilities/ɴᴇᴄᴛᴏʀ`);
 let fs = require(`fs`);
 let util = require(`util`);
 let hxz = require(`hxz-api`);
@@ -41,53 +41,53 @@ let { Video_Button } = require("../../👒Buttons/Video_Button");
 let { Bot_Not_Admin } = require("../../Vlkyre🧭Compass/Bot_Not_Admin");
 let { Sender_Not_Admin } = require("../../Vlkyre🧭Compass/Sender_Not_Admin");
 let YouTube_Regex =
-/(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
+  /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
 let Present_Path = require(`path`);
 let Script_Name = Present_Path.basename(__filename);
 let Final_Name = Script_Name.slice(0, -3).toLowerCase();
 let { Caught } = require("../../👒Buttons/Caught");
 ("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
 exports.pokemon = async (
-ӄryӄnz,
-Vlkyre,
-pushName,
-F𝖚𝖑𝖑_A𝖗𝖌𝖘,
-quoted,
-isMedia,
-isGroup,
-isBotAdmin,
-isSenderAdmin,
-isSenderTUF,
-commandName,
-body,
+  ӄryӄnz,
+  Vlkyre,
+  pushName,
+  F𝖚𝖑𝖑_A𝖗𝖌𝖘,
+  quoted,
+  isMedia,
+  isGroup,
+  isBotAdmin,
+  isSenderAdmin,
+  isSenderTUF,
+  commandName,
+  body
 ) => {
-try {
-if (!F𝖚𝖑𝖑_A𝖗𝖌𝖘) {
-return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-ӄryӄnz,
-Vlkyre,
-`*❌ERROR:* Which Pokemon Do You Want To Get information about?
+  try {
+    if (!F𝖚𝖑𝖑_A𝖗𝖌𝖘) {
+      return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+        ӄryӄnz,
+        Vlkyre,
+        `*❌ERROR:* Which Pokemon Do You Want To Get information about?
 *⚡USAGE:* ${prefix}${Final_Name} <Pokemon>`
-);
-}
+      );
+    }
 
-let { data: data } = await axios.get(
-`https://pokeapi.co/api/v2/pokemon/${F𝖚𝖑𝖑_A𝖗𝖌𝖘}`
-);
-if (!data.name) {
-return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
-ӄryӄnz,
-Vlkyre,
-`*❌ERROR:* No suck pokemon!
+    let { data: data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${F𝖚𝖑𝖑_A𝖗𝖌𝖘}`
+    );
+    if (!data.name) {
+      return await N𝖊𝖊𝖉__A𝖗𝖌𝖘(
+        ӄryӄnz,
+        Vlkyre,
+        `*❌ERROR:* No suck pokemon!
 *⚡USAGE:* ${prefix}${Final_Name} <Pokemon>`
-);
-}
-return await Image_Button(
-ӄryӄnz,
-Vlkyre,
-Vlkyre.A𝖗𝖌𝖘,
-data.sprites.front_default,
-`💫 *Name: ${data.name}*
+      );
+    }
+    return await Image_Button(
+      ӄryӄnz,
+      Vlkyre,
+      Vlkyre.A𝖗𝖌𝖘,
+      data.sprites.front_default,
+      `💫 *Name: ${data.name}*
 〽️ *Pokedex ID: ${data.id}*
 ⚖ *Weight: ${data.weight}*
 🔆 *Height: ${data.height}*
@@ -100,11 +100,11 @@ data.sprites.front_default,
 ☄ *Special Attack: ${data.stats[3].base_stat}*
 🛡 *Special Defense:${data.stats[4].base_stat}*
 🎐 *Speed: ${data.stats[5].base_stat}*`
-);
-} catch (Èrrðr) {
-Vlkyre.reply(`An Error Occurred`);
-console.log(Èrrðr);
-}
+    );
+  } catch (Èrrðr) {
+    Vlkyre.reply(`An Error Occurred`);
+    console.log(Èrrðr);
+  }
 };
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
 /*
