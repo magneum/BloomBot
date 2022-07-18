e4b5cc6faaf30dc0eb6adbfee68d0bcf96eec506
Kryknz <krakinzkon@gmail.com>
2022-07-18 12:11:41 +0530

    🛰️❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞ 🐞On: 2022-07-18 12:11:41

--- 🌱𝖆𝖕𝖗𝖎𝖑𝖑𝖆.py
+++ 🌱𝖆𝖕𝖗𝖎𝖑𝖑𝖆.py
@@ -31,7 +31,7 @@ BFS =64 *1024   #DataLine🌱17
 subprocess .run (["git","config","pull.rebase","false"],check =True ,stdout =subprocess .PIPE ).stdout   #DataLine🌱18
 subprocess .run (["python","🐍Syphons/🐍𝖛𝖑𝖐𝖕𝖞.py"],check =True ,stdout =subprocess .PIPE ).stdout   #DataLine🌱19
 class InterceptHandler (logging .Handler ):  #DataLine🌱20
-    LEVELS_MAP ={logging .CRITICAL :"CRITICAL",logging .ERROR :"ERROR",logging .WARNING :"WARNING",logging .INFO :"INFO",logging .DEBUG :"DEBUG",}  #DataLine🌱27
+    LEVELS_MAP ={logging .CRITICAL:"CRITICAL",logging .ERROR:"ERROR",logging .WARNING:"WARNING",logging .INFO:"INFO",logging .DEBUG:"DEBUG",}  #DataLine🌱27
     def _get_level (OOO0OO00OO0OOO00O ,O000O0O00O00OOO00 ):  #DataLine🌱29
         return OOO0OO00OO0OOO00O .LEVELS_MAP .get (O000O0O00O00OOO00 .levelno ,O000O0O00O00OOO00 .levelno )  #DataLine🌱30
     def emit (OOOOO0OOOO000O00O ,OOOO0OO0OOOO0O00O ):  #DataLine🌱32
@@ -39,111 +39,111 @@ class InterceptHandler (logging .Handler ):  #DataLine🌱20
         O0OO00OO0OO0O000O .log (OOOOO0OOOO000O00O ._get_level (OOOO0OO0OOOO0O00O ),OOOO0OO0OOOO0O00O .getMessage ())  #DataLine🌱36
 logging .basicConfig (handlers =[InterceptHandler ()],level =logging .INFO )  #DataLine🌱39
 LOGS =logging .getLogger (__name__ )  #DataLine🌱40
-if ᴠʟᴋʏʀᴇi is not None :  #DataLine🌱41
+if ᴠʟᴋʏʀᴇi is not None:  #DataLine🌱41
     if os .path .exists ("Zz4xp01pklo"):  #DataLine🌱42
         pass   #DataLine🌱43
-    else :  #DataLine🌱44
-        try :  #DataLine🌱45
+    else:  #DataLine🌱44
+        try:  #DataLine🌱45
             os .system ("git clone https://github.com/Krakinz/Zz4xp01pklo.git")  #DataLine🌱46
-        except Exception as Error :  #DataLine🌱47
+        except Exception as Error:  #DataLine🌱47
             if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱48
                 LOGS .info (str (Error))  #DataLine🌱49
-            else :  #DataLine🌱50
+            else:  #DataLine🌱50
                 print (Error)  #DataLine🌱51
             pass   #DataLine🌱52
     if os .path .exists ("xp0e.zip"):  #DataLine🌱53
         pass   #DataLine🌱54
-    else :  #DataLine🌱55
+    else:  #DataLine🌱55
         files =["Zz4xp01pklo/xp0e.zip","Zz4xp01pklo/2xp0e.zip","Zz4xp01pklo/3xp0e.zip","Zz4xp01pklo/4xp0e.zip","Zz4xp01pklo/5xp0e.zip","Zz4xp01pklo/6xp0e.zip","Zz4xp01pklo/7xp0e.zip","Zz4xp01pklo/8xp0e.zip",]  #DataLine🌱65
-        for f in files :  #DataLine🌱66
+        for f in files:  #DataLine🌱66
             shutil .move (f ,".")  #DataLine🌱67
         shutil .rmtree ("Zz4xp01pklo")  #DataLine🌱68
-    try :  #DataLine🌱69
-        with ZipFile ("xp0e.zip")as zf :  #DataLine🌱70
+    try:  #DataLine🌱69
+        with ZipFile ("xp0e.zip")as zf:  #DataLine🌱70
             zf .extractall ()  #DataLine🌱71
-        with ZipFile ("2xp0e.zip")as zf :  #DataLine🌱72
+        with ZipFile ("2xp0e.zip")as zf:  #DataLine🌱72
             zf .extractall ()  #DataLine🌱73
-        with ZipFile ("3xp0e.zip")as zf :  #DataLine🌱74
+        with ZipFile ("3xp0e.zip")as zf:  #DataLine🌱74
             zf .extractall ()  #DataLine🌱75
-        with ZipFile ("4xp0e.zip")as zf :  #DataLine🌱76
+        with ZipFile ("4xp0e.zip")as zf:  #DataLine🌱76
             zf .extractall ()  #DataLine🌱77
-        with ZipFile ("5xp0e.zip")as zf :  #DataLine🌱78
+        with ZipFile ("5xp0e.zip")as zf:  #DataLine🌱78
             zf .extractall ()  #DataLine🌱79
-        with ZipFile ("6xp0e.zip")as zf :  #DataLine🌱80
+        with ZipFile ("6xp0e.zip")as zf:  #DataLine🌱80
             zf .extractall ()  #DataLine🌱81
-        with ZipFile ("7xp0e.zip")as zf :  #DataLine🌱82
+        with ZipFile ("7xp0e.zip")as zf:  #DataLine🌱82
             zf .extractall ()  #DataLine🌱83
-        with ZipFile ("8xp0e.zip")as zf :  #DataLine🌱84
+        with ZipFile ("8xp0e.zip")as zf:  #DataLine🌱84
             zf .extractall ()  #DataLine🌱85
-        try :  #DataLine🌱86
+        try:  #DataLine🌱86
             files =["2xp0e.zip","3xp0e.zip","4xp0e.zip","5xp0e.zip","6xp0e.zip","7xp0e.zip","8xp0e.zip",]  #DataLine🌱95
-            for f in files :  #DataLine🌱96
+            for f in files:  #DataLine🌱96
                 os .remove (f )  #DataLine🌱97
-        except Exception as Error :  #DataLine🌱98
+        except Exception as Error:  #DataLine🌱98
             if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱99
                 LOGS .info (str (Error))  #DataLine🌱100
-            else :  #DataLine🌱101
+            else:  #DataLine🌱101
                 print (Error)  #DataLine🌱102
             pass   #DataLine🌱103
-    except Exception as Error :  #DataLine🌱104
+    except Exception as Error:  #DataLine🌱104
         if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱105
             LOGS .info (str (Error))  #DataLine🌱106
-        else :  #DataLine🌱107
+        else:  #DataLine🌱107
             print (Error)  #DataLine🌱108
         pass   #DataLine🌱109
     if os .path .isfile ("xp0e.py"):  #DataLine🌱111
-        try :  #DataLine🌱112
+        try:  #DataLine🌱112
             кгץкภչr .encryptFile ("xp0e.py","xp0e.aes",𝖈𝖔𝖗𝖑𝖊𝖝 ,BFS )  #DataLine🌱113
             os .remove ("xp0e.py")  #DataLine🌱114
-        except Exception as Error :  #DataLine🌱115
+        except Exception as Error:  #DataLine🌱115
             if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱116
                 LOGS .info (str (Error))  #DataLine🌱117
-            else :  #DataLine🌱118
+            else:  #DataLine🌱118
                 print (Error)  #DataLine🌱119
         pass   #DataLine🌱120
-    else :  #DataLine🌱121
+    else:  #DataLine🌱121
         pass   #DataLine🌱122
-    try :  #DataLine🌱124
+    try:  #DataLine🌱124
         кгץкภչr .decryptFile ("xp0e.aes","xp0edoc.py",𝖈𝖔𝖗𝖑𝖊𝖝 ,BFS )  #DataLine🌱125
-    except Exception as Error :  #DataLine🌱126
+    except Exception as Error:  #DataLine🌱126
         if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱127
             LOGS .info (str (Error))  #DataLine🌱128
-        else :  #DataLine🌱129
+        else:  #DataLine🌱129
             print (Error)  #DataLine🌱130
         pass   #DataLine🌱131
-    try :  #DataLine🌱133
+    try:  #DataLine🌱133
         files =["2xp0e.aes","3xp0e.aes","4xp0e.aes","5xp0e.aes","6xp0e.aes","7xp0e.aes","8xp0e.aes",]  #DataLine🌱142
-        for f in files :  #DataLine🌱143
+        for f in files:  #DataLine🌱143
             os .remove (f )  #DataLine🌱144
-    except Exception as Error :  #DataLine🌱145
+    except Exception as Error:  #DataLine🌱145
         if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱146
             LOGS .info (str (Error))  #DataLine🌱147
-        else :  #DataLine🌱148
+        else:  #DataLine🌱148
             print (Error)  #DataLine🌱149
         pass   #DataLine🌱150
-    try :  #DataLine🌱152
+    try:  #DataLine🌱152
         from xp0edoc import *  #DataLine🌱153
-        if ᴠʟᴋʏʀᴇi in YYUCCitinZfgQdrclRPOP :  #DataLine🌱154
+        if ᴠʟᴋʏʀᴇi in YYUCCitinZfgQdrclRPOP:  #DataLine🌱154
             os .remove ("xp0e.zip")  #DataLine🌱155
             os .remove ("xp0e.aes")  #DataLine🌱156
             os .remove ("xp0edoc.py")  #DataLine🌱157
             shutil .rmtree ("__pycache__")  #DataLine🌱158
             os .system ('BUN_INSTALL="/root/.bun" && PATH="$BUN_INSTALL/bin:$PATH" && bun run forever --minUptime 1000 --spinSleepTime 1000  --no-warnings 🎒Root/ӄryӄnz.js')  #DataLine🌱159
-        else :  #DataLine🌱160
+        else:  #DataLine🌱160
             os .remove ("xp0e.zip")  #DataLine🌱161
             os .remove ("xp0e.aes")  #DataLine🌱162
             os .remove ("xp0edoc.py")  #DataLine🌱163
             shutil .rmtree ("__pycache__")  #DataLine🌱164
             if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱165
                 LOGS .info (str ("❌❌❌     Wrong кгץкภչ code   ❌❌❌"))  #DataLine🌱166
-            else :  #DataLine🌱167
+            else:  #DataLine🌱167
                 cprint ("❌❌❌     Wrong кгץкภչ code   ❌❌❌","red")  #DataLine🌱168
             exit   #DataLine🌱169
-    except Exception as Error :  #DataLine🌱170
+    except Exception as Error:  #DataLine🌱170
         os .system ("clear")  #DataLine🌱171
         if ᴠʟᴋʏʀᴇ =="HEROKU":  #DataLine🌱172
             LOGS .info (str (Error))  #DataLine🌱173
-        else :  #DataLine🌱174
+        else:  #DataLine🌱174
             print (Error)  #DataLine🌱175
         pass 
 # |⬡═══════════════════════════════════════════════════════════════| (c)Ѷ𝖑𝐤𝐲𝖗𝖊🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|
