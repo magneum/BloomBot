c3b95653133a75dd79ac329f9e415d9221e30001
Kryknz <krakinzkon@gmail.com>
2022-07-18 10:42:21 +0530

    🕊️🛰️❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞❞ 🛰️🕊️ 🐞On: 2022-07-18 10:42:21

--- Vlkyre🌗Events/ӄօʟօʀs.js
+++ Vlkyre🌗Events/ӄօʟօʀs.js
@@ -64,7 +64,7 @@ function _0x1dab() {
     "❗\x20𝐒𝐞𝐧𝐝𝐞𝐫\x20𝐀𝐝𝐦𝐢𝐧:\x20",
     "4988088hLCWWs",
     "concat",
-    "../Vlkyre🖋️Utilities/𝕷𝖆ყO𝖚𝖙",
+    "../🤖Utilities/𝕷𝖆ყO𝖚𝖙",
     "@heroku-cli/color",
     "subject",
     "Gamboge",
