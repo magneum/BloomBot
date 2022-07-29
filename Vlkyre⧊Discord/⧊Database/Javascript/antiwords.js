2cd0d2a1423c6e04200806e2f206d8a114675026
Kryknz <krakinzkon@gmail.com>
2022-07-29 21:06:01 +0530

    Delete Vlkyre⧊Discord directory

--- Vlkyre⧊Discord/⧊Database/Javascript/antiwords.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const antiwordsSchema = new mongoose.Schema({
-  vlkyreֆɨɖ: String,
-});
-const model = mongoose.model("antiwords", antiwordsSchema);
-module.exports = model;
--- Vlkyre⧊Discord/⧊Utils/patterns.ts
+++
@@ -1,5 +0,0 @@
-export const videoPattern =
-  /^(https?:\/\/)?(www\.)?(m\.|music\.)?(youtube\.com|youtu\.?be)\/.+$/;
-export const playlistPattern = /^.*(list=)([^#\&\?]*).*/;
-export const scRegex = /^https?:\/\/(soundcloud\.com)\/(.*)$/;
-export const mobileScRegex = /^https?:\/\/(soundcloud\.app\.goo\.gl)\/(.*)$/;
