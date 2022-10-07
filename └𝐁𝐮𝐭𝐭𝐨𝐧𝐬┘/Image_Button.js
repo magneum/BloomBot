e17e2e0da4be04898989bfc09ac1a99358857184
KryKnz <KrakinzKon@gmail.com>
2022-10-07 19:11:43 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊 by 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ (4.4.0)

--- └𝐁𝐮𝐭𝐭𝐨𝐧𝐬┘/Image_Button.js
+++ └𝐁𝐮𝐭𝐭𝐨𝐧𝐬┘/Image_Button.js
@@ -16,158 +16,159 @@
 require("../└𝐊𝐫𝐲𝐙𝐨𝐧𝐞┘/Settings");
 let Economy = require(`../└𝐃𝐚𝐭𝐚𝐛𝐚𝐬𝐞𝐬┘/🍃𝖒𝖔𝖓𝖌𝖔/economy`);
 exports.Image_Button = async (ӄryӄnz, Vlkyre, A𝖗𝖌𝖘, M𝖊𝖉𝖎𝖆, 𝕮𝖔𝖓𝖙𝖊𝖓𝖙) => {
-async function checkBalance() {
-await Economy.findOne(
-{
-ID: Vlkyre.sender,
-},
-async (Èrrðr, userEco) => {
-if (Èrrðr) return Caught(ӄryӄnz, Vlkyre, util.format(Èrrðr));
-if (!userEco) {
-await new Economy({
-ID: Vlkyre.sender,
-money: 0,
-daily: 0,
-timeout: 86400000,
-fishdone: 0,
-fishtimeout: 1800000,
-workdone: 0,
-worktimeout: 900000,
-}).catch((Èrrðr) => Caught(ӄryӄnz, Vlkyre, Èrrðr));
-return 0;
-} else {
-return userEco.money;
-}
-}
-);
-}
-("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
-let ʀᴇᴀᴄᴛ;
-if (Vlkyre.isReply) {
-var Receiver =
-Vlkyre.mtype == "extendedTextMessage" &&
-Vlkyre.message.extendedTextMessage.contextInfo != null
-? Vlkyre.message.extendedTextMessage.contextInfo.participant || ""
-: "";
-ʀᴇᴀᴄᴛ = await ӄryӄnz
-.sendMessage(
-Vlkyre.chatID,
-{
-contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
-image: { url: M𝖊𝖉𝖎𝖆 },
-caption: `┌╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+  async function checkBalance() {
+    await Economy.findOne(
+      {
+        ID: Vlkyre.sender,
+      },
+      async (Èrrðr, userEco) => {
+        if (Èrrðr) return Caught(ӄryӄnz, Vlkyre, util.format(Èrrðr));
+        if (!userEco) {
+          await new Economy({
+            ID: Vlkyre.sender,
+            money: 0,
+            daily: 0,
+            timeout: 86400000,
+            fishdone: 0,
+            fishtimeout: 1800000,
+            workdone: 0,
+            worktimeout: 900000,
+          }).catch((Èrrðr) => Caught(ӄryӄnz, Vlkyre, Èrrðr));
+          return 0;
+        } else {
+          return userEco.money;
+        }
+      }
+    );
+  }
+  ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
+  console.log(checkBalance);
+  let ʀᴇᴀᴄᴛ;
+  if (Vlkyre.isReply) {
+    var Receiver =
+      Vlkyre.mtype == "extendedTextMessage" &&
+      Vlkyre.message.extendedTextMessage.contextInfo != null
+        ? Vlkyre.message.extendedTextMessage.contextInfo.participant || ""
+        : "";
+    ʀᴇᴀᴄᴛ = await ӄryӄnz
+      .sendMessage(
+        Vlkyre.chatID,
+        {
+          contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
+          image: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `┌╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 │║⦁ 🕊️𝐍𝐚𝐦𝐞: ${Vlkyre.pushName || "ɴᴏ_ɴᴀᴍᴇ"}
 │║⦁ 🎭𝐂𝐡𝐚𝐭𝐈𝐃: ${Vlkyre.chatID.split("@")[0]}
 └╚═══════⋑
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n💰𝗕𝗮𝗹𝗮𝗻𝗰𝗲: ${checkBalance() || 0}`,
-buttons: [
-{
-buttonId: prefix + "HELP",
-buttonText: { displayText: prefix + "HELP" },
-type: 1,
-},
-{
-buttonId: prefix + "VLKYRE",
-buttonText: { displayText: prefix + "VLKYRE" },
-type: 1,
-},
-],
-headerType: 4,
-},
-{
-contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
-quoted: Vlkyre,
-}
-)
-.catch((e) => console.log(e));
-("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
-} else if (Vlkyre.mentionByTag) {
-var Receiver =
-Vlkyre.mtype == "extendedTextMessage" &&
-Vlkyre.message.extendedTextMessage.contextInfo != null
-? Vlkyre.message.extendedTextMessage.contextInfo.mentionedJid
-: [];
-ʀᴇᴀᴄᴛ = await ӄryӄnz
-.sendMessage(
-Vlkyre.chatID,
-{
-contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
-image: { url: M𝖊𝖉𝖎𝖆 },
-caption: `┌╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+          footer: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n💰𝗕𝗮𝗹𝗮𝗻𝗰𝗲: ${checkBalance() || 0}`,
+          buttons: [
+            {
+              buttonId: prefix + "HELP",
+              buttonText: { displayText: prefix + "HELP" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "VLKYRE",
+              buttonText: { displayText: prefix + "VLKYRE" },
+              type: 1,
+            },
+          ],
+          headerType: 4,
+        },
+        {
+          contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
+          quoted: Vlkyre,
+        }
+      )
+      .catch((e) => console.log(e));
+    ("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
+  } else if (Vlkyre.mentionByTag) {
+    var Receiver =
+      Vlkyre.mtype == "extendedTextMessage" &&
+      Vlkyre.message.extendedTextMessage.contextInfo != null
+        ? Vlkyre.message.extendedTextMessage.contextInfo.mentionedJid
+        : [];
+    ʀᴇᴀᴄᴛ = await ӄryӄnz
+      .sendMessage(
+        Vlkyre.chatID,
+        {
+          contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
+          image: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `┌╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 │║⦁ 🕊️𝐍𝐚𝐦𝐞: ${Vlkyre.pushName || "ɴᴏ_ɴᴀᴍᴇ"}
 │║⦁ 🎭𝐂𝐡𝐚𝐭𝐈𝐃: ${Vlkyre.chatID.split("@")[0]}
 └╚═══════⋑
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n💰𝗕𝗮𝗹𝗮𝗻𝗰𝗲: ${checkBalance() || 0}`,
-buttons: [
-{
-buttonId: prefix + "HELP",
-buttonText: { displayText: prefix + "HELP" },
-type: 1,
-},
-{
-buttonId: prefix + "VLKYRE",
-buttonText: { displayText: prefix + "VLKYRE" },
-type: 1,
-},
-],
-headerType: 4,
-},
-{
-contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
-quoted: Vlkyre,
-}
-)
-.catch((e) => console.log(e));
-("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
-} else {
-ʀᴇᴀᴄᴛ = await ӄryӄnz
-.sendMessage(
-Vlkyre.chatID,
-{
-contextInfo: { mentionedJid: [Vlkyre.sender] },
-image: { url: M𝖊𝖉𝖎𝖆 },
-caption: `┌╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
+          footer: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n💰𝗕𝗮𝗹𝗮𝗻𝗰𝗲: ${checkBalance() || 0}`,
+          buttons: [
+            {
+              buttonId: prefix + "HELP",
+              buttonText: { displayText: prefix + "HELP" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "VLKYRE",
+              buttonText: { displayText: prefix + "VLKYRE" },
+              type: 1,
+            },
+          ],
+          headerType: 4,
+        },
+        {
+          contextInfo: { mentionedJid: [Vlkyre.sender, Receiver] },
+          quoted: Vlkyre,
+        }
+      )
+      .catch((e) => console.log(e));
+    ("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
+  } else {
+    ʀᴇᴀᴄᴛ = await ӄryӄnz
+      .sendMessage(
+        Vlkyre.chatID,
+        {
+          contextInfo: { mentionedJid: [Vlkyre.sender] },
+          image: { url: M𝖊𝖉𝖎𝖆 },
+          caption: `┌╔══『 𝐏𝐫𝐨𝐟𝐢𝐥𝐞𝐫® 』
 │║⦁ 🕊️𝐍𝐚𝐦𝐞: ${Vlkyre.pushName || "ɴᴏ_ɴᴀᴍᴇ"}
 │║⦁ 🎭𝐂𝐡𝐚𝐭𝐈𝐃: ${Vlkyre.chatID.split("@")[0]}
 └╚═══════⋑
 
 ${𝕮𝖔𝖓𝖙𝖊𝖓𝖙}`,
-footer: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n💰𝗕𝗮𝗹𝗮𝗻𝗰𝗲: ${checkBalance() || 0}`,
-buttons: [
-{
-buttonId: prefix + "HELP",
-buttonText: { displayText: prefix + "HELP" },
-type: 1,
-},
-{
-buttonId: prefix + "VLKYRE",
-buttonText: { displayText: prefix + "VLKYRE" },
-type: 1,
-},
-],
-headerType: 4,
-},
-{
-quoted: Vlkyre,
-}
-)
-.catch((e) => console.log(e));
-("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
-let ʀᴇᴀᴄᴛionMessage = {
-ʀᴇᴀᴄᴛ: {
-text: `⚡`,
-key: ʀᴇᴀᴄᴛ.key,
-},
-};
-console.log(ʀᴇᴀᴄᴛ);
-return await ӄryӄnz
-.sendMessage(Vlkyre.chatID, ʀᴇᴀᴄᴛionMessage)
-.catch((e) => console.log(e));
-}
+          footer: `❝ Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞 ❞\n💰𝗕𝗮𝗹𝗮𝗻𝗰𝗲: ${checkBalance() || 0}`,
+          buttons: [
+            {
+              buttonId: prefix + "HELP",
+              buttonText: { displayText: prefix + "HELP" },
+              type: 1,
+            },
+            {
+              buttonId: prefix + "VLKYRE",
+              buttonText: { displayText: prefix + "VLKYRE" },
+              type: 1,
+            },
+          ],
+          headerType: 4,
+        },
+        {
+          quoted: Vlkyre,
+        }
+      )
+      .catch((e) => console.log(e));
+    ("|⬡════════════════════════════════════════════════════════════════════════════════════|▷◁|═════════════════════════════════════════════════════════════════════⬡|");
+    let ʀᴇᴀᴄᴛionMessage = {
+      ʀᴇᴀᴄᴛ: {
+        text: `⚡`,
+        key: ʀᴇᴀᴄᴛ.key,
+      },
+    };
+    console.log(ʀᴇᴀᴄᴛ);
+    return await ӄryӄnz
+      .sendMessage(Vlkyre.chatID, ʀᴇᴀᴄᴛionMessage)
+      .catch((e) => console.log(e));
+  }
 };
 ("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
 /*
