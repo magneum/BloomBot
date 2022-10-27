89f79cad58e0b5afb4ebe369971d277ca1c064b8
KryKnz <krakinzkon@gmail.com>
2022-10-27 19:46:37 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊 𝖇𝔂 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ [𝐯𝟓.𝟎.𝟎]

--- YouTube.js
+++ YouTube.js
@@ -1,5 +1,5 @@
 (async () => {
   let { ytv } = require("./core/y2mate");
-  let media = await ytv("https://www.youtube.com/watch?v=GeAq6Mft17k", "360");
+  let media = await ytv("https://www.youtube.com/watch?v=GeAq6Mft17k", "360p");
   console.log(media.dl_link);
 })();
