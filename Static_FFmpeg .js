a46931295a424ea7e7a5546de247085c46a2e122
KryKnz <krakinzkon@gmail.com>
2022-10-27 12:19:54 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊 𝖇𝔂 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ [𝐯𝟓.𝟎.𝟎]

--- Static_FFmpeg .js
+++ Static_FFmpeg .js
@@ -1,2 +1,12 @@
 var Path_To_Static_FFmpeg = require("ffmpeg-static");
 console.log(Path_To_Static_FFmpeg);
+
+const { exec } = require("child_process");
+exec("ffmpeg", (error, stdout, stderr) => {
+if (error) {
+console.error(`exec error: ${error}`);
+return;
+}
+console.log(`stdout: No. of directories = ${stdout}`);
+if (stderr != "") console.error(`stderr: ${stderr}`);
+});
