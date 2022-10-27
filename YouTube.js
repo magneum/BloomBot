a14ac49f3a4632d3dc030290e05ab416e63f8de9
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-10-27 14:27:43 +0000

    Update 2 files

--- YouTube.js
+++ YouTube.js
@@ -1,5 +1,5 @@
 (async () => {
-  let { ytv } = require("./Core/y2mate");
-  let media = await ytv("https://www.youtube.com/watch?v=GeAq6Mft17k", "360p");
+  let { ytv, yta } = require("./Core/y2mate");
+  let media = await yta("https://www.youtube.com/watch?v=GeAq6Mft17k", "128kbps");
   console.log(media.dl_link);
 })();
