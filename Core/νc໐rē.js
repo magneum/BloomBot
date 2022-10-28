03aa76fd53813fdc4b00955175dda27e047edaad
KryKnz <krakinzkon@gmail.com>
2022-10-28 16:24:45 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊 𝖇𝔂 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ [𝐯𝟓.𝟎.𝟎]

--- Core/νc໐rē.js
+++ Core/νc໐rē.js
@@ -28,7 +28,7 @@ await νℓкуяє.FastHub.fetch();
 var newCommits = await νℓкуяє.FastHub.log([`${νℓкуяє.FastGit}..origin/${νℓкуяє.FastGit}`]);
 if (newCommits.total != 0) {
 console.clear();
-console.log("Update Found Rebooting.....");
+console.log("🛸𝐆𝐢𝐭𝐇𝐮𝐛: Update Found Rebooting.....");
 process.exit(0);
 }
 function ShowRed(Topic, Text) {
