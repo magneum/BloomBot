77b9f118eaebb389b6dcef8593d6837bdc8b3be1
KryKnz <krakinzkon@gmail.com>
2022-10-31 09:46:45 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊 𝖇𝔂 𝐊𝐫𝐚𝐤𝐢𝐧𝐳𝐋𝐚𝐛™ [𝐯𝟓.𝟎.𝟎]

--- Core/Authenticator.js
+++ Core/Authenticator.js
@@ -14,225 +14,263 @@
 */
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-return new (P || (P = Promise))(function (resolve, reject) {
-function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-step((generator = generator.apply(thisArg, _arguments || [])).next());
-});
+const _0x3afda0 = _0x15f5;
+function _0x4fac() {
+const _0x2e8149 = [
+"log",
+"@adiwajshing/baileys",
+"useRemoteFileAuthState",
+"update",
+"then",
+"map",
+"8ZSiDtu",
+"initAuthCreds",
+"BufferJSON",
+"fromObject",
+"354232UhcfGw",
+"credential",
+"82470XexctE",
+"proto",
+"8851890QojJaw",
+"AppStateSyncKeyData",
+"next",
+"replacer",
+"1807770UKcDyz",
+"creds",
+"__awaiter",
+"__esModule",
+"1938215QHmfbg",
+"26RggKmH",
+"reviver",
+"stringify",
+"catch",
+"774QPwRIz",
+"Auth",
+"36536DcisBp",
+"push",
+"create",
+"21iUeLoC",
+"value",
+"destroy",
+"done",
+"40692278pwpYot",
+];
+_0x4fac = function () {
+return _0x2e8149;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-const baileys_1 = require("@adiwajshing/baileys");
-const chalk_1 = __importDefault(require("chalk"));
-const auth_1 = require("./Authy");
-const KEY_MAP = {
-'pre-key': 'preKeys',
-'session': 'sessions',
-'sender-key': 'senderKeys',
-'app-state-sync-key': 'appStateSyncKeys',
-'app-state-sync-version': 'appStateVersions',
-'sender-key-memory': 'senderKeyMemory'
-};
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-const Authenticator = (logger) =>
-__awaiter(void 0, void 0, void 0, function* () {
-// require fs here so that in case "fs" is not available -- the app does not crash
-const { readFileSync, writeFileSync, existsSync } = require("fs");
-let creds;
-let keys = {};
-const checkCreds = () =>
-__awaiter(void 0, void 0, void 0, function* () {
-const lock = yield auth_1.Cred.findOne({
-where: {
-key: "noiseKey",
-},
-});
-if (lock) {
-return true;
-} else {
-return false;
+return _0x4fac();
 }
-});
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-const loadCreds = () =>
-__awaiter(void 0, void 0, void 0, function* () {
-const allCreds = yield auth_1.Cred.findAll();
-let temp = {};
-allCreds.forEach((res) => {
-let val = res.getDataValue("value");
-let key = res.getDataValue("key");
-val = JSON.parse(val, baileys_1.BufferJSON.reviver);
-temp[key] = val;
-});
-return temp;
-});
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-const loadKeys = () =>
-__awaiter(void 0, void 0, void 0, function* () {
-let keys = {
-preKeys: {},
-sessions: {},
-senderKeys: {},
-appStateSyncKeys: {},
-appStateVersions: {},
-senderKeyMemory: {},
-};
-const allKeys = yield auth_1.Key.findAll();
-allKeys.forEach((res) => {
-let val = res.getDataValue("value");
-let key = res.getDataValue("key");
-let type = res.getDataValue("type");
-val = JSON.parse(val, baileys_1.BufferJSON.reviver);
-keys[type][key] = val;
-});
-return keys;
-});
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-const saveCreds = (data) =>
-__awaiter(void 0, void 0, void 0, function* () {
-if (!data) {
-// console.log('Saving all creds');
-data = creds;
-}
-for (const _key in data) {
-const cred = yield auth_1.Cred.findOne({
-where: {
-key: _key,
-},
-});
-if (cred) {
-yield cred.update({
-value: JSON.stringify(
-data[_key],
-baileys_1.BufferJSON.replacer,
-2
-),
-});
-// .then((res) => { console.log(`updated value ${_key} `) }).catch(err => { console.log(chalk.whiteBright(err)) });
-} else {
-yield auth_1.Cred.create({
-key: _key,
-value: JSON.stringify(
-data[_key],
-baileys_1.BufferJSON.replacer,
-2
-),
-});
-// .then((res) => { console.log(`inserted value ${_key}`) }).catch(err => { console.log(chalk.whiteBright(err)) });
+(function (_0x508b97, _0x336cd7) {
+const _0x5e0057 = _0x15f5,
+_0xa7ad36 = _0x508b97();
+while (!![]) {
+try {
+const _0x38a33c =
+-parseInt(_0x5e0057(0x8d)) / 0x1 +
+(-parseInt(_0x5e0057(0x75)) / 0x2) *
+(-parseInt(_0x5e0057(0x6a)) / 0x3) +
+(-parseInt(_0x5e0057(0x89)) / 0x4) * (parseInt(_0x5e0057(0x74)) / 0x5) +
+(parseInt(_0x5e0057(0x70)) / 0x6) * (-parseInt(_0x5e0057(0x7e)) / 0x7) +
+(parseInt(_0x5e0057(0x7b)) / 0x8) * (-parseInt(_0x5e0057(0x79)) / 0x9) +
+-parseInt(_0x5e0057(0x6c)) / 0xa +
+parseInt(_0x5e0057(0x82)) / 0xb;
+if (_0x38a33c === _0x336cd7) break;
+else _0xa7ad36["push"](_0xa7ad36["shift"]());
+} catch (_0x1d81aa) {
+_0xa7ad36["push"](_0xa7ad36["shift"]());
 }
 }
-});
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-const saveKey = (key, data, _key) =>
-__awaiter(void 0, void 0, void 0, function* () {
-for (const subKey in data[_key]) {
-// console.log(`Trying to find key ${key} and subKey ${subKey}.`);
-const res = yield auth_1.Key.findOne({
-where: {
-key: subKey,
-type: key,
-},
-});
-if (res) {
-yield res
-.update({
-value: JSON.stringify(
-data[_key][subKey],
-baileys_1.BufferJSON.replacer,
-2
-),
-})
-.then((res) => {
-// console.log(`updated key ${key} and subKey ${subKey}`)
-})
-.catch((err) => {
-console.log(chalk_1.default.blueBright(err));
-});
-} else {
-yield auth_1.Key.create({
-key: subKey,
-value: JSON.stringify(
-data[_key][subKey],
-baileys_1.BufferJSON.replacer,
-2
-),
-type: key,
-})
-.then((res) => {
-// console.log(`inserted key ${key} and subKey ${subKey}`)
-})
-.catch((err) => {
-console.log(chalk_1.default.blueBright(err));
-});
-}
-}
-return;
-});
-let credsExist = yield checkCreds();
-if (credsExist) {
-// console.log('loading values back.');
-let parent = {
-creds: {},
-keys: {},
-};
-const allCreds = yield loadCreds();
-const allKeys = yield loadKeys();
-parent.creds = allCreds;
-parent.keys = allKeys;
-const final = JSON.parse(
-JSON.stringify(parent),
-baileys_1.BufferJSON.reviver
+})(_0x4fac, 0xb5f62);
+function _0x15f5(_0x1e61be, _0x299c77) {
+const _0x4fac38 = _0x4fac();
+return (
+(_0x15f5 = function (_0x15f571, _0x3c168e) {
+_0x15f571 = _0x15f571 - 0x6a;
+let _0x15df2d = _0x4fac38[_0x15f571];
+return _0x15df2d;
+}),
+_0x15f5(_0x1e61be, _0x299c77)
 );
-// console.log(final);
-creds = final.creds;
-keys = final.keys;
-} else {
-creds = (0, baileys_1.initAuthCreds)();
-keys = {};
-saveCreds();
 }
+var __awaiter =
+(this && this[_0x3afda0(0x72)]) ||
+function (_0xe947b2, _0x565e88, _0x2d9c38, _0xb1673a) {
+function _0x41b0ca(_0x594b52) {
+return _0x594b52 instanceof _0x2d9c38
+? _0x594b52
+: new _0x2d9c38(function (_0x2ab125) {
+_0x2ab125(_0x594b52);
+});
+}
+return new (_0x2d9c38 || (_0x2d9c38 = Promise))(function (
+_0x36b387,
+_0x3983a6
+) {
+const _0x3934b3 = _0x15f5;
+function _0x22f00f(_0x7d6792) {
+const _0x2d7269 = _0x15f5;
+try {
+_0x54a191(_0xb1673a[_0x2d7269(0x6e)](_0x7d6792));
+} catch (_0x19b5c3) {
+_0x3983a6(_0x19b5c3);
+}
+}
+function _0x7e6a8a(_0x552fe7) {
+try {
+_0x54a191(_0xb1673a["throw"](_0x552fe7));
+} catch (_0x507f32) {
+_0x3983a6(_0x507f32);
+}
+}
+function _0x54a191(_0x1fcd53) {
+const _0xe253ca = _0x15f5;
+_0x1fcd53[_0xe253ca(0x81)]
+? _0x36b387(_0x1fcd53[_0xe253ca(0x7f)])
+: _0x41b0ca(_0x1fcd53["value"])[_0xe253ca(0x87)](
+_0x22f00f,
+_0x7e6a8a
+);
+}
+_0x54a191(
+(_0xb1673a = _0xb1673a["apply"](_0xe947b2, _0x565e88 || []))[
+_0x3934b3(0x6e)
+]()
+);
+});
+};
+Object["defineProperty"](exports, _0x3afda0(0x73), { value: !![] }),
+(exports[_0x3afda0(0x85)] = void 0x0);
+const baileys_1 = require(_0x3afda0(0x84)),
+auth_1 = require("./auth"),
+useRemoteFileAuthState = () =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0x12efa4 = _0x3afda0,
+_0x54def0 = (_0x516877, _0x4e11e6, _0x2c7103) =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0xd16931 = _0x15f5;
+return auth_1[_0xd16931(0x7a)]
+["findOne"]({ where: { type: _0x4e11e6, key: _0x2c7103 } })
+[_0xd16931(0x87)]((_0x58f271) => {
+const _0x43ed94 = _0xd16931;
+_0x58f271
+? _0x58f271[_0x43ed94(0x86)]({
+value: JSON["stringify"](
+_0x516877,
+baileys_1[_0x43ed94(0x8b)]["replacer"]
+),
+})
+[_0x43ed94(0x87)]((_0x24e3b4) => {})
+[_0x43ed94(0x78)]((_0x582f48) => {
+const _0x1816fb = _0x43ed94;
+console[_0x1816fb(0x83)](_0x582f48);
+})
+: auth_1[_0x43ed94(0x7a)]
+[_0x43ed94(0x7d)]({
+key: _0x2c7103,
+value: JSON[_0x43ed94(0x77)](
+_0x516877,
+baileys_1[_0x43ed94(0x8b)][_0x43ed94(0x6f)]
+),
+type: _0x4e11e6,
+})
+[_0x43ed94(0x87)]((_0x523347) => {})
+[_0x43ed94(0x78)]((_0x47d6b9) => {
+const _0x201ae8 = _0x43ed94;
+console[_0x201ae8(0x83)](_0x47d6b9);
+});
+});
+}),
+_0x1b813b = (_0x38c456, _0x18cdbe) =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0x4648d6 = _0x15f5;
+try {
+let _0x5a1dfd = yield auth_1[_0x4648d6(0x7a)]["findAll"]({
+where: { type: _0x38c456, key: _0x18cdbe },
+raw: !![],
+});
+if (!_0x5a1dfd[0x0]) return null;
+return JSON["parse"](
+_0x5a1dfd[0x0][_0x4648d6(0x7f)],
+baileys_1[_0x4648d6(0x8b)][_0x4648d6(0x76)]
+);
+} catch (_0x54415d) {
+return (
+console[_0x4648d6(0x83)](
+"Got\x20error\x20while\x20reading\x20" +
+_0x38c456 +
+"\x20" +
+_0x18cdbe +
+"!!"
+),
+console[_0x4648d6(0x83)](_0x54415d),
+null
+);
+}
+}),
+_0x422f6e = (_0xc9b503, _0x516fc9) =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0x1169f2 = _0x15f5;
+try {
+yield auth_1[_0x1169f2(0x7a)][_0x1169f2(0x80)]({
+where: { type: _0xc9b503, key: _0x516fc9 },
+});
+} catch (_0x37f12d) {}
+}),
+_0x206229 =
+(yield _0x1b813b(_0x12efa4(0x71), _0x12efa4(0x8e))) ||
+(0x0, baileys_1[_0x12efa4(0x8a)])();
 return {
 state: {
-creds,
+creds: _0x206229,
 keys: {
-get: (type, ids) => {
-const key = KEY_MAP[type];
-return ids.reduce((dict, id) => {
-let _a;
-let value =
-(_a = keys[key]) === null || _a === void 0 ? void 0 : _a[id];
-if (value) {
-if (type === "app-state-sync-key") {
-value = baileys_1.proto.AppStateSyncKeyData.fromObject(value);
+get: (_0x3fc378, _0x54e6fb) =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0x44652e = _0x12efa4,
+_0x56e420 = {};
+return (
+yield Promise["all"](
+_0x54e6fb[_0x44652e(0x88)]((_0x25ad9d) =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0x50691c = _0x44652e;
+let _0x31650 = yield _0x1b813b(_0x3fc378, _0x25ad9d);
+_0x3fc378 === "app-state-sync-key" &&
+_0x31650 &&
+(_0x31650 =
+baileys_1[_0x50691c(0x6b)][_0x50691c(0x6d)][
+_0x50691c(0x8c)
+](_0x31650)),
+(_0x56e420[_0x25ad9d] = _0x31650);
+})
+)
+),
+_0x56e420
+);
+}),
+set: (_0x4363bd) =>
+__awaiter(void 0x0, void 0x0, void 0x0, function* () {
+const _0x3ea249 = _0x12efa4,
+_0x9e23ef = [];
+for (const _0x13e71b in _0x4363bd) {
+for (const _0x45fc65 in _0x4363bd[_0x13e71b]) {
+const _0x5d0b4e = _0x4363bd[_0x13e71b][_0x45fc65];
+_0x9e23ef[_0x3ea249(0x7c)](
+_0x5d0b4e
+? _0x54def0(_0x5d0b4e, _0x13e71b, _0x45fc65)
+: _0x422f6e(_0x13e71b, _0x45fc65)
+);
 }
-dict[id] = value;
 }
-return dict;
-}, {});
-},
-set: (data) =>
-__awaiter(void 0, void 0, void 0, function* () {
-for (const _key in data) {
-const key = KEY_MAP[_key];
-// console.log(`Got raw key - ${_key} and got mapped key ${key}. The value is ${data[_key]}`)
-keys[key] = keys[key] || {};
-Object.assign(keys[key], data[_key]);
-yield saveKey(key, data, _key);
-}
-// saveState();
+yield Promise["all"](_0x9e23ef);
 }),
 },
 },
-saveCreds,
+saveCreds: () => {
+const _0x1ec5af = _0x12efa4;
+return _0x54def0(_0x206229, _0x1ec5af(0x71), _0x1ec5af(0x8e));
+},
 };
 });
-exports.default = Authenticator;
+exports["useRemoteFileAuthState"] = useRemoteFileAuthState;
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 /*
 ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
