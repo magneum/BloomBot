5d0e9c20579959b6a6533bcbab1f59b073c73dda
krykenz <krakinzkon@gmail.com>
2022-12-17 10:17:05 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊™ 𝖇𝔂 𝐊𝐫𝐲𝐊𝐞𝐧𝐳 [𝟒.𝟒.𝟎]

--- 🐍𝖛𝖑𝖐𝖕𝖞.py
+++ 🐍𝖛𝖑𝖐𝖕𝖞.py
@@ -14,8 +14,8 @@ from termcolor import *#line:8
 from os import getenv #line:9
 from dotenv import load_dotenv #line:10
 load_dotenv ("./ᴠʟᴋʏʀᴇ™.env")#line:11
-ᴠʟᴋʏʀᴇ =getenv ("HEROKU",None )#line:12
-𝖈𝖔𝖗𝖑𝖊𝖝 =getenv ("HEROKU",None )#line:13
+ᴠʟᴋʏʀᴇ =getenv ("DOCKER",None )#line:12
+𝖈𝖔𝖗𝖑𝖊𝖝 =getenv ("DOCKER",None )#line:13
 ᴠʟᴋʏʀᴇi =getenv ("CODE",None )#line:14
 class InterceptHandler (logging .Handler ):#line:15
     LEVELS_MAP ={logging .CRITICAL :"CRITICAL",logging .ERROR :"ERROR",logging .WARNING :"WARNING",logging .INFO :"INFO",logging .DEBUG :"DEBUG",}#line:22
@@ -29,48 +29,48 @@ LOGS =logging .getLogger (__name__ )#line:35
 pkgFl =pathlib .Path ("package-lock.json")#line:36
 if pkgFl .exists ():#line:37
     os .remove ("package-lock.json")#line:38
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:39
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:39
         LOGS .info (str (f"🐍𝐏𝐲: package-lock.json has been cleaned!"))#line:40
     else :#line:41
         cprint (f"🐍𝐏𝐲: package-lock.json has been cleaned!","yellow")#line:42
 else :#line:43
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:44
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:44
         LOGS .info (str (f"🐍𝐏𝐲: package-lock.json File does not exist!"))#line:45
     else :#line:46
         cprint (f"🐍𝐏𝐲: package-lock.json File does not exist!","green")#line:47
 try :#line:48
-    subprocess .run (["node","•Route/Krank.js"],check =True ,stdout =subprocess .PIPE ).stdout #line:49
+    subprocess .run (["node", "--no-warnings", "•Route/Krank"],check =True ,stdout =subprocess .PIPE ).stdout #line:49
 except Exception as Error :#line:50
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:51
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:51
         LOGS .info (str (f"🐍𝐏𝐲: {Error}"))#line:52
     else :#line:53
         cprint (f"🐍𝐏𝐲: {Error}")#line:54
 pkgFl =pathlib .Path ("package.json")#line:55
 if pkgFl .exists ():#line:56
     os .remove ("package.json")#line:57
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:58
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:58
         LOGS .info (str (f"🐍𝐏𝐲: package.json has been cleaned!"))#line:59
     else :#line:60
         cprint (f"🐍𝐏𝐲: package.json has been cleaned!","yellow")#line:61
 else :#line:62
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:63
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:63
         LOGS .info (str (f"🐍𝐏𝐲: package.json File does not exist!"))#line:64
     else :#line:65
         cprint (f"🐍𝐏𝐲: package.json File does not exist!","green")#line:66
 try :#line:67
-    subprocess .run (["git","config","--global","user.name","KryKnz"],check =True ,stdout =subprocess .PIPE ).stdout #line:68
-    subprocess .run (["git","config","--global","user.email","krakinzkon@gmail.com"],check =True ,stdout =subprocess .PIPE ).stdout #line:69
+    # subprocess .run (["git","config","--global","user.name","KryKnz"],check =True ,stdout =subprocess .PIPE ).stdout #line:68
+    # subprocess .run (["git","config","--global","user.email","krakinzkon@gmail.com"],check =True ,stdout =subprocess .PIPE ).stdout #line:69
     subprocess .run (["git","config","pull.rebase","false"],check =True ,stdout =subprocess .PIPE ).stdout #line:70
     subprocess .run (["git","init","--initial-branch=Primary"],check =True ,stdout =subprocess .PIPE ).stdout #line:71
     subprocess .run (["git","fetch","origin","Primary"],check =True ,stdout =subprocess .PIPE ).stdout #line:72
     subprocess .run (["git","reset","--hard","origin/Primary"],check =True ,stdout =subprocess .PIPE ).stdout #line:73
     subprocess .run (["git","pull"],check =True ,stdout =subprocess .PIPE ).stdout #line:74
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:75
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:75
         LOGS .info (str ("🐍𝐏𝐲: git sync done!"))#line:76
     else :#line:77
         cprint ("🐍𝐏𝐲: git sync done!","green")#line:78
 except Exception as Error :#line:79
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:80
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:80
         LOGS .info (str (f"🐍𝐏𝐲: {Error}"))#line:81
         LOGS .info (str ("🐍𝐏𝐲: Error In git sync!"))#line:82
         LOGS .info (str ("🐍𝐏𝐲: Trying TO Stash and ReTry!"))#line:83
@@ -78,8 +78,8 @@ except Exception as Error :#line:79
         cprint ("🐍𝐏𝐲: Error In git sync!","red")#line:85
         cprint ("🐍𝐏𝐲: Trying TO Stash and ReTry!","yellow")#line:86
     try :#line:87
-        subprocess .run (["git","config","--global","user.name","KryKnz"],check =True ,stdout =subprocess .PIPE ).stdout #line:88
-        subprocess .run (["git","config","--global","user.email","krakinzkon@gmail.com"],check =True ,stdout =subprocess .PIPE ).stdout #line:89
+        # subprocess .run (["git","config","--global","user.name","KryKnz"],check =True ,stdout =subprocess .PIPE ).stdout #line:88
+        # subprocess .run (["git","config","--global","user.email","krakinzkon@gmail.com"],check =True ,stdout =subprocess .PIPE ).stdout #line:89
         subprocess .run (["git","config","pull.rebase","false"],check =True ,stdout =subprocess .PIPE ).stdout #line:90
         subprocess .run (["git","init","--initial-branch=Primary"],check =True ,stdout =subprocess .PIPE ).stdout #line:91
         subprocess .run (["git","fetch","origin","Primary"],check =True ,stdout =subprocess .PIPE ).stdout #line:92
@@ -88,7 +88,7 @@ except Exception as Error :#line:79
         subprocess .run (["git","stash","drop"],check =True ,stdout =subprocess .PIPE ).stdout #line:95
         subprocess .run (["git","pull"],check =True ,stdout =subprocess .PIPE ).stdout #line:96
     except Exception as Error :#line:97
-        if ᴠʟᴋʏʀᴇ =="HEROKU":#line:98
+        if ᴠʟᴋʏʀᴇ =="DOCKER":#line:98
             LOGS .info (str (f"🐍𝐏𝐲: {Error}"))#line:99
             LOGS .info (str ("🐍𝐏𝐲: Nothing To Stash and Drop!"))#line:100
         else :#line:101
@@ -96,24 +96,24 @@ except Exception as Error :#line:79
             cprint ("🐍𝐏𝐲: Nothing To Stash and Drop!","blue")#line:103
 try :#line:104
     subprocess .run (["npm","install","--force","--save"],check =True ,stdout =subprocess .PIPE ).stdout #line:105
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:106
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:106
         LOGS .info (str (f"🐍𝐏𝐲: npm install done using python subprocess!"))#line:107
     else :#line:108
         cprint (f"🐍𝐏𝐲: npm install done using python subprocess!")#line:109
 except Exception as Error :#line:110
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:111
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:111
         LOGS .info (str (f"🐍𝐏𝐲: {Error}"))#line:112
     else :#line:113
         cprint (f"🐍𝐏𝐲: {Error}")#line:114
 pkgFl =pathlib .Path ("package-lock.json")#line:115
 if pkgFl .exists ():#line:116
     os .remove ("package-lock.json")#line:117
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:118
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:118
         LOGS .info (str (f"🐍𝐏𝐲: package-lock.json has been cleaned!"))#line:119
     else :#line:120
         cprint (f"🐍𝐏𝐲: package-lock.json has been cleaned!","yellow")#line:121
 else :#line:122
-    if ᴠʟᴋʏʀᴇ =="HEROKU":#line:123
+    if ᴠʟᴋʏʀᴇ =="DOCKER":#line:123
         LOGS .info (str (f"🐍𝐏𝐲: package-lock.json File does not exist!"))#line:124
     else :#line:125
         cprint (f"🐍𝐏𝐲: package-lock.json File does not exist!","green")
