c847ac1660d701fe51dbd788f91282823bdab63f
KryKenz <KrakinzKon@gmail.com>
2022-12-17 12:37:33 +0530

    Ѷ𝖑𝐤𝐲𝖗𝖊™ 𝖇𝔂 𝐊𝐫𝐲𝐊𝐞𝐧𝐳 [𝟒.𝟒.𝟒]

--- Quick.js
+++ Quick.js
@@ -1,6 +1,4 @@
 (async () => {
-  var Path_To_Static_FFmpeg = require("./FFmpeg/ffmpeg-static");
-  var path = require("path");
-  console.log(Path_To_Static_FFmpeg);
-  console.log(process.cwd() + "/FFmpeg/ffmpeg-static/ffmpeg");
+var { getBuffer } = require("./Core/myfunc");
+let h = await getBuffer(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=+918436686758`);
 })();
