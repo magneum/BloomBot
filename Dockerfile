c313a4a5d75f12d681cddf4dc50e469fad10c52d
KryKenz <krakinzkon@gmail.com>
2022-12-17 23:12:21 +0530

    vlkyre ⚡version_4

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM python:latest
+ENV BUN "/venv"
+RUN python -m venv $BUN
+ENV PATH "$BUN/bin:$PATH"
+
+# Vlkyre os dependency Instalation
+RUN apt-get update && apt-get install -y \
+jq \
+npm \
+git \
+curl \
+wget \
+nodejs \
+ffmpeg \
+bpm-tools \
+opus-tools \
+python3-pip \
+python-is-python3
+RUN git clone https://github.com/Krykenz/Vlkyre.git
+RUN rm -rf node_modules && npm install -g n
+RUN n install 16 && hash -r
+
+# Vlkyre Runtime support Installation
+RUN cd Vlkyre
+WORKDIR /Vlkyre
+RUN git init --initial-branch=Primary && git fetch origin Primary && git reset --hard origin/Primary
+RUN npm install -g spotify-dl spdl-core forever --force
+RUN npm install --force youtube-sr@4.2.0 yt-search@2.10.3 ytdl-core
+CMD node --no-warnings •Route/ӄryӄnz.js
---
+++ •dBase/🍃𝖒𝖔𝖓𝖌𝖔/economy.js
@@ -0,0 +1,109 @@
+("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
+function _0x5c4b(_0x7a0e1c, _0x3d0d92) {
+  const _0x2df4f9 = _0x2df4();
+  return (
+    (_0x5c4b = function (_0x5c4bdc, _0xcc102d) {
+      _0x5c4bdc = _0x5c4bdc - 0x1b5;
+      let _0x5e1bd1 = _0x2df4f9[_0x5c4bdc];
+      return _0x5e1bd1;
+    }),
+    _0x5c4b(_0x7a0e1c, _0x3d0d92)
+  );
+}
+const _0x226f1c = _0x5c4b;
+function _0x2df4() {
+  const _0x5c80e0 = [
+    "19938jGsZrW",
+    "2471MKMuue",
+    "12gpnKLq",
+    "30JQHuin",
+    "exports",
+    "20BcWKXr",
+    "Economy",
+    "1175031bqdDcq",
+    "40150344oedMuq",
+    "2469156fKlWUV",
+    "mongoose",
+    "2TsYCSk",
+    "8455432ZWZMSJ",
+    "5086301uUAnTv",
+    "model",
+    "9acCsFz",
+    "Schema",
+    "166295yVmzWT",
+  ];
+  _0x2df4 = function () {
+    return _0x5c80e0;
+  };
+  return _0x2df4();
+}
+(function (_0x5a5f71, _0x34d258) {
+  const _0x506db2 = _0x5c4b,
+    _0x1abe83 = _0x5a5f71();
+  while (!![]) {
+    try {
+      const _0x5e663c =
+        parseInt(_0x506db2(0x1bf)) / 0x1 +
+        (parseInt(_0x506db2(0x1c3)) / 0x2) *
+          (-parseInt(_0x506db2(0x1c1)) / 0x3) +
+        (-parseInt(_0x506db2(0x1bd)) / 0x4) *
+          (parseInt(_0x506db2(0x1b7)) / 0x5) +
+        (-parseInt(_0x506db2(0x1b8)) / 0x6) *
+          (-parseInt(_0x506db2(0x1b9)) / 0x7) +
+        (parseInt(_0x506db2(0x1c4)) / 0x8) *
+          (parseInt(_0x506db2(0x1b5)) / 0x9) +
+        (-parseInt(_0x506db2(0x1bb)) / 0xa) *
+          (-parseInt(_0x506db2(0x1c5)) / 0xb) +
+        (-parseInt(_0x506db2(0x1ba)) / 0xc) *
+          (parseInt(_0x506db2(0x1c0)) / 0xd);
+      if (_0x5e663c === _0x34d258) break;
+      else _0x1abe83["push"](_0x1abe83["shift"]());
+    } catch (_0x566f38) {
+      _0x1abe83["push"](_0x1abe83["shift"]());
+    }
+  }
+})(_0x2df4, 0xae64d);
+let mongoose = require(_0x226f1c(0x1c2)),
+  EconomySchema = new mongoose[_0x226f1c(0x1b6)]({
+    ID: String,
+    money: Number,
+    daily: Number,
+    timeout: Number,
+    fishdone: Number,
+    fishtimeout: Number,
+    workdone: Number,
+    worktimeout: Number,
+  });
+module[_0x226f1c(0x1bc)] = mongoose[_0x226f1c(0x1c6)](
+  _0x226f1c(0x1be),
+  EconomySchema
+);
+("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("|⬡═══════════════════════════════════════════════════════════════| (c)𝐕𝐥𝐤𝐲𝐫𝐞🕊️ʙʏ🕊️ᴋʀᴀᴋɪɴᴢʟᴀʙ™ |═══════════════════════════════════════════════════════════════⬡|");
