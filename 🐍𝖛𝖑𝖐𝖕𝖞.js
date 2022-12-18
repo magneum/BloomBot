8fb21cf4a105f05cfe9a2e6624cf0da617f0e5b8
KryKenz <krakinzkon@gmail.com>
2022-12-18 20:54:46 +0530

    vlkyre❣️version_4

--- 🐍𝖛𝖑𝖐𝖕𝖞.js
+++ 🐍𝖛𝖑𝖐𝖕𝖞.js
@@ -1,41 +1,42 @@
-let fs = require("fs");
-let rimraf = require("rimraf");
-async function execute(argument) {
-  require("child_process").exec("echo test", (error, stdout, stderr) => {
-    if (error) {
-      return console.error(`exec error: ${error}`);
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-}
+(async () => {
+  let fs = require("fs");
+  let rimraf = require("rimraf");
 
-// if (fs.existsSync("package-lock.json")) {
-// let folders = [".", ...Object.keys(require("./package.json").directories)];
-// for (let folder of folders) {
-// rimraf(folder, function (Error) {
-// if (Error) console.log(Error);
-// });
-// }
-// } else {
-// console.log("🐍𝐋𝐨𝐠: directries cleaned!");
-// }
+  function execute(argument) {
+    require("child_process").exec(argument, (error, stdout, stderr) => {
+      if (error) return console.error(`🐍𝐋𝐨𝐠:  Exec Error ${error}`);
+      console.log("🐍𝐋𝐨𝐠: " + stdout);
+    });
+  }
 
-try {
-  await execute("git config --global user.email krakinzkon@gmail.com");
-  await execute("git config --global user.name KryKenz");
-  await execute("git config pull.rebase false");
-  await execute("git stash");
-  await execute("git stash drop");
-  await execute("git fetch origin version_4");
-  await execute("git reset --hard origin/version_4");
-  await execute("git pull --all");
-  console.log("🐍𝐋𝐨𝐠: Git sync done!");
-} catch (error) {
-  console.log("🐍𝐋𝐨𝐠: " + error);
-}
+  // if (fs.existsSync("package-lock.json")) {
+  // let folders = [".", ...Object.keys(require("./package.json").directories)];
+  // for (let folder of folders) {
+  // rimraf(folder, function (Error) {
+  // if (Error) console.log(Error);
+  // });
+  // }
+  // } else {
+  // console.log("🐍𝐋𝐨𝐠: directries cleaned!");
+  // }
 
-try {
-  await execute("npm install --save --force");
-} catch (error) {
-  console.log("🐍𝐋𝐨𝐠: " + error);
-}
+  try {
+    execute("git config --global user.email krakinzkon@gmail.com");
+    execute("git config --global user.name KryKenz");
+    execute("git config pull.rebase false");
+    execute("git stash");
+    execute("git stash drop");
+    execute("git fetch origin version_4");
+    execute("git reset --hard origin/version_4");
+    execute("git pull --all");
+    console.log("🐍𝐋𝐨𝐠: Git sync done!");
+  } catch (error) {
+    console.log("🐍𝐋𝐨𝐠: " + error);
+  }
+
+  try {
+    execute("npm install --save --force");
+  } catch (error) {
+    console.log("🐍𝐋𝐨𝐠: " + error);
+  }
+})();
