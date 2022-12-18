6d13c724fd698ad5be49d679cb544abbcf3b7b55
KryKenz <krakinzkon@gmail.com>
2022-12-18 20:52:49 +0530

    vlkyre❣️version_4

--- 🐍𝖛𝖑𝖐𝖕𝖞.js
+++ 🐍𝖛𝖑𝖐𝖕𝖞.js
@@ -1,73 +1,41 @@
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-/*
-╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
-║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
-║ 
-║ We won't be responsible for any kind of ban due to this bot.
-║ νℓкуяє was made for fun purpose and to make group management easier.
-║ It's your concern if you spam and gets your account banned.
-║ Also, Forks won't be entertained.
-║ If you fork this repo and edit plugins, it's your concern for further updates.
-║ Forking Repo is fine. But if you edit something we will not provide any help.
-║ In short, Fork At Your Own Risk.
-╚════════════╝
-*/
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 let fs = require("fs");
 let rimraf = require("rimraf");
-function execute(argument) {
-require("child_process").exec("echo test", (error, stdout, stderr) => {
-if (error) {
-return console.error(`exec error: ${error}`);
+async function execute(argument) {
+  require("child_process").exec("echo test", (error, stdout, stderr) => {
+    if (error) {
+      return console.error(`exec error: ${error}`);
+    }
+    console.log(`stdout: ${stdout}`);
+  });
 }
-console.log(`stdout: ${stdout}`);
-});
-}
-// ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-if (fs.existsSync("package-lock.json")) {
-let folders = [".", ...Object.keys(require("./package.json").directories)];
-for (let folder of folders) {
-rimraf(folder, function (Error) {
-if (Error) console.log(Error);
-});
-}
-} else {
-console.log("🐍𝐋𝐨𝐠: directries cleaned!");
-}
-// ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+
+// if (fs.existsSync("package-lock.json")) {
+// let folders = [".", ...Object.keys(require("./package.json").directories)];
+// for (let folder of folders) {
+// rimraf(folder, function (Error) {
+// if (Error) console.log(Error);
+// });
+// }
+// } else {
+// console.log("🐍𝐋𝐨𝐠: directries cleaned!");
+// }
+
 try {
-execute("git config --global user.email krakinzkon@gmail.com");
-execute("git config --global user.name KryKenz");
-execute("git config pull.rebase false");
-execute("git stash");
-execute("git stash drop");
-execute("git fetch origin version_4");
-execute("git reset --hard origin/version_4");
-execute("git pull --all");
-console.log("🐍𝐋𝐨𝐠: Git sync done!");
+  await execute("git config --global user.email krakinzkon@gmail.com");
+  await execute("git config --global user.name KryKenz");
+  await execute("git config pull.rebase false");
+  await execute("git stash");
+  await execute("git stash drop");
+  await execute("git fetch origin version_4");
+  await execute("git reset --hard origin/version_4");
+  await execute("git pull --all");
+  console.log("🐍𝐋𝐨𝐠: Git sync done!");
 } catch (error) {
-console.log("🐍𝐋𝐨𝐠: Error In git sync!");
-console.log("🐍𝐋𝐨𝐠: " + error);
+  console.log("🐍𝐋𝐨𝐠: " + error);
 }
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+
 try {
-execute("npm install --save --force");
+  await execute("npm install --save --force");
 } catch (error) {
-console.log("🐍𝐋𝐨𝐠: Error In npm install!");
-console.log("🐍𝐋𝐨𝐠: " + error);
+  console.log("🐍𝐋𝐨𝐠: " + error);
 }
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-/*
-╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
-║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
-║ 
-║ We won't be responsible for any kind of ban due to this bot.
-║ νℓкуяє was made for fun purpose and to make group management easier.
-║ It's your concern if you spam and gets your account banned.
-║ Also, Forks won't be entertained.
-║ If you fork this repo and edit plugins, it's your concern for further updates.
-║ Forking Repo is fine. But if you edit something we will not provide any help.
-║ In short, Fork At Your Own Risk.
-╚════════════╝
-*/
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
