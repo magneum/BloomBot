e6880be731e0d06cb8c6fa029a31e1f8d665c3aa
KryKenz <krakinzkon@gmail.com>
2022-12-23 02:37:53 -0800

    vlkyre by krykenz

---
+++ Modular/𝘊𝘰𝘮𝘮𝘢𝘯𝘥💗𝘫𝘴/_hentai.js
@@ -0,0 +1,79 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+ppath = require("path");
+require("../../global.js");
+psname = ppath.basename(__filename);
+pfname = psname.slice(0, -3).toLowerCase();
+module.exports = async (νℓкуяє, vcнaт) => {
+  try {
+    await νℓкуяє.imgB(
+      νℓкуяє,
+      vcнaт,
+      `*🤖Hello, I am Vlkyre (v5 Bot)🤖*
+> Ⓒ𝐕𝐥𝐤𝐲𝐫𝐞: is a whatsapp userbot with automation,moderation,music,games and 100+ commands!
+> My developers are working on my code.
+> Untill then you can send your group links in my private.
+> Once bot will be ready, it will auto join all groups.
+
+
+┌『 *🍌HENTAI HUB🍌* 』 
+│║⦁ ${νℓкуяє.prefix}ass
+│║⦁ ${νℓкуяє.prefix}bdsm
+│║⦁ ${νℓкуяє.prefix}blowjob
+│║⦁ ${νℓкуяє.prefix}cum
+│║⦁ ${νℓкуяє.prefix}doujin
+│║⦁ ${νℓкуяє.prefix}feet
+│║⦁ ${νℓкуяє.prefix}femdom  
+│║⦁ ${νℓкуяє.prefix}foxgirl  
+│║⦁ ${νℓкуяє.prefix}glasses  
+│║⦁ ${νℓкуяє.prefix}hentai
+│║⦁ ${νℓкуяє.prefix}maid
+│║⦁ ${νℓкуяє.prefix}masturbation
+│║⦁ ${νℓкуяє.prefix}netorare
+│║⦁ ${νℓкуяє.prefix}orgy
+│║⦁ ${νℓкуяє.prefix}panties
+│║⦁ ${νℓкуяє.prefix}pussy
+│║⦁ ${νℓкуяє.prefix}school
+│║⦁ ${νℓкуяє.prefix}succubus
+│║⦁ ${νℓкуяє.prefix}tentacles
+│║⦁ ${νℓкуяє.prefix}thighs
+│║⦁ ${νℓкуяє.prefix}uglyBastard
+│║⦁ ${νℓкуяє.prefix}uniform
+│║⦁ ${νℓкуяє.prefix}yuri
+│║⦁ ${νℓкуяє.prefix}zettaiRyouiki
+┕╚═══════⋑`,
+      "./Gallery/KryPage.png"
+    );
+    ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  } catch (error) {
+    return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+  }
+};
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
