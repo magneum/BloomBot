e4d2c5382c375887e0c4669329aef7773ff31279
KryKenz <krakinzkon@gmail.com>
2022-12-23 22:00:27 -0800

    🦌vlkyre whatsapp bot

---
+++ Enforcers/antilink.js
@@ -0,0 +1,81 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱LinkList☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ νℓкуяє was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+require("../global.js");
+exports.noLink = async (νℓкуяє, vcнaт) => {
+  let FetchCurrentGroupLink = await νℓкуяє.groupInviteCode(vcнaт.chat);
+  let GroupLinkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+  let isGroupLink = GroupLinkRegex.exec(νℓкуяє.body);
+  let PresentGroupLink = new RegExp(
+    `https://chat.whatsapp.com/${FetchCurrentGroupLink}`,
+    "i"
+  );
+  let isCurrentGroupLink = PresentGroupLink.test(νℓкуяє.body);
+  if (isGroupLink && !isCurrentGroupLink) {
+    await νℓкуяє
+      .groupParticipantsUpdate(vcнaт.chat, [νℓкуяє.sender], "remove")
+      .catch((error) => νℓкуяє.grab(νℓкуяє, vcнaт, error));
+    await vcнaт.reply(
+      `*😥Sorry:* _${νℓкуяє.pushname}_
+*KryZen❌Anti-Link*
+> _Kicked! One Less MoFo!_`
+    );
+    return await νℓкуяє.sendMessage(vcнaт.chat, {
+      delete: {
+        remoteJid: vcнaт.chat,
+        fromMe: false,
+        id: νℓкуяє.quoted.id,
+        participant: νℓкуяє.quoted.sender,
+      },
+    });
+    ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  } else if (
+    νℓкуяє.body.includes("https://t.me/") &&
+    νℓкуяє.body.includes("discord.gg") &&
+    νℓкуяє.body.includes("discord.com") &&
+    νℓкуяє.body.includes("/t.me/") &&
+    νℓкуяє.body.includes("wa.me/") &&
+    νℓкуяє.body.includes("www.")
+  ) {
+    await νℓкуяє
+      .groupParticipantsUpdate(vcнaт.chat, [νℓкуяє.sender], "remove")
+      .catch((error) => νℓкуяє.grab(νℓкуяє, vcнaт, error));
+    await vcнaт.reply(
+      `*😥Sorry:* _${νℓкуяє.pushname}_
+*KryZen❌Anti-Link*
+> _Kicked! One Less MoFo!_`
+    );
+    return await νℓкуяє.sendMessage(vcнaт.chat, {
+      delete: {
+        remoteJid: vcнaт.chat,
+        fromMe: false,
+        id: νℓкуяє.quoted.id,
+        participant: νℓкуяє.quoted.sender,
+      },
+    });
+  } else {
+  }
+};
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ νℓкуяє was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
---
+++ Modular/𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥📥𝘫𝘴/youtube.js
@@ -0,0 +1,125 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+ppath = require("path");
+require("../../global.js");
+psname = ppath.basename(__filename);
+pfname = psname.slice(0, -3).toLowerCase();
+module.exports = async (νℓкуяє, vcнaт) => {
+  if (!νℓкуяє.args.join(" ")) {
+    return vcнaт.reply(
+      `*😥Sorry:* _${νℓкуяє.pushname}_
+*❌Error* 
+> _No query provided!_
+
+*⚡Usage*   
+> _${νℓкуяє.prefix}${pfname} song/link_`
+    );
+  }
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  var gotArgument = νℓкуяє.args.join(" ");
+  if (gotArgument.includes("yout")) {
+    return vcнaт.reply(
+      `*😥Sorry:* _${νℓкуяє.pushname}_
+*❌Error* 
+> _No query provided!_
+
+*⚡Usage* 
+> _${νℓкуяє.prefix}${pfname} song/link_`
+    );
+  }
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+var Found = await νℓкуяє.ySearch(gotArgument);
+var vFound = Found.videos.slice(0, 1);
+vFound.forEach(async function (vData) {
+  if (!vData.url) {
+    return vcнaт.reply(`*😥Sorry:* _${νℓкуяє.pushname}_
+
+*❌Error*
+> _No Music Found!_`);
+  } else if (vData.seconds > 600) {
+    return vcнaт.reply(`*😥Sorry:* _${νℓкуяє.pushname}_
+
+*❌Error*
+> _Cannot Download More Then 10m audio!_`);
+  } else {
+    console.log(vData);
+    await νℓкуяє.imgB(
+      νℓкуяє,
+      vcнaт,
+      `*🔖Here, ${pfname} For ${νℓкуяє.pushname}:*
+*🍻Title:* ${vData.title || "null"}
+*🙈Views:* ${vData.views || "null"}
+*⏰Duration:* ${vData.timestamp || "null"} | ${vData.ago || "null"}
+*🔗Link:* ${vData.url || "null"}
+*🖊️Author:* ${vData.author.name || "null"}
+
+
+*📜Description:*
+${vData.description || "null"}`,
+      vData.thumbnail
+    );
+    ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+    let audiOut = νℓкуяє.between(1000, 2000) + vData.videoId + ".mp3";
+    var audio = νℓкуяє
+      .yClient(vData.url, {
+        filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
+      })
+      .pipe(νℓкуяє.fs.createWriteStream(`./${audiOut}`));
+    await new Promise((resolve, reject) => {
+      audio.on("error", reject);
+      audio.on("finish", resolve);
+    });
+    await νℓкуяє.sendMessage(
+      vcнaт.chat,
+      {
+        audio: νℓкуяє.fs.readFileSync(`./${audiOut}`),
+        mimetype: "audio/mpeg",
+        fileName: vData.title + ".mp3",
+        headerType: 4,
+        contextInfo: {
+          externalAdReply: {
+            title: vData.title,
+            body: "❣️Made by KryKenz.",
+            renderLargerThumbnail: true,
+            thumbnailUrl: vData.thumbnail,
+            mediaUrl: vData.url,
+            mediaType: 1,
+            thumbnail: await νℓкуяє.getBuffer(vData.thumbnail),
+            sourceUrl: "https://bit.ly/krykenz",
+          },
+        },
+      },
+      { quoted: vcнaт }
+    );
+    return νℓкуяє.fs.unlinkSync(`./${audiOut}`);
+  }
+});
+};
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
