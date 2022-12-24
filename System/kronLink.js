539f09ddd9494e0dedc74e17c123461e560757cd
KryKenz <krakinzkon@gmail.com>
2022-12-24 03:29:57 -0800

    🦌vlkyre whatsapp bot
    🦌vlkyre whatsapp bot

---
+++ System/kronLink.js
@@ -0,0 +1,256 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║ 
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ νℓкуяє was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+require("../global.js");
+var {
+  Simp,
+  Pokemon,
+  Ship,
+  IShipOptions,
+} = require("@shineiichijo/canvas-chan");
+var {
+  νkmake,
+  formatp,
+  formatDate,
+  getTime,
+  isUrl,
+  sleep,
+  clockString,
+  runtime,
+  fetchJson,
+  getBuffer,
+  jsonformat,
+  format,
+  parseMention,
+  GIFBufferToVideoBuffer,
+  getRandom,
+} = require("./myfunc");
+var { tmpdir } = require("os");
+var { JSDOM } = require("jsdom");
+var { Character } = require("mailist");
+var { readFile } = require("fs/promises");
+var { Chalk } = require("cfonts/lib/Chalk");
+var { performance } = require("perf_hooks");
+var { Primbon } = require("scrape-primbon");
+var { getDadjoke } = require("random-jokes");
+var { createWorker } = require("tesseract.js");
+var { Manga } = require("@shineiichijo/marika");
+var { AnimeWallpaper } = require("anime-wallpaper");
+var { Doujin } = require("@shineiichijo/nhentai-pdf");
+var { exec, spawn, execSync } = require("child_process");
+var { TelegraPh, UploadFileUgu, webp2mp4File } = require("./uploader");
+var { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+module.exports = async (νℓкуяє, vcнaт, update, store) => {
+  νℓкуяє.performance = performance;
+  νℓкуяє.createWorker = createWorker;
+  νℓкуяє.JSDOM = JSDOM;
+  νℓкуяє.Character = Character;
+  νℓкуяє.execSync = execSync;
+  νℓкуяє.spawn = spawn;
+  νℓкуяє.exec = exec;
+  νℓкуяє.Primbon = Primbon;
+  νℓкуяє.IShipOptions = IShipOptions;
+  νℓкуяє.Ship = Ship;
+  νℓкуяє.Pokemon = Pokemon;
+  νℓкуяє.Simp = Simp;
+  νℓкуяє.readFile = readFile;
+  νℓкуяє.tmpdir = tmpdir;
+  νℓкуяє.Doujin = Doujin;
+  νℓкуяє.Chalk = Chalk;
+  νℓкуяє.Sticker = Sticker;
+  νℓкуяє.createSticker = createSticker;
+  νℓкуяє.StickerTypes = StickerTypes;
+  νℓкуяє.νkmake = νkmake;
+  νℓкуяє.formatp = formatp;
+  νℓкуяє.formatDate = formatDate;
+  νℓкуяє.getTime = getTime;
+  νℓкуяє.isUrl = isUrl;
+  νℓкуяє.sleep = sleep;
+  νℓкуяє.clockString = clockString;
+  νℓкуяє.runtime = runtime;
+  νℓкуяє.fetchJson = fetchJson;
+  νℓкуяє.getBuffer = getBuffer;
+  νℓкуяєonformat = jsonformat;
+  νℓкуяє.format = format;
+  νℓкуяє.parseMention = parseMention;
+  νℓкуяє.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer;
+  νℓкуяє.getRandom = getRandom;
+  νℓкуяє.getDadjoke = getDadjoke;
+  νℓкуяє.Manga = Manga;
+  νℓкуяє.AnimeWallpaper = AnimeWallpaper;
+  νℓкуяє.TelegraPh = TelegraPh;
+  νℓкуяє.UploadFileUgu = UploadFileUgu;
+  νℓкуяє.webp2mp4File = webp2mp4File;
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  νℓкуяє.msgFilter = require("./msgFilter");
+  νℓкуяє.imgB = require("../Buttons/imgB");
+  νℓкуяє.vidB = require("../Buttons/vidB");
+  νℓкуяє.grab = require("../Buttons/grab");
+  νℓкуяє.LinkList = require("../mongBase/antilink");
+  νℓкуяє.Ranker = require("../mongBase/autorank");
+  νℓкуяє.Bagde = require("../mongBase/badge");
+  νℓкуяє.userBanCheck = require("../mongBase/ban");
+  νℓкуяє.Cooldown = require("../mongBase/cooldown");
+  νℓкуяє.DebugMode = require("../mongBase/debug");
+  νℓкуяє.Economy = require("../mongBase/economy");
+  νℓкуяє.Gamble = require("../mongBase/gamble");
+  νℓкуяє.Halt = require("../mongBase/halt");
+  νℓкуяє.nsfwCheck = require("../mongBase/nsfw");
+  νℓкуяє.Pokemon = require("../mongBase/pokemon");
+  νℓкуяє.UserPrivate = require("../mongBase/private");
+  νℓкуяє.Robbery = require("../mongBase/robbery");
+  νℓкуяє.ServerDB = require("../mongBase/ServerDB");
+  νℓкуяє.Welcome = require("../mongBase/setwelcome");
+  νℓкуяє.Warning = require("../mongBase/warning");
+  νℓкуяє.Zoology = require("../mongBase/zoo");
+  νℓкуяє.Fishes = require("../Bin/fishes.json");
+  νℓкуяє.animals = require("../Bin/animals.json");
+  νℓкуяє.Jwork = require("../Bin/works.json");
+  νℓкуяє.dare = require("../Bin/dare.json");
+  νℓкуяє.truth = require("../Bin/truth.json");
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  νℓкуяє.os = require("os");
+  νℓкуяє.fs = require("fs");
+  νℓкуяє.gis = require("g-i-s");
+  νℓкуяє.path = require("path");
+  νℓкуяє.util = require("util");
+  νℓкуяє.primbon = new Primbon();
+  νℓкуяє.ms = require("parse-ms");
+  νℓкуяє.db = require("quick.db");
+  νℓкуяє.hxz = require("hxz-api");
+  νℓкуяє.axios = require("axios");
+  νℓкуяє.chalk = require("chalk");
+  νℓкуяє.playdl = require("play-dl");
+  νℓкуяє.FFmpeg = require("fluent-ffmpeg");
+  νℓкуяє.request = require("request");
+  νℓкуяє.xfar = require("xfarr-api");
+  νℓкуяє.cron = require("node-cron");
+  νℓкуяє.nHentai = require("shentai");
+  νℓкуяє.cheerio = require("cheerio");
+  νℓкуяє.akaneko = require("akaneko");
+  νℓкуяє.fetch = require("node-fetch");
+  νℓкуяє.google = require("google-it");
+  νℓкуяє.Spinnies = require("spinnies");
+  νℓкуяє.Anime = require("anime-actions");
+  νℓкуяє.Tinyurl = require("tinyurl-api");
+  νℓкуяє.FastHub = require("simple-git")();
+  νℓкуяє.speed = require("performance-now");
+  νℓкуяє.moment = require("moment-timezone");
+  νℓкуяє.pathFFmpeg = require("ffmpeg-static");
+  νℓкуяє.Carbon = require("unofficial-carbon-now");
+  νℓкуяє.Levels = require("discord-xp");
+  νℓкуяє.canvacord = require("canvacord");
+  νℓкуяє.randomMC = require("random-material-color");
+
+  νℓкуяє.yClient = require("krytek-ytdl");
+  νℓкуяє.ySearch = require("krytek-yts");
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  νℓкуяє.memberRespA = [];
+  νℓкуяє.resp1 = await νℓкуяє.groupInviteCode("120363020792949649@g.us");
+  νℓкуяє.letResp = [
+    "h",
+    "m",
+    "c",
+    "help",
+    "menu",
+    "command",
+    "commands",
+    "Vlkyre",
+    "Commands",
+  ];
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  function ShowGreen(Topic, Text) {
+    let TShow = νℓкуяє.chalk.hex("#2D5A27").bold(Topic);
+    let Show = νℓкуяє.chalk.hex("#849871").italic.bold(Text);
+    console.log(
+      νℓкуяє.chalk.black(νℓкуяє.chalk.bgBlack(TShow)),
+      νℓкуяє.chalk.black(Show)
+    );
+  }
+  function ShowBlue(Topic, Text) {
+    let TShow = νℓкуяє.chalk.hex("#008B8B").bold(Topic);
+    let Show = νℓкуяє.chalk.hex("#818d94").italic.bold(Text);
+    console.log(
+      νℓкуяє.chalk.black(νℓкуяє.chalk.bgBlack(TShow)),
+      νℓкуяє.chalk.black(Show)
+    );
+  }
+  function ShowRed(Topic, Text) {
+    let TShow = νℓкуяє.chalk.hex("#ff6347").bold(Topic);
+    let Show = νℓкуяє.chalk.hex("#ed7777").italic.bold(Text);
+    console.log(
+      νℓкуяє.chalk.black(νℓкуяє.chalk.bgBlack(TShow)),
+      νℓкуяє.chalk.black(Show)
+    );
+  }
+  function ShowYellow(Topic, Text) {
+    let TShow = νℓкуяє.chalk.hex("#8B8000").bold(Topic);
+    let Show = νℓкуяє.chalk.hex("#ECCF8D").italic.bold(Text);
+    console.log(
+      νℓкуяє.chalk.black(νℓкуяє.chalk.bgBlack(TShow)),
+      νℓкуяє.chalk.black(Show)
+    );
+  }
+  function between(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  function clearStorage(files) {
+    for (const file of files) {
+      νℓкуяє.fs.unlink(file, (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  νℓкуяє.ShowGreen = ShowGreen;
+  νℓкуяє.ShowBlue = ShowBlue;
+  νℓкуяє.ShowRed = ShowRed;
+  νℓкуяє.ShowYellow = ShowYellow;
+  νℓкуяє.between = between;
+  νℓкуяє.clearStorage = clearStorage;
+  νℓкуяє.spinner = {
+    interval: 80,
+    frames: ["◜", "◟", "◝", "◞", "◠", "◡", "⧬", "⧭", "⧬", "⧭"],
+  };
+  νℓкуяє.spinnies = new νℓкуяє.Spinnies({
+    color: "cyan",
+    succeedColor: "green",
+    spinner: νℓкуяє.spinner,
+  });
+  νℓкуяє.TubeRegex =
+    /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/;
+  νℓкуяє.ShowLogger = ShowLogger;
+  νℓкуяє.packname = packname;
+  νℓкуяє.pgdb = DATABASE_URL;
+  νℓкуяє.ShowInfo = ShowInfo;
+  νℓкуяє.MONGO_URL = MONGO_URL;
+  νℓкуяє.author = author;
+  νℓкуяє.prefix = prefix;
+  νℓкуяє.mods = mods;
+  ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  return νℓкуяє;
+};
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║ 
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ νℓкуяє was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
