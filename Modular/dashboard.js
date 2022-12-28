5bd26580af126d3ac66c3e5950b3cc1ad59050d5
krykenz <krakinzkon@gmail.com>
2022-12-28 09:51:22 +0530

    🦌vlkyre whatsapp bot

--- Modular/dashboard.js
+++ Modular/dashboard.js
@@ -16,15 +16,21 @@ require("../global.js");
 psname = ppath.basename(__filename);
 pfname = psname.slice(0, -3).toLowerCase();
 module.exports = async (νℓкуяє, vcнaт) => {
-await νℓкуяє.imgB(
-νℓкуяє,
-vcнaт,
-`*🤖Hello, I am Vlkyre User-Bot🤖*
-> Please Login To Your Dashboard:
-> https://7g036e-5000.preview.csb.app/login
-> Must Include country code in phone number`,
-"./Gallery/νℓкуяє_dark.png"
-);
+  return await νℓкуяє.sendMessage(
+    vcнaт.chat,
+    {
+      gifPlayback: true,
+      video: νℓкуяє.fs.readFileSync("./Gallery/how.mp4"),
+      caption: `*🔖Here, ${pfname} For ${νℓкуяє.pushname}:*
+> First watch the video.
+
+*⚙️Webpage:*
+> bit.ly/krykenz
+> Login To Your Dashboard`,
+      mentions: [vcнaт.sender],
+    },
+    { quoted: vcнaт }
+  );
 };
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 // ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
