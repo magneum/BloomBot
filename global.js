567419162ef4a2b27dc590c4eda9dabc420e2ea5
krykenz <krakinzkon@gmail.com>
2022-12-30 01:44:18 +0530

    🛰️vlkyre whatsapp user-bot🚀

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ require("dotenv");
 var fs = require("fs");
 var sequelize = require("sequelize");
 if (fs.existsSync(".env")) {
-require("dotenv").config({ path: ".env" });
+  require("dotenv").config({ path: ".env" });
 }
 var mod = process.env.mods;
 if (!mod) mod = "918436686758,917430922909";
@@ -24,6 +24,7 @@ global.ShowLogger = process.env.ShowLogger;
 global.ShowInfo = process.env.ShowInfo;
 global.mods = mod.split(",");
 global.MONGO_URL = process.env.MONGO_URL;
+global.REDIS_URL = process.env.REDIS_URL;
 global.PORT = process.env.PORT || 5000;
 global.pgdb = process.env.pgdb;
 global.prefix = process.env.prefix || "!";
@@ -31,75 +32,75 @@ global.sessionName = process.env.sessionName;
 global.timezone = process.env.timezone;
 global.upbranch = process.env.upbranch;
 global.nameApi = {
-nrtm: "https://nurutomo.herokuapp.com",
-dzx: "https://api.dhamzxploit.my.id",
-xteam: "https://api.xteam.xyz",
-zahir: "https://zahirr-web.herokuapp.com",
-zeks: "https://api.zeks.xyz",
-pencarikode: "https://pencarikode.xyz",
-LeysCoder: "https://leyscoders-api.herokuapp.com",
+  nrtm: "https://nurutomo.herokuapp.com",
+  dzx: "https://api.dhamzxploit.my.id",
+  xteam: "https://api.xteam.xyz",
+  zahir: "https://zahirr-web.herokuapp.com",
+  zeks: "https://api.zeks.xyz",
+  pencarikode: "https://pencarikode.xyz",
+  LeysCoder: "https://leyscoders-api.herokuapp.com",
 };
 global.keyApi = {
-"https://api.xteam.xyz": "d90a9e986e18778b",
-"https://zahirr-web.herokuapp.com": "zahirgans",
-"https://api.zeks.xyz": "apivinz",
-"https://pencarikode.xyz": "pais",
-"https://leyscoders-api.herokuapp.com": "dappakntlll",
+  "https://api.xteam.xyz": "d90a9e986e18778b",
+  "https://zahirr-web.herokuapp.com": "zahirgans",
+  "https://api.zeks.xyz": "apivinz",
+  "https://pencarikode.xyz": "pais",
+  "https://leyscoders-api.herokuapp.com": "dappakntlll",
 };
 global.packname = "Vlkyre:v5";
 global.author = "KryKenz";
 global.name = "Vlkyre:v5";
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 var Log = (value) => {
-var log = false;
-if (typeof value === "string") {
-if (value.toLowerCase() === "true") {
-log = console.log;
-}
-}
-return log;
+  var log = false;
+  if (typeof value === "string") {
+    if (value.toLowerCase() === "true") {
+      log = console.log;
+    }
+  }
+  return log;
 };
 global.apiGet = (name, path = "/", query = {}, queryname) =>
-(name in global.nameApi ? global.nameApi[name] : name) +
-path +
-(query || queryname
-? "?" +
-new URLSearchParams(
-Object.entries({
-...query,
-...(queryname
-? {
-[queryname]:
-global.keyApi[
-name in global.nameApi ? global.nameApi[name] : name
-],
-}
-: {}),
-})
-)
-: "");
+  (name in global.nameApi ? global.nameApi[name] : name) +
+  path +
+  (query || queryname
+    ? "?" +
+      new URLSearchParams(
+        Object.entries({
+          ...query,
+          ...(queryname
+            ? {
+                [queryname]:
+                  global.keyApi[
+                    name in global.nameApi ? global.nameApi[name] : name
+                  ],
+              }
+            : {}),
+        })
+      )
+    : "");
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 process.env.DATABASE_URL =
-process.env.DATABASE_URL === undefined
-? "./νℓкуяє.db"
-: process.env.DATABASE_URL;
+  process.env.DATABASE_URL === undefined
+    ? "./νℓкуяє.db"
+    : process.env.DATABASE_URL;
 global.DATABASE_URL =
-process.env.DATABASE_URL === undefined
-? "./νℓкуяє.db"
-: process.env.DATABASE_URL;
+  process.env.DATABASE_URL === undefined
+    ? "./νℓкуяє.db"
+    : process.env.DATABASE_URL;
 global.DATABASE =
-process.env.DATABASE_URL === "./νℓкуяє.db"
-? new sequelize.Sequelize({
-dialect: "sqlite",
-storage: process.env.DATABASE_URL,
-logging: Log("false"),
-})
-: new sequelize.Sequelize(process.env.DATABASE_URL, {
-dialect: "postgres",
-protocol: "postgres",
-logging: Log("false"),
-dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-});
+  process.env.DATABASE_URL === "./νℓкуяє.db"
+    ? new sequelize.Sequelize({
+        dialect: "sqlite",
+        storage: process.env.DATABASE_URL,
+        logging: Log("false"),
+      })
+    : new sequelize.Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        protocol: "postgres",
+        logging: Log("false"),
+        dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+      });
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 // ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
 // ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
