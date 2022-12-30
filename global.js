30ac65ae7409f83a03efdadf239119c6a8293787
krykenz <krakinzkon@gmail.com>
2022-12-30 15:47:23 +0530

    🛰️vlkyre whatsapp user-bot🚀

--- global.js
+++ global.js
@@ -54,7 +54,7 @@ global.sessionName = process.env.sessionName;
 global.timezone = process.env.timezone;
 global.upbranch = process.env.upbranch;
 global.KryTek_URL =
-  process.env.KryTek_URL || "https://ipjon1-8000.preview.csb.app";
+  process.env.KryTek_URL || "https://ipjon1-5000.preview.csb.app";
 global.nameApi = {
   nrtm: "https://nurutomo.herokuapp.com",
   dzx: "https://api.dhamzxploit.my.id",
