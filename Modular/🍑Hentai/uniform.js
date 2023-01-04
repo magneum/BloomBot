f2406e2deebb0eea83a57e38080d4fe14221465c
KryKenz <krakinzkon@gmail.com>
2023-01-05 00:04:08 +0530

    </ Designed & Developed by xhåÐr ?>

---
+++ Modular/🍑Hentai/uniform.js
@@ -0,0 +1,47 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+ppath = require("path");
+require("../../global.js");
+psname = ppath.basename(__filename);
+pfname = psname.slice(0, -3).toLowerCase();
+module.exports = async (νℓкуяє, vcнaт) => {
+  try {
+    await νℓкуяє.imgB(
+      νℓкуяє,
+      vcнaт,
+      `*🔖Here, ${pfname} For ${νℓкуяє.pushname}:*
+> Military, Konbini, Work, Nurse Uniforms, etc!~ Sexy~`,
+      await νℓкуяє.akaneko.nsfw.uniform()
+    );
+  } catch (error) {
+    return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+  }
+};
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
---
+++ global.js
@@ -0,0 +1,174 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ νℓкуяє was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+require("dotenv");
+var fs = require("fs");
+var chalk = require("chalk");
+var monGoose = require("mongoose");
+var sequelize = require("sequelize");
+var { createClient } = require("redis");
+if (fs.existsSync(".env")) {
+require("dotenv").config({ path: ".env" });
+}
+function ShowRed(Topic, Text) {
+var TShow = chalk.hex("#ff6347").bold(Topic);
+var Show = chalk.hex("#ed7777").italic.bold(Text);
+console.log(chalk.black(chalk.bgBlack(TShow)), chalk.black(Show));
+}
+function ShowBlue(Topic, Text) {
+var TShow = chalk.hex("#008B8B").bold(Topic);
+var Show = chalk.hex("#818d94").italic.bold(Text);
+console.log(chalk.black(chalk.bgBlack(TShow)), chalk.black(Show));
+}
+function ShowGreen(Topic, Text) {
+var TShow = chalk.hex("#2D5A27").bold(Topic);
+var Show = chalk.hex("#849871").italic.bold(Text);
+console.log(chalk.black(chalk.bgBlack(TShow)), chalk.black(Show));
+}
+function ShowYellow(Topic, Text) {
+var TShow = chalk.hex("#8B8000").bold(Topic);
+var Show = chalk.hex("#ECCF8D").italic.bold(Text);
+console.log(chalk.black(chalk.bgBlack(TShow)), chalk.black(Show));
+}
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+var mod = process.env.mods;
+if (!mod) mod = "918436686758,917430922909";
+global.upVersion = process.env.upVersion;
+global.ShowLogger = process.env.ShowLogger;
+global.ShowInfo = process.env.ShowInfo;
+global.mods = mod.split(",");
+global.PORT = process.env.PORT || 5000;
+global.pgdb = process.env.pgdb;
+global.prefix = process.env.prefix || "!";
+global.sessionName = process.env.sessionName;
+global.timezone = process.env.timezone;
+global.upbranch = process.env.upbranch;
+global.KryTek_URL =
+process.env.KryTek_URL || "https://vp10fo-9999.preview.csb.app";
+global.nameApi = {
+nrtm: "https://nurutomo.herokuapp.com",
+dzx: "https://api.dhamzxploit.my.id",
+xteam: "https://api.xteam.xyz",
+zahir: "https://zahirr-web.herokuapp.com",
+zeks: "https://api.zeks.xyz",
+pencarikode: "https://pencarikode.xyz",
+LeysCoder: "https://leyscoders-api.herokuapp.com",
+};
+global.keyApi = {
+"https://api.xteam.xyz": "d90a9e986e18778b",
+"https://zahirr-web.herokuapp.com": "zahirgans",
+"https://api.zeks.xyz": "apivinz",
+"https://pencarikode.xyz": "pais",
+"https://leyscoders-api.herokuapp.com": "dappakntlll",
+};
+global.packname = "Vlkyre:v5";
+global.author = "xhadr";
+global.name = "Vlkyre:v5";
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+var Log = (value) => {
+var log = false;
+if (typeof value === "string") {
+if (value.toLowerCase() === "true") {
+log = console.log;
+}
+}
+return log;
+};
+global.apiGet = (name, path = "/", query = {}, queryname) =>
+(name in global.nameApi ? global.nameApi[name] : name) +
+path +
+(query || queryname
+? "?" +
+new URLSearchParams(
+Object.entries({
+...query,
+...(queryname
+? {
+[queryname]:
+global.keyApi[
+name in global.nameApi ? global.nameApi[name] : name
+],
+}
+: {}),
+})
+)
+: "");
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+process.env.DATABASE_URL =
+process.env.DATABASE_URL === undefined
+? "./νℓкуяє.db"
+: process.env.DATABASE_URL;
+global.DATABASE_URL =
+process.env.DATABASE_URL === undefined
+? "./νℓкуяє.db"
+: process.env.DATABASE_URL;
+global.DATABASE =
+process.env.DATABASE_URL === "./νℓкуяє.db"
+? new sequelize.Sequelize({
+dialect: "sqlite",
+storage: process.env.DATABASE_URL,
+logging: Log("false"),
+})
+: new sequelize.Sequelize(process.env.DATABASE_URL, {
+dialect: "postgres",
+protocol: "postgres",
+logging: Log("false"),
+dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
+
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+global.MONGO_URL = process.env.MONGO_URL;
+global.REDIS_URL = process.env.REDIS_URL;
+global.REDISHOST = process.env.REDISHOST;
+const redisClient = createClient({
+url: process.env.REDIS_URL,
+socket: {
+tls: true,
+servername: process.env.REDISHOST,
+},
+});
+(async () => {
+await monGoose
+.connect(process.env.MONGO_URL, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false,
+})
+.catch((error) => {
+ShowRed("🦋Info:", "Unable to Connected with Mongoose.");
+console.log(error);
+})
+.then(ShowGreen("🦋Info:", "Connected with Mongoose."));
+// await redisClient.connect();
+})().catch((error) => ShowRed("🦋Info:", error));
+// redisClient.on("ready", () => {
+// ShowGreen("🦋Info:", "Connected with Redis.");
+// });
+// redisClient.on("error", (error) => {
+// ShowRed("🦋Info:", error);
+// ShowRed("Error in the Connection with Redis.");
+// });
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// ╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+// ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+// ║
+// ║ We won't be responsible for any kind of ban due to this bot.
+// ║ νℓкуяє was made for fun purpose and to make group management easier.
+// ║ It's your concern if you spam and gets your account banned.
+// ║ Also, Forks won't be entertained.
+// ║ If you fork this repo and edit plugins, it's your concern for further updates.
+// ║ Forking Repo is fine. But if you edit something we will not provide any help.
+// ║ In short, Fork At Your Own Risk.
+// ╚════════════╝
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву xhåÐr ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
