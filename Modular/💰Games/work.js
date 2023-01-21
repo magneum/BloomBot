4083710b5a665b6771b6c6d90cf42215a5146994
Detronx <krakinzkon@gmail.com>
2023-01-21 11:20:11 +0530

    updated to version 0.0.5

---
+++ Modular/💰Games/work.js
@@ -0,0 +1,94 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+ppath = require("path");
+require("../../global.js");
+psname = ppath.basename(__filename);
+pfname = psname.slice(0, -3).toLowerCase();
+module.exports = async (νℓкуяє, vcнaт) => {
+  try {
+    var JworkR = νℓкуяє.Jwork[Math.floor(Math.random() * νℓкуяє.Jwork.length)];
+    νℓкуяє.Economy.findOne(
+      {
+        ID: vcнaт.sender,
+      },
+      async (error, userEco) => {
+        if (error) {
+          return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+        }
+        ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+        if (!userEco) {
+          new νℓкуяє.Economy({
+            ID: vcнaт.sender,
+            money: 0,
+            daily: 0,
+            timeout: 86400000,
+            fishdone: 0,
+            fishtimeout: 1800000,
+            workdone: Date.now(),
+            worktimeout: 900000,
+          })
+            .save()
+            .catch((error) => {
+              return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+            });
+          return await νℓкуяє.imgB(
+            νℓкуяє,
+            vcнaт,
+            `*🔖Here, ${pfname} For @${νℓкуяє.Tname || νℓкуяє.pushname}:*
+*🧈Status:* Added To DB!
+*💰Balance:* Just Opened Your Account!`,
+            "./Gallery/vlkyre.jpg"
+          );
+          ("|⬡═══════════════════════════════════════════════════════════════|▷ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ◁|═══════════════════════════════════════════════════════════════⬡|");
+        } else {
+          if (userEco.worktimeout - (Date.now() - userEco.workdone) > 0) {
+            let time = νℓкуяє.ms(
+              userEco.worktimeout - (Date.now() - userEco.workdone)
+            );
+            return await νℓкуяє.imgB(
+              νℓкуяє,
+              vcнaт,
+              `*🔖Here, ${pfname} For @${νℓкуяє.Tname || νℓкуяє.pushname}:*
+❌ 𝗘𝗿𝗿𝗼𝗿: _You have already worked recently._ 
+🕐 𝗪𝗼𝗿𝗸_𝗔𝗴𝗮𝗶𝗻: ${time.minutes}m ${time.seconds}s`,
+              "./Gallery/vlkyre.jpg"
+            );
+            ("|⬡═══════════════════════════════════════════════════════════════|▷ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋʀᴀᴋɪɴᴢʟᴀʙ™ ◁|═══════════════════════════════════════════════════════════════⬡|");
+          } else {
+            let amount = Math.floor(Math.random() * 80) + 1;
+            userEco.money = userEco.money + amount;
+            userEco.workdone = Date.now();
+            userEco.worktimeout = 900000;
+            userEco.save().catch((error) => {
+              return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+            });
+            return await νℓкуяє.imgB(
+              νℓкуяє,
+              vcнaт,
+              `*🔖Here, ${pfname} For @${νℓкуяє.Tname || νℓкуяє.pushname}:*
+👔𝗪𝗼𝗿𝗸_𝗦𝘁𝗮𝘁𝘂𝘀: ${JworkR}.
+🪙𝗘𝗮𝗿𝗻𝗲𝗱: ${amount}`,
+              "./Gallery/vlkyre.jpg"
+            );
+          }
+        }
+      }
+    );
+    ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  } catch (error) {
+    return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+  }
+};
---
+++ Modular/🔎Searches/quote.js
@@ -0,0 +1,58 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+ppath = require("path");
+require("../../global.js");
+psname = ppath.basename(__filename);
+pfname = psname.slice(0, -3).toLowerCase();
+module.exports = async (νℓкуяє, vcнaт) => {
+  try {
+    var кяуяєs = await νℓкуяє.axios.get("https://favqs.com/api/qotd");
+    try {
+      var кяуяєsi = await νℓкуяє.fetch(
+        global.apiGet("https://wall.alphacoders.com/api2.0", "/get.php", {
+          auth: "3e7756c85df54b78f934a284c11abe4e",
+          method: "search",
+          term: "random",
+        })
+      );
+      var bson = await кяуяєsi.json();
+      var bsoni =
+        bson.wallpapers[Math.floor(Math.random() * bson.wallpapers.length)];
+      await νℓкуяє.imgB(
+        νℓкуяє,
+        vcнaт,
+        `*🔖Here, ${pfname} For @${νℓкуяє.Tname || νℓкуяє.pushname}:* 
+> ${кяуяєs.data.fact}
+> *📝Content:* ${кяуяєs.data.quote.body}
+> *✍️Author:* ${кяуяєs.data.quote.author}`,
+        bsoni.url_image
+      );
+    } catch {
+      await νℓкуяє.imgB(
+        νℓкуяє,
+        vcнaт,
+        `*🔖Here, ${pfname} For @${νℓкуяє.Tname || νℓкуяє.pushname}:* 
+> ${кяуяєs.data.fact}
+> *📝Content:* ${кяуяєs.data.quote.body}
+> *✍️Author:* ${кяуяєs.data.quote.author}`,
+        "./Gallery/νℓкуяє_beta.jpg"
+      );
+    }
+    ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+  } catch (error) {
+    return νℓкуяє.grab(νℓкуяє, vcнaт, error);
+  }
+};
