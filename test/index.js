879e7ad23ea676cc2dfc2d81203b93d53ae544db
magneum <krakinzkon@gmail.com>
2023-02-04 20:20:16 +0530

    updated to 0.1.8

---
+++ test/index.js
@@ -0,0 +1,17 @@
+return {
+  bassboost: "-af 'bass=g=10,dynaudnorm=f=150'",
+  echo: "-af 'aecho=0.8:0.9:1000:0.3'",
+  flanger: "-af 'flanger'",
+  nightcore: "-af 'aresample=48000,asetrate=48000*1.25'",
+  panning: "-af 'apulsator=hz=0.08'",
+  phaser: "-af 'aphaser=in_gain=0.4'",
+  reverse: "-filter_complex 'areverse'",
+  slow: "-af 'atempo=0.8'",
+  speed: "-af 'atempo=2'",
+  subboost: "-af 'asubboost'",
+  superslow: "-af 'atempo=0.5'",
+  superspeed: "-af 'atempo=3'",
+  surround: "-af 'surround'",
+  vaporwave: "-af'aresample=48000,asetrate=48000*0.8'",
+  vibrato: "-af 'vibrato=f=6.5'",
+};
