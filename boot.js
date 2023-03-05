7bd9541c70d35d29e0f2a64f636038c4b0bb0ca7
Mågnëum / <krakinzkon@gmail.com>
2023-03-06 03:59:29 +0530

    updated

--- boot.js
+++ boot.js
@@ -13,156 +13,157 @@ const Table = new ascii().setHeading("✭CATEGORY✭", "✭COMMANDS✭");
 const mFolders = fs.readdirSync("./models");
 
 say("VLKYRE BOT\nWhatsApp Multi Device", {
-  font: "chrome",
-  align: "center",
-  gradient: ["red", "yellow"],
+font: "chrome",
+align: "center",
+gradient: ["red", "yellow"],
 });
 say(`~ by magneum`, {
-  font: "console",
-  align: "center",
-  gradient: ["green", "red"],
+font: "console",
+align: "center",
+gradient: ["green", "red"],
 });
 
 function showCommands(path) {
-  say("✭ Loading Commands From Folders ✭", {
-    font: "console",
-    align: "left",
-    gradient: ["red", "blue"],
-  });
-  for (const cFolders of mFolders) {
-    const cFiles = fs
-      .readdirSync(`./${path}/${cFolders}`)
-      .filter((cFile) => cFile.endsWith(".js"));
+say("✭ Loading Commands From Folders ✭", {
+font: "console",
+align: "left",
+gradient: ["red", "blue"],
+});
+for (const cFolders of mFolders) {
+const cFiles = fs
+.readdirSync(`./${path}/${cFolders}`)
+.filter((cFile) => cFile.endsWith(".js"));
 
-    // return;
-    // Table.addRow(`> `, cFolders);
-    // console.log(Table.toString());
-    // console.log(cFiles);
+// return;
+// Table.addRow(`> `, cFolders);
+// console.log(Table.toString());
+// console.log(cFiles);
 
-    if (cFolders === "⚙️System") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-      Table.addRow("> " + cFolders, cFiles);
-      console.log(Table.toString());
-    } else if (cFolders === "⭕YTFilter") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🍁ᴏᴡɴᴇʀ") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🍑Hentai") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🐉Emotions") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "👅NSFW") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "💗Commands") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "💰Games") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "📢aFilter") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "📥Downloader") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "📼Conversion") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🔎Searches") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🔰Group") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🖼️Photogenic") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    } else if (cFolders === "🦄SFW") {
-      console.log(
-        cL.bgGreen(cL.black("> " + cFolders)),
-        cL.yellow("  | " + cFiles)
-      );
-    }
-  }
+if (cFolders === "⚙️System") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+Table.addRow("> " + cFolders, cFiles);
+console.log(Table.toString());
+} else if (cFolders === "⭕YTFilter") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🍁ᴏᴡɴᴇʀ") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🍑Hentai") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🐉Emotions") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "👅NSFW") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "💗Commands") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "💰Games") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "📢aFilter") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "📥Downloader") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "📼Conversion") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🔎Searches") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🔰Group") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🖼️Photogenic") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+} else if (cFolders === "🦄SFW") {
+console.log(
+cL.bgGreen(cL.black("> " + cFolders)),
+cL.yellow("  | " + cFiles)
+);
+}
+}
 }
 
 var isRunning = false;
 function ignite(cFile) {
-  if (isRunning) return;
-  isRunning = true;
-  let args = [join(__dirname, cFile), ...process.argv.slice(2)];
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  });
-  let p = fork();
-  p.on("message", (data) => {
-    console.log(cL.bgGreen(cL.black("[RECEIVED]")), cL.yellow(data));
-    switch (data) {
-      case "reset":
-        p.process.kill();
-        isRunning = false;
-        ignite.apply(this, arguments);
-        break;
-      case "uptime":
-        p.send(process.uptime());
-        break;
-    }
-  });
-  p.on("exit", (_, code) => {
-    isRunning = false;
-    console.log(cL.bgRed("❌ An unexpected error occurred:"), cL.red(error));
-    p.process.kill();
-    isRunning = false;
-    ignite.apply(this, arguments);
-    if (code === 0) return;
-    watchFile(args[0], () => {
-      unwatchFile(args[0]);
-      ignite(cFile);
-    });
-  });
-  let opts = new Object(
-    yargs(process.argv.slice(2)).exitProcess(false).parse()
-  );
-  if (!opts["test"])
-    if (!rl.listenerCount())
-      rl.on("line", (line) => {
-        p.emit("message", line.trim());
-      });
+if (isRunning) return;
+isRunning = true;
+let args = [join(__dirname, cFile), ...process.argv.slice(2)];
+setupMaster({
+exec: args[0],
+args: args.slice(1),
+});
+let p = fork();
+p.on("message", (data) => {
+console.log(cL.bgGreen(cL.black("[RECEIVED]")), cL.yellow(data));
+switch (data) {
+case "reset":
+p.process.kill();
+isRunning = false;
+ignite.apply(this, arguments);
+break;
+case "uptime":
+p.send(process.uptime());
+break;
+}
+});
+p.on("exit", (_, code) => {
+isRunning = false;
+console.log(cL.bgRed("❌ An unexpected error occurred:"), cL.red(error));
+p.process.kill();
+isRunning = false;
+ignite.apply(this, arguments);
+if (code === 0) return;
+watchFile(args[0], () => {
+unwatchFile(args[0]);
+ignite(cFile);
+});
+});
+let opts = new Object(
+yargs(process.argv.slice(2)).exitProcess(false).parse()
+);
+if (!opts["test"])
+if (!rl.listenerCount())
+rl.on("line", (line) => {
+p.emit("message", line.trim());
+});
 }
 
-showCommands("models").then(ignite("index.js", "uptime"));
+showCommands("models");
+ignite("index.js", "uptime");
--- dockerfile
+++ dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update && apt-get install -y \
     git \
     curl \
     wget \
+    ffmpeg \
     bpm-tools \
     opus-tools \
     python3-pip \
@@ -11,10 +12,12 @@ RUN apt-get update && apt-get install -y \
 RUN curl -s https://deb.nodesource.com/setup_16.x | bash
 RUN apt-get update && apt-get install nodejs -y
 RUN npm install -g spotify-dl spdl-core forever pm2 yarn corepack && hash -r
-RUN git clone --branch magneum https://github.com/magneum/Vlkyre
+RUN git clone https://github.com/magneum/Vlkyre
 RUN cd Vlkyre
 WORKDIR /Vlkyre
-RUN yarn set version classic
 RUN git init --initial-branch=magneum && git fetch origin magneum && git reset --hard origin/magneum
 RUN rm -f yarn.lock && pip install -r requirements.txt && yarn install && yarn global add forever
+# EXPOSE 3000
+# EXPOSE 5000
+# EXPOSE 8000
 CMD [ "yarn", "run", "server" ]
