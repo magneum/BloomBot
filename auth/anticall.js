11787f242e6111d8f290ad39fa5ab56ef4a7dc17
magneum <krakinzkon@gmail.com>
2023-05-28 08:06:09 -0400

    ⋆ 𝖇𝖞 𝖒𝖆𝖌𝖓𝖊𝖚𝖒 ⋆

--- auth/anticall.js
+++ auth/anticall.js
@@ -1,14 +1,19 @@
-//  ╔⧉༻ [ 𝐕𝐨𝐱𝐁𝐨𝐭🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞 - 𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧, 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+//  ╔◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ᴠᴏxʙᴏᴛ вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱"
+//  ║⧉༻ 🤖 𝐕𝐨𝐱𝐁𝐨𝐭🕊️ 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞 🤖
+//  ║  𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞 - 𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧, 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
 //  ║
-//  ║ We won't be responsible for any kind of ban due to this bot.
-//  ║ VoxBot was made for fun purpose and to make group management easier.
-//  ║ It's your concern if you spam and gets your account banned.
-//  ║ Also, Forks won't be entertained.
-//  ║ If you fork this repo and edit plugins, it's your concern for further updates.
-//  ║ Forking Repo is fine. But if you edit something we will not provide any help.
-//  ║ In short, Fork At Your Own Risk.
+//  ║🌟 A versatile WhatsApp multi-purpose bot designed for group management and user convenience.
+//  ║🚀 Simplifies group management tasks and enhances the overall user experience.
+//  ║⚠️ Please note: Engaging in spamming activities may lead to account suspension. Use responsibly!
+//  ║🎉 VoxBot is intended for fun and convenience, but we're not responsible for account bans.
+//  ║🔀 Forking the repository is allowed, but customized versions or modified plugins are unsupported.
+//  ║⚠️ Exercise caution and take responsibility for any modifications made to the bot.
+//  ║📞 Need assistance or have issues? Contact our developers at +918436686758 and +918250889325.
+//  ║🔄 We'll continue providing updates and support for the original version of the bot.
+//  ║👉 Enjoy the features and functionality of VoxBot responsibly! Make the most out of your
+//  ║   WhatsApp group management experience! 🎉
 //  ║
-//  ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: +918436686758, +918250889325
-//  ╚◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ꪜᴏxʙᴏᴛ вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎"
+//  ║🐞 DEVELOPERS: +918436686758, +918250889325
+//  ╚◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ᴠᴏxʙᴏᴛ вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱"
 require("../global.js");
 exports.dBoard = async (ꪜᴏxʙᴏᴛ, ᴠᴏxᴄ) => {};
--- routes/🍑Hentai/ass.js
+++ routes/🍑Hentai/ass.js
@@ -1,78 +1,81 @@
-//  ╔⧉༻ [ 𝐕𝐨𝐱𝐁𝐨𝐭🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞 - 𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧, 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
+//  ╔◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ᴠᴏxʙᴏᴛ вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱"
+//  ║⧉༻ 🤖 𝐕𝐨𝐱𝐁𝐨𝐭🕊️ 𝐌𝐮𝐥𝐭𝐢 𝐃𝐞𝐯𝐢𝐜𝐞 🤖
+//  ║  𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞 - 𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧, 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬!
 //  ║
-//  ║ We won't be responsible for any kind of ban due to this bot.
-//  ║ VoxBot was made for fun purpose and to make group management easier.
-//  ║ It's your concern if you spam and gets your account banned.
-//  ║ Also, Forks won't be entertained.
-//  ║ If you fork this repo and edit plugins, it's your concern for further updates.
-//  ║ Forking Repo is fine. But if you edit something we will not provide any help.
-//  ║ In short, Fork At Your Own Risk.
+//  ║🌟 A versatile WhatsApp multi-purpose bot designed for group management and user convenience.
+//  ║🚀 Simplifies group management tasks and enhances the overall user experience.
+//  ║⚠️ Please note: Engaging in spamming activities may lead to account suspension. Use responsibly!
+//  ║🎉 VoxBot is intended for fun and convenience, but we're not responsible for account bans.
+//  ║🔀 Forking the repository is allowed, but customized versions or modified plugins are unsupported.
+//  ║⚠️ Exercise caution and take responsibility for any modifications made to the bot.
+//  ║📞 Need assistance or have issues? Contact our developers at +918436686758 and +918250889325.
+//  ║🔄 We'll continue providing updates and support for the original version of the bot.
+//  ║👉 Enjoy the features and functionality of VoxBot responsibly! Make the most out of your
+//  ║   WhatsApp group management experience! 🎉
 //  ║
-//  ║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: +918436686758, +918250889325
-//  ╚◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ꪜᴏxʙᴏᴛ вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎"
+//  ║🐞 DEVELOPERS: +918436686758, +918250889325
+//  ╚◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ᴠᴏxʙᴏᴛ вσт ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱"
 require("../../global.js");
 const ppath = require("path");
 const psname = ppath.basename(__filename);
 const pfname = psname.slice(0, -3).toLowerCase();
-module.exports = async (ꪜᴏxʙᴏᴛ, ᴠᴏxᴄ, update, store) => {
+module.exports = async (VօxB໐t, ᴠᴏxᴄ, update, store) => {
   try {
-    return await ꪜᴏxʙᴏᴛ.nsfwCheck.findOne(
+    return await VօxB໐t.nsfwCheck.findOne(
       {
         serverID: ᴠᴏxᴄ.chat,
       },
       async (error, server) => {
-        if (error) return ꪜᴏxʙᴏᴛ.grab(ꪜᴏxʙᴏᴛ, ᴠᴏxᴄ, error);
+        if (error) return VօxB໐t.grab(VօxB໐t, ᴠᴏxᴄ, error);
         if (!server) {
-          await ꪜᴏxʙᴏᴛ.sendMessage(ᴠᴏxᴄ.chat, {
+          await VօxB໐t.sendMessage(ᴠᴏxᴄ.chat, {
             react: {
               text: "❌",
               key: ᴠᴏxᴄ.key,
             },
           });
           return ᴠᴏxᴄ.reply(
-            `*😥Sorry:* _${ꪜᴏxʙᴏᴛ.pushname || ꪜᴏxʙᴏᴛ.Tname}_
+            `*😥Sorry:* _${VօxB໐t.pushname || VօxB໐t.Tname}_
 
 *❌ Error* 
 > NSFW Commands have been turned off for this group.
 > You may ask the admins to turn it on.`
           );
         } else {
-          ꪜᴏxʙᴏᴛ
-            .magfetch(
-              ꪜᴏxʙᴏᴛ,
-              "https://magneum.vercel.app/api/hentai?q=" + pfname
-            )
-            .then(async (response) => {
-              var mData = response.data;
-              console.log(mData);
-              if (!mData[0].meta.url) {
-                await ꪜᴏxʙᴏᴛ.sendMessage(ᴠᴏxᴄ.chat, {
-                  react: {
-                    text: "❌",
-                    key: ᴠᴏxᴄ.key,
-                  },
-                });
-                return ᴠᴏxᴄ.reply(
-                  `*😥Sorry:* _${ꪜᴏxʙᴏᴛ.pushname || ꪜᴏxʙᴏᴛ.Tname}_
+          VօxB໐t.magfetch(
+            VօxB໐t,
+            "https://magneum.vercel.app/api/hentai?q=" + pfname
+          ).then(async (response) => {
+            var mData = response.data;
+            console.log(mData);
+            if (!mData[0].meta.url) {
+              await VօxB໐t.sendMessage(ᴠᴏxᴄ.chat, {
+                react: {
+                  text: "❌",
+                  key: ᴠᴏxᴄ.key,
+                },
+              });
+              return ᴠᴏxᴄ.reply(
+                `*😥Sorry:* _${VօxB໐t.pushname || VօxB໐t.Tname}_
 
 *❌ Error* 
 > There has been an API Error. Please try again later.`
-                );
-              } else
-                await ꪜᴏxʙᴏᴛ.imgB(
-                  ꪜᴏxʙᴏᴛ,
-                  ᴠᴏxᴄ,
-                  `*🔖Here, ${pfname} For ${ꪜᴏxʙᴏᴛ.pushname}:*
+              );
+            } else
+              await VօxB໐t.imgB(
+                VօxB໐t,
+                ᴠᴏxᴄ,
+                `*🔖Here, ${pfname} For ${VօxB໐t.pushname}:*
 
 > *Description:* ${mData[0].meta.description}
 > *Api Fetch Url:* https://magneum.vercel.app/api/hentai`,
-                  mData[0].meta.url
-                );
-            });
+                mData[0].meta.url
+              );
+          });
         }
       }
     );
   } catch (error) {
-    return ꪜᴏxʙᴏᴛ.grab(ꪜᴏxʙᴏᴛ, ᴠᴏxᴄ, error);
+    return VօxB໐t.grab(VօxB໐t, ᴠᴏxᴄ, error);
   }
 };
