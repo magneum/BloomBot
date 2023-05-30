6e570b3a7a706d8323e17ede1780da00a30f04f2
magneum <krakinzkon@gmail.com>
2023-05-30 08:23:04 +0530

    Foxbot

--- Foxbot.js
+++ Foxbot.js
@@ -26,6 +26,7 @@ var { watchFile, unwatchFile } = require("fs");
 var { setupMaster, fork } = require("cluster");
 var rl = createInterface(process.stdin, process.stdout);
 process.removeAllListeners("warning");
+process.env.NODE_NO_WARNINGS = "1";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 process.on("uncaughtException", (error) => {
   logger.error(error);
