31b4ee048ea8190d43e3833a055a7ab579d9f832
magneum <krakinzkon@gmail.com>
2023-05-31 09:37:58 +0530

    BloomBot

--- logger/config.js
+++ logger/config.js
@@ -22,28 +22,28 @@ if (fs.existsSync(".env")) {
   require("dotenv").config({ path: ".env" });
 }
 
-process.env.DATABASE_URL =
-  process.env.DATABASE_URL === undefined
-    ? "./BloomBot.db"
-    : process.env.DATABASE_URL;
-global.DATABASE_URL =
-  process.env.DATABASE_URL === undefined
-    ? "./BloomBot.db"
-    : process.env.DATABASE_URL;
+// process.env.DATABASE_URL =
+// process.env.DATABASE_URL === undefined
+// ? "./BloomBot.db"
+// : process.env.DATABASE_URL;
+// global.DATABASE_URL =
+// process.env.DATABASE_URL === undefined
+// ? "./BloomBot.db"
+// : process.env.DATABASE_URL;
 
-global.DATABASE =
-  process.env.DATABASE_URL === "./BloomBot.db"
-    ? new sequelize.Sequelize({
-        dialect: "sqlite",
-        storage: process.env.DATABASE_URL,
-        logging: false,
-      })
-    : new sequelize.Sequelize(process.env.DATABASE_URL, {
-        dialect: "postgres",
-        protocol: "postgres",
-        logging: false,
-        dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-      });
+// global.DATABASE =
+// process.env.DATABASE_URL === "./BloomBot.db"
+// ? new sequelize.Sequelize({
+// dialect: "sqlite",
+// storage: process.env.DATABASE_URL,
+// logging: false,
+// })
+// : new sequelize.Sequelize(process.env.DATABASE_URL, {
+// dialect: "postgres",
+// protocol: "postgres",
+// logging: false,
+// dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+// });
 
 global.MONGODB_URL = process.env.MONGODB_URL;
 global.VERBOSE = process.env.VERBOSE || "0";
