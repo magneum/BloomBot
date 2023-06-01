fce4d9fa04ffa05f7454fb4315e6442b56989bc0
magneum <krakinzkon@gmail.com>
2023-06-01 12:59:54 +0530

    ⒸBloomBot by Magneum™ 0.0.3

--- mongo.js
+++ mongo.js
@@ -7,6 +7,14 @@ dotenv.config();
 // Get the database URL from the environment variable
 const url = process.env.MONGODB_URL;
 
+// Validate the database URL
+try {
+  url = url.parse();
+} catch (error) {
+  console.error(`Invalid database URL: ${url}`);
+  process.exit(1);
+}
+
 // Connect to the database
 const db = mongoose.connect(url, {
   useNewUrlParser: true,
@@ -18,9 +26,16 @@ const collections = db.listCollections();
 
 // Iterate over the collections and delete all the documents
 collections.forEach((collection) => {
-  collection.deleteMany({}).then(() => {
-    console.log(`Deleted all documents from ${collection.name}`);
-  });
+  collection.deleteMany({}).then(
+    () => {
+      console.log(`Deleted all documents from ${collection.name}`);
+    },
+    (error) => {
+      console.error(
+        `Error deleting documents from ${collection.name}: ${error}`
+      );
+    }
+  );
 });
 
 // Close the connection to the database
