b5561ae93e243ccbc7b2344cd10fb140bc5c19e5
magneum <krakinzkon@gmail.com>
2023-06-01 13:09:48 +0530

    ⒸBloomBot by Magneum™ 0.0.3

--- BloomBot.js
+++ BloomBot.js
@@ -32,7 +32,7 @@ process.env.NODE_NO_WARNINGS = "1";
 process.removeAllListeners("warning");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 process.on("uncaughtException", (error) => {
-  logger.error(error);
+  console.error(error);
 });
 require("events").EventEmitter.prototype._maxListeners = 0;
 say("BloomBot\nwhatsApp Multi Device\nUser bot", {
